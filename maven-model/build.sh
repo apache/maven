d2a2ab2e19c0a9b5de739a8c740ce3240b936e59
Jason van Zyl <jvanzyl@apache.org>
2004-03-10 20:45:24 +0000

    o modello can now generate xpp3 marshallers and unmarshallers based on   a model so we are generating those now too.
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@162597 13f79535-47bb-0310-9956-ffa450edef68

--- maven-model/build.sh
+++ maven-model/build.sh
@@ -3,6 +3,9 @@
 # Generate the sources from the model
 modello --model=maven.mdo --mode=java --dir=src/main/java
 
+# Generate the source for the xpp3 marshaller and unmarshaller
+modello --model=maven.mdo --mode=xpp3 --dir=src/main/java
+
 # Build the sources
 mboot
 
@@ -11,3 +14,4 @@ modello --model=maven.mdo --mode=xsd --dir=.
 
 # Generate XDoc
 modello --model=maven.mdo --mode=xdoc --dir=.
+
