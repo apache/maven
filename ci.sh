9de0992370727856e42d487368fa7094fb5ff1e0
Jason van Zyl <jvanzyl@apache.org>
2004-04-09 14:11:57 +0000

    *** empty log message ***
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@162635 13f79535-47bb-0310-9956-ffa450edef68

--- ci.sh
+++ ci.sh
@@ -1,23 +1,38 @@
 #!/bin/sh
 
-# To run this script on your machine you must set:
+# ----------------------------------------------------------------------------------
+# To run this script on your machine you must:
 #
-# MBOOT_HOME
-# PATH=$PATH:$MBOOT_HOME/bin
-# MAVEN_HOME
-# PATH=$PATH:$MAVEN_HOME/bin
+# 1. export MBOOT_HOME=~/mboot
+#
+# 2. export MAVEN_HOME=~/maven
+#    
+# 3. export PATH=$PATH:$MBOOT:$MAVEN_HOME/bin
+#
+# 4. Your ~/build.properties must have: maven.repo.local = ~/path/to/repo
+#
+# ----------------------------------------------------------------------------------
 
 DIR=maven2
 REPO=maven-repo-local
+FROM=jvanzyl@maven.org
+TO=maven2-user@lists.codehaus.org
+
+# ----------------------------------------------------------------------------------
+
+# Wipe out the working directory and the repository and start entirely
+# from scratch.
 
 rm -rf $DIR > /dev/null 2>&1
 rm -rf $REPO > /dev/null 2>&1
 mkdir $DIR
 mkdir $REPO
 
+# ----------------------------------------------------------------------------------
+
 DATE=`date`
-echo "From: jvanzyl@maven.org" > log
-echo "To: maven2-user@lists.codehaus.org" >> log
+echo "From: $FROM" > log
+echo "To: $TO" >> log
 echo "Subject: Maven bootstrap on beaver [$DATE]" >> log
 echo "" >> log
 
