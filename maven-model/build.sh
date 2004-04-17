2256dae4cfe763df1337d64c225c4d0b8eb584db
Jason van Zyl <jvanzyl@apache.org>
2004-04-17 15:13:31 +0000

    o add versions so that the 4.0.0 version of the model is used to   output the source, xpp3 reader/writer, xdoc and XSD.
    o more tweaks to the model for correct output.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@162680 13f79535-47bb-0310-9956-ffa450edef68

--- maven-model/build.sh
+++ maven-model/build.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # Generate the sources from the model
-modello --model=maven.mdo --mode=java --dir=src/main/java
+modello --model=maven.mdo --version=4.0.0 --mode=java --dir=src/main/java
 
 # Generate the source for the xpp3 marshaller and unmarshaller
-modello --model=maven.mdo --mode=xpp3 --dir=src/main/java
+modello --model=maven.mdo --version=4.0.0 --mode=xpp3 --dir=src/main/java
 
 # Build the sources
 mboot --install
 
 # Generate XML Schema
-modello --model=maven.mdo --mode=xsd --dir=.
+modello --model=maven.mdo --version=4.0.0 --mode=xsd --dir=.
 
 # Generate XDoc
-modello --model=maven.mdo --mode=xdoc --dir=./xdocs
+modello --model=maven.mdo --version=4.0.0 --mode=xdoc --dir=./xdocs
