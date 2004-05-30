555932d95f472330ab2296fae33275c6aaa0d695
Jason van Zyl <jvanzyl@apache.org>
2004-05-30 16:55:51 +0000

    o generate sources to target/generated-sources
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@162730 13f79535-47bb-0310-9956-ffa450edef68

--- maven-model/build.sh
+++ maven-model/build.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # Generate the sources from the model
-modello --model=maven.mdo --version=4.0.0 --mode=java --dir=src/main/java
+modello --model=maven.mdo --version=4.0.0 --mode=java --dir=target/generated-sources
 
 # Generate the source for the xpp3 marshaller and unmarshaller
-modello --model=maven.mdo --version=4.0.0 --mode=xpp3 --dir=src/main/java
-
-# Build the sources
-mboot --install
+modello --model=maven.mdo --version=4.0.0 --mode=xpp3 --dir=target/generated-sources
 
 # Generate XML Schema
 modello --model=maven.mdo --version=4.0.0 --mode=xsd --dir=.
 
 # Generate XDoc
-modello --model=maven.mdo --version=4.0.0 --mode=xdoc --dir=./xdocs
+modello --model=maven.mdo --version=4.0.0 --mode=xdoc --dir=target/generated-xdocs
+
+# Build the sources
+mboot --install
