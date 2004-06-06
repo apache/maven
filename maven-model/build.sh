fa1c1cdf4d67ba59d8986ef652e2f17dd793b4d3
Jason van Zyl <jvanzyl@apache.org>
2004-06-06 05:52:30 +0000

    o generate the sources for the v3 model o generate the sources for the v3 xpp3 reader/writer
      both of these necessary in order to adapt the v3 POM into a v4 POM
      so that m2 can slurp up v3 POMs to build m1 projects transparently.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@162736 13f79535-47bb-0310-9956-ffa450edef68

--- maven-model/build.sh
+++ maven-model/build.sh
@@ -8,6 +8,12 @@ modello --model=maven.mdo --version=4.0.0 --mode=java --dir=target/generated-sou
 # Generate the source for the xpp3 marshaller and unmarshaller
 modello --model=maven.mdo --version=4.0.0 --mode=xpp3 --dir=target/generated-sources
 
+# Generate the 3.0.0 source from the model
+modello --model=maven.mdo --version=3.0.0 --mode=java --dir=target/generated-sources --package-with-version
+
+# Generate the 3.0.0 source for the xpp3 marshaller and unmarshaller
+modello --model=maven.mdo --version=3.0.0 --mode=xpp3 --dir=target/generated-sources --package-with-version
+
 # Generate XML Schema
 modello --model=maven.mdo --version=4.0.0 --mode=xsd --dir=.
 
