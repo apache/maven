85107479c032e250ec212915e7693758c30f7f1d
Emmanuel Venisse <evenisse@apache.org>
2004-07-05 10:54:50 +0000

    Initial version of pom plugin
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@162852 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-plugins/maven-pom-plugin/src/main/java/org/apache/maven/plugin/pom/PomDeployMojo.java
@@ -0,0 +1,62 @@
+package org.apache.maven.plugin.jar;
+
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.AbstractPlugin;
+import org.apache.maven.plugin.PluginExecutionRequest;
+import org.apache.maven.plugin.PluginExecutionResponse;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.artifact.installer.ArtifactInstaller;
+import org.apache.maven.artifact.deployer.ArtifactDeployer;
+import org.codehaus.plexus.util.FileUtils;
+
+import java.io.File;
+
+/**
+ * @goal deploy
+ *
+ * @description deploys a pom to remote repository
+ *
+ * @parameter
+ *  name="project"
+ *  type="org.apache.maven.project.MavenProject"
+ *  required="true"
+ *  validator=""
+ *  expression="#project"
+ *  description=""
+ *
+ * @parameter
+ *  name="deployer"
+ *  type="org.apache.maven.artifact.deployer.ArtifactDeployer"
+ *  required="true"
+ *  validator=""
+ *  expression="#component.org.apache.maven.artifact.deployer.ArtifactDeployer"
+ *  description=""
+ */
+public class PomDeployMojo
+    extends AbstractPlugin
+{
+    public void execute( PluginExecutionRequest request, PluginExecutionResponse response )
+        throws Exception
+    {
+        MavenProject project = (MavenProject) request.getParameter( "project" );
+
+        ArtifactDeployer artifactDeployer = (ArtifactDeployer) request.getParameter( "deployer" );
+
+        artifactDeployer.deploy( project.getFile(), "pom", project );
+    }
+}
---
+++ maven-plugins/maven-pom-plugin/src/main/java/org/apache/maven/plugin/pom/PomInstallMojo.java
@@ -0,0 +1,61 @@
+package org.apache.maven.plugin.jar;
+
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.AbstractPlugin;
+import org.apache.maven.plugin.PluginExecutionRequest;
+import org.apache.maven.plugin.PluginExecutionResponse;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.artifact.installer.ArtifactInstaller;
+import org.codehaus.plexus.util.FileUtils;
+
+import java.io.File;
+
+/**
+ * @goal install
+ *
+ * @description install a jar in local repository
+ *
+ * @parameter
+ *  name="project"
+ *  type="org.apache.maven.project.MavenProject"
+ *  required="true"
+ *  validator=""
+ *  expression="#project"
+ *  description=""
+ *
+ * @parameter
+ *  name="installer"
+ *  type="org.apache.maven.artifact.installer.ArtifactInstaller"
+ *  required="true"
+ *  validator=""
+ *  expression="#component.org.apache.maven.artifact.installer.ArtifactInstaller"
+ *  description=""
+ */
+public class PomInstallMojo
+    extends AbstractPlugin
+{
+    public void execute( PluginExecutionRequest request, PluginExecutionResponse response )
+        throws Exception
+    {
+        MavenProject project = (MavenProject) request.getParameter( "project" );
+
+        ArtifactInstaller artifactInstaller = (ArtifactInstaller) request.getParameter( "installer" );
+
+        artifactInstaller.install( project.getFile(), "pom", project );
+    }
+}
