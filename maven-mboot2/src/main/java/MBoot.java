0e8ae273df20e8dc3bab313b44278a45e0908e02
Jason van Zyl <jvanzyl@apache.org>
2004-08-09 19:42:29 +0000

    o use plexus 0.16
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@162932 13f79535-47bb-0310-9956-ffa450edef68

--- maven-mboot2/src/main/java/MBoot.java
+++ maven-mboot2/src/main/java/MBoot.java
@@ -56,7 +56,7 @@ public class MBoot
     String[] plexusDeps = new String[]
     {
         "classworlds/jars/classworlds-1.1-SNAPSHOT.jar",
-        "plexus/jars/plexus-0.16-SNAPSHOT.jar",
+        "plexus/jars/plexus-0.16.jar",
         "xpp3/jars/xpp3-1.1.3.3.jar",
         "xstream/jars/xstream-1.0-SNAPSHOT.jar",
         "maven/jars/maven-artifact-2.0-SNAPSHOT.jar",
