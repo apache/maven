d303a99fa58e2de2bfe8dff47c319f5f18b2feee
Brett Leslie Porter <brett@apache.org>
2004-08-15 04:28:31 +0000

    ignore commented lines, fail script on failure of any test
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@162975 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-it/maven-core-it.sh
+++ maven-core-it/maven-core-it.sh
@@ -8,11 +8,11 @@ cp=../../maven-core-it-verifier/target/maven-core-it-verifier-1.0.jar
 
 verifier=org.apache.maven.it.Verifier
 
-integration_tests=`cat integration-tests.txt`
+integration_tests=`cat integration-tests.txt | egrep -v '^#'`
 
 for integration_test in $integration_tests
 do
-  
+
   echo "----------------------------------------------------------"
   echo "Running integration test $integration_test ..."
   echo "----------------------------------------------------------"  
@@ -27,6 +27,7 @@ do
     fi
     
     m2 clean:clean `cat goals.txt`
+    ret=$?; if [ $ret != 0 ]; then exit $ret; fi
     
     if [ -f postbuild.hook ]
     then    
@@ -38,5 +39,7 @@ do
     basedir=.
     
     java -cp "$cp" $verifier "$basedir"
+    ret=$?; if [ $ret != 0 ]; then exit $ret; fi
   )
+  ret=$?; if [ $ret != 0 ]; then exit $ret; fi
 done
