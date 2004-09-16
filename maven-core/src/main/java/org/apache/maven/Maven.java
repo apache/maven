2d4e9a1a0e6916f9a24ee8d4cba595c857e3f6a8
John Dennis Casey <jdcasey@apache.org>
2004-09-16 16:06:13 +0000

    o Commented earlier changes until I have time to make them work properly o Added DEFAULT_MODEL_VERSION constant to Maven.class for referencing in case there is no project POM
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@163087 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/main/java/org/apache/maven/Maven.java
+++ maven-core/src/main/java/org/apache/maven/Maven.java
@@ -32,6 +32,8 @@ import java.util.Map;
 public interface Maven
 {
     static String ROLE = Maven.class.getName();
+    
+    static String DEFAULT_MODEL_VERSION = "4.0.0";
 
     // ----------------------------------------------------------------------
     // Execution
