ba64bcb3ce802391bd865084ce77bbb79674431d
Jason van Zyl <jvanzyl@apache.org>
2004-09-20 03:42:46 +0000

    surefire 1.2-SNAPSHOT
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@163123 13f79535-47bb-0310-9956-ffa450edef68

--- maven-mboot2/src/main/java/SurefireBooter.java
+++ maven-mboot2/src/main/java/SurefireBooter.java
@@ -157,7 +157,7 @@ public class SurefireBooter
 
         surefireBooter.addClassPathUrl( new File( mavenRepoLocal, "junit/jars/junit-3.8.1.jar" ).getPath() );
 
-        surefireBooter.addClassPathUrl( new File( mavenRepoLocal, "surefire/jars/surefire-1.1.jar" ).getPath() );
+        surefireBooter.addClassPathUrl( new File( mavenRepoLocal, "surefire/jars/surefire-1.2-SNAPSHOT.jar" ).getPath() );
 
         surefireBooter.addClassPathUrl( new File( basedir, "target/classes/" ).getPath() );
 
--- maven-mboot2/src/main/java/SurefirePlugin.java
+++ maven-mboot2/src/main/java/SurefirePlugin.java
@@ -20,7 +20,7 @@ public class SurefirePlugin
 
         surefireBooter.addClassPathUrl( new File( mavenRepoLocal, "junit/jars/junit-3.8.1.jar" ).getPath() );
 
-        surefireBooter.addClassPathUrl( new File( mavenRepoLocal, "surefire/jars/surefire-1.1.jar" ).getPath() );
+        surefireBooter.addClassPathUrl( new File( mavenRepoLocal, "surefire/jars/surefire-1.2-SNAPSHOT.jar" ).getPath() );
 
         surefireBooter.addClassPathUrl( new File( classesDirectory ).getPath() );
 
