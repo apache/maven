f279ac17c273f14221ef6b8b2c88f215743a378c
Jason van Zyl <jvanzyl@apache.org>
2004-10-24 04:10:47 +0000

    o license
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@163210 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/main/java/org/apache/maven/util/GoalWalker.java
+++ maven-core/src/main/java/org/apache/maven/util/GoalWalker.java
@@ -1,5 +1,20 @@
-/* Created on Sep 21, 2004 */
 package org.apache.maven.util;
+
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import java.util.Iterator;
 import java.util.List;
@@ -21,25 +36,31 @@ public final class GoalWalker
             visitor.preVisit( goal, session );
 
             List preGoals = session.getPreGoals( goal );
+
             if ( preGoals != null )
             {
                 for ( Iterator it = preGoals.iterator(); it.hasNext(); )
                 {
                     String preGoal = (String) it.next();
+
                     visitor.visitPreGoal( goal, preGoal, session );
                 }
             }
 
             PluginManager pluginManager = session.getPluginManager();
+
             MojoDescriptor mojoDescriptor = pluginManager.getMojoDescriptor( goal );
+
             if ( mojoDescriptor != null )
             {
                 List prereqs = mojoDescriptor.getPrereqs();
+
                 if ( prereqs != null )
                 {
                     for ( Iterator it = prereqs.iterator(); it.hasNext(); )
                     {
                         String prereq = (String) it.next();
+
                         visitor.visitPrereq( goal, prereq, session );
                     }
                 }
@@ -48,11 +69,13 @@ public final class GoalWalker
             visitor.visitGoal( goal, session );
 
             List postGoals = session.getPostGoals( goal );
+
             if ( postGoals != null )
             {
                 for ( Iterator it = postGoals.iterator(); it.hasNext(); )
                 {
                     String postGoal = (String) it.next();
+
                     visitor.visitPostGoal( goal, postGoal, session );
                 }
             }
@@ -60,5 +83,4 @@ public final class GoalWalker
             visitor.postVisit( goal, session );
         }
     }
-
 }
