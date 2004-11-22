aa438b8137717e9c812dfd79743167d898cebed6
Trygve Laugstol <trygvis@apache.org>
2004-11-22 12:46:55 +0000

    o Adding a artifact handler for sources.
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@163235 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-artifact/src/main/java/org/apache/maven/artifact/handler/SourceHandler.java
@@ -0,0 +1,28 @@
+package org.apache.maven.artifact.handler;
+
+import java.io.File;
+
+import org.apache.maven.artifact.Artifact;
+
+/**
+ * @author <a href="mailto:trygvis@inamo.no">Trygve Laugst&oslash;l</a>
+ * @version $Id$
+ */
+public class SourceHandler
+    extends AbstractArtifactHandler
+{
+    public File source( String basedir, Artifact artifact )
+    {
+        return new File( basedir, artifact.getArtifactId() + "-" + artifact.getVersion() + "-sources." + extension() );
+    }
+
+    public String extension()
+    {
+        return "jar";
+    }
+
+    public String directory()
+    {
+        return "sources";
+    }
+}
