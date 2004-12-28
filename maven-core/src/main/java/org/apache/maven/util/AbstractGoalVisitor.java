fe05b1efc9a58e822115ea5326ac23f3c1d56c28
Jason van Zyl <jvanzyl@apache.org>
2004-12-28 19:09:31 +0000

    o formatting
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@163271 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/main/java/org/apache/maven/util/AbstractGoalVisitor.java
+++ maven-core/src/main/java/org/apache/maven/util/AbstractGoalVisitor.java
@@ -25,53 +25,50 @@ import org.apache.maven.lifecycle.session.MavenSession;
 public abstract class AbstractGoalVisitor
     implements GoalVisitor
 {
-
     protected AbstractGoalVisitor()
     {
     }
 
-
-    public boolean shouldVisit( String goal, MavenSession session ) 
-    throws GraphTraversalException
+    public boolean shouldVisit( String goal, MavenSession session )
+        throws GraphTraversalException
     {
         // visit all by default
         return true;
     }
-    
-    public void visitGoal( String goal, MavenSession session ) 
-    throws GraphTraversalException
+
+    public void visitGoal( String goal, MavenSession session )
+        throws GraphTraversalException
     {
         // do nothing by default
     }
-    
+
     public void visitPostGoal( String goal, String postGoal, MavenSession session )
-    throws GraphTraversalException
+        throws GraphTraversalException
     {
         // do nothing by default
     }
 
     public void visitPreGoal( String goal, String preGoal, MavenSession session )
-    throws GraphTraversalException
+        throws GraphTraversalException
     {
         // do nothing by default
     }
 
     public void visitPrereq( String goal, String prereq, MavenSession session )
-    throws GraphTraversalException
+        throws GraphTraversalException
     {
         // do nothing by default
     }
-    
+
     public void preVisit( String goal, MavenSession session )
-    throws GraphTraversalException
+        throws GraphTraversalException
     {
         // do nothing by default
     }
 
-    public void postVisit( String goal, MavenSession session ) 
-    throws GraphTraversalException
+    public void postVisit( String goal, MavenSession session )
+        throws GraphTraversalException
     {
         // do nothing by default
     }
-
 }
--- maven-core/src/main/java/org/apache/maven/util/GoalVisitor.java
+++ maven-core/src/main/java/org/apache/maven/util/GoalVisitor.java
@@ -24,19 +24,24 @@ import org.apache.maven.lifecycle.session.MavenSession;
  */
 public interface GoalVisitor
 {
-
-    boolean shouldVisit( String goal, MavenSession session ) throws GraphTraversalException;
+    boolean shouldVisit( String goal, MavenSession session )
+        throws GraphTraversalException;
     
-    void preVisit( String goal, MavenSession session ) throws GraphTraversalException;
+    void preVisit( String goal, MavenSession session )
+        throws GraphTraversalException;
 
-    void visitGoal( String goal, MavenSession session ) throws GraphTraversalException;
+    void visitGoal( String goal, MavenSession session )
+        throws GraphTraversalException;
 
-    void visitPreGoal( String goal, String preGoal, MavenSession session ) throws GraphTraversalException;
+    void visitPreGoal( String goal, String preGoal, MavenSession session )
+        throws GraphTraversalException;
 
-    void visitPrereq( String goal, String prereq, MavenSession session ) throws GraphTraversalException;
+    void visitPrereq( String goal, String prereq, MavenSession session )
+        throws GraphTraversalException;
 
-    void visitPostGoal( String goal, String postGoal, MavenSession session ) throws GraphTraversalException;
+    void visitPostGoal( String goal, String postGoal, MavenSession session )
+        throws GraphTraversalException;
 
-    void postVisit( String goal, MavenSession session ) throws GraphTraversalException;
-
+    void postVisit( String goal, MavenSession session )
+        throws GraphTraversalException;
 }
