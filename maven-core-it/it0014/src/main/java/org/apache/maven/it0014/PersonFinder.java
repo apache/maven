cf55279551273c95e8bb1b8c4f3a20b28c88102b
Brett Leslie Porter <brett@apache.org>
2005-02-24 04:52:04 +0000

    Initial revision
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@163402 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-core-it/it0014/src/main/java/org/apache/maven/it0014/PersonFinder.java
@@ -0,0 +1,12 @@
+package org.apache.maven.it0014;
+
+import org.codehaus.classworlds.ClassRealm;
+import junit.framework.TestCase;
+import A.A;
+
+public class PersonFinder
+{
+    private ClassRealm classRealm;
+
+    private A a;
+}
