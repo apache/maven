1e121e8d1ab1d33b8b20c4084dfb539ee247af60
Brett Leslie Porter <brett@apache.org>
2005-03-22 13:44:17 +0000

    convert plugin plugin to new execute()
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@163664 13f79535-47bb-0310-9956-ffa450edef68

--- maven-plugins/maven-plugin-plugin/src/main/java/org/apache/maven/plugin/plugin/AbstractGeneratorMojo.java
+++ maven-plugins/maven-plugin-plugin/src/main/java/org/apache/maven/plugin/plugin/AbstractGeneratorMojo.java
@@ -1,8 +1,23 @@
 package org.apache.maven.plugin.plugin;
 
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import org.apache.maven.plugin.AbstractPlugin;
-import org.apache.maven.plugin.PluginExecutionRequest;
-import org.apache.maven.plugin.PluginExecutionResponse;
+import org.apache.maven.plugin.PluginExecutionException;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.tools.plugin.scanner.MojoScanner;
 
@@ -15,28 +30,28 @@ import java.util.Set;
 public abstract class AbstractGeneratorMojo
     extends AbstractPlugin
 {
+    protected String outputDirectory;
+
+    protected MavenProject project;
+
+    protected MojoScanner mojoScanner;
+
     protected abstract void generate( String outputDirectory, Set mavenMojoDescriptors, MavenProject project )
         throws Exception;
 
-    public void execute( PluginExecutionRequest request, PluginExecutionResponse response )
-        throws Exception
+    public void execute()
+        throws PluginExecutionException
     {
-        // ----------------------------------------------------------------------
-        //
-        // ----------------------------------------------------------------------
+        try
+        {
+            Set mavenMojoDescriptors = mojoScanner.execute( project );
 
-        String outputDirectory = (String) request.getParameter( "outputDirectory" );
-
-        MavenProject project = (MavenProject)request.getParameter( "project" );
-        
-        MojoScanner scanner = (MojoScanner)request.getParameter("mojoScanner");
-        
-        Set mavenMojoDescriptors = scanner.execute(project);
-
-        // ----------------------------------------------------------------------
-        //
-        // ----------------------------------------------------------------------
-
-        generate( outputDirectory, mavenMojoDescriptors, project );
+            generate( outputDirectory, mavenMojoDescriptors, project );
+        }
+        catch ( Exception e )
+        {
+            // TODO: improve error handling
+            throw new PluginExecutionException( "Error generating plugin descriptor", e );
+        }
     }
 }
--- maven-plugins/maven-plugin-plugin/src/main/java/org/apache/maven/plugin/plugin/BeanGeneratorMojo.java
+++ maven-plugins/maven-plugin-plugin/src/main/java/org/apache/maven/plugin/plugin/BeanGeneratorMojo.java
@@ -1,39 +1,49 @@
 package org.apache.maven.plugin.plugin;
 
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.tools.plugin.generator.BeanGenerator;
 
 import java.util.Set;
 
 /**
- * @goal bean
- *
- * @description Goal for generating a plugin descriptor.
- *
- * @parameter
- *  name="mojoScanner"
- *  type="org.apache.maven.tools.plugin.scanner.MojoScanner"
- *  required="true"
- *  validator=""
- *  expression="#component.org.apache.maven.tools.plugin.scanner.MojoScanner"
- *  description="Scanner used to discover mojo descriptors from this project"
- * @parameter
- *  name="project"
- *  type="org.apache.maven.project.MavenProject"
- *  required="true"
- *  validator=""
- *  expression="#project"
- *  description=""
- * @parameter
- *  name="outputDirectory"
- *  type="String"
- *  required="true" 
- *  validator=""
- *  expression="#project.build.directory/generated-sources"
- *  description=""
- *
  * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
  * @version $Id$
+ * @goal bean
+ * @description Goal for generating a plugin descriptor.
+ * @parameter name="mojoScanner"
+ * type="org.apache.maven.tools.plugin.scanner.MojoScanner"
+ * required="true"
+ * validator=""
+ * expression="#component.org.apache.maven.tools.plugin.scanner.MojoScanner"
+ * description="Scanner used to discover mojo descriptors from this project"
+ * @parameter name="project"
+ * type="org.apache.maven.project.MavenProject"
+ * required="true"
+ * validator=""
+ * expression="#project"
+ * description=""
+ * @parameter name="outputDirectory"
+ * type="String"
+ * required="true"
+ * validator=""
+ * expression="#project.build.directory/generated-sources"
+ * description=""
  */
 public class BeanGeneratorMojo
     extends AbstractGeneratorMojo
