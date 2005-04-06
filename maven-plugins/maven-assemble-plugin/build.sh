2cf5c6c0a5fae6b96f0a039a2e45d989e59e62f0
Brett Leslie Porter <brett@apache.org>
2005-04-06 01:49:06 +0000

    fix build issues
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@163793 13f79535-47bb-0310-9956-ffa450edef68

--- maven-plugins/maven-assemble-plugin/build.sh
+++ maven-plugins/maven-assemble-plugin/build.sh
@@ -4,6 +4,15 @@
 # WORKAROUND FOR http://jira.codehaus.org/browse/MNG-214
 #
 
-m2 plugin:descriptor
-m2 modello:xpp3-reader modello:xpp3-writer modello:java resources:resources compiler:compile resources:testResources compiler:testCompile surefire:test jar:jar
-cp target/maven-assemble-plugin-1.0-SNAPSHOT.jar $HOME/repository-m2/org.apache.maven.plugins/maven-plugins
+
+REPO=$HOME/repository-new
+
+cp pom.xml pom.xml.backup
+cat pom.xml.backup | sed 's#<packaging>maven-plugin</packaging>##' >pom.xml
+
+m2 clean:clean plugin:descriptor package
+cp target/maven-assemble-plugin-1.0-SNAPSHOT.jar $REPO/org/apache/maven/plugins/maven-assemble-plugin/1.0-SNAPSHOT
+
+mv pom.xml.backup pom.xml
+cp pom.xml $REPO/org/apache/maven/plugins/maven-assemble-plugin/1.0-SNAPSHOT/maven-assemble-plugin-1.0-SNAPSHOT.pom
+
