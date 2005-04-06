6d30c273a06e1bf83863e1110b371eae237c64b0
Brett Leslie Porter <brett@apache.org>
2005-04-06 06:54:30 +0000

    repairs on scripts
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@163809 13f79535-47bb-0310-9956-ffa450edef68

--- maven-plugins/maven-assembly-plugin/deploy.sh
+++ maven-plugins/maven-assembly-plugin/deploy.sh
@@ -9,7 +9,7 @@ REPO=$HOME/repository-new
 cp pom.xml pom.xml.backup
 cat pom.xml.backup | sed 's#<packaging>maven-plugin</packaging>##' >pom.xml
 
-#m2 clean:clean plugin:descriptor deploy
+m2 clean:clean plugin:descriptor deploy
 
 version=`cat $REPO/org/apache/maven/plugins/maven-assembly-plugin/1.0-SNAPSHOT/maven-assembly-plugin-1.0-SNAPSHOT.version.txt`
 cat pom.xml.backup | sed "s#<version>1.0-SNAPSHOT</version>#<version>$version</version>#" >pom.xml
