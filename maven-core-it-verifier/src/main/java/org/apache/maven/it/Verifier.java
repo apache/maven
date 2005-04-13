567412e9a0ad382316972e7539c86c297ba762b0
Brett Leslie Porter <brett@apache.org>
2005-04-13 08:31:38 +0000

    missed import
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@163933 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-it-verifier/src/main/java/org/apache/maven/it/Verifier.java
+++ maven-core-it-verifier/src/main/java/org/apache/maven/it/Verifier.java
@@ -23,6 +23,7 @@ import java.io.PrintStream;
 import java.io.Writer;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
