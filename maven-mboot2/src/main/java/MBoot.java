0aeab76ac38900f6277faf160e69c5a3e7079b19
Brett Leslie Porter <brett@apache.org>
2005-04-22 13:06:17 +0000

    fix order
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@164235 13f79535-47bb-0310-9956-ffa450edef68

--- maven-mboot2/src/main/java/MBoot.java
+++ maven-mboot2/src/main/java/MBoot.java
@@ -35,9 +35,9 @@ import java.util.TreeMap;
 
 public class MBoot
 {
-    String[] builds = new String[]{"maven-model", "maven-project", "maven-settings", "maven-monitor", "maven-plugin-api",
+    String[] builds = new String[]{"maven-model", "maven-settings", "maven-monitor", "maven-plugin-api",
                                    "maven-plugin-descriptor", "maven-artifact", "maven-script/maven-script-marmalade",
-                                   "maven-core", "maven-archiver", "maven-plugin-tools/maven-plugin-tools-api",
+                                   "maven-project", "maven-core", "maven-archiver", "maven-plugin-tools/maven-plugin-tools-api",
                                    "maven-plugin-tools/maven-plugin-tools-java",
                                    "maven-plugin-tools/maven-plugin-tools-pluggy",
                                    "maven-plugin-tools/maven-plugin-tools-marmalade", "maven-core-it-verifier"};
