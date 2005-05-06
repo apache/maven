6de708da599bc3d97e20342aa0ed4bcd7acbbbba
John Dennis Casey <jdcasey@apache.org>
2005-05-06 00:36:00 +0000

    IT for testing failure when duplicate goal names are specified in multiple mojos...
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@168483 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-core-it/it1005/src/main/java/org/apache/maven/plugin/coreit/CoreIt1005Mojo.java
@@ -0,0 +1,41 @@
+package org.apache.maven.plugin.coreit;
+
+/*
+ * Copyright 2001-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * @goal it1005
+ *
+ * @description test for goal collision. This is copy #2
+ *
+ */
+public class CoreIt1005Mojo
+    extends AbstractMojo
+{
+    
+    public void execute()
+        throws MojoExecutionException
+    {
+        System.out.println( "Test mojo (copy 2)." );
+    }
+}
