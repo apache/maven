854b76e61f1acc2263ee9cea5a3d14a8efe2b886
John Dennis Casey <jdcasey@apache.org>
2005-06-22 03:27:27 +0000

    Fixing plugin-version prompting once and for all (hopefully)...at least, the logic.
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@191757 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/main/java/org/apache/maven/plugin/version/DefaultPluginVersionManager.java
+++ maven-core/src/main/java/org/apache/maven/plugin/version/DefaultPluginVersionManager.java
@@ -289,7 +289,7 @@ public class DefaultPluginVersionManager
                 message.append( "Registered Version: " ).append( version ).append( "\n" );
             }
 
-            message.append( "Detected (NEW) Version: " ).append( updatedVersion ).append( "\n" );
+            message.append( "Detected plugin version: " ).append( updatedVersion ).append( "\n" );
             message.append( "\n" );
             message.append( "Would you like to use this new version from now on? ( [Y]es, [n]o, [a]ll, n[o]ne ) " );
 
@@ -299,7 +299,7 @@ public class DefaultPluginVersionManager
 
             String persistAnswer = inputHandler.readLine();
 
-            boolean shouldPersist = false;
+            boolean shouldPersist = true;
 
             if ( !StringUtils.isEmpty( persistAnswer ) )
             {
