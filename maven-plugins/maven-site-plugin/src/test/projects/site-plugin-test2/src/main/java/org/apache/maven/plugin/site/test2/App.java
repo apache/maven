045b43f08e3c290b58b6395c04c237d6ef856454
Vincent Siveton <vsiveton@apache.org>
2005-07-14 04:17:00 +0000

    Added test cases
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@218984 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-plugins/maven-site-plugin/src/test/projects/site-plugin-test2/src/main/java/org/apache/maven/plugin/site/test2/App.java
@@ -0,0 +1,13 @@
+package org.apache.maven.plugin.site.test2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ maven-plugins/maven-site-plugin/src/test/projects/site-plugin-test3/src/main/java/org/apache/maven/plugin/site/test3/App.java
@@ -0,0 +1,13 @@
+package org.apache.maven.plugin.site.test3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
