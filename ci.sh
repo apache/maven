3b6503172bbae0c0a290a994fa329725c3ec0948
Brett Leslie Porter <brett@apache.org>
2005-07-20 14:33:41 +0000

    zone settings
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@219910 13f79535-47bb-0310-9956-ffa450edef68

--- ci.sh
+++ ci.sh
@@ -6,7 +6,7 @@ CMD=$1
 
 [ "$1" = "" ] && echo && echo "You must specify a checkout or update!" && echo && exit 1
 
-FROM=jvanzyl@maven.org
+FROM=continuum@maven.zones.apache.org
 TO=dev@maven.apache.org
 DATE=`date`
 
@@ -28,19 +28,21 @@ DIR=m2-build
 REPO=maven-repo-local
 SCM_LOG=scm.log
 TIMESTAMP=`date +%Y%m%d.%H%M%S`
-DEPLOY_DIR=$HOME_DIR/public_html/m2
-DEPLOY_SITE=http://www.codehaus.org/~maven/m2
+WWW=$HOME/public_html
+DEPLOY_DIR=$WWW/builds
+DEPLOY_SITE=http://maven.zones.apache.org/~maven/builds
 DIST=m2-${TIMESTAMP}.tar.gz
-SVN=/usr/local/subversion/bin/svn
+SVN=svn
 
-export JAVA_HOME=/usr/local/java
-# Required until classworlds.conf is updated
-#export M2_HOME=$HOME_DIR/m2
-export M2_HOME=$HOME/m2
-export PATH=$PATH:$JAVA_HOME/bin:$M2_HOME/bin
-export MESSAGE_DIR=$HOME_DIR/public_html/m2-build-logs
-export MESSAGE_NAME=m2-build-log-${TIMESTAMP}.txt
-export MESSAGE=${MESSAGE_DIR}/${MESSAGE_NAME}
+M2_HOME=$HOME/m2
+export M2_HOME
+PATH=$PATH:$JAVA_HOME/bin:$M2_HOME/bin
+MESSAGE_DIR=$WWW/logs
+MESSAGE_NAME=m2-build-log-${TIMESTAMP}.txt
+MESSAGE=${MESSAGE_DIR}/${MESSAGE_NAME}
+
+mkdir -p $DEPLOY_DIR
+mkdir -p $MESSAGE_DIR
 
 # ----------------------------------------------------------------------------------
 
@@ -165,7 +167,7 @@ then
   fi
   echo "" >> log
   echo "Log:" >> log
-  echo "http://www.codehaus.org/~maven/m2-build-logs/${MESSAGE_NAME}" >> log
+  echo "http://maven.zones.apache.org/~maven/logs/${MESSAGE_NAME}" >> log
 
   /usr/sbin/sendmail -t < log
 fi
