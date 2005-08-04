22744c798dd81c853a8f2312102a8372fe6b1e35
John Dennis Casey <jdcasey@apache.org>
2005-08-04 02:31:33 +0000

    trying to resurrect this one...
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@227292 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-script/maven-script-marmalade/src/main/java/org/apache/maven/script/marmalade/tags/LifecyclePhaseTag.java
@@ -0,0 +1,15 @@
+package org.apache.maven.script.marmalade.tags;
+
+public class LifecyclePhaseTag
+{
+
+    /**
+     * @param args
+     */
+    public static void main( String[] args )
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
