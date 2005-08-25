ea20af3cab27a1d153a0cdc88d8eac1d2619a299
Brett Leslie Porter <brett@apache.org>
2005-08-25 02:41:01 +0000

    updates from the server
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@239979 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/ibiblio-sync/synchronize-codehaus-to-ibiblio-m2.sh
+++ maven-meeper/src/bin/ibiblio-sync/synchronize-codehaus-to-ibiblio-m2.sh
@@ -6,4 +6,5 @@ dest=/home/projects/maven/repository-staging/to-ibiblio
 #rsync -e ssh -v -rt $dest/maven login.ibiblio.org:/public/html/maven
 
 date > $dest/maven2/last-sync.txt
+chmod a+r $dest/maven2/last-sync.txt
 rsync -e ssh -v -rtp $dest/maven2/ login.ibiblio.org:/public/html/maven2
--- maven-meeper/src/bin/ibiblio-sync/synchronize-codehaus-to-ibiblio.sh
+++ maven-meeper/src/bin/ibiblio-sync/synchronize-codehaus-to-ibiblio.sh
@@ -3,7 +3,9 @@
 dest=/home/projects/maven/repository-staging/to-ibiblio
 
 date > $dest/maven/last-sync.txt
+chmod a+r $dest/maven/last-sync.txt
 rsync -e ssh --exclude='*/distributions/*' -v -rplt $dest/maven/ login.ibiblio.org:/public/html/maven
 
 date > $dest/maven2/last-sync.txt
-rsync -e ssh --exclude='*/distributions/*' -v -rplt $dest/maven2/ login.ibiblio.org:/public/html/maven2
+chmod a+r $dest/maven2/last-sync.txt
+rsync -e ssh --delete --exclude='*/distributions/*' -v -rplt $dest/maven2/ login.ibiblio.org:/public/html/maven2
