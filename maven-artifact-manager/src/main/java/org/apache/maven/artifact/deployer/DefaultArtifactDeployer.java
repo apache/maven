14f4c980c1756441caa28a2bb76289a1f90b9305
Brett Leslie Porter <brett@apache.org>
2005-08-27 12:48:12 +0000

    use the correct repository for finding the original snapshot data
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@240414 13f79535-47bb-0310-9956-ffa450edef68

--- maven-artifact-manager/src/main/java/org/apache/maven/artifact/deployer/DefaultArtifactDeployer.java
+++ maven-artifact-manager/src/main/java/org/apache/maven/artifact/deployer/DefaultArtifactDeployer.java
@@ -35,10 +35,12 @@ public class DefaultArtifactDeployer
     implements ArtifactDeployer
 {
     private WagonManager wagonManager;
-    
+
     private ArtifactTransformationManager transformationManager;
 
-    /** @deprecated we want to use the artifact method only, and ensure artifact.file is set correctly. */
+    /**
+     * @deprecated we want to use the artifact method only, and ensure artifact.file is set correctly.
+     */
     public void deploy( String basedir, String finalName, Artifact artifact, ArtifactRepository deploymentRepository,
                         ArtifactRepository localRepository )
         throws ArtifactDeploymentException
@@ -54,7 +56,7 @@ public class DefaultArtifactDeployer
     {
         try
         {
-            transformationManager.transformForDeployment( artifact, localRepository );
+            transformationManager.transformForDeployment( artifact, deploymentRepository );
 
             // Copy the original file to the new one if it was transformed
             File artifactFile = new File( localRepository.getBasedir(), localRepository.pathOf( artifact ) );
