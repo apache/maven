b3dae8c7ccc1b621d77ce1564c4d2d8a42b5b4e3
Jason van Zyl <jvanzyl@apache.org>
2005-09-28 23:08:41 +0000

    o updating the metadata for the core and bumping the version in the scripts
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@292343 13f79535-47bb-0310-9956-ffa450edef68

--- maven-embedder/run.sh
+++ maven-embedder/run.sh
@@ -2,7 +2,7 @@
 
 m2 clean:clean assembly:assembly
 
-CLASSPATH=target/maven-embedder-2.0-beta-2-SNAPSHOT-dep.jar
+CLASSPATH=target/maven-embedder-2.0-beta-3-SNAPSHOT-dep.jar
 
 javac -classpath $CLASSPATH Plugin.java
 
