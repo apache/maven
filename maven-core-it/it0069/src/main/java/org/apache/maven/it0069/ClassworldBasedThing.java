bea5e00e1e052e2d00be0340e04a47d821945b9a
Brett Leslie Porter <brett@apache.org>
2005-10-03 05:49:59 +0000

    use classworld dep instead
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@293258 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-core-it/it0069/src/main/java/org/apache/maven/it0069/ClassworldBasedThing.java
@@ -0,0 +1,7 @@
+package org.apache.maven.it0069;
+
+public class ClassworldBasedThing
+{
+    private org.codehaus.classworlds.ClassRealm realm;
+}
+
--- maven-core-it/it0069/src/main/java/org/apache/maven/it0069/ProjectBasedThing.java
+++
@@ -1,9 +0,0 @@
-package org.apache.maven.it0069;
-
-import org.apache.maven.project.MavenProject;
-
-public class ProjectBasedThing
-{
-    private MavenProject project;
-}
-
