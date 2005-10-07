1c02566be7f872a735fb1130f191066a0b91aa3b
Jason van Zyl <jvanzyl@apache.org>
2005-10-07 15:23:37 +0000

    o start of an archetype that can be used with doco describing how best   to target different environments.
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@307145 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-archetype/maven-archetypes/maven-archetype-profiles/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $package;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
