1957f5118d99f36d5e23854eb20785a4f5683e04
Brett Leslie Porter <brett@apache.org>
2005-10-13 07:12:00 +0000

    add copyright
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@320680 13f79535-47bb-0310-9956-ffa450edef68

--- maven-plugins/maven-antlr-plugin/src/main/java/org/apache/maven/plugin/antlr/AntlrPlugin.java
+++ maven-plugins/maven-antlr-plugin/src/main/java/org/apache/maven/plugin/antlr/AntlrPlugin.java
@@ -1,5 +1,21 @@
 package org.apache.maven.plugin.antlr;
 
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import antlr.Tool;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -32,7 +48,7 @@ public class AntlrPlugin
      */
     private File sourceDirectory;
 
-     /**
+    /**
      * @parameter expression="${project.build.directory}/generated-sources/antlr"
      * @required
      */
@@ -87,12 +103,7 @@ public class AntlrPlugin
                 generated.getParentFile().mkdirs();
             }
 
-            String[] args = new String[]
-            {
-                "-o",
-                generated.getParentFile().getPath(),
-                grammar.getPath(),
-            };
+            String[] args = new String[]{"-o", generated.getParentFile().getPath(), grammar.getPath(),};
 
             SecurityManager oldSm = System.getSecurityManager();
 
@@ -184,7 +195,8 @@ public class AntlrPlugin
 
 }
 
-class NoExitSecurityManager extends SecurityManager
+class NoExitSecurityManager
+    extends SecurityManager
 {
     static final NoExitSecurityManager INSTANCE = new NoExitSecurityManager();
 
