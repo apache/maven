5b9d2cf6451174af82c6e719122778572b42da65
Jason van Zyl <jvanzyl@apache.org>
2005-10-14 17:19:38 +0000

    o adding updated toc generator, thanks to bob allison
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@321155 13f79535-47bb-0310-9956-ffa450edef68

--- maven-site/src/site/apt/guides/mktoc.sh
+++ maven-site/src/site/apt/guides/mktoc.sh
@@ -1,8 +1,29 @@
 
 toc=`pwd`/index.apt
 
-cp header.apt $toc
+#
+# Top matter
+#
 
+echo " ------ " > $toc
+echo " Summary of Maven 2.x documentation " >> $toc
+echo " ------ " >> $toc
+echo " Jason van Zyl " >> $toc
+echo " ------ " >> $toc
+echo " 12 October 2005 " >> $toc
+echo " ------ " >> $toc
+echo >> $toc
+echo "Documentation" >> $toc
+echo >> $toc
+echo "* Getting Started Guide" >> $toc
+echo >> $toc
+echo " * {{{getting-started/index.html}Getting Started Guide}}" >> $toc
+
+#
+# Mini Guides
+#
+
+echo >> $toc
 echo "* Mini Guides" >> $toc
 echo >> $toc
 
@@ -16,6 +37,10 @@ echo >> $toc
    [ ! -z "$title" ] && echo " * {{{mini/$i}$title}}" >> $toc && echo >> $toc
   done       
 )       
+
+#
+# Introductions
+#
 
 echo >> $toc
 echo "* Introductory Material" >> $toc
@@ -31,3 +56,33 @@ echo >> $toc
    [ ! -z "$title" ] && echo " * {{{introduction/$i}$title}}" >> $toc && echo >> $toc
   done       
 )       
+
+#
+# Plugins
+#
+
+echo >> $toc
+echo "* Plugin Guides" >> $toc
+echo >> $toc
+
+(
+  cd plugin
+
+  for i in `ls guide-*.apt`
+  do
+   title=`grep "^ Guide" $i | sed 's/^ *//'`
+   i=`echo $i | sed 's/\.apt/\.html/'`
+   [ ! -z "$title" ] && echo " * {{{plugin/$i}$title}}" >> $toc && echo >> $toc
+  done       
+)       
+
+#
+# Developer Guides
+#
+
+echo "" >> $toc
+echo "* Developer Guides " >> $toc
+echo "" >> $toc
+echo " * {{{development/guide-m2-development.html}Guide to Developing Maven 2.x}}" >> $toc
+echo " " >> $toc
+echo " * {{{development/guide-building-m2.html}Guide to Building Maven 2.x}}" >> $toc
