6102a9cf292c737ba081b463a09ba8c5ee3fc343
Carlos Sanchez Gonzalez <carlos@apache.org>
2005-10-24 03:04:40 +0000

    Added max-delete option
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@327934 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/ibiblio-sync/synchronize-codehaus-to-ibiblio.sh
+++ maven-meeper/src/bin/ibiblio-sync/synchronize-codehaus-to-ibiblio.sh
@@ -4,8 +4,8 @@ dest=/home/projects/maven/repository-staging/to-ibiblio
 
 date > $dest/maven/last-sync.txt
 chmod a+r $dest/maven/last-sync.txt
-rsync -e ssh --delete --exclude-from=$dest/exclusions.txt -v -rplt $dest/maven/ login.ibiblio.org:/public/html/maven
+rsync -e ssh --delete --max-delete=10 --exclude-from=$dest/exclusions.txt -v -rplt $dest/maven/ login.ibiblio.org:/public/html/maven
 
 date > $dest/maven2/last-sync.txt
 chmod a+r $dest/maven2/last-sync.txt
-rsync -e ssh --delete --exclude-from=$dest/exclusions.txt -v -rplt $dest/maven2/ login.ibiblio.org:/public/html/maven2
+rsync -e ssh --delete --max-delete=10 --exclude-from=$dest/exclusions.txt -v -rplt $dest/maven2/ login.ibiblio.org:/public/html/maven2
