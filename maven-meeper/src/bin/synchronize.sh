52b457411e79077092bd4d3d4bda0d347ae96528
Carlos Sanchez Gonzalez <carlos@apache.org>
2005-11-09 00:35:28 +0000

    Moved update poms section outside to be triggered by svn commits
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@331929 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/synchronize.sh
+++ maven-meeper/src/bin/synchronize.sh
@@ -24,20 +24,6 @@ retval=$?; if [ $retval != 0 ]; then exit $retval; fi
 )
 retval=$?; if [ $retval != 0 ]; then exit $retval; fi
 
-
-# get poms from svn and generate checksums
-(
-  src=/home/projects/maven/repository-staging/pom-svn-repository
-  dest=/home/projects/maven/repository-staging/to-ibiblio/maven2
-  cd $src
-  /usr/local/subversion/bin/svn update
-  rsync -e ssh -v -rpt --exclude=.svn --exclude=updated-poms.log $src/ $dest/ > updated-poms.log
-  for f in `grep .pom updated-poms.log` ; do md5sum $dest/$f > $dest/$f.md5 ; sha1sum $dest/$f > $dest/$f.sha1; done
-  retval=$?; if [ $retval != 0 ]; then exit $retval; fi
-)
-retval=$?; if [ $retval != 0 ]; then exit $retval; fi
-
-
 (
   cd $TOOLS_BASE/ibiblio-sync
   ./synchronize-codehaus-to-ibiblio.sh
---
+++ maven-meeper/src/bin/update-poms.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+src=/home/projects/maven/repository-staging/pom-svn-repository
+dest=/home/projects/maven/repository-staging/to-ibiblio/maven2
+
+cd $src
+/usr/local/subversion/bin/svn update
+
+rsync -e ssh -v -rpt --exclude=.svn --exclude=updated-poms.log $src/ $dest/ > updated-poms.log
+
+for f in `grep .pom updated-poms.log` ; do 
+  md5sum $dest/$f > $dest/$f.md5 ; 
+  sha1sum $dest/$f > $dest/$f.sha1;
+done
+
+rm updated-poms.log
