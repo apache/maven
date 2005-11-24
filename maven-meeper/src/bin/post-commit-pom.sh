374776dd403bace93173a1d1f5e7144817687dcf
Brett Leslie Porter <brett@apache.org>
2005-11-24 11:46:56 +0000

    mkdir
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@348714 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/post-commit-pom.sh
+++ maven-meeper/src/bin/post-commit-pom.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+set -x
+
 svnlook=/usr/local/subversion/bin/svnlook 
 repos=/home/projects/maven/repository-staging/to-ibiblio/maven2
 
 $svnlook changed -r $2 $1 | egrep '\.pom$' | while read t1 t2
 do
+  mkdir -p `dirname $repos/$t2`
   $svnlook cat -r $2 $1 $t2 >$repos/$t2
   sha1sum $repos/$t2 >$repos/$t2.sha1
   md5sum $repos/$t2 >$repos/$t2.md5
