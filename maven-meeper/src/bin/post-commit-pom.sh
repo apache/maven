7928a66ef55c5b935a1e8fcd8384481e6bd64d72
Brett Leslie Porter <brett@apache.org>
2005-11-24 10:59:18 +0000

    add post commit script
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@348705 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-meeper/src/bin/post-commit-pom.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+svnlook=/usr/local/subversion/bin/svnlook 
+repos=/home/projects/maven/repository-staging/to-ibiblio/maven2
+
+$svnlook changed -r $2 $1 | egrep '\.pom$' | while read t1 t2
+do
+  $svnlook cat -r $2 $1 $t2 >$repos/$t2
+  sha1sum $repos/$t2 >$repos/$t2.sha1
+  md5sum $repos/$t2 >$repos/$t2.md5
+done
+
