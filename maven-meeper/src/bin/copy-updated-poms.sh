b248997b7f36f003542a9e3b1f0a26191434d71f
Carlos Sanchez Gonzalez <carlos@apache.org>
2005-12-23 16:44:53 +0000

    Don't delete the log file at the end
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@358827 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/copy-updated-poms.sh
+++ maven-meeper/src/bin/copy-updated-poms.sh
@@ -3,11 +3,9 @@
 src=/home/projects/maven/repository-staging/pom-svn-repository
 dest=/home/projects/maven/repository-staging/to-ibiblio/maven2
 
-rsync -e ssh -v -rpt --exclude=.svn --exclude=updated-poms.log $src/ $dest/ > updated-poms.log
+rsync -e ssh -v -rpt --exclude=.svn --exclude=updated-poms.log $src/ $dest/ > $src/updated-poms.log
 
-for f in `grep .pom updated-poms.log` ; do 
+for f in `grep .pom $src/updated-poms.log` ; do 
   md5sum $dest/$f > $dest/$f.md5 ; 
   sha1sum $dest/$f > $dest/$f.sha1;
 done
-
-rm updated-poms.log
