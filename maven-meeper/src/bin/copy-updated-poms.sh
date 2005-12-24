f734b900a7380acd053cb92f47cf0cbdfaba294f
Carlos Sanchez Gonzalez <carlos@apache.org>
2005-12-24 17:47:42 +0000

    Allow sync of subfolders only
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@358941 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/copy-updated-poms.sh
+++ maven-meeper/src/bin/copy-updated-poms.sh
@@ -2,20 +2,23 @@
 
 GROUPID=$1
 
-src=/home/projects/maven/repository-staging/pom-svn-repository
-dest=/home/projects/maven/repository-staging/to-ibiblio/maven2
+basesrc=/home/projects/maven/repository-staging/pom-svn-repository
+basedest=/home/projects/maven/repository-staging/to-ibiblio/maven2
 
 
 if [ ! -z $GROUPID ]
 then
   src=$src/$GROUPID
-  dest=$src/$GROUPID
+  dest=$dest/$GROUPID
+else
+  src=$basesrc
+  dest=$basedest
 fi
 
 
-rsync -e ssh -v -rpt --exclude=.svn --exclude=updated-poms.log $src/ $dest/ > $src/updated-poms.log
+rsync -e ssh -v -rpt --exclude=.svn --exclude=updated-poms.log $src/ $dest/ > $basesrc/updated-poms.log
 
-for f in `grep .pom $src/updated-poms.log` ; do 
+for f in `grep .pom $basesrc/updated-poms.log` ; do 
   md5sum $dest/$f > $dest/$f.md5 ; 
   sha1sum $dest/$f > $dest/$f.sha1;
 done
