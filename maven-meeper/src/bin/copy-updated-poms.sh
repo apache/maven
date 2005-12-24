a06b58fd7d83e9078930058d78fcb14a30929388
Carlos Sanchez Gonzalez <carlos@apache.org>
2005-12-24 17:43:44 +0000

    Allow sync of subfolders only
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@358940 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/copy-updated-poms.sh
+++ maven-meeper/src/bin/copy-updated-poms.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+GROUPID=$1
+
 src=/home/projects/maven/repository-staging/pom-svn-repository
 dest=/home/projects/maven/repository-staging/to-ibiblio/maven2
+
+
+if [ ! -z $GROUPID ]
+then
+  src=$src/$GROUPID
+  dest=$src/$GROUPID
+fi
+
 
 rsync -e ssh -v -rpt --exclude=.svn --exclude=updated-poms.log $src/ $dest/ > $src/updated-poms.log
 
