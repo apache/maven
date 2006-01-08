0542f670ab72203c149a81d828a4b68ec3c5ab69
Brett Leslie Porter <brett@apache.org>
2006-01-08 20:42:12 +0000

    turn off m1 sync
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@367094 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/ibiblio-sync/synchronize-codehaus-to-ibiblio.sh
+++ maven-meeper/src/bin/ibiblio-sync/synchronize-codehaus-to-ibiblio.sh
@@ -2,9 +2,11 @@
 
 dest=/home/projects/maven/repository-staging/to-ibiblio
 
-date > $dest/maven/last-sync.txt
-chmod a+r $dest/maven/last-sync.txt
-rsync -e ssh --delete --max-delete=10 -v -rplt $dest/maven/ login.ibiblio.org:/public/html/maven
+# NO MORE m1 SYNC
+
+#date > $dest/maven/last-sync.txt
+#chmod a+r $dest/maven/last-sync.txt
+#rsync -e ssh --delete --max-delete=10 -v -rplt $dest/maven/ login.ibiblio.org:/public/html/maven
 
 date > $dest/maven2/last-sync.txt
 chmod a+r $dest/maven2/last-sync.txt
