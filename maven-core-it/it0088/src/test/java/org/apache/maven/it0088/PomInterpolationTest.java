1636a473d5924044358263e1af06e8d48f73cf2c
Jason van Zyl <jvanzyl@apache.org>
2006-01-09 20:41:34 +0000

    o adding IT for mng-1927
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@367389 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-core-it/it0088/src/test/java/org/apache/maven/it0088/PomInterpolationTest.java
@@ -0,0 +1,35 @@
+package org.apache.maven.it0088;
+
+import junit.framework.TestCase;
+
+import java.util.Properties;
+import java.io.File;
+import java.io.FileInputStream;
+
+public class PomInterpolationTest
+    extends TestCase
+{
+    private String basedir;
+
+    protected void setUp()
+        throws Exception
+    {
+        basedir = System.getProperty( "basedir" );
+    }
+
+    public void testProjectBuildDirectory()
+        throws Exception
+    {
+        Properties testProperties = new Properties();
+
+        File testPropertiesFile = new File( basedir, "target/classes/test.properties" );
+
+        assertTrue( testPropertiesFile.exists() );
+
+        testProperties.load( new FileInputStream( testPropertiesFile ) );
+
+        File projectBuildDirectory = new File( basedir, "target" );
+
+        assertEquals( testProperties.getProperty( "project.build.directory" ), projectBuildDirectory.getAbsolutePath() );
+    }
+}
