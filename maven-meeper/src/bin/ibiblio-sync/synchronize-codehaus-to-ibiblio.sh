49226c17c8e8806f93ff1afb60f8446b555ca55f
Carlos Sanchez Gonzalez <carlos@apache.org>
2006-01-23 22:08:59 +0000

    Fix syntax error
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@371685 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/ibiblio-sync/synchronize-codehaus-to-ibiblio.sh
+++ maven-meeper/src/bin/ibiblio-sync/synchronize-codehaus-to-ibiblio.sh
@@ -11,14 +11,14 @@ dest=/home/projects/maven/repository-staging/to-ibiblio
 # EXCEPT FOR PLUGINS
 # TODO - FIX
 rsync -e ssh --delete --max-delete=10 -v -rplt $dest/maven/ login.ibiblio.org:/public/html/maven
-(
+#(
   #cd $dest/maven
   #rsync -n -e ssh --delete --max-delete=10 -v -rplt --include='*/plugins/*' ./ login.ibiblio.org:/public/html/maven
   #for i in */plugins
   #do
     #rsync -e ssh --delete --max-delete=10 -v -rplt $i login.ibiblio.org:/public/html/maven/$i
   #done
-)
+#)
 
 date > $dest/maven2/last-sync.txt
 chmod a+r $dest/maven2/last-sync.txt
