3cd5bc957c50c2c73e68eea3e77bea452ce3ffda
Carlos Sanchez Gonzalez <carlos@apache.org>
2006-04-19 02:21:05 +0000

    Use correctly slashes in rsync
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@395108 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/m2-sync/maven-taglib-m2-sync.sh
+++ maven-meeper/src/bin/m2-sync/maven-taglib-m2-sync.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-FROM=carlossg@shell.sourceforge.net:/home/groups/m/ma/maven-taglib/htdocs/m2repo/net/sourceforge/maven-taglib/*
+FROM=carlossg@shell.sourceforge.net:/home/groups/m/ma/maven-taglib/htdocs/m2repo/net/sourceforge/maven-taglib/
 TO=net/sourceforge/maven-taglib/
 
 source ./m2-sync.sh
--- maven-meeper/src/bin/m2-sync/objectweb-m2-sync.sh
+++ maven-meeper/src/bin/m2-sync/objectweb-m2-sync.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-FROM=maven@forge.objectweb.org:../../groups/maven/htdocs/maven2/org/objectweb
-TO=org
+FROM=maven@forge.objectweb.org:../../groups/maven/htdocs/maven2/org/objectweb/
+TO=org/objectweb/
 
 source ./m2-sync.sh
