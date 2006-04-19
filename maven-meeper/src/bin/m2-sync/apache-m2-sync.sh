4e7cc4fcacf81b4c16cb28996f6e1933d980ea47
Carlos Sanchez Gonzalez <carlos@apache.org>
2006-04-19 02:06:56 +0000

    Sync only org.apache from apache
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@395105 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/m2-sync/apache-m2-sync.sh
+++ maven-meeper/src/bin/m2-sync/apache-m2-sync.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-FROM=jvanzyl@cvs.apache.org:/www/www.apache.org/dist/maven-repository/*
-TO=.
+FROM=jvanzyl@cvs.apache.org:/www/www.apache.org/dist/maven-repository/org/apache/*
+TO=org/apache/.
 
 source ./m2-sync.sh
