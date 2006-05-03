c7657a948d376e435531ff45bdaca20492e5ad23
Brett Leslie Porter <brett@apache.org>
2006-05-03 17:03:34 +0000

    updated
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/branches/maven-2.0.x@399353 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-it/it2002/test-cvs.sh
+++ maven-core-it/it2002/test-cvs.sh
@@ -4,8 +4,6 @@ rm -Rf target
 
 mkdir target
 
-cvs -d target/cvsroot init
-
 dir=`readlink -f ${PWD}`
 
 if [ "$1" == "windows" ]; then
@@ -13,18 +11,27 @@ if [ "$1" == "windows" ]; then
   echo setting dir to $dir
 fi
 
-export CVSROOT=$PWD/target/cvsroot
+export CVSROOT=:ext:localhost:$dir/target/cvsroot
 
-cvs import cvs-project -m "import." project ASF INIT
+cvs init
+ret=$?; if [ $ret != 0 ]; then exit $ret; fi
+
+cd cvs-project
+cvs import -I '.svn' -m "import." project ASF INIT
+ret=$?; if [ $ret != 0 ]; then exit $ret; fi
+cd ..
 
 cvs co -d target/project.checkout project
+ret=$?; if [ $ret != 0 ]; then exit $ret; fi
 
+(
 cd target/project.checkout
 
 cat pom.xml | sed "s#\${project.file.parentFile.parentFile}#$dir#g" >tmp
 mv tmp pom.xml
 
 cvs ci -m 'update scm' pom.xml
+ret=$?; if [ $ret != 0 ]; then exit $ret; fi
 
 rm -Rf target
 
@@ -36,4 +43,4 @@ ret=$?; if [ $ret != 0 ]; then exit $ret; fi
 
 mvn -DreleasePom=release-pom.xml -e release:perform -Denv=test
 ret=$?; if [ $ret != 0 ]; then exit $ret; fi
-
+)
