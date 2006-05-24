526aa0e6d57c6651843d682e0cecb4b1163d33c2
Carlos Sanchez Gonzalez <carlos@apache.org>
2006-05-24 02:47:19 +0000

    Change apache hosts to people.apache.org
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@409039 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/m2-sync/apache-m2-sync.sh
+++ maven-meeper/src/bin/m2-sync/apache-m2-sync.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-FROM=jvanzyl@cvs.apache.org:/www/www.apache.org/dist/maven-repository/org/apache/
+FROM=jvanzyl@people.apache.org:/www/www.apache.org/dist/maven-repository/org/apache/
 TO=org/apache/
 
 source ./m2-sync.sh
