058661a78f3fcee8a82c53abb1ec9e87df147da7
Carlos Sanchez Gonzalez <carlos@apache.org>
2006-05-31 22:33:44 +0000

    Remove --ignore-existing, add -c and -i
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@410693 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/m2-sync/m2-sync.sh
+++ maven-meeper/src/bin/m2-sync/m2-sync.sh
@@ -4,6 +4,15 @@ echo This script is very temporary. Please validate all input files in the sourc
 echo Ensure all artifacts have a valid POM, and are signed used PGP.
 echo This will be removed when the repository manager is in place.
 
+echo
+
+echo
+echo If you see a "c" flag in the output for something different than maven-metadata files
+echo then we have a problem, because it means the checksum of the file changed
+echo
+echo For a better explanation of the output flags please check --itemize-changes at rsync man page
+echo
+
 OPTS=-n
 if [ "$1" == "go" ]; then
   echo Doing sync for real
@@ -13,6 +22,9 @@ else
 fi
 
 cd $HOME/repository-staging/to-ibiblio/maven2
-CMD="rsync --exclude-from=/home/projects/maven/repository-tools/syncopate/exclusions.txt -e ssh $OPTS --ignore-existing -avz $FROM $TO"
+
+# ideally we would use --ignore-existing but we need to copy the metadata files
+
+CMD="rsync --exclude-from=/home/projects/maven/repository-tools/syncopate/exclusions.txt -e ssh $OPTS -acivz $FROM $TO"
 echo "Syncing $FROM -> $TO"
 $CMD
