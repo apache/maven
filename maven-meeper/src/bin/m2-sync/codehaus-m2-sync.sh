73c7fc23099658b3daa465d9917c22a2d6770382
Brett Leslie Porter <brett@apache.org>
2006-07-03 05:47:23 +0000

    add a note
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@418689 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/m2-sync/codehaus-m2-sync.sh
+++ maven-meeper/src/bin/m2-sync/codehaus-m2-sync.sh
@@ -5,4 +5,7 @@ TO=org/codehaus/
 SSH_OPTS="-i $HOME/.ssh/new-id_dsa"
 RSYNC_OPTS="-k"
 
+# NOTE: If the rsync options change, the codehaus configuration may need to be changed.
+# It currently runs "rsync --server --sender -vnlkogDtprcz . /var/www/domains/codehaus.org/repository/htdocs/org/codehaus/" regardless of the rsync command sent
+
 source ./m2-sync.sh
