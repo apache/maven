310dc674d784dc2905c02aaaaf7709b64230fdb5
Brett Leslie Porter <brett@apache.org>
2006-07-05 04:16:13 +0000

    [MPLUGIN-7] merge r418201, 417945 from trunk
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/branches/maven-2.0.x@419161 13f79535-47bb-0310-9956-ffa450edef68

--- maven-plugin-tools/maven-plugin-tools-api/src/main/java/org/apache/maven/tools/plugin/generator/PluginXdocGenerator.java
+++ maven-plugin-tools/maven-plugin-tools-api/src/main/java/org/apache/maven/tools/plugin/generator/PluginXdocGenerator.java
@@ -28,6 +28,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ArrayList;
 
 /**
  * @todo add example usage tag that can be shown in the doco
@@ -113,11 +114,7 @@ public class PluginXdocGenerator
 
         w.endElement(); // p
 
-        w.startElement( "p" );
-
-        w.writeText( "Parameters for the goal: " );
-
-        w.endElement(); // p
+        writeGoalAttributes( mojoDescriptor, w );
 
         writeGoalParameterTable( mojoDescriptor, w );
 
@@ -128,188 +125,257 @@ public class PluginXdocGenerator
         w.endElement(); // document
     }
 
+    private void writeGoalAttributes( MojoDescriptor mojoDescriptor, XMLWriter w )
+    {
+        w.startElement( "p" );
+        w.writeMarkup( "<b>Mojo Attributes</b>:" );
+        w.startElement( "ul" );
+
+        String value = mojoDescriptor.getDeprecated();
+        if ( StringUtils.isNotEmpty( value ) )
+        {
+            w.writeMarkup( "<li>This plugin goal has been deprecated: " + value + "</li>" );
+        }
+
+        if ( mojoDescriptor.isProjectRequired() )
+        {
+            w.writeMarkup( "<li>Requires a Maven 2.0 project to execute.</li>" );
+        }
+
+        if ( mojoDescriptor.isAggregator() )
+        {
+            w.writeMarkup( "<li>Executes as an aggregator plugin.</li>" );
+        }
+
+        if ( mojoDescriptor.isDirectInvocationOnly() )
+        {
+            w.writeMarkup( "<li>Executes by direct invocation only.</li>" );
+        }
+
+        value = mojoDescriptor.isDependencyResolutionRequired();
+        if ( StringUtils.isNotEmpty( value ) )
+        {
+            w.writeMarkup( "<li>Requires dependency resolution of artifacts in scope: <code>" + value +
+                "</code></li>" );
+        }
+
+        value = mojoDescriptor.getPhase();
+        if ( StringUtils.isNotEmpty( value ) )
+        {
+            w.writeMarkup( "<li>Automatically executes within the lifecycle phase: <code>" + value + "</code></li>" );
+        }
+
+        value = mojoDescriptor.getExecutePhase();
+        if ( StringUtils.isNotEmpty( value ) )
+        {
+            w.writeMarkup( "<li>Invokes the execution of the lifecycle phase <code>" + value +
+                "</code> prior to executing itself.</li>" );
+        }
+
+        value = mojoDescriptor.getExecuteGoal();
+        if ( StringUtils.isNotEmpty( value ) )
+        {
+            w.writeMarkup( "<li>Invokes the execution of this plugin's goal <code>" + value +
+                "</code> prior to executing itself.</li>" );
+        }
+
+        value = mojoDescriptor.getExecuteLifecycle();
+        if ( StringUtils.isNotEmpty( value ) )
+        {
+            w.writeMarkup( "<li>Executes in its own lifecycle: <code>" + value + "</code></li>" );
+        }
+
+        if ( mojoDescriptor.isOnlineRequired() )
+        {
+            w.writeMarkup( "<li>Requires that mvn runs in online mode.</li>" );
+        }
+
+        if ( !mojoDescriptor.isInheritedByDefault() )
+        {
+            w.writeMarkup( "<li>Is NOT inherited by default in multi-project builds.</li>" );
+        }
+
+        w.endElement();//ul
+        w.endElement();//p
+    }
+
     private void writeGoalParameterTable( MojoDescriptor mojoDescriptor, XMLWriter w )
     {
-        w.startElement( "table" );
+        List parameterList = mojoDescriptor.getParameters();
 
-        w.startElement( "tr" );
+        //remove components and read-only parameters
+        List list = filterParameters( parameterList );
 
-        w.startElement( "th" );
-
-        w.writeText( "Parameter" );
-
-        w.endElement(); // th
-
-        w.startElement( "th" );
-
-        w.writeText( "Type" );
-
-        w.endElement(); // th
-
-        w.startElement( "th" );
-
-        w.writeText( "Expression" );
-
-        w.endElement(); // th
-
-        w.startElement( "th" );
-
-        w.writeText( "Default Value" );
-
-        w.endElement(); // th
-
-        w.startElement( "th" );
-
-        w.writeText( "Description" );
-
-        w.endElement(); // th
-
-        w.endElement(); // tr
-
-        List parameters = mojoDescriptor.getParameters();
-
-        if ( parameters != null )
+        if ( list != null )
         {
-            for ( int i = 0; i < parameters.size(); i++ )
+            if ( list.size() > 0 )
             {
-                Parameter parameter = (Parameter) parameters.get( i );
+                writeParameterSummary( list, w );
 
-                w.startElement( "tr" );
+                writeParameterDetails( list, w );
+            }
+            else
+            {
+                w.startElement( "" );
+            }
+        }
+    }
 
-                // ----------------------------------------------------------------------
-                //
-                // ----------------------------------------------------------------------
+    private List filterParameters( List parameterList )
+    {
+        List filtered = new ArrayList();
 
-                w.startElement( "td" );
+        for ( Iterator parameters = parameterList.iterator(); parameters.hasNext(); )
+        {
+            Parameter parameter = (Parameter) parameters.next();
 
-                String paramName = parameter.getAlias();
+            if ( parameter.isEditable() )
+            {
+                String expression = parameter.getExpression();
 
-                if ( StringUtils.isEmpty( paramName ) )
+                if ( expression != null && !expression.startsWith( "${component." ) )
                 {
-                    paramName = parameter.getName();
+                    filtered.add( parameter );
                 }
-
-                w.startElement( "code" );
-
-                w.writeText( paramName );
-
-                w.endElement(); // code
-
-                if ( !parameter.isRequired() )
-                {
-                    w.writeMarkup( " <i>(Optional)</i>" );
-                }
-
-                if ( parameter.getExpression() != null && parameter.getExpression().startsWith( "${component." ) )
-                {
-                    w.writeMarkup( " <i>(Discovered)</i>" );
-                }
-                else if ( parameter.getRequirement() != null )
-                {
-                    w.writeMarkup( " <i>(Discovered)</i>" );
-                }
-
-                w.endElement(); // td
-
-                // ----------------------------------------------------------------------
-                //
-                // ----------------------------------------------------------------------
-
-                w.startElement( "td" );
-
-                w.startElement( "code" );
-
-                w.addAttribute( "title", parameter.getType() );
-
-                int index = parameter.getType().lastIndexOf( "." );
-                if ( index >= 0 )
-                {
-                    w.writeText( parameter.getType().substring( index + 1 ) );
-                }
-                else
-                {
-                    w.writeText( parameter.getType() );
-                }
-
-                w.endElement(); // code
-
-                w.endElement(); // td
-
-                // ----------------------------------------------------------------------
-                //
-                // ----------------------------------------------------------------------
-
-                w.startElement( "td" );
-
-                w.startElement( "code" );
-
-                if ( StringUtils.isNotEmpty( parameter.getExpression() ) &&
-                    !parameter.getExpression().startsWith( "${component." ) )
-                {
-                    w.writeText( parameter.getExpression() );
-                }
-                else
-                {
-                    w.writeText( "-" );
-                }
-
-                w.endElement(); // code
-
-                w.endElement(); // td
-
-                // ----------------------------------------------------------------------
-                //
-                // ----------------------------------------------------------------------
-
-                w.startElement( "td" );
-
-                w.startElement( "code" );
-
-                if ( StringUtils.isNotEmpty( parameter.getDefaultValue() ) )
-                {
-                    w.writeText( parameter.getDefaultValue() );
-                }
-                else
-                {
-                    w.writeText( "-" );
-                }
-
-                w.endElement(); // code
-
-                w.endElement(); // td
-
-                // ----------------------------------------------------------------------
-                //
-                // ----------------------------------------------------------------------
-
-                w.startElement( "td" );
-
-                if ( StringUtils.isNotEmpty( parameter.getDescription() ) )
-                {
-                    w.writeMarkup( parameter.getDescription() );
-                }
-                else
-                {
-                    w.writeText( "No description." );
-                }
-
-                String deprecationWarning = parameter.getDeprecated();
-                if ( deprecationWarning != null )
-                {
-                    w.writeMarkup( "<br/><b>Deprecated:</b> " );
-                    w.writeMarkup( deprecationWarning );
-                    if ( deprecationWarning.length() == 0 )
-                    {
-                        w.writeText( "No reason given." );
-                    }
-                }
-
-                w.endElement(); // td
-
-                w.endElement(); // tr
             }
         }
 
-        w.endElement(); // table
+        return filtered;
+    }
+
+    private void writeParameterDetails( List parameterList, XMLWriter w )
+    {
+        w.startElement( "subsection" );
+        w.addAttribute( "name", "Parameter Details" );
+
+        for( Iterator parameters = parameterList.iterator(); parameters.hasNext(); )
+        {
+            Parameter parameter = (Parameter) parameters.next();
+
+            w.writeMarkup( "<p><b><a name=\"" + parameter.getName() + "\">" + parameter.getName() + "</a></b></p>" );
+
+            String description = parameter.getDescription();
+            if ( StringUtils.isEmpty( description ) )
+            {
+                description = "No Description.";
+            }
+            w.writeMarkup( "<p>" + description + "</p>" );
+
+            w.startElement( "ul" );
+
+            writeDetail( "Type", parameter.getType(), w );
+
+            // TODO: requires that this field is in the plugin descriptor
+            // writeDetail( "Since", parameter.getSince(), w );
+
+            if ( parameter.isRequired() )
+            {
+                writeDetail( "Required", "Yes", w );
+            }
+            else
+            {
+                writeDetail( "Required", "No", w );
+            }
+
+            writeDetail( "Expression", parameter.getExpression(), w );
+
+            writeDetail( "Default", parameter.getDefaultValue(), w );
+
+            w.endElement();//ul
+
+            w.writeMarkup( "<hr/>" );
+        }
+
+        w.endElement();
+    }
+
+    private void writeDetail( String param, String value, XMLWriter w )
+    {
+        if ( StringUtils.isNotEmpty( value ) )
+        {
+            w.writeMarkup( "<li><b>" + param + "</b>: <code>" + value + "</code></li>" );
+        }
+    }
+
+    private void writeParameterSummary( List parameterList, XMLWriter w )
+    {
+        List requiredParams = getParametersByRequired( true, parameterList );
+        if ( requiredParams.size() > 0 )
+        {
+            writeParameterList( "Required Parameters", requiredParams, w );
+        }
+
+        List optionalParams = getParametersByRequired( false, parameterList );
+        if ( optionalParams.size() > 0 )
+        {
+            writeParameterList( "Optional Parameters", optionalParams, w );
+        }
+    }
+
+    private void writeParameterList( String title, List parameterList, XMLWriter w )
+    {
+        w.startElement( "subsection" );
+        w.addAttribute( "name", title );
+
+        w.startElement( "table" );
+
+        w.startElement( "tr" );
+        w.startElement( "th" );
+        w.writeText( "Name" );
+        w.endElement();//th
+        w.startElement( "th" );
+        w.writeText( "Type" );
+        w.endElement();//th
+        w.startElement( "th" );
+        w.writeText( "Description" );
+        w.endElement();//th
+        w.endElement();//tr
+
+        for( Iterator parameters = parameterList.iterator(); parameters.hasNext(); )
+        {
+            Parameter parameter = (Parameter) parameters.next();
+
+            w.startElement( "tr" );
+            w.startElement( "td" );
+            w.writeMarkup( "<b><a href=\"#" + parameter.getName() + "\">" + parameter.getName() + "</a></b>");
+            w.endElement();//td
+            w.startElement( "td" );
+            int index = parameter.getType().lastIndexOf( "." );
+            w.writeMarkup( "<code>" + parameter.getType().substring( index + 1 ) + "</code>" );
+            w.endElement();//td
+            w.startElement( "td" );
+            String description = parameter.getDescription();
+            if ( StringUtils.isEmpty( description ) )
+            {
+                description = "No description.";
+            }
+            if ( StringUtils.isNotEmpty( parameter.getDefaultValue() ) )
+            {
+                description = description + " Default value is <code>" + parameter.getDefaultValue() + "</code>.";
+            }
+            w.writeMarkup( description );
+            w.endElement();//td
+            w.endElement(); //tr
+        }
+
+        w.endElement();//table
+        w.endElement();//section
+    }
+
+    private List getParametersByRequired( boolean required, List parameterList )
+    {
+        List list = new ArrayList();
+
+        for ( Iterator parameters = parameterList.iterator(); parameters.hasNext(); )
+        {
+            Parameter parameter = (Parameter) parameters.next();
+
+            if ( parameter.isRequired() == required )
+            {
+                list.add( parameter );
+            }
+        }
+
+        return list;
     }
 }
