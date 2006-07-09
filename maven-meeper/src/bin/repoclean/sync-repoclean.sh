9d9bfefdfa80bd5be1e6e6c8d0a448f901e47ab0
Carlos Sanchez Gonzalez <carlos@apache.org>
2006-07-09 14:35:14 +0000

    Fix repoclean generation of maven-metadata.xml checksums
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@420299 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/repoclean/sync-repoclean.sh
+++ maven-meeper/src/bin/repoclean/sync-repoclean.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-dest=/home/maven/repository-staging/to-ibiblio
-repocleanhome=$HOME/repository-tools/repoclean
+export dest=/home/maven/repository-staging/to-ibiblio
+export repocleanhome=$HOME/repository-tools/repoclean
+log=$repocleanhome/last-changes.log
 
 $repocleanhome/repoclean.sh ~/components/maven-meeper/src/bin/repoclean/synchronize.properties
 
-rsync --ignore-existing -ripl $dest/maven2-repoclean/ $dest/maven2/
+rsync --ignore-existing -rvpl $dest/maven2-repoclean/ $dest/maven2/ > $log
+
+for f in `cat $log | grep maven-metadata.xml` ; do
+  md5sum $dest/maven2/$f > $dest/maven2/$f.md5;
+  sha1sum $dest/maven2/$f > $dest/maven2/$f.sha1;
+  md5sum $dest/maven2-repoclean/$f > $dest/maven2-repoclean/$f.md5;
+  sha1sum $dest/maven2-repoclean/$f > $dest/maven2-repoclean/$f.sha1;
+done
