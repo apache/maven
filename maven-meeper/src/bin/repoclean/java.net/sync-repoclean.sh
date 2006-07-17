ac2f8d86808397dd6e662f0866e7cf4bda8ded39
Carlos Sanchez Gonzalez <carlos@apache.org>
2006-07-17 11:22:20 +0000

    Fix metadata checksums
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@422688 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/repoclean/java.net/sync-repoclean.sh
+++ maven-meeper/src/bin/repoclean/java.net/sync-repoclean.sh
@@ -2,6 +2,7 @@
 
 dest=/home/maven/repository-staging/to-ibiblio
 repocleanhome=$HOME/repository-tools/repoclean
+log=$repocleanhome/last-changes-java.net.log
 
 cd $dest/maven-java.net
 
@@ -9,4 +10,11 @@ cvs update -P
 
 $repocleanhome/repoclean.sh ~/components/maven-meeper/src/bin/repoclean/java.net/synchronize.properties
 
-rsync --ignore-existing -ripl $dest/maven2-repoclean-java.net/com/sun/ $dest/maven2/com/sun/
+rsync --ignore-existing -ripl $dest/maven2-repoclean-java.net/com/sun/ $dest/maven2/com/sun/ > $log
+
+for f in `cat $log | grep maven-metadata.xml` ; do
+  md5sum $dest/maven2/$f > $dest/maven2/$f.md5;
+  sha1sum $dest/maven2/$f > $dest/maven2/$f.sha1;
+  md5sum $dest/maven2-repoclean-java.net/$f > $dest/maven2-repoclean-java.net/$f.md5;
+  sha1sum $dest/maven2-repoclean-java.net/$f > $dest/maven2-repoclean-java.net/$f.sha1;
+done
