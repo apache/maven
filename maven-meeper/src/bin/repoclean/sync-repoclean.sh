c9b9bf6f52653dcf24eabe933262c7b5cf843e06
Carlos Sanchez Gonzalez <carlos@apache.org>
2006-07-18 09:27:12 +0000

    Don't checksum checksums
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@423023 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/repoclean/sync-repoclean.sh
+++ maven-meeper/src/bin/repoclean/sync-repoclean.sh
@@ -8,7 +8,7 @@ $repocleanhome/repoclean.sh ~/components/maven-meeper/src/bin/repoclean/synchron
 
 rsync --ignore-existing -rvpl $dest/maven2-repoclean/ $dest/maven2/ > $log
 
-for f in `cat $log | grep maven-metadata.xml` ; do
+for f in `cat $log | grep maven-metadata.xml$` ; do
   md5sum $dest/maven2/$f > $dest/maven2/$f.md5;
   sha1sum $dest/maven2/$f > $dest/maven2/$f.sha1;
   md5sum $dest/maven2-repoclean/$f > $dest/maven2-repoclean/$f.md5;
