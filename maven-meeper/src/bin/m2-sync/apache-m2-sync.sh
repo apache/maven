2cebbb8eccce6ea1a15b356832de7e917573bb33
Carlos Sanchez Gonzalez <carlos@apache.org>
2006-07-18 09:49:55 +0000

    Use new location of apache repos
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@423031 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/m2-sync/apache-m2-sync.sh
+++ maven-meeper/src/bin/m2-sync/apache-m2-sync.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-FROM=jvanzyl@people.apache.org:/www/www.apache.org/dist/maven-repository/org/apache/
+FROM=jvanzyl@people.apache.org:/www/people.apache.org/repo/m2-ibiblio-rsync-repository/org/apache/
 TO=org/apache/
 
 source ./m2-sync.sh
