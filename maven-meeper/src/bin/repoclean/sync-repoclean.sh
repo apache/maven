5a31c30acbd3da6e87dc9c5a5bdf356d3dcf9187
Carlos Sanchez Gonzalez <carlos@apache.org>
2006-07-18 09:32:43 +0000

    general improvements
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@423026 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/repoclean/sync-repoclean.sh
+++ maven-meeper/src/bin/repoclean/sync-repoclean.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-export dest=/home/maven/repository-staging/to-ibiblio
-export repocleanhome=$HOME/repository-tools/repoclean
+dir=/home/maven/repository-staging/to-ibiblio
+repocleanhome=$HOME/repository-tools/repoclean
 log=$repocleanhome/last-changes.log
+
+src=maven2-repoclean
+dst=maven2
 
 $repocleanhome/repoclean.sh ~/components/maven-meeper/src/bin/repoclean/synchronize.properties
 
-rsync --ignore-existing -rvpl $dest/maven2-repoclean/ $dest/maven2/ > $log
+rsync --ignore-existing -rvpl $dir/$src/ $dir/dst/ > $log
 
 for f in `cat $log | grep maven-metadata.xml$` ; do
-  md5sum $dest/maven2/$f > $dest/maven2/$f.md5;
-  sha1sum $dest/maven2/$f > $dest/maven2/$f.sha1;
-  md5sum $dest/maven2-repoclean/$f > $dest/maven2-repoclean/$f.md5;
-  sha1sum $dest/maven2-repoclean/$f > $dest/maven2-repoclean/$f.sha1;
+  md5sum $dir/$dst/$f > $dir/$dst/$f.md5;
+  sha1sum $dir/$dst/$f > $dir/$dst/$f.sha1;
+  md5sum $dir/$src/$f > $dir/$src/$f.md5;
+  sha1sum $dir/$src/$f > $dir/$src/$f.sha1;
 done
