90eff70f0aa04abd3b5b2885e3ab90fae8ad060d
Brett Leslie Porter <brett@apache.org>
2006-07-19 23:49:45 +0000

    must still sync to the staging area
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@423664 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/deploy-ibiblio-htaccess.sh
+++ maven-meeper/src/bin/deploy-ibiblio-htaccess.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-#if [ "`hostname`" == "beaver.codehaus.org" ]; then
-  #cp ibiblio-htaccess $HOME/repository-staging/to-ibiblio/maven/.htaccess
-#else
-  #scp ibiblio-htaccess maven@beaver.codehaus.org:$HOME/repository-staging/to-ibiblio/maven/.htaccess
-#fi
+if [ "`hostname`" == "maven01.managed.contegix.com" ]; then
+  cp ibiblio-htaccess $HOME/repository-staging/to-ibiblio/maven/.htaccess
+else
+  scp ibiblio-htaccess maven@maven.org:~maven/repository-staging/to-ibiblio/maven/.htaccess
+fi
 
 scp ibiblio-htaccess maven@login.ibiblio.org:/public/html/maven/.htaccess
 
