21f590404f4a5358b10d40a2e0fa7e5b01f9ba6b
Brett Leslie Porter <brett@apache.org>
2006-07-19 12:54:13 +0000

    add another dom4j rewrite
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@423452 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/deploy-ibiblio-htaccess.sh
+++ maven-meeper/src/bin/deploy-ibiblio-htaccess.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ "`hostname`" == "beaver.codehaus.org" ]; then
-  cp ibiblio-htaccess $HOME/repository-staging/to-ibiblio/maven/.htaccess
-else
-  scp ibiblio-htaccess maven@beaver.codehaus.org:$HOME/repository-staging/to-ibiblio/maven/.htaccess
-fi
+#if [ "`hostname`" == "beaver.codehaus.org" ]; then
+  #cp ibiblio-htaccess $HOME/repository-staging/to-ibiblio/maven/.htaccess
+#else
+  #scp ibiblio-htaccess maven@beaver.codehaus.org:$HOME/repository-staging/to-ibiblio/maven/.htaccess
+#fi
 
 scp ibiblio-htaccess maven@login.ibiblio.org:/public/html/maven/.htaccess
 
