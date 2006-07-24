234a1174e8c4b99d78d00792366ef519a4b066eb
Carlos Sanchez Gonzalez <carlos@apache.org>
2006-07-24 17:33:57 +0000

    Merged rev# 425096 from trunk. Added PluginException to aggregate the different plugin exceptions
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/branches/maven-2.0.x@425129 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/main/java/org/apache/maven/plugin/PluginConfigurationException.java
+++ maven-core/src/main/java/org/apache/maven/plugin/PluginConfigurationException.java
@@ -1,7 +1,7 @@
 package org.apache.maven.plugin;
 
 /*
- * Copyright 2001-2005 The Apache Software Foundation.
+ * Copyright 2001-2006 The Apache Software Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,7 +39,7 @@ import java.util.regex.Pattern;
  * @version $Id$
  */
 public class PluginConfigurationException
-    extends Exception
+    extends PluginException
 {
     private final PluginDescriptor pluginDescriptor;
 
