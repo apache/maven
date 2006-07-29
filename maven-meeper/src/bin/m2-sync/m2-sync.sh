ff82c96fa1a4b748963716d089f0fb846fefa7dc
Carlos Sanchez Gonzalez <carlos@apache.org>
2006-07-29 15:15:23 +0000

    Doc changes
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@426816 13f79535-47bb-0310-9956-ffa450edef68

--- maven-meeper/src/bin/m2-sync/m2-sync.sh
+++ maven-meeper/src/bin/m2-sync/m2-sync.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
 echo This script is very temporary. Please validate all input files in the source repository before blindly copying them in.
-echo Ensure all artifacts have a valid POM, and are signed used PGP.
+echo Ensure all artifacts have a valid POM.
 echo This will be removed when the repository manager is in place.
 
 echo
 
-echo
-echo If you see a "c" flag in the output for something different than maven-metadata files
-echo then we have a problem, because it means the checksum of the file changed
 echo
 echo For a better explanation of the output flags please check --itemize-changes at rsync man page
 echo
