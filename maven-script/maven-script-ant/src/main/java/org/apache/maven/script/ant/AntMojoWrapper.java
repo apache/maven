a315b8e83e78cfaea521b9b5b58a4dde51d721ce
Kenney Westerhof <kenney@apache.org>
2006-10-12 15:42:40 +0000

    Removed catch of IOException - not thrown anymode by UnArchiver
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@463285 13f79535-47bb-0310-9956-ffa450edef68

--- maven-script/maven-script-ant/src/main/java/org/apache/maven/script/ant/AntMojoWrapper.java
+++ maven-script/maven-script-ant/src/main/java/org/apache/maven/script/ant/AntMojoWrapper.java
@@ -1,31 +1,30 @@
 package org.apache.maven.script.ant;
 
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.ContextEnabled;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.descriptor.PluginDescriptor;
 import org.apache.maven.project.MavenProject;
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.DependencyResolutionRequiredException;
-import org.apache.tools.ant.types.Path;
 import org.apache.tools.ant.Project;
+import org.apache.tools.ant.types.Path;
+import org.codehaus.plexus.archiver.ArchiverException;
+import org.codehaus.plexus.archiver.UnArchiver;
+import org.codehaus.plexus.archiver.zip.ZipUnArchiver;
 import org.codehaus.plexus.component.MapOrientedComponent;
 import org.codehaus.plexus.component.configurator.ComponentConfigurationException;
 import org.codehaus.plexus.component.factory.ant.AntComponentExecutionException;
 import org.codehaus.plexus.component.factory.ant.AntScriptInvoker;
 import org.codehaus.plexus.component.repository.ComponentRequirement;
-import org.codehaus.plexus.archiver.UnArchiver;
-import org.codehaus.plexus.archiver.ArchiverException;
-import org.codehaus.plexus.archiver.zip.ZipUnArchiver;
 import org.codehaus.plexus.util.StringUtils;
 
-import java.util.Map;
-import java.util.Collection;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.io.File;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -102,7 +101,7 @@ public class AntMojoWrapper
     private void unpackFileBasedResources()
         throws MojoExecutionException
     {
-        // What we need to write out any resources in the plugin to the target directory of the 
+        // What we need to write out any resources in the plugin to the target directory of the
         // mavenProject using the Ant-based plugin:
         //
         // 1. Need a reference to the plugin JAR itself
@@ -121,10 +120,6 @@ public class AntMojoWrapper
             ua.extract( resourcesPath, outputDirectory );
         }
         catch ( ArchiverException e )
-        {
-            throw new MojoExecutionException( "Error extracting resources from your Ant-based plugin.", e );
-        }
-        catch( IOException e )
         {
             throw new MojoExecutionException( "Error extracting resources from your Ant-based plugin.", e );
         }
