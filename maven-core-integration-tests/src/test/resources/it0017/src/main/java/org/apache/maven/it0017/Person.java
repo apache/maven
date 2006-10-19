1166efe5156f6f1ab03f2cd9521dc8a31ccd2e84
Jason van Zyl <jvanzyl@apache.org>
2006-10-19 20:42:04 +0000

    o adding IT it0017
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@465791 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-core-integration-tests/src/test/resources/it0017/src/main/java/org/apache/maven/it0017/Person.java
@@ -0,0 +1,16 @@
+package org.apache.maven.it0017;
+
+public class Person
+{
+    private String name;
+    
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+}
