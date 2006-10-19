5767f15e88330d7ec48a5e09024a7c3c83ccdac9
Jason van Zyl <jvanzyl@apache.org>
2006-10-19 20:47:31 +0000

    o adding IT it0067
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@465841 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-core-integration-tests/src/test/resources/it0067/src/main/java/org/apache/maven/it0021/Person.java
@@ -0,0 +1,18 @@
+package org.apache.maven.it0021;
+
+import junit.framework.TestCase;
+
+public class Person
+{
+    private String name;
+    
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+}
