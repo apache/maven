833a237654c90618d0472a8eacb3eaa2b627b8e7
Jason van Zyl <jvanzyl@apache.org>
2006-10-19 20:48:08 +0000

    o adding IT it0075
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@465849 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-core-integration-tests/src/test/resources/it0075/sub1/src/main/java/org/apache/maven/it0075/Person.java
@@ -0,0 +1,6 @@
+package org.apache.maven.it0075;
+
+public class Person
+{
+    private String name;
+}
---
+++ maven-core-integration-tests/src/test/resources/it0075/sub2/src/main/java/org/apache/maven/it0075/Person2.java
@@ -0,0 +1,6 @@
+package org.apache.maven.it0075;
+
+public class Person2
+{
+    private String name;
+}
