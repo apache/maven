fbd916ee5ec0dc02702e2a84ad9f938541ed9bea
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:28:58 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466459 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0010Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0010Test.java
@@ -1,33 +1,39 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
 
-public class MavenIT0010Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0010Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Since the artifact resolution does not use the project builder, we must
-        ensure that the full hierarchy of all dependencies is resolved. This
-        includes the dependencies of the parent-pom's of dependencies. This test
-        will check this, by depending on classworlds, which is a dependency of
-        maven-component, which is the parent of maven-plugin, which is an
-        explicit dependency of this test.
-        # TODO: must correct the assumptions of this test
-         */
-public void testit0010() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0010 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0010", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.executeGoal("compile");
-verifier.assertFilePresent("target/classes/org/apache/maven/it0010/PersonFinder.class");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Since the artifact resolution does not use the project builder, we must
+     * ensure that the full hierarchy of all dependencies is resolved. This
+     * includes the dependencies of the parent-pom's of dependencies. This test
+     * will check this, by depending on classworlds, which is a dependency of
+     * maven-component, which is the parent of maven-plugin, which is an
+     * explicit dependency of this test.
+     * # TODO: must correct the assumptions of this test
+     */
+    public void testit0010()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0010 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0010", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.executeGoal( "compile" );
+        verifier.assertFilePresent( "target/classes/org/apache/maven/it0010/PersonFinder.class" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
