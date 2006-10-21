98efd57e91fba5d0b219d936d36cf4eb7a656106
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:31:25 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466508 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0060Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0060Test.java
@@ -1,31 +1,38 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
 
-public class MavenIT0060Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0060Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test aggregation of list configuration items when using
-        'combine.children=append' attribute. Specifically, merge the list of
-        excludes for the testCompile mojo. */
-public void testit0060() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0060 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0060", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.executeGoal("test");
-verifier.assertFilePresent("subproject/target/classes/org/apache/maven/it0001/Person.class");
-verifier.assertFilePresent("subproject/target/test-classes/org/apache/maven/it0001/PersonTest.class");
-verifier.assertFileNotPresent("subproject/target/test-classes/org/apache/maven/it0001/PersonTwoTest.class");
-verifier.assertFileNotPresent("subproject/target/test-classes/org/apache/maven/it0001/PersonThreeTest.class");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test aggregation of list configuration items when using
+     * 'combine.children=append' attribute. Specifically, merge the list of
+     * excludes for the testCompile mojo.
+     */
+    public void testit0060()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0060 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0060", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.executeGoal( "test" );
+        verifier.assertFilePresent( "subproject/target/classes/org/apache/maven/it0001/Person.class" );
+        verifier.assertFilePresent( "subproject/target/test-classes/org/apache/maven/it0001/PersonTest.class" );
+        verifier.assertFileNotPresent( "subproject/target/test-classes/org/apache/maven/it0001/PersonTwoTest.class" );
+        verifier.assertFileNotPresent( "subproject/target/test-classes/org/apache/maven/it0001/PersonThreeTest.class" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
