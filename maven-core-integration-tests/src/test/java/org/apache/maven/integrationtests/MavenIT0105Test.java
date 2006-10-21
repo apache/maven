f6acaf6c48d5f4a44a1f06c203d5ef5c72929a1b
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:33:41 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466553 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0105Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0105Test.java
@@ -1,29 +1,38 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MavenIT0105Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0105Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** MRESOURCES-18 */
-public void testit0105() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0105 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0105", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-List cliOptions = new ArrayList();
-cliOptions.add("-Dparam=PARAM");
-verifier.setCliOptions(cliOptions);
-verifier.executeGoal("test");
-verifier.assertFilePresent("target/classes/test.properties");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * MRESOURCES-18
+     */
+    public void testit0105()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0105 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0105", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        List cliOptions = new ArrayList();
+        cliOptions.add( "-Dparam=PARAM" );
+        verifier.setCliOptions( cliOptions );
+        verifier.executeGoal( "test" );
+        verifier.assertFilePresent( "target/classes/test.properties" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
