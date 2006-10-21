0b69d3811cfaac25ed6e7c2f6c77c4f938fb3f05
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:32:56 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466538 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0089Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0089Test.java
@@ -1,29 +1,35 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
 
-public class MavenIT0089Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0089Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test that Checkstyle PackageNamesLoader.loadModuleFactory(..) method will complete as-is with
-        the context classloader available to the plugin.
-        */
-public void testit0089() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0089 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0089", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.deleteArtifact("org.apache.maven.plugins", "maven-core-it-plugin", "1.0", "maven-plugin");
-verifier.executeGoal("install");
-verifier.assertFilePresent("project/target/output.txt");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test that Checkstyle PackageNamesLoader.loadModuleFactory(..) method will complete as-is with
+     * the context classloader available to the plugin.
+     */
+    public void testit0089()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0089 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0089", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.deleteArtifact( "org.apache.maven.plugins", "maven-core-it-plugin", "1.0", "maven-plugin" );
+        verifier.executeGoal( "install" );
+        verifier.assertFilePresent( "project/target/output.txt" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
