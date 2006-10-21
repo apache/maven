b8e05ad8dffc426e0da29bc69356efea8ed91ccd
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:30:04 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466481 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0033Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0033Test.java
@@ -1,28 +1,35 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
 
-public class MavenIT0033Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0033Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test an EAR generation */
-public void testit0033() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0033 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0033", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.executeGoal("package");
-verifier.assertFilePresent("target/maven-core-it00xx-1.0.ear");
-verifier.assertFilePresent("target/maven-core-it00xx-1.0.ear!/META-INF/application.xml");
-verifier.assertFilePresent("target/maven-core-it00xx-1.0.ear!/META-INF/appserver-application.xml");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test an EAR generation
+     */
+    public void testit0033()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0033 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0033", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.executeGoal( "package" );
+        verifier.assertFilePresent( "target/maven-core-it00xx-1.0.ear" );
+        verifier.assertFilePresent( "target/maven-core-it00xx-1.0.ear!/META-INF/application.xml" );
+        verifier.assertFilePresent( "target/maven-core-it00xx-1.0.ear!/META-INF/appserver-application.xml" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
