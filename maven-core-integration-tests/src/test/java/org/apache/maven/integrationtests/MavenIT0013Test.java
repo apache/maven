343e6ac545306d4bc6cb61e5920a5ba11e3b2ef5
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:29:07 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466462 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0013Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0013Test.java
@@ -1,31 +1,40 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
-public class MavenIT0013Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0013Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test plugin-plugin, which tests maven-plugin-tools-api and 
-        maven-plugin-tools-java. This will generate a plugin descriptor from 
-        java-based mojo sources, install the plugin, and then use it. */
-public void testit0013() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0013 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0013", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.deleteArtifact("org.apache.maven.plugins", "maven-it0013-plugin", "1.0-SNAPSHOT", "maven-plugin");
-List goals = Arrays.asList(new String[] {"install", "it0013:it0013"});
-verifier.executeGoals(goals);
-verifier.assertFilePresent("target/maven-it0013-plugin-1.0-SNAPSHOT.jar");
-verifier.assertFilePresent("target/it0013-verify");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test plugin-plugin, which tests maven-plugin-tools-api and
+     * maven-plugin-tools-java. This will generate a plugin descriptor from
+     * java-based mojo sources, install the plugin, and then use it.
+     */
+    public void testit0013()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0013 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0013", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.deleteArtifact( "org.apache.maven.plugins", "maven-it0013-plugin", "1.0-SNAPSHOT", "maven-plugin" );
+        List goals = Arrays.asList( new String[]{"install", "it0013:it0013"} );
+        verifier.executeGoals( goals );
+        verifier.assertFilePresent( "target/maven-it0013-plugin-1.0-SNAPSHOT.jar" );
+        verifier.assertFilePresent( "target/it0013-verify" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
