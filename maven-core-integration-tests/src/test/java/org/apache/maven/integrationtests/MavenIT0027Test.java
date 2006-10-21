b94e3cb3bad3e45663d171cd7b8647a38f7ce7c6
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:29:46 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466475 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0027Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0027Test.java
@@ -1,28 +1,37 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
-public class MavenIT0027Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0027Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test @execute with a custom lifecycle, including configuration */
-public void testit0027() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0027 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0027", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-List goals = Arrays.asList(new String[] {"core-it:fork", "core-it:fork-goal"});
-verifier.executeGoals(goals);
-verifier.assertFilePresent("target/forked/touch.txt");
-verifier.assertFilePresent("target/forked2/touch.txt");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test @execute with a custom lifecycle, including configuration
+     */
+    public void testit0027()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0027 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0027", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        List goals = Arrays.asList( new String[]{"core-it:fork", "core-it:fork-goal"} );
+        verifier.executeGoals( goals );
+        verifier.assertFilePresent( "target/forked/touch.txt" );
+        verifier.assertFilePresent( "target/forked2/touch.txt" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
