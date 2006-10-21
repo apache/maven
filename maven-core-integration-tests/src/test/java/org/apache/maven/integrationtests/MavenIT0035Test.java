1a164901d8f981273cac9e42f5a555b7ad021a66
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:30:10 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466483 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0035Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0035Test.java
@@ -1,31 +1,38 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
 
-public class MavenIT0035Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0035Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test artifact relocation. */
-public void testit0035() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0035 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0035", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.deleteArtifact("org.apache.maven", "maven-core-it-support", "1.1", "jar");
-verifier.deleteArtifact("org.apache.maven", "maven-core-it-support", "1.1", "pom");
-verifier.deleteArtifact("org.apache.maven", "maven-core-it-support-old-location", "1.1", "pom");
-verifier.executeGoal("package");
-verifier.assertArtifactPresent("org.apache.maven", "maven-core-it-support", "1.1", "jar");
-verifier.assertArtifactPresent("org.apache.maven", "maven-core-it-support", "1.1", "pom");
-verifier.assertArtifactPresent("org.apache.maven", "maven-core-it-support-old-location", "1.1", "pom");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test artifact relocation.
+     */
+    public void testit0035()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0035 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0035", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.deleteArtifact( "org.apache.maven", "maven-core-it-support", "1.1", "jar" );
+        verifier.deleteArtifact( "org.apache.maven", "maven-core-it-support", "1.1", "pom" );
+        verifier.deleteArtifact( "org.apache.maven", "maven-core-it-support-old-location", "1.1", "pom" );
+        verifier.executeGoal( "package" );
+        verifier.assertArtifactPresent( "org.apache.maven", "maven-core-it-support", "1.1", "jar" );
+        verifier.assertArtifactPresent( "org.apache.maven", "maven-core-it-support", "1.1", "pom" );
+        verifier.assertArtifactPresent( "org.apache.maven", "maven-core-it-support-old-location", "1.1", "pom" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
