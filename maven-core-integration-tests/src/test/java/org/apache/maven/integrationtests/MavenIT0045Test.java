47d1192a2817eaa65f98184c2848f3a10c5267f5
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:30:40 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466493 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0045Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0045Test.java
@@ -1,31 +1,40 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MavenIT0045Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0045Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test non-reactor behavior when plugin declares "@requiresProject false" */
-public void testit0045() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0045 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0045", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.deleteArtifact("org.apache.maven.plugins", "maven-core-it-plugin", "1.0", "maven-plugin");
-List cliOptions = new ArrayList();
-cliOptions.add("--no-plugin-registry");
-verifier.setCliOptions(cliOptions);
-verifier.executeGoal("core-it:light-touch");
-verifier.assertFilePresent("target/touch.txt");
-verifier.assertFileNotPresent("subproject/target/touch.txt");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test non-reactor behavior when plugin declares "@requiresProject false"
+     */
+    public void testit0045()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0045 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0045", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.deleteArtifact( "org.apache.maven.plugins", "maven-core-it-plugin", "1.0", "maven-plugin" );
+        List cliOptions = new ArrayList();
+        cliOptions.add( "--no-plugin-registry" );
+        verifier.setCliOptions( cliOptions );
+        verifier.executeGoal( "core-it:light-touch" );
+        verifier.assertFilePresent( "target/touch.txt" );
+        verifier.assertFileNotPresent( "subproject/target/touch.txt" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
