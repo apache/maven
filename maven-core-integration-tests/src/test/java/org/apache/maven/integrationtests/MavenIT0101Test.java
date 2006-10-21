f0473257c7ee1effffb19f861cfa0f1ab7e18347
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:33:29 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466549 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0101Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0101Test.java
@@ -1,30 +1,39 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MavenIT0101Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0101Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test that properties defined in an active profile in the user's
-        settings are available for interpolation of systemPath in a dependency.
-        [MNG-2052] */
-public void testit0101() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0101 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0101", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-List cliOptions = new ArrayList();
-cliOptions.add("--settings settings.xml");
-verifier.setCliOptions(cliOptions);
-verifier.executeGoal("compile");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test that properties defined in an active profile in the user's
+     * settings are available for interpolation of systemPath in a dependency.
+     * [MNG-2052]
+     */
+    public void testit0101()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0101 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0101", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        List cliOptions = new ArrayList();
+        cliOptions.add( "--settings settings.xml" );
+        verifier.setCliOptions( cliOptions );
+        verifier.executeGoal( "compile" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
