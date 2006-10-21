c2370a285b5a404623245283f3afce365edcc9d2
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:31:01 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466500 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0052Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0052Test.java
@@ -1,31 +1,40 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MavenIT0052Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0052Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test that source attachment doesn't take place when
-        -DperformRelease=true is missing. */
-public void testit0052() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0052 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0052", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-List cliOptions = new ArrayList();
-cliOptions.add("--no-plugin-registry");
-verifier.setCliOptions(cliOptions);
-verifier.executeGoal("package");
-verifier.assertFilePresent("target/maven-core-it0051-1.0.jar");
-verifier.assertFileNotPresent("target/maven-core-it0051-1.0-sources.jar");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test that source attachment doesn't take place when
+     * -DperformRelease=true is missing.
+     */
+    public void testit0052()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0052 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0052", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        List cliOptions = new ArrayList();
+        cliOptions.add( "--no-plugin-registry" );
+        verifier.setCliOptions( cliOptions );
+        verifier.executeGoal( "package" );
+        verifier.assertFilePresent( "target/maven-core-it0051-1.0.jar" );
+        verifier.assertFileNotPresent( "target/maven-core-it0051-1.0-sources.jar" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
