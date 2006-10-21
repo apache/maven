e543e9da3978d1e839e9e7ad4f19133b84f14657
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:30:07 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466482 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0034Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0034Test.java
@@ -1,29 +1,36 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
 
-public class MavenIT0034Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0034Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test version range junit [3.7,) resolves to 3.8.1 */
-public void testit0034() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0034 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0034", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.deleteArtifact("org.apache.maven", "maven-core-it-support", "1.4", "jar");
-verifier.deleteArtifact("junit", "junit", "3.8", "jar");
-verifier.executeGoal("package");
-verifier.assertArtifactPresent("junit", "junit", "3.8", "jar");
-verifier.assertArtifactPresent("org.apache.maven", "maven-core-it-support", "1.4", "jar");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test version range junit [3.7,) resolves to 3.8.1
+     */
+    public void testit0034()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0034 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0034", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.deleteArtifact( "org.apache.maven", "maven-core-it-support", "1.4", "jar" );
+        verifier.deleteArtifact( "junit", "junit", "3.8", "jar" );
+        verifier.executeGoal( "package" );
+        verifier.assertArtifactPresent( "junit", "junit", "3.8", "jar" );
+        verifier.assertArtifactPresent( "org.apache.maven", "maven-core-it-support", "1.4", "jar" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
