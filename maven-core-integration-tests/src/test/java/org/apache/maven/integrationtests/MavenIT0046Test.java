50de5a465a52c845d1801ea859a3d8aa950a050d
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:30:43 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466494 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0046Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0046Test.java
@@ -1,33 +1,42 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MavenIT0046Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0046Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test fail-never reactor behavior. Forces an exception to be thrown in
-        the first module, but checks that the second modules is built. */
-public void testit0046() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0046 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0046", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.deleteArtifact("org.apache.maven.plugins", "maven-core-it-plugin", "1.0", "maven-plugin");
-List cliOptions = new ArrayList();
-cliOptions.add("--no-plugin-registry --fail-never");
-verifier.setCliOptions(cliOptions);
-verifier.executeGoal("core-it:touch");
-verifier.assertFilePresent("target/touch.txt");
-verifier.assertFileNotPresent("subproject/target/touch.txt");
-verifier.assertFilePresent("subproject2/target/touch.txt");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test fail-never reactor behavior. Forces an exception to be thrown in
+     * the first module, but checks that the second modules is built.
+     */
+    public void testit0046()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0046 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0046", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.deleteArtifact( "org.apache.maven.plugins", "maven-core-it-plugin", "1.0", "maven-plugin" );
+        List cliOptions = new ArrayList();
+        cliOptions.add( "--no-plugin-registry --fail-never" );
+        verifier.setCliOptions( cliOptions );
+        verifier.executeGoal( "core-it:touch" );
+        verifier.assertFilePresent( "target/touch.txt" );
+        verifier.assertFileNotPresent( "subproject/target/touch.txt" );
+        verifier.assertFilePresent( "subproject2/target/touch.txt" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
