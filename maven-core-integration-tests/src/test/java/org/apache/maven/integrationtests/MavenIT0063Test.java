cb04cfc837d1b305280b557db8fc5c2c6fb977ce
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:31:34 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466511 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0063Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0063Test.java
@@ -1,29 +1,36 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
 
-public class MavenIT0063Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0063Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test the use of a system scoped dependency to tools.jar. */
-public void testit0063() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0063 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0063", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.executeGoal("package");
-verifier.assertFilePresent("target/classes/org/apache/maven/it0001/Person.class");
-verifier.assertFilePresent("target/test-classes/org/apache/maven/it0001/PersonTest.class");
-verifier.assertFilePresent("target/maven-core-it0063-1.0.jar");
-verifier.assertFilePresent("target/maven-core-it0063-1.0.jar!/it0001.properties");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test the use of a system scoped dependency to tools.jar.
+     */
+    public void testit0063()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0063 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0063", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.executeGoal( "package" );
+        verifier.assertFilePresent( "target/classes/org/apache/maven/it0001/Person.class" );
+        verifier.assertFilePresent( "target/test-classes/org/apache/maven/it0001/PersonTest.class" );
+        verifier.assertFilePresent( "target/maven-core-it0063-1.0.jar" );
+        verifier.assertFilePresent( "target/maven-core-it0063-1.0.jar!/it0001.properties" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
