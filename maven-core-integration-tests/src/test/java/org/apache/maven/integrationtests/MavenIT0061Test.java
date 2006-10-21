b33dc2dc4acff4d40c8c71ec92d2f7669d41c443
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:31:28 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466509 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0061Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0061Test.java
@@ -1,29 +1,36 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
 
-public class MavenIT0061Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0061Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Verify that deployment of artifacts to a legacy-layout repository
-        results in a groupId directory of 'the.full.group.id' instead of
-        'the/full/group/id'. */
-public void testit0061() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0061 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0061", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.executeGoal("deploy");
-verifier.assertFilePresent("target/test-repo/org.apache.maven.it/jars/maven-core-it0061-1.0.jar");
-verifier.assertFilePresent("target/test-repo/org.apache.maven.it/poms/maven-core-it0061-1.0.pom");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Verify that deployment of artifacts to a legacy-layout repository
+     * results in a groupId directory of 'the.full.group.id' instead of
+     * 'the/full/group/id'.
+     */
+    public void testit0061()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0061 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0061", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.executeGoal( "deploy" );
+        verifier.assertFilePresent( "target/test-repo/org.apache.maven.it/jars/maven-core-it0061-1.0.jar" );
+        verifier.assertFilePresent( "target/test-repo/org.apache.maven.it/poms/maven-core-it0061-1.0.pom" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
