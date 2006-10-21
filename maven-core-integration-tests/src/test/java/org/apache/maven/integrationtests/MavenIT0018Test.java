15cddb61b07f6a26026e29264692fcd7a8687ce1
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:29:19 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466466 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0018Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0018Test.java
@@ -1,30 +1,37 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
 
-public class MavenIT0018Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0018Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Ensure that managed dependencies for dependency POMs are calculated
-        correctly when resolved. Removes commons-logging-1.0.3 and checks it is
-        redownloaded. */
-public void testit0018() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0018 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0018", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.deleteArtifact("commons-logging", "commons-logging", "1.0.3", "jar");
-verifier.executeGoal("package");
+    /**
+     * Ensure that managed dependencies for dependency POMs are calculated
+     * correctly when resolved. Removes commons-logging-1.0.3 and checks it is
+     * redownloaded.
+     */
+    public void testit0018()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0018 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0018", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.deleteArtifact( "commons-logging", "commons-logging", "1.0.3", "jar" );
+        verifier.executeGoal( "package" );
 // TODO: I would like to build some small core-it artifacts for this purpose instead
-verifier.assertArtifactPresent("commons-logging", "commons-logging", "1.0.3", "jar");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+        verifier.assertArtifactPresent( "commons-logging", "commons-logging", "1.0.3", "jar" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
