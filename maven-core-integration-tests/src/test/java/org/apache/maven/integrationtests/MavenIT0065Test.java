a571fa0602c2210b462de5646e56b7f3137f4618
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:31:39 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466513 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0065Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0065Test.java
@@ -1,28 +1,35 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
 
-public class MavenIT0065Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0065Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test that the basedir of the parent is set correctly. */
-public void testit0065() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0065 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0065", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-FileUtils.deleteFile(new File(basedir, "parent-basedir"));
-verifier.executeGoal("install");
-verifier.assertFilePresent("subproject/target/child-basedir");
-verifier.assertFilePresent("parent-basedir");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test that the basedir of the parent is set correctly.
+     */
+    public void testit0065()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0065 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0065", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        FileUtils.deleteFile( new File( basedir, "parent-basedir" ) );
+        verifier.executeGoal( "install" );
+        verifier.assertFilePresent( "subproject/target/child-basedir" );
+        verifier.assertFilePresent( "parent-basedir" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
