4d171f3a00bd0f34fb6d4448ed7e8919d1af96f1
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:28:27 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466449 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0000Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0000Test.java
@@ -1,32 +1,39 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
 
-public class MavenIT0000Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0000Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** The simplest of builds. We have one application class and one test
-        class. There are no resources, no source generation, no resource
-        generation and a the super model is employed to provide the build
-        information. */
-public void testit0000() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0000 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0000", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.executeGoal("package");
-verifier.assertFilePresent("target/classes/org/apache/maven/it0000/Person.class");
-verifier.assertFilePresent("target/test-classes/org/apache/maven/it0000/PersonTest.class");
-verifier.assertFilePresent("target/maven-core-it0000-1.0.jar");
-verifier.assertFilePresent("target/surefire-reports/org.apache.maven.it0000.PersonTest.txt");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * The simplest of builds. We have one application class and one test
+     * class. There are no resources, no source generation, no resource
+     * generation and a the super model is employed to provide the build
+     * information.
+     */
+    public void testit0000()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0000 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0000", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.executeGoal( "package" );
+        verifier.assertFilePresent( "target/classes/org/apache/maven/it0000/Person.class" );
+        verifier.assertFilePresent( "target/test-classes/org/apache/maven/it0000/PersonTest.class" );
+        verifier.assertFilePresent( "target/maven-core-it0000-1.0.jar" );
+        verifier.assertFilePresent( "target/surefire-reports/org.apache.maven.it0000.PersonTest.txt" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
