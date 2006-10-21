d3d59f16bb7027d35a15b52546067515ea5ff4ff
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:33:14 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466544 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0096Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0096Test.java
@@ -1,25 +1,32 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
 
-public class MavenIT0096Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0096Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test that plugin executions from &gt;1 step of inheritance don't run multiple times.  */
-public void testit0096() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0096 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0096", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.executeGoal("package");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test that plugin executions from &gt;1 step of inheritance don't run multiple times.
+     */
+    public void testit0096()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0096 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0096", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.executeGoal( "package" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
