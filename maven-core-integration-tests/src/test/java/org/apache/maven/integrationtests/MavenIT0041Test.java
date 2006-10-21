6e3bd7c38037be4ecb71b6e64983d625c3dfb55b
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:30:28 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466489 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0041Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0041Test.java
@@ -1,29 +1,36 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
 
-public class MavenIT0041Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0041Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test the use of a new type from a plugin */
-public void testit0041() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0041 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0041", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.deleteArtifact("org.apache.maven", "maven-core-it-support", "1.2", "coreit-artifact");
-verifier.executeGoal("package");
-verifier.assertFilePresent("target/maven-core-it0041-1.0-SNAPSHOT.jar");
-verifier.assertArtifactPresent("org.apache.maven", "maven-core-it-support", "1.2", "coreit-artifact");
-verifier.assertArtifactPresent("org.apache.maven", "maven-core-it-support", "1.2", "pom");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test the use of a new type from a plugin
+     */
+    public void testit0041()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0041 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0041", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.deleteArtifact( "org.apache.maven", "maven-core-it-support", "1.2", "coreit-artifact" );
+        verifier.executeGoal( "package" );
+        verifier.assertFilePresent( "target/maven-core-it0041-1.0-SNAPSHOT.jar" );
+        verifier.assertArtifactPresent( "org.apache.maven", "maven-core-it-support", "1.2", "coreit-artifact" );
+        verifier.assertArtifactPresent( "org.apache.maven", "maven-core-it-support", "1.2", "pom" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
