06d5de44597f8d8c3efc923992790aed47948a3f
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:28:49 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466456 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0007Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0007Test.java
@@ -1,32 +1,39 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
 
-public class MavenIT0007Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0007Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** We specify a parent in the POM and make sure that it is downloaded as
-        part of the process.         */
-public void testit0007() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0007 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0007", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.deleteArtifact("org.apache.maven.plugins", "maven-plugin-parent", "2.0", "pom");
-verifier.executeGoal("package");
-verifier.assertFilePresent("target/classes/org/apache/maven/it0007/Person.class");
-verifier.assertFilePresent("target/test-classes/org/apache/maven/it0007/PersonTest.class");
-verifier.assertFilePresent("target/maven-core-it0007-1.0.jar");
-verifier.assertFilePresent("target/maven-core-it0007-1.0.jar!/it0007.properties");
-verifier.assertArtifactPresent("org.apache.maven.plugins", "maven-plugin-parent", "2.0", "pom");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * We specify a parent in the POM and make sure that it is downloaded as
+     * part of the process.
+     */
+    public void testit0007()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0007 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0007", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.deleteArtifact( "org.apache.maven.plugins", "maven-plugin-parent", "2.0", "pom" );
+        verifier.executeGoal( "package" );
+        verifier.assertFilePresent( "target/classes/org/apache/maven/it0007/Person.class" );
+        verifier.assertFilePresent( "target/test-classes/org/apache/maven/it0007/PersonTest.class" );
+        verifier.assertFilePresent( "target/maven-core-it0007-1.0.jar" );
+        verifier.assertFilePresent( "target/maven-core-it0007-1.0.jar!/it0007.properties" );
+        verifier.assertArtifactPresent( "org.apache.maven.plugins", "maven-plugin-parent", "2.0", "pom" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
