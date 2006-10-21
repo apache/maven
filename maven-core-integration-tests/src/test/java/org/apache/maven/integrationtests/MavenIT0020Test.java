5a299cef5b7b0e674e5bee235030513348153df8
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:29:25 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466468 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0020Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0020Test.java
@@ -1,29 +1,38 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
-public class MavenIT0020Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0020Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test beanshell mojo support. */
-public void testit0020() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0020 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0020", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.deleteArtifact("org.apache.maven.plugins", "maven-it0020-plugin", "1.0-SNAPSHOT", "maven-plugin");
-FileUtils.deleteFile(new File(basedir, "target/out.txt"));
-List goals = Arrays.asList(new String[] {"install", "it0020:it0020"});
-verifier.executeGoals(goals);
-verifier.assertFilePresent("target/out.txt");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test beanshell mojo support.
+     */
+    public void testit0020()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0020 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0020", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.deleteArtifact( "org.apache.maven.plugins", "maven-it0020-plugin", "1.0-SNAPSHOT", "maven-plugin" );
+        FileUtils.deleteFile( new File( basedir, "target/out.txt" ) );
+        List goals = Arrays.asList( new String[]{"install", "it0020:it0020"} );
+        verifier.executeGoals( goals );
+        verifier.assertFilePresent( "target/out.txt" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
