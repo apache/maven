06ea6ebc081a8abe3022d8029df5ff9cafa901be
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:31:19 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466506 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0058Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0058Test.java
@@ -1,30 +1,39 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MavenIT0058Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0058Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Verify that profiles from settings.xml do not pollute module lists
-        across projects in a reactorized build. */
-public void testit0058() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0058 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0058", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-List cliOptions = new ArrayList();
-cliOptions.add("--settings ./settings.xml");
-verifier.setCliOptions(cliOptions);
-verifier.executeGoal("package");
-verifier.assertFilePresent("subproject/target/maven-core-it0058-subproject-1.0.jar");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Verify that profiles from settings.xml do not pollute module lists
+     * across projects in a reactorized build.
+     */
+    public void testit0058()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0058 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0058", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        List cliOptions = new ArrayList();
+        cliOptions.add( "--settings ./settings.xml" );
+        verifier.setCliOptions( cliOptions );
+        verifier.executeGoal( "package" );
+        verifier.assertFilePresent( "subproject/target/maven-core-it0058-subproject-1.0.jar" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
