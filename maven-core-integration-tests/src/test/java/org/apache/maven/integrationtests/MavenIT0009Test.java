3a0a0eb7a87c25f2c1ac747a9e805e0579aa3a6d
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:28:55 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466458 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0009Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0009Test.java
@@ -1,31 +1,37 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
 
-public class MavenIT0009Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0009Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test plugin configuration and goal configuration that overrides what the
-        mojo has specified.
-         */
-public void testit0009() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0009 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0009", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.deleteArtifact("org.apache.maven.plugins", "maven-core-it-plugin", "1.0", "maven-plugin");
-verifier.executeGoal("generate-resources");
-verifier.assertFilePresent("target/pluginItem");
-verifier.assertFilePresent("target/goalItem");
-verifier.assertFileNotPresent("target/bad-item");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test plugin configuration and goal configuration that overrides what the
+     * mojo has specified.
+     */
+    public void testit0009()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0009 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0009", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.deleteArtifact( "org.apache.maven.plugins", "maven-core-it-plugin", "1.0", "maven-plugin" );
+        verifier.executeGoal( "generate-resources" );
+        verifier.assertFilePresent( "target/pluginItem" );
+        verifier.assertFilePresent( "target/goalItem" );
+        verifier.assertFileNotPresent( "target/bad-item" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
