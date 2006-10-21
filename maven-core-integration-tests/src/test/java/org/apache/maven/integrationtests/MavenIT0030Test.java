8697d10f7310a67df59250726817d3c4ff5787cc
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:29:55 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466478 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0030Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0030Test.java
@@ -1,31 +1,38 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
 
-public class MavenIT0030Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0030Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test for injection of dependencyManagement through parents of 
-        dependency poms. */
-public void testit0030() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0030 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0030", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.deleteArtifact("org.apache.maven.it", "maven-core-it0030", "1.0-SNAPSHOT", "jar");
-verifier.deleteArtifact("org.apache.maven.it", "maven-core-it0030-child-hierarchy", "1.0-SNAPSHOT", "jar");
-verifier.deleteArtifact("org.apache.maven.it", "maven-core-it0030-child-project1", "1.0-SNAPSHOT", "jar");
-verifier.deleteArtifact("org.apache.maven.it", "maven-core-it0030-child-project2", "1.0-SNAPSHOT", "jar");
-verifier.executeGoal("install");
-verifier.assertFilePresent("child-hierarchy/project2/target/classes/org/apache/maven/it0001/Person.class");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test for injection of dependencyManagement through parents of
+     * dependency poms.
+     */
+    public void testit0030()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0030 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0030", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.deleteArtifact( "org.apache.maven.it", "maven-core-it0030", "1.0-SNAPSHOT", "jar" );
+        verifier.deleteArtifact( "org.apache.maven.it", "maven-core-it0030-child-hierarchy", "1.0-SNAPSHOT", "jar" );
+        verifier.deleteArtifact( "org.apache.maven.it", "maven-core-it0030-child-project1", "1.0-SNAPSHOT", "jar" );
+        verifier.deleteArtifact( "org.apache.maven.it", "maven-core-it0030-child-project2", "1.0-SNAPSHOT", "jar" );
+        verifier.executeGoal( "install" );
+        verifier.assertFilePresent( "child-hierarchy/project2/target/classes/org/apache/maven/it0001/Person.class" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
