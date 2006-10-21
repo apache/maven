76619083b63a2a6bfe30954af0e2d4f4eef1d544
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:31:22 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466507 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0059Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0059Test.java
@@ -1,30 +1,38 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
+import java.util.Properties;
 
-public class MavenIT0059Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0059Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Verify that maven-1 POMs will be ignored but not stop the resolution
-        process. */
-public void testit0059() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0059 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0059", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-Properties verifierProperties = new Properties();
-verifierProperties.put("failOnErrorOutput", "false");
-verifier.setVerifierProperties(verifierProperties);
-verifier.executeGoal("package");
-verifier.assertFilePresent("target/maven-core-it0059-1.0.jar");
+    /**
+     * Verify that maven-1 POMs will be ignored but not stop the resolution
+     * process.
+     */
+    public void testit0059()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0059 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0059", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        Properties verifierProperties = new Properties();
+        verifierProperties.put( "failOnErrorOutput", "false" );
+        verifier.setVerifierProperties( verifierProperties );
+        verifier.executeGoal( "package" );
+        verifier.assertFilePresent( "target/maven-core-it0059-1.0.jar" );
 // don't verify error free log
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
