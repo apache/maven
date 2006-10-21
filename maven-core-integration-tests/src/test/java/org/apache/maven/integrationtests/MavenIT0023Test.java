2fd45e34fa9437a735508a0139118fa9204f8f82
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:29:34 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466471 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0023Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0023Test.java
@@ -1,30 +1,38 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
+import java.util.Properties;
 
-public class MavenIT0023Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0023Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test profile inclusion from settings.xml (this one is activated by an id
-        in the activeProfiles section). */
-public void testit0023() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0023 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0023", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-Properties systemProperties = new Properties();
-systemProperties.put("org.apache.maven.user-settings", "settings.xml");
-verifier.setSystemProperties(systemProperties);
-verifier.executeGoal("core-it:touch");
-verifier.assertFilePresent("target/test.txt");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test profile inclusion from settings.xml (this one is activated by an id
+     * in the activeProfiles section).
+     */
+    public void testit0023()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0023 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0023", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        Properties systemProperties = new Properties();
+        systemProperties.put( "org.apache.maven.user-settings", "settings.xml" );
+        verifier.setSystemProperties( systemProperties );
+        verifier.executeGoal( "core-it:touch" );
+        verifier.assertFilePresent( "target/test.txt" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
