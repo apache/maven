66a6b5850d2fb0708378f9bc0f74219c1baa8d0f
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:29:16 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466465 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0017Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0017Test.java
@@ -1,29 +1,36 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
 
-public class MavenIT0017Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0017Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test an EJB generation */
-public void testit0017() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0017 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0017", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.executeGoal("package");
-verifier.assertFilePresent("target/classes/org/apache/maven/it0017/Person.class");
-verifier.assertFilePresent("target/maven-core-it0017-1.0.jar");
-verifier.assertFilePresent("target/maven-core-it0017-1.0.jar!/META-INF/ejb-jar.xml");
-verifier.assertFilePresent("target/maven-core-it0017-1.0.jar!/org/apache/maven/it0017/Person.class");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test an EJB generation
+     */
+    public void testit0017()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0017 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0017", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.executeGoal( "package" );
+        verifier.assertFilePresent( "target/classes/org/apache/maven/it0017/Person.class" );
+        verifier.assertFilePresent( "target/maven-core-it0017-1.0.jar" );
+        verifier.assertFilePresent( "target/maven-core-it0017-1.0.jar!/META-INF/ejb-jar.xml" );
+        verifier.assertFilePresent( "target/maven-core-it0017-1.0.jar!/org/apache/maven/it0017/Person.class" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
