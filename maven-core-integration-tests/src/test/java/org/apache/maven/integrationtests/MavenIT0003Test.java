a6a778940a8152dfdb4cf63d009b80689b321561
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:28:37 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466452 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0003Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0003Test.java
@@ -1,33 +1,39 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
 
-public class MavenIT0003Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0003Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Builds upon it0001: we add a jar installation step. We delete the JAR
-        from the local repository to make sure it is there post build.
-        */
-public void testit0003() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0003 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0003", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.deleteArtifact("org.apache.maven", "maven-core-it0003", "1.0", "jar");
-verifier.executeGoal("install");
-verifier.assertFilePresent("target/classes/org/apache/maven/it0003/Person.class");
-verifier.assertFilePresent("target/test-classes/org/apache/maven/it0003/PersonTest.class");
-verifier.assertFilePresent("target/maven-core-it0003-1.0.jar");
-verifier.assertFilePresent("target/maven-core-it0003-1.0.jar!/it0003.properties");
-verifier.assertArtifactPresent("org.apache.maven", "maven-core-it0003", "1.0", "jar");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Builds upon it0001: we add a jar installation step. We delete the JAR
+     * from the local repository to make sure it is there post build.
+     */
+    public void testit0003()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0003 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0003", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.deleteArtifact( "org.apache.maven", "maven-core-it0003", "1.0", "jar" );
+        verifier.executeGoal( "install" );
+        verifier.assertFilePresent( "target/classes/org/apache/maven/it0003/Person.class" );
+        verifier.assertFilePresent( "target/test-classes/org/apache/maven/it0003/PersonTest.class" );
+        verifier.assertFilePresent( "target/maven-core-it0003-1.0.jar" );
+        verifier.assertFilePresent( "target/maven-core-it0003-1.0.jar!/it0003.properties" );
+        verifier.assertArtifactPresent( "org.apache.maven", "maven-core-it0003", "1.0", "jar" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
