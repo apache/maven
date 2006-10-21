a0575c3eb1e3523712f9bf687051ca1aedbd9966
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:31:13 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466504 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0056Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0056Test.java
@@ -1,32 +1,39 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
 
-public class MavenIT0056Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0056Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test that multiple executions of the compile goal with different
-        includes/excludes will succeed. */
-public void testit0056() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0056 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0056", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-verifier.executeGoal("test-compile");
-verifier.assertFilePresent("target/classes/org/apache/maven/it0001/Person.class");
-verifier.assertFilePresent("target/classes/org/apache/maven/it0001/PersonTwo.class");
-verifier.assertFilePresent("target/classes/org/apache/maven/it0001/PersonThree.class");
-verifier.assertFilePresent("target/test-classes/org/apache/maven/it0001/PersonTest.class");
-verifier.assertFilePresent("target/test-classes/org/apache/maven/it0001/PersonTwoTest.class");
-verifier.assertFilePresent("target/test-classes/org/apache/maven/it0001/PersonThreeTest.class");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test that multiple executions of the compile goal with different
+     * includes/excludes will succeed.
+     */
+    public void testit0056()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0056 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0056", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.executeGoal( "test-compile" );
+        verifier.assertFilePresent( "target/classes/org/apache/maven/it0001/Person.class" );
+        verifier.assertFilePresent( "target/classes/org/apache/maven/it0001/PersonTwo.class" );
+        verifier.assertFilePresent( "target/classes/org/apache/maven/it0001/PersonThree.class" );
+        verifier.assertFilePresent( "target/test-classes/org/apache/maven/it0001/PersonTest.class" );
+        verifier.assertFilePresent( "target/test-classes/org/apache/maven/it0001/PersonTwoTest.class" );
+        verifier.assertFilePresent( "target/test-classes/org/apache/maven/it0001/PersonThreeTest.class" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
