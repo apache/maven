3669052eeba0cdc0ed13b1bcfd5b0211bb085af1
Jason van Zyl <jvanzyl@apache.org>
2006-10-21 20:31:04 +0000

    o reformat and optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@466501 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0053Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0053Test.java
@@ -1,32 +1,41 @@
 package org.apache.maven.integrationtests;
-import java.io.*;
-import java.util.*;
 
-import junit.framework.*;
+import junit.framework.TestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.FileUtils;
+import org.apache.maven.it.util.ResourceExtractor;
 
-import org.apache.maven.it.*;
-import org.apache.maven.it.util.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MavenIT0053Test extends TestCase /*extends AbstractMavenIntegrationTest*/ {    
+public class MavenIT0053Test
+    extends TestCase /*extends AbstractMavenIntegrationTest*/
+{
 
-/** Test that attached artifacts have the same buildnumber and timestamp
-        as the main artifact. This will not correctly verify until we have
-        some way to pattern-match the buildnumber/timestamp... */
-public void testit0053() throws Exception {
-String basedir = System.getProperty("maven.test.tmpdir", System.getProperty("java.io.tmpdir"));
-File testDir = new File(basedir, getName());
-FileUtils.deleteDirectory(testDir);
-System.out.println("Extracting it0053 to " + testDir.getAbsolutePath());
-ResourceExtractor.extractResourcePath(getClass(), "/it0053", testDir);
-Verifier verifier = new Verifier(testDir.getAbsolutePath());
-List cliOptions = new ArrayList();
-cliOptions.add("--no-plugin-registry");
-verifier.setCliOptions(cliOptions);
-verifier.executeGoal("package");
-verifier.assertFilePresent("target/maven-core-it0053-1.0-SNAPSHOT.jar");
-verifier.assertFileNotPresent("target/maven-core-it0053-1.0-SNAPSHOT-sources.jar");
-verifier.verifyErrorFreeLog();
-verifier.resetStreams();
-System.out.println("PASS");
-}}
+    /**
+     * Test that attached artifacts have the same buildnumber and timestamp
+     * as the main artifact. This will not correctly verify until we have
+     * some way to pattern-match the buildnumber/timestamp...
+     */
+    public void testit0053()
+        throws Exception
+    {
+        String basedir = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File testDir = new File( basedir, getName() );
+        FileUtils.deleteDirectory( testDir );
+        System.out.println( "Extracting it0053 to " + testDir.getAbsolutePath() );
+        ResourceExtractor.extractResourcePath( getClass(), "/it0053", testDir );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        List cliOptions = new ArrayList();
+        cliOptions.add( "--no-plugin-registry" );
+        verifier.setCliOptions( cliOptions );
+        verifier.executeGoal( "package" );
+        verifier.assertFilePresent( "target/maven-core-it0053-1.0-SNAPSHOT.jar" );
+        verifier.assertFileNotPresent( "target/maven-core-it0053-1.0-SNAPSHOT-sources.jar" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        System.out.println( "PASS" );
+    }
+}
 
