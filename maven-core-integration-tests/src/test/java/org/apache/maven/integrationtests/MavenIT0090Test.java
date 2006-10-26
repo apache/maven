f7c9d2aef8c35cf9746bca0d245eed95d1837034
Jason van Zyl <jvanzyl@apache.org>
2006-10-26 02:06:39 +0000

    o setup the envars for the verifier
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@467832 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0090Test.java
+++ maven-core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0090Test.java
@@ -5,6 +5,8 @@ import org.apache.maven.it.Verifier;
 import org.apache.maven.it.util.ResourceExtractor;
 
 import java.io.File;
+import java.util.Map;
+import java.util.HashMap;
 
 public class MavenIT0090Test
     extends TestCase /*extends AbstractMavenIntegrationTest*/
@@ -18,8 +20,10 @@ public class MavenIT0090Test
         throws Exception
     {
         File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/it0090" );
-        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
-        verifier.executeGoal( "test" );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath(), true );
+        Map envVars = new HashMap();
+        envVars.put( "MAVEN_TEST_ENVAR", "MAVEN_TEST_ENVAR_VALUE" );
+        verifier.executeGoal( "test", envVars );
         verifier.assertFilePresent( "target/mojo-generated.properties" );
         verifier.verifyErrorFreeLog();
         verifier.resetStreams();
