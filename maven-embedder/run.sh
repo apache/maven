a6977b53c76d83af18564057d19ef2017375ce34
Jason van Zyl <jvanzyl@apache.org>
2006-11-30 04:45:40 +0000

    o not sure what happened but i couldn't make an assembly the way i used to, so i reverted o added an OSGi manifest to make it easier to Eugene to integrate into Eclipse
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@480835 13f79535-47bb-0310-9956-ffa450edef68

--- maven-embedder/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mvn clean:clean assembly:assembly
-
-CLASSPATH=target/maven-embedder-2.1-SNAPSHOT-dep.jar
-
-javac -classpath $CLASSPATH Plugin.java
-
-java -classpath $CLASSPATH:. Plugin
