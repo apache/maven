e7c7a056c4f3ab100a1472c699b07e2eb2dc44c5
Jason van Zyl <jvanzyl@apache.org>
2006-12-07 21:45:25 +0000

    o abstract test case
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@483680 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-integration-tests/src/test/java/org/apache/maven/integrationtests/AbstractMavenIntegrationTestCase.java
@@ -0,0 +1,11 @@
+package org.apache.maven.integrationtests;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Jason van Zyl
+ */
+public abstract class AbstractMavenIntegrationTestCase
+    extends TestCase
+{
+}
