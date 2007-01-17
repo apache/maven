d0690823afe2c5cc16daadf451ceb7c320d2f0b7
Brett Leslie Porter <brett@apache.org>
2007-01-17 09:34:28 +0000

    [MNG-2780] snapshots are not updated based on metadata deployed with uniqueVersion = false Merged from: r496972 (trunk)
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/branches/maven-2.0.x@496975 13f79535-47bb-0310-9956-ffa450edef68

--- maven-artifact-manager/src/main/java/org/apache/maven/artifact/repository/metadata/DefaultRepositoryMetadataManager.java
+++ maven-artifact-manager/src/main/java/org/apache/maven/artifact/repository/metadata/DefaultRepositoryMetadataManager.java
@@ -203,7 +203,7 @@ public class DefaultRepositoryMetadataManager
                     {
                         m.getVersioning().setSnapshot( new Snapshot() );
                     }
-
+/*
                     if ( !m.getVersioning().getSnapshot().isLocalCopy() )
                     {
                         // TODO: I think this is incorrect (it results in localCopy set in a remote profile). Probably
@@ -212,6 +212,7 @@ public class DefaultRepositoryMetadataManager
                         metadata.setMetadata( m );
                         metadata.storeInLocalRepository( localRepository, repository );
                     }
+*/
                 }
                 else
                 {
