8386704a348db072a4fa887727a41f522e991c0d
Kenney Westerhof <kenney@apache.org>
2007-01-17 22:11:47 +0000

    Add some progress information
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@497207 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-integration-tests/src/test/java/org/apache/maven/integrationtests/AbstractMavenIntegrationTestCase.java
+++ its/core-integration-tests/src/test/java/org/apache/maven/integrationtests/AbstractMavenIntegrationTestCase.java
@@ -8,4 +8,18 @@ import junit.framework.TestCase;
 public abstract class AbstractMavenIntegrationTestCase
     extends TestCase
 {
+    private boolean printed = false;
+
+    protected void setUp()
+    {
+        if ( !printed )
+        {
+            String simpleName = getClass().getSimpleName();
+            simpleName = simpleName.startsWith( "MavenIT" ) ? simpleName.substring( "MavenIT".length() ) : simpleName;
+            simpleName = simpleName.endsWith( "Test" ) ? simpleName.substring(0, simpleName.length() -4 ) : simpleName;
+
+            System.out.println( simpleName + ".." );
+            printed = true;
+        }
+    }
 }
