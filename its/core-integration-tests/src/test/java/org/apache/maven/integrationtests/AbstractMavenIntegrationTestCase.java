460715f80dee882e88313f2a7860bb7cd0b894c6
Kenney Westerhof <kenney@apache.org>
2007-01-19 15:12:01 +0000

    add possibility of using a new empty local repo for each IT
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@497840 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-integration-tests/src/test/java/org/apache/maven/integrationtests/AbstractMavenIntegrationTestCase.java
+++ its/core-integration-tests/src/test/java/org/apache/maven/integrationtests/AbstractMavenIntegrationTestCase.java
@@ -1,5 +1,9 @@
 package org.apache.maven.integrationtests;
 
+import org.apache.maven.it.util.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 import java.io.PrintStream;
 
 import junit.framework.TestCase;
@@ -14,15 +18,15 @@ public abstract class AbstractMavenIntegrationTestCase
     protected void runTest()
         throws Throwable
     {
-        String simpleName = getClass().getName();
-        int idx = simpleName.lastIndexOf( '.' );
-        simpleName = idx >= 0 ? simpleName.substring( idx + 1 ) : simpleName;
-        simpleName = simpleName.startsWith( "MavenIT" ) ? simpleName.substring( "MavenIT".length() ) : simpleName;
-        simpleName = simpleName.endsWith( "Test" ) ? simpleName.substring( 0, simpleName.length() - 4 ) : simpleName;
+        if ( "true".equals( System.getProperty( "useEmptyLocalRepository", "false" ) ) )
+        {
+            setupLocalRepo();
+        }
 
+        // save System.out since running the test will replace it
         PrintStream out = System.out;
 
-        out.print( simpleName + ".." );
+        out.print( getITName() + "(" + getName() + ").." );
 
         try
         {
@@ -34,5 +38,30 @@ public abstract class AbstractMavenIntegrationTestCase
             out.println( " Failure" );
             throw t;
         }
+    }
+
+    private String getITName()
+    {
+        String simpleName = getClass().getName();
+        int idx = simpleName.lastIndexOf( '.' );
+        simpleName = idx >= 0 ? simpleName.substring( idx + 1 ) : simpleName;
+        simpleName = simpleName.startsWith( "MavenIT" ) ? simpleName.substring( "MavenIT".length() ) : simpleName;
+        simpleName = simpleName.endsWith( "Test" ) ? simpleName.substring( 0, simpleName.length() - 4 ) : simpleName;
+        return simpleName;
+    }
+
+    protected File setupLocalRepo()
+        throws IOException
+    {
+        String tempDirPath = System.getProperty( "maven.test.tmpdir", System.getProperty( "java.io.tmpdir" ) );
+        File localRepo = new File( tempDirPath, "local-repository/" + getITName() );
+        if ( localRepo.isDirectory() )
+        {
+            FileUtils.deleteDirectory( localRepo );
+        }
+
+        System.setProperty( "maven.repo.local", localRepo.getAbsolutePath() );
+
+        return localRepo;
     }
 }
