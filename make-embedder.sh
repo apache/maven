de30bce7f65d082644a1b8bddaafb36bfe6d8b08
Jason van Zyl <jvanzyl@apache.org>
2007-02-25 01:03:51 +0000

    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@511386 13f79535-47bb-0310-9956-ffa450edef68

---
+++ make-embedder.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+ds=`date "+2.1.0.v%Y%m%d-%H%M"`
+dir=`pwd`
+
+mvn source:aggregate
+mv target/maven-2.1-SNAPSHOT-sources.jar $dir/maven-embedder-$ds-depsrc.zip
+
+(
+
+  cd maven-embedder
+  sed "s@<bundleVersion>.*</bundleVersion>@<bundleVersion>$ds</bundleVersion>@" pom.xml > tmp; mv tmp pom.xml
+  mvn clean assembly:assembly
+  mv target/maven-embedder-$ds-dep.jar $dir
+)
