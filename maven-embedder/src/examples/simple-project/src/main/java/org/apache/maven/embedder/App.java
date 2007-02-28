eb87f15ce741bc886d6343843b1f2191b92ee27a
Jason van Zyl <jvanzyl@apache.org>
2007-02-28 02:29:42 +0000

    o adding a simple example that can be used as a snippet
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@512570 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-embedder/src/examples/simple-project/src/main/java/org/apache/maven/embedder/App.java
@@ -0,0 +1,13 @@
+package org.apache.maven.embedder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
