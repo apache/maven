5fe08423334b3beafb84222677632dff70beea27
Jason van Zyl <jvanzyl@apache.org>
2007-03-04 00:56:51 +0000

    o i guess there are a couple people using windows ... buy a MAC!
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@514300 13f79535-47bb-0310-9956-ffa450edef68

--- maven-embedder/src/bin/mvn.bat
+++ maven-embedder/src/bin/mvn.bat
@@ -122,7 +122,7 @@ goto Win9xApp
 SET MAVEN_JAVA_EXE="%JAVA_HOME%\bin\java.exe"
 
 @REM Start MAVEN2
-for %%i in ("%M2_HOME%"\core\boot\plexus-classworlds-*) do set CLASSWORLDS_JAR="%%i"
+for %%i in ("%M2_HOME%"\lib\maven-embedder-*) do set CLASSWORLDS_JAR="%%i"
 %MAVEN_JAVA_EXE% %MAVEN_OPTS% -classpath %CLASSWORLDS_JAR% "-Dclassworlds.conf=%M2_HOME%\bin\m2.conf" "-Dmaven.home=%M2_HOME%" org.codehaus.plexus.classworlds.launcher.Launcher %MAVEN_CMD_LINE_ARGS%
 if ERRORLEVEL 1 goto error
 goto end
