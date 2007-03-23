05162de0d0b42adb09673fa36a01ab7a0ab5b984
Jason van Zyl <jvanzyl@apache.org>
2007-03-23 00:11:31 +0000

    o bits for mng-1577
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@521543 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-artifact/src/main/java/org/apache/maven/artifact/versioning/ManagedVersionMap.java
@@ -0,0 +1,52 @@
+package org.apache.maven.artifact.versioning;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
+
+public class ManagedVersionMap extends HashMap
+{
+    public ManagedVersionMap(Map map)
+    {
+        super();
+        if (map != null)
+        {
+            putAll(map);
+        }
+    }
+
+    public String toString()
+    {
+        StringBuffer buffer = new StringBuffer("ManagedVersionMap\n");
+        Iterator iter = this.keySet().iterator();
+        while (iter.hasNext())
+        {
+            String key = (String)iter.next();
+            buffer.append(key).append("=").append(get(key));
+            if (iter.hasNext())
+            {
+                buffer.append("\n");
+            }
+        }
+        return buffer.toString();
+    }
+}
---
+++ maven-project/src/test/java/org/apache/maven/project/inheritance/t07/ProjectInheritanceTest.java
@@ -0,0 +1,91 @@
+package org.apache.maven.project.inheritance.t07;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.util.List;
+import java.util.Set;
+import java.util.Iterator;
+
+import org.apache.maven.model.Build;
+import org.apache.maven.model.MailingList;
+import org.apache.maven.model.Plugin;
+import org.apache.maven.model.PluginExecution;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.inheritance.AbstractProjectInheritanceTestCase;
+import org.apache.maven.artifact.Artifact;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.codehaus.plexus.logging.LoggerManager;
+import org.codehaus.plexus.logging.Logger;
+
+/**
+ * A test which demonstrates maven's dependency management
+ *
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
+ * @version $Id$
+ */
+public class ProjectInheritanceTest
+    extends AbstractProjectInheritanceTestCase
+{
+    // ----------------------------------------------------------------------
+    //
+    // p1 inherits from p0
+    // p0 inhertis from super model
+    //
+    // or we can show it graphically as:
+    //
+    // p1 ---> p0 --> super model
+    //
+    // ----------------------------------------------------------------------
+
+    public void testDependencyManagement()
+        throws Exception
+    {
+        File localRepo = getLocalRepositoryPath();
+        File pom0 = new File( localRepo, "p0/pom.xml" );
+
+        File pom0Basedir = pom0.getParentFile();
+
+        File pom1 = new File( pom0Basedir, "p1/pom.xml" );
+
+        // load everything...
+        MavenProject project0 = getProjectWithDependencies( pom0 );
+        MavenProject project1 = getProjectWithDependencies( pom1 );
+
+        assertEquals( pom0Basedir, project1.getParent().getBasedir() );
+        System.out.println("Project " + project1.getId() + " " + project1);
+        Set set = project1.getArtifacts();
+        assertNotNull("No artifacts", set);
+        assertTrue("No Artifacts", set.size() > 0);
+        Iterator iter = set.iterator();
+        assertTrue("Set size should be 3, is " + set.size(), set.size() == 3);
+        
+        while (iter.hasNext())
+        {
+            Artifact artifact = (Artifact)iter.next();
+            assertFalse("", artifact.getArtifactId().equals("maven-test-d"));
+            System.out.println("Artifact: " + artifact.getDependencyConflictId() + " " + artifact.getVersion() +
+              " Optional=" + (artifact.isOptional() ? "true" : "false"));
+            assertTrue("Incorrect version for " + artifact.getDependencyConflictId(), artifact.getVersion().equals("1.0"));
+        }
+
+    }
+}
