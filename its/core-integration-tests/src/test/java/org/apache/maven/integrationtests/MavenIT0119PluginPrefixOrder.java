ca2504d5df47c8d892f006aad06b1bdc1a41aa31
Brian E Fox <brianf@apache.org>
2007-04-07 20:42:13 +0000

    updated IT tests to handle prefixes from the settings.xml
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@526481 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0119PluginPrefixOrder.java
+++ its/core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0119PluginPrefixOrder.java
@@ -1,7 +1,6 @@
 package org.apache.maven.integrationtests;
 
 import java.io.File;
-import java.util.List;
 import java.util.ArrayList;
 
 import org.apache.maven.it.Verifier;
@@ -13,31 +12,34 @@ public class MavenIT0119PluginPrefixOrder
     public void testit0119()
         throws Exception
     {
-        File testDir =
-            ResourceExtractor.simpleExtractResources( getClass(), "/it0119-pluginprefixorder" );
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/it0119-pluginprefixorder" );
 
         Verifier verifier;
 
-        // Install the parent POM, extension and the plugin 
+        // Install the parent POM, extension and the plugin
         verifier = new Verifier( testDir.getAbsolutePath() );
-        verifier.deleteArtifact( "org.apache.maven.plugins", "it0119", "1.0", "jar" );                
-        verifier.deleteArtifact( "org.codehaus.mojo", "it0119", "1.0", "jar" );
-        verifier.deleteArtifact( "it0119", "it0119", "1.0", "jar" );
-        verifier.deleteArtifact( "org.apache.maven.its.it0119", "it0119-parent", "1.0", "pom" );
-        
-        
-        List cliOptions = new ArrayList();        
+
         verifier.executeGoal( "install" );
         verifier.verifyErrorFreeLog();
         verifier.resetStreams();
         
-        //now run the test
-        testDir =
-         ResourceExtractor.simpleExtractResources( getClass(), "/it0119-pluginprefixorder/test-project" );
+
+        // now run the test. Since we have apache and codehaus, i should get the apache one first
+        testDir = ResourceExtractor.simpleExtractResources( getClass(), "/it0119-pluginprefixorder/test-project" );
         verifier = new Verifier( testDir.getAbsolutePath() );
         verifier.executeGoal( "it0119:apache" );
         verifier.verifyErrorFreeLog();
-        verifier.resetStreams();
+
         
+//      now run the test. Since we have apache and codehaus and a prefix in my settings, i should get the custom one first
+        testDir = ResourceExtractor.simpleExtractResources( getClass(), "/it0119-pluginprefixorder/test-project" );
+        verifier = new Verifier( testDir.getAbsolutePath() );
+        
+        //use my custom settings upon invocation.
+        ArrayList cli = new ArrayList();
+        cli.add("-s "+testDir.getAbsolutePath()+"/settings.xml");
+        verifier.setCliOptions( cli );
+        verifier.executeGoal( "it0119:custom" );
+        verifier.verifyErrorFreeLog();
     }
 }
--- its/core-integration-tests/src/test/resources/it0119-pluginprefixorder/Readme.txt
+++ its/core-integration-tests/src/test/resources/it0119-pluginprefixorder/Readme.txt
@@ -1,3 +1,5 @@
 This test checks the order of plugin searching. In <2.0.7, Maven searched codehaus before apache.
 
-This is to fix MNG-2926. This test will fail with 2.0.6 and less.
+This test also verifies that prefixes set by a user in the settings are searched first before the standard ones.
+
+This is to fix MNG-2926. This test will fail with Maven <=2.0.6.
