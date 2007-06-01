b4f380dd3cbe4875eac191ada2451ac098e6f0b4
Jason van Zyl <jvanzyl@apache.org>
2007-06-01 20:46:48 +0000

    MNG-2683 Better exception messages Submitted by: Mark Lundquist
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@543604 13f79535-47bb-0310-9956-ffa450edef68

--- maven-project/src/main/java/org/apache/maven/project/ProjectBuildingException.java
+++ maven-project/src/main/java/org/apache/maven/project/ProjectBuildingException.java
@@ -44,4 +44,9 @@ public class ProjectBuildingException
     {
         return projectId;
     }
+
+    public String getMessage() 
+    {
+        return super.getMessage() + " for project " + this.projectId;
+    }
 }
