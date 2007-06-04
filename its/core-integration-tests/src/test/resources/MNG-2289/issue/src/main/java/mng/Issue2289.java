4dc2f57442ad9b391c405ac26a0cf9091f020d59
Jason van Zyl <jvanzyl@apache.org>
2007-06-04 20:40:59 +0000

    o adding in a test project to try and solve MNG-2289. working with patrick so we'll call this something else later
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@544253 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-integration-tests/src/test/resources/MNG-2289/issue/src/main/java/mng/Issue2289.java
@@ -0,0 +1,12 @@
+package mng;
+
+import junit.framework.TestCase;
+
+public class Issue2289
+{
+	public static void main(final String[] args)
+	{
+		TestCase tc = new TestCase("Dummy") {};
+		System.exit(tc == null ? -1 : 0);
+	}
+}
