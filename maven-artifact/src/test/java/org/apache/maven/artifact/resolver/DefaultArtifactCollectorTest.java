d20f6c96fe3a5fc59bd3604bbc813b50fb69ae7f
Brett Leslie Porter <brett@apache.org>
2007-06-07 12:39:05 +0000

    [MNG-2988] Ranges with inclusive upper bounds are not validated against metadata Submitted by: Mark Hobson Merged from: maven-2.0.x branch r545150
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@545156 13f79535-47bb-0310-9956-ffa450edef68

--- maven-artifact/src/test/java/org/apache/maven/artifact/resolver/DefaultArtifactCollectorTest.java
+++ maven-artifact/src/test/java/org/apache/maven/artifact/resolver/DefaultArtifactCollectorTest.java
@@ -292,10 +292,10 @@ public class DefaultArtifactCollectorTest
         ArtifactSpec b = a.addDependency( "b", "1.0" );
         a.addDependency( "c", "[2.0,2.5]" );
         b.addDependency( "c", "[1.0,3.0]" );
+        ArtifactSpec c = createArtifact( "c", "2.5" );
 
         ArtifactResolutionResult res = collect( a );
 
-        ArtifactSpec c = createArtifact( "c", "2.5" );
         assertEquals( "Check artifact list", createSet( new Object[]{a.artifact, b.artifact, c.artifact} ),
                       res.getArtifacts() );
         assertEquals( "Check version", "2.5", getArtifact( "c", res.getArtifacts() ).getVersion() );
