0bf07fd860d3f6f0a80a066936208a670d4c2097
Jason van Zyl <jvanzyl@apache.org>
2007-06-07 13:24:15 +0000

    MNG-3039 Fix .bat file (who uses Windows anyway? Get a MAC!) Submitted by: Mark Hobson
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/branches/maven-2.0.x@545172 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/bin/mvn.bat
+++ maven-core/src/bin/mvn.bat
@@ -130,12 +130,12 @@ SET MAVEN_JAVA_EXE="%JAVA_HOME%\bin\java.exe"
 if "%@eval[2+2]" == "4" goto 4NTCWJars
 
 @REM -- Regular WinNT shell
-for %%i in ("%M2_HOME%"\core\boot\classworlds-*) do set CLASSWORLDS_JAR="%%i"
+for %%i in ("%M2_HOME%"\boot\classworlds-*) do set CLASSWORLDS_JAR="%%i"
 goto runm2
 
 @REM The 4NT Shell from jp software
 :4NTCWJars
-for %%i in ("%M2_HOME%\core\boot\classworlds-*") do set CLASSWORLDS_JAR="%%i"
+for %%i in ("%M2_HOME%\boot\classworlds-*") do set CLASSWORLDS_JAR="%%i"
 goto runm2
 
 @REM Start MAVEN2
