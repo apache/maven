2f361cfafc891e1bd7a65714252662f12fc3dbe7
Vincent Siveton <vsiveton@apache.org>
2007-07-06 20:31:48 +0000

    o beautified the renderer with a line separator
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/branches/maven-2.0.x@554043 13f79535-47bb-0310-9956-ffa450edef68

--- maven-artifact/src/main/java/org/apache/maven/artifact/resolver/AbstractArtifactResolutionException.java
+++ maven-artifact/src/main/java/org/apache/maven/artifact/resolver/AbstractArtifactResolutionException.java
@@ -42,7 +42,7 @@ public class AbstractArtifactResolutionException
     private String version;
 
     private String type;
-    
+
     private String classifier;
 
     private List remoteRepositories;
@@ -232,7 +232,7 @@ public class AbstractArtifactResolutionException
                 sb.append( indentation );
                 sb.append( "Try downloading the file manually from the project website." );
             }
-            
+
             sb.append( LS );
             sb.append( LS );
             sb.append( indentation );
@@ -248,7 +248,7 @@ public class AbstractArtifactResolutionException
             sb.append( "        " );
             sb.append( "-Dversion=" );
             sb.append( version );
-            
+
             //insert classifier only if it was used in the artifact
             if (classifier !=null && !classifier.equals( "" ))
             {
@@ -263,6 +263,7 @@ public class AbstractArtifactResolutionException
             // If people want to deploy it
 
             sb.append( "Alternatively, if you host your own repository you can deploy the file there: " );
+            sb.append( LS );
             sb.append( indentation );
             sb.append( "    mvn deploy:deploy-file -DgroupId=" );
             sb.append( groupId );
@@ -288,7 +289,7 @@ public class AbstractArtifactResolutionException
             sb.append( "        " );
             sb.append( " -Durl=[url] -DrepositoryId=[id]" );
             sb.append( LS );
-            
+
         }
 
         sb.append( constructArtifactPath( path, indentation ) );
@@ -296,7 +297,7 @@ public class AbstractArtifactResolutionException
 
         return sb.toString();
     }
-    
+
     public String getArtifactPath()
     {
         return path;
