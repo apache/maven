41977f976a0fab5f70c75e7920e71c93dead8ccf
Jason van Zyl <jvanzyl@apache.org>
2007-09-01 21:56:32 +0000

    o removing debug
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@571883 13f79535-47bb-0310-9956-ffa450edef68

--- maven-embedder/src/test/java/org/apache/maven/embedder/AbstractEmbedderTestCase.java
+++ maven-embedder/src/test/java/org/apache/maven/embedder/AbstractEmbedderTestCase.java
@@ -20,15 +20,6 @@ package org.apache.maven.embedder;
  */
 
 import org.codehaus.plexus.PlexusTestCase;
-import org.codehaus.plexus.util.FileUtils;
-import org.apache.maven.execution.MavenExecutionRequest;
-import org.apache.maven.execution.DefaultMavenExecutionRequest;
-import org.apache.maven.execution.MavenExecutionResult;
-
-import java.io.File;
-import java.util.List;
-import java.util.Arrays;
-import java.util.Iterator;
 
 public abstract class AbstractEmbedderTestCase
     extends PlexusTestCase
@@ -39,8 +30,6 @@ public abstract class AbstractEmbedderTestCase
         throws Exception
     {
         super.setUp();
-
-        System.out.println( ">>> " + getBasedir() );
         
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 
