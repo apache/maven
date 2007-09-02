fc05e3e050f11c48f6858a06baeb462ae2ced809
Jason van Zyl <jvanzyl@apache.org>
2007-09-02 00:02:23 +0000

    o a project to test the resolution of projects when they are not installed in the local repository. this makes sure that   IDE integration works for newly created projects that have inter-module dependencies.
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@571905 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-embedder/src/test/projects/no-artifact-in-repository-test/child-1/src/main/java/org/apache/maven/test/App.java
@@ -0,0 +1,13 @@
+package org.apache.maven.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
