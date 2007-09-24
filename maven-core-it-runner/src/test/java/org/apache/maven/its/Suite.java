9253ec60badec321e864c171bb82ca3c8b6f6943
Brett Leslie Porter <brett@apache.org>
2007-09-24 01:31:22 +0000

    utilise the bundled integration tests and add an optional module to be able to run against the just-built Maven
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@578617 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-core-it-runner/src/test/java/org/apache/maven/its/Suite.java
@@ -0,0 +1,16 @@
+package org.apache.maven.its;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import org.apache.maven.integrationtests.IntegrationTestSuite;
+import org.apache.maven.it.VerificationException;
+
+public class Suite
+    extends TestCase
+{
+    public static Test suite()
+        throws VerificationException
+    {
+        return IntegrationTestSuite.suite();
+    }
+}
