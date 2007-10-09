975862e3533a8322be46477f7ef29e77f7a0d7bc
Jason van Zyl <jvanzyl@apache.org>
2007-10-09 00:46:28 +0000

    test project for the missing parent pom
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@583031 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-embedder/src/test/projects/isolated-project-getting-parent-pom/project/src/main/java/org/codehaus/m2eclipse/App.java
@@ -0,0 +1,13 @@
+package org.codehaus.m2eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
