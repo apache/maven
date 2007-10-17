572a1a4be1d2483786ab0263eb1f6232d5c3aad7
Jason van Zyl <jvanzyl@apache.org>
2007-10-17 18:14:40 +0000

    o a test for using resources that plugins need from dependencies and not extensions. So the practice of using extensions for this purpose   will not be allowed for 2.1 and we can actually deprecate this in 2.0.x. This is simply a bad practice, if a plugin needs something   the plugin should specify that.
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@585617 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-integration-tests/src/test/resources/it0129-resourcesForAPluginProvidedAsAPluginDependency/test-plugin/src/main/java/org/apache/maven/plugin/It0129Mojo.java
@@ -0,0 +1,31 @@
+package org.apache.maven.plugin;
+
+import java.net.URL;
+
+/**
+ * @author <a href="mailto:brianf@apache.org">Brian Fox</a>
+ * @goal test
+ * @phase verify
+ */
+public class It0129Mojo
+    extends AbstractMojo
+{
+
+    public void execute()
+        throws MojoExecutionException, MojoFailureException
+    {
+        ClassLoader cl = this.getClass().getClassLoader();
+        URL url = cl.getResource( "it0129_rule_set.xml" );
+        
+        if (url != null)
+        {
+            this.getLog().info( "Found Url: "+ url.getFile() );
+        }
+        else
+        {
+            throw new MojoExecutionException("Can't find it0129_rule_set.xml on classpath!!");
+        }
+        
+    }
+
+}
