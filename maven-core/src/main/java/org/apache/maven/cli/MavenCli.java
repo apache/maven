657a57ea79429f9a20dcac82ae74b1482d4cd127
Carlos Sanchez Gonzalez <carlos@apache.org>
2007-10-23 23:01:40 +0000

    Restore backwards compatibility readding static OS_* vars. Merged rev# 587684 from trunk
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/branches/maven-2.0.x@587686 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/main/java/org/apache/maven/cli/MavenCli.java
+++ maven-core/src/main/java/org/apache/maven/cli/MavenCli.java
@@ -72,8 +72,17 @@ import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
  */
 public class MavenCli
 {
+    /** @deprecated use {@link Os#OS_NAME} */
+    public static final String OS_NAME = Os.OS_NAME;
+
+    /** @deprecated use {@link Os#OS_ARCH} */
+    public static final String OS_ARCH = Os.OS_ARCH;
+
+    /** @deprecated use {@link Os#OS_VERSION} */
+    public static final String OS_VERSION = Os.OS_VERSION;
+
     private static Embedder embedder;
-   
+
     /**
      * @noinspection ConfusingMainMethod
      */
