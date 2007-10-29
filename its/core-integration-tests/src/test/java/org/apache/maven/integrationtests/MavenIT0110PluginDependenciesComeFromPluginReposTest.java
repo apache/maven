581d7ff407fd27373944d962e984b4951d3e41a2
Brett Porter <brett@apache.org>
2007-10-29 12:22:32 +0000

    make test work on an environment that locks down external repositories from settings
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@589597 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0110PluginDependenciesComeFromPluginReposTest.java
+++ its/core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0110PluginDependenciesComeFromPluginReposTest.java
@@ -4,6 +4,8 @@ import org.apache.maven.it.Verifier;
 import org.apache.maven.it.util.ResourceExtractor;
 
 import java.io.File;
+import java.util.List;
+import java.util.ArrayList;
 
 public class MavenIT0110PluginDependenciesComeFromPluginReposTest
     extends AbstractMavenIntegrationTestCase
@@ -14,6 +16,9 @@ public class MavenIT0110PluginDependenciesComeFromPluginReposTest
         File testDir =
             ResourceExtractor.simpleExtractResources( getClass(), "/it0110-pluginDependenciesComeFromPluginRepos" );
         Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        List cliOptions = new ArrayList();
+        cliOptions.add( "--settings settings.xml" );
+        verifier.setCliOptions( cliOptions );
         verifier.deleteArtifact( "org.apache.maven.it", "mail", "1.3.2", "jar" );        
         verifier.executeGoal( "clean" );
         verifier.verifyErrorFreeLog();
