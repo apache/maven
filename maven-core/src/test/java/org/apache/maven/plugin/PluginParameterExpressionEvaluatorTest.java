40c84ecb795b4772ef49c585d0dabd5fb25da3af
John Dennis Casey <jdcasey@apache.org>
2007-12-18 07:58:45 +0000

    Fixing for windows users everywhere.
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@605125 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/test/java/org/apache/maven/plugin/PluginParameterExpressionEvaluatorTest.java
+++ maven-core/src/test/java/org/apache/maven/plugin/PluginParameterExpressionEvaluatorTest.java
@@ -261,7 +261,7 @@ public class PluginParameterExpressionEvaluatorTest
 
         Object value = expressionEvaluator.evaluate( "${project.build.directory}" + FS + "${project.build.finalName}" );
 
-        assertEquals( "expected-directory/expected-finalName", value );
+        assertEquals( "expected-directory" + File.separatorChar + "expected-finalName", value );
     }
 
     public void testShouldExtractPluginArtifacts()
