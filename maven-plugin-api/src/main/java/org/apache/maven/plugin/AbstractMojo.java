c5cca1fabb6464b39bb4af38b464df8fdf4b279c
Vincent Siveton <vsiveton@apache.org>
2008-01-15 04:17:35 +0000

    MNG-3273: Point out known pitfalls when developing plugins Submitted by: Benjamin Bentmann Reviewed by: Vincent Siveton
    o applied
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/branches/maven-2.0.x@612012 13f79535-47bb-0310-9956-ffa450edef68

--- maven-plugin-api/src/main/java/org/apache/maven/plugin/AbstractMojo.java
+++ maven-plugin-api/src/main/java/org/apache/maven/plugin/AbstractMojo.java
@@ -159,8 +159,15 @@ public abstract class AbstractMojo
     }
 
     /**
-     * By default, return a <code>SystemStreamLog</code> logger.
-     *
+     * Returns the logger that has been injected into this mojo. If no logger has been setup yet, a <code>SystemStreamLog</code>
+     * logger will be created and returned.
+     * <br/><br/>
+     * <strong>Note:</strong>
+     * The logger returned by this method must not be cached in an instance field during the construction of the mojo.
+     * This would cause the mojo to use a wrongly configured default logger when being run by Maven. The proper logger
+     * gets injected by the Plexus container <em>after</em> the mojo has been constructed. Therefore, simply call this
+     * method directly whenever you need the logger, it is fast enough and needs no caching.
+     * 
      * @see org.apache.maven.plugin.Mojo#getLog()
      */
     public Log getLog()
