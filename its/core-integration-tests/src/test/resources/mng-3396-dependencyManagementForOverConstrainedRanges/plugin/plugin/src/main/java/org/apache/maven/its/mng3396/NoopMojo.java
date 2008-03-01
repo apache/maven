78f359ffbf3740fc95d8adee70fe10fd49bb9b13
Carlos Sanchez Gonzalez <carlos@apache.org>
2008-03-01 22:15:23 +0000

    [MNG-3410] Fix the integration test
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@632664 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-integration-tests/src/test/resources/mng-3396-dependencyManagementForOverConstrainedRanges/plugin/plugin/src/main/java/org/apache/maven/its/mng3396/NoopMojo.java
@@ -0,0 +1,35 @@
+package org.apache.maven.its.mng3396;
+
+/*
+ * Copyright 2008 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * @goal noop
+ * @phase compile
+ * @requiresDependencyResolution test
+ */
+public class NoopMojo
+    extends AbstractMojo
+{
+
+    public void execute()
+        throws MojoExecutionException, MojoFailureException
+    {
+    }
+
+}
