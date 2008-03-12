652f2f7a18ac1e5a1c88b30019f8445676d41bce
John Dennis Casey <jdcasey@apache.org>
2008-03-12 19:17:17 +0000

    [MNG-3284] Enabling the integration test for this issue.
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@636462 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-integration-tests/src/test/java/org/apache/maven/integrationtests/IntegrationTestSuite.java
+++ its/core-integration-tests/src/test/java/org/apache/maven/integrationtests/IntegrationTestSuite.java
@@ -184,10 +184,8 @@ public class IntegrationTestSuite
         suite.addTestSuite( MavenITmng3394POMPluginVersionDominanceTest.class );
         suite.addTestSuite( MavenITmng2861RelocationsAndRanges.class );
         suite.addTestSuite( MavenITmng2972OverridePluginDependency.class );
-        
-        //not fixed in the code yet. Test is correct.
-        //suite.addTestSuite( MavenITmng3284UsingCachedPluginsTest.class );
-        
+        suite.addTestSuite( MavenITmng3284UsingCachedPluginsTest.class );
+
         // ----------------------------------------------------------------------------------------------------
         // Tests that need to be fixed.
         // ----------------------------------------------------------------------------------------------------
