aac4eea1c276c57b4c88d1a4809dad52a9c597dd
John Dennis Casey <jdcasey@apache.org>
2008-03-17 19:16:14 +0000

    [MNG-3284] Re-committing this, as I have 100% IT success here...let's see what the CI server says.
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/branches/maven-2.0.x@638034 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/main/java/org/apache/maven/plugin/MavenPluginCollector.java
+++ maven-core/src/main/java/org/apache/maven/plugin/MavenPluginCollector.java
@@ -21,47 +21,81 @@ package org.apache.maven.plugin;
 
 import org.apache.maven.model.Plugin;
 import org.apache.maven.plugin.descriptor.PluginDescriptor;
+import org.apache.maven.plugin.identifier.PluginCoordinate;
+import org.apache.maven.plugin.identifier.PluginIdentifier;
 import org.codehaus.plexus.component.discovery.ComponentDiscoveryEvent;
 import org.codehaus.plexus.component.discovery.ComponentDiscoveryListener;
 import org.codehaus.plexus.component.repository.ComponentSetDescriptor;
 import org.codehaus.plexus.logging.AbstractLogEnabled;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * This class contains references to the plugins that have been loaded,
+ * indexed in a way so that they can be looked up.
+ * 
+ * It listens for new plugins being loaded, and adds them into the index
+ * as this happens.
+ * 
+ */
 public class MavenPluginCollector
     extends AbstractLogEnabled
     implements ComponentDiscoveryListener
 {
-
-    private Set pluginsInProcess = new HashSet();
-
-    private Map pluginDescriptors = new HashMap();
-
+    /**
+     * A 1st-past-the post list of prefixes to descriptors
+     * Map {prefix:String -> PluginDescriptor}
+     */
     private Map pluginIdsByPrefix = new HashMap();
 
+    /**
+     * Map {PluginCoordinate -> PluginDescriptor}}
+     */
+    private Map pluginDescriptors = new HashMap();
+
+    /**
+     * Keep track of the order that particular plugins have been
+     * seen, in case a move to a non 1st past the post for 
+     * descriptors. 
+     * 
+     * Map {PluginIdentifier -> List {PluginDescriptor}}
+     */
+    private Map pluginDeclarations = new HashMap();
+    
     // ----------------------------------------------------------------------
     // Mojo discovery
     // ----------------------------------------------------------------------
     public void componentDiscovered( ComponentDiscoveryEvent event )
-    {
+    {      
         ComponentSetDescriptor componentSetDescriptor = event.getComponentSetDescriptor();
-
+        
         if ( componentSetDescriptor instanceof PluginDescriptor )
         {
             PluginDescriptor pluginDescriptor = (PluginDescriptor) componentSetDescriptor;
             
             // TODO: see comment in getPluginDescriptor
-            String key = Plugin.constructKey( pluginDescriptor.getGroupId(), pluginDescriptor.getArtifactId() );
+                        
+            PluginCoordinate coordinate = new PluginCoordinate( pluginDescriptor.getGroupId(), pluginDescriptor.getArtifactId(), pluginDescriptor.getVersion() );
+            PluginIdentifier identifier = coordinate.getIdentifier();
             
-            if ( !pluginsInProcess.contains( key ) )
+        	// Don't overwrite any existing found descriptor, as things
+        	// like the ClassRealm that has been constructed will disappear,
+        	// causing reactor builds to fail.
+        	if( !pluginDescriptors.containsKey(coordinate))
+        	{
+        		pluginDescriptors.put( coordinate, pluginDescriptor );
+        	} 
+           
+            // Store that we've seen the plugin by prefix, for the 1st past the post
+            if( !pluginDeclarations.containsKey(identifier) )
             {
-                pluginsInProcess.add( key );
-
-                pluginDescriptors.put( key, pluginDescriptor );
+            	pluginDeclarations.put(identifier, new ArrayList());
 
                 // TODO: throw an (not runtime) exception if there is a prefix overlap - means doing so elsewhere
                 // we also need to deal with multiple versions somehow - currently, first wins
@@ -70,6 +104,7 @@ public class MavenPluginCollector
                     pluginIdsByPrefix.put( pluginDescriptor.getGoalPrefix(), pluginDescriptor );
                 }
             }
+            ((List)pluginDeclarations.get(identifier)).add(pluginDescriptor);
         }
     }
 
@@ -78,15 +113,34 @@ public class MavenPluginCollector
         // TODO: include version, but can't do this in the plugin manager as it is not resolved to the right version
         // at that point. Instead, move the duplication check to the artifact container, or store it locally based on
         // the unresolved version?
-        return (PluginDescriptor) pluginDescriptors.get( plugin.getKey() );
+      
+    	// Has the plugin got a version specified (think that it ought to)
+    	PluginCoordinate coordinate = new PluginCoordinate(plugin);
+    	PluginIdentifier pluginIdentifier = coordinate.getIdentifier();
+    	
+    	if( plugin.getVersion() != null )
+        {
+    		PluginDescriptor pluginDescriptor = (PluginDescriptor)pluginDescriptors.get(coordinate);
+    		if( pluginDescriptor != null )
+    		{
+    			return pluginDescriptor;
+    		}
+        }
+    	
+    	getLogger().error("No version for " + pluginIdentifier);
+    	
+    	return null;
     }
 
     public boolean isPluginInstalled( Plugin plugin )
-    {
+    {      
         // TODO: see comment in getPluginDescriptor
-        return pluginDescriptors.containsKey( plugin.getKey() );
+    
+        PluginCoordinate coordinate = new PluginCoordinate(plugin);
+        
+        return pluginDescriptors.containsKey(coordinate);
     }
-
+    
     public PluginDescriptor getPluginDescriptorForPrefix( String prefix )
     {
         return (PluginDescriptor) pluginIdsByPrefix.get( prefix );
@@ -94,8 +148,11 @@ public class MavenPluginCollector
 
     public void flushPluginDescriptor( Plugin plugin )
     {
-        pluginsInProcess.remove( plugin.getKey() );
-        pluginDescriptors.remove( plugin.getKey() );
+        PluginCoordinate coordinate = new PluginCoordinate(plugin);
+        PluginIdentifier identifier = coordinate.getIdentifier();
+                  
+        pluginDeclarations.remove( identifier );
+        pluginDescriptors.remove ( coordinate );
         
         for ( Iterator it = pluginIdsByPrefix.entrySet().iterator(); it.hasNext(); )
         {
