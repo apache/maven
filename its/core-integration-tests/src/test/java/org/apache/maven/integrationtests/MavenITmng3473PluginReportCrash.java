979d9db01bdfbfbe510f0cb91907b11902361143
Brian E Fox <brianf@apache.org>
2008-03-20 17:41:07 +0000

    MNG-3473: adding an IT
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@639383 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenITmng3473PluginReportCrash.java
@@ -0,0 +1,32 @@
+package org.apache.maven.integrationtests;
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+
+/**
+ * Tests that the PluginDescriptor.getArtifacts() call returns all of the dependencies of the plugin,
+ * not just those that made it past the filter excluding Maven's core artifacts.
+ */
+public class MavenITmng3473PluginReportCrash
+    extends AbstractMavenIntegrationTestCase
+{
+    public void testitMNG3473 ()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-3473PluginReportCrash" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+
+        verifier.executeGoal( "org.apache.maven.plugin:maven-plugin-plugin:2.3" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        
+        verifier = new Verifier( testDir.getAbsolutePath() );
+
+        verifier.executeGoal( "org.apache.maven.plugin:maven-plugin-plugin:2.4" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+    }
+}
