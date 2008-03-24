e7087db955b0b55d0ca3ad9427f6919f334b98fd
Herve Boutemy <hboutemy@apache.org>
2008-03-24 14:34:05 +0000

    avoid script to continue if one step fails
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@640425 13f79535-47bb-0310-9956-ffa450edef68

--- its/run-its.sh
+++ its/run-its.sh
@@ -12,19 +12,18 @@ mvn -N install
   cd core-integration-testing-plugins
   mvn install
   ret=$?; if [ $ret != 0 ]; then echo "Failed to install IT plugins" && exit $ret; fi
-)
-
-( 
+) && ( 
   cd core-integration-testing-support
   mvn install 
   ret=$?; if [ $ret != 0 ]; then echo "Failed to install IT support artifacts." && exit $ret; fi
-)
-
-( 
+) && ( 
   cd core-integration-tests
   mvn clean test
   ret=$?; if [ $ret != 0 ]; then exit $ret; fi
 )
+ret=$?
 
 rm -rf "${LOCAL_REPO}"
 mv "${LOCAL_REPO}.its" "${LOCAL_REPO}"
+
+exit $ret
