ca6cda7888660e694cb07c0615e59e52dcf99dfe
Brian E Fox <brianf@apache.org>
2008-03-27 22:08:20 +0000

    enhanced the IT to validate both positive and negative expectations using 2.4 as failure and 2.4.1 as pass
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@642000 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenITmng3473PluginReportCrash.java
+++ its/core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenITmng3473PluginReportCrash.java
@@ -1,9 +1,13 @@
 package org.apache.maven.integrationtests;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.maven.artifact.versioning.InvalidVersionSpecificationException;
+import org.apache.maven.it.VerificationException;
 import org.apache.maven.it.Verifier;
 import org.apache.maven.it.util.ResourceExtractor;
-
-import java.io.File;
 
 /**
  * Tests that the PluginDescriptor.getArtifacts() call returns all of the dependencies of the plugin,
@@ -12,6 +16,11 @@ import java.io.File;
 public class MavenITmng3473PluginReportCrash
     extends AbstractMavenIntegrationTestCase
 {
+    public MavenITmng3473PluginReportCrash()
+        throws InvalidVersionSpecificationException
+    {
+        super( "(2.0.8,)" ); // >2.0.8
+    }
     public void testitMNG3473 ()
         throws Exception
     {
@@ -19,12 +28,34 @@ public class MavenITmng3473PluginReportCrash
 
         Verifier verifier = new Verifier( testDir.getAbsolutePath() );
 
+
         verifier.executeGoal( "install" );
         verifier.verifyErrorFreeLog();
         verifier.resetStreams();
-
+        
+        List cliOptions = new ArrayList();
+        cliOptions.add( "-Dplugin.version=2.4.1" );
+        verifier.setCliOptions( cliOptions );
+        
+        //should succeed with 2.4.1
         verifier.executeGoal( "site" );
         verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        
+        //should fail with 2.4
+        cliOptions.clear();
+        cliOptions.add( "-Dplugin.version=2.4" );
+        verifier.setCliOptions( cliOptions );
+        
+        try
+        {
+          verifier.executeGoal( "site" );
+        }
+        catch (VerificationException e)
+        {
+          //expected this but don't require it cause some os's don't return the correct error code   
+        }
+        verifier.verifyTextInLog( "org/apache/maven/doxia/module/site/manager/SiteModuleNotFoundException" );
         verifier.resetStreams();
     }
 }
