1746cf2d43014c66604db728c1110de5c818e9f7
Herve Boutemy <hboutemy@apache.org>
2008-04-14 17:35:57 +0000

    [MNG-3509] added default locale and platform encoding to "mvn -v" output (merged from trunk r647626)
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/branches/maven-2.0.x@647917 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/main/java/org/apache/maven/cli/MavenCli.java
+++ maven-core/src/main/java/org/apache/maven/cli/MavenCli.java
@@ -64,6 +64,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Properties;
 import java.util.StringTokenizer;
 import java.util.Map.Entry;
