cc68735fcc6f2bab16125de0ed9ea2618b9fda5a
Jason van Zyl <jvanzyl@apache.org>
2008-04-16 01:19:28 +0000

    o we don't need the scripts as everyone is using the same embedder now
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@648481 13f79535-47bb-0310-9956-ffa450edef68

--- maven-embedder/embedder-idea.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# At this point I've already tested the embedder. This is just an easy way for the JetBrains folks
-# to create the embedder they need for integration.
-
-mvn -Dmaven.test.skip=true -Pidea clean package
