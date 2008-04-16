73672a558d73ea572ecd2f0e1d5897b9c58a5ee9
Jason van Zyl <jvanzyl@apache.org>
2008-04-16 05:31:24 +0000

    o add a standard main method that the ant-based bootstrap can hook into
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/branches/maven-2.0.x@648525 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/main/java/org/apache/maven/cli/MavenCli.java
+++ maven-core/src/main/java/org/apache/maven/cli/MavenCli.java
@@ -87,6 +87,15 @@ public class MavenCli
 
     private static Embedder embedder;
 
+    public static void main( String[] args )
+    {
+        ClassWorld classWorld = new ClassWorld( "plexus.core", Thread.currentThread().getContextClassLoader() );
+
+        int result = main( args, classWorld );
+
+        System.exit( result );
+    }
+
     /**
      * @noinspection ConfusingMainMethod
      */
