05f8e32c888370cdf6d82bcb16bb30a0f0a8499e
John Dennis Casey <jdcasey@apache.org>
2008-07-09 02:26:18 +0000

    [MNG-3380] Adding the integration test.
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@675088 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-integration-tests/src/test/java/org/apache/maven/integrationtests/IntegrationTestSuite.java
+++ its/core-integration-tests/src/test/java/org/apache/maven/integrationtests/IntegrationTestSuite.java
@@ -79,6 +79,7 @@ public class IntegrationTestSuite
         suite.addTestSuite( MavenITmng3426PluginsClasspathOverrideTest.class );
         suite.addTestSuite( MavenITmng3396DependencyManagementForOverConstrainedRanges.class );
         suite.addTestSuite( MavenITmng3394POMPluginVersionDominanceTest.class );
+        suite.addTestSuite( MavenITmng3380ManagedRelocatedTransdepsTest.class );
         suite.addTestSuite( MavenITmng3372DirectInvocationOfPlugins.class );
         suite.addTestSuite( MavenITmng3355TranslatedPathInterpolationTest.class );
         suite.addTestSuite( MavenITmng3341MetadataUpdatedFromDeploymentRepositoryTest.class );
