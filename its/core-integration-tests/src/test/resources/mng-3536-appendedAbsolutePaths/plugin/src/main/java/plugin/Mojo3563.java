b26ae6b9f6fb1d48f721973b2fb8d5df686f357e
John Dennis Casey <jdcasey@apache.org>
2008-07-16 19:35:14 +0000

    [MNG-3536] Modifying test plugin and test project to check paths injected into plugin configuration as well.
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@677395 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-integration-tests/src/test/resources/mng-3536-appendedAbsolutePaths/plugin/src/main/java/plugin/Mojo3563.java
+++ its/core-integration-tests/src/test/resources/mng-3536-appendedAbsolutePaths/plugin/src/main/java/plugin/Mojo3563.java
@@ -5,20 +5,33 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.model.Model;
+import org.codehaus.plexus.util.IOUtil;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
- *
  * @goal validate
  * @phase validate
  */
-public class Mojo3563 extends AbstractMojo {
+public class Mojo3563
+    extends AbstractMojo
+{
 
     /**
-     * @parameter expression="${project}"
+     * @parameter default-value="${project}"
      */
     private MavenProject project;
+
+    /**
+     * @parameter
+     * @required
+     */
+    private File foo;
 
     public void execute() throws MojoExecutionException, MojoFailureException {
         Model model = project.getModel();
@@ -32,6 +45,46 @@ public class Mojo3563 extends AbstractMojo {
             throw new MojoExecutionException("Test file does not exist: File = " + testFile.getAbsolutePath() + "Property = " + property);
         }
         getLog().info("Property = " + property);
-
+        
+        File f = new File( project.getBuild().getOutputDirectory(), "foo.txt" );
+        getLog().info( "Creating test file using project.getBuild().getDirectory(): " + f );
+        
+        String testValue = "" + System.currentTimeMillis();
+        
+        FileWriter w = null;
+        try
+        {
+            f.getParentFile().mkdirs();
+            w = new FileWriter( f );
+            
+            w.write( testValue );
+        }
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException( "Failed to write test file.", e );
+        }
+        finally
+        {
+            IOUtil.close( w );
+        }
+        
+        getLog().info( "Attempting to read test file using path from plugin parameter: " + foo );
+        BufferedReader r = null;
+        try
+        {
+            r = new BufferedReader( new FileReader( foo ) );
+            if ( !testValue.equals( r.readLine() ) )
+            {
+                throw new MojoExecutionException( "Test file: " + foo + " has wrong contents." );
+            }
+        }
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException( "Failed to read test file.", e );
+        }
+        finally
+        {
+            IOUtil.close( r );
+        }
     }
 }
