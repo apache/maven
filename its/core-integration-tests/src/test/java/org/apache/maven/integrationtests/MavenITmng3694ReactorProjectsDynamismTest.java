f03afb770316a1bb6ab156c99ab0b189fcd9bf06
John Dennis Casey <jdcasey@apache.org>
2008-08-01 20:38:09 +0000

    [MNG-3694] integration test for this issue.
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@681815 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenITmng3694ReactorProjectsDynamismTest.java
@@ -0,0 +1,40 @@
+package org.apache.maven.integrationtests;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.maven.integrationtests.AbstractMavenIntegrationTestCase;
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+/**
+ * Verify that any plugin injecting reactorProjects gets project instances that
+ * have their concrete state calculated.
+ * 
+ * @author jdcasey
+ */
+public class MavenITmng3694ReactorProjectsDynamismTest
+    extends AbstractMavenIntegrationTestCase
+{
+    public void testitMNG3694 ()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-3694-reactorProjectsDynamism" );
+
+        File pluginDir = new File( testDir, "maven-mng3694-plugin" );
+        File projectDir = new File( testDir, "projects" );
+        
+        Verifier verifier = new Verifier( pluginDir.getAbsolutePath() );
+
+        verifier.executeGoal( "install" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        
+        verifier = new Verifier( projectDir.getAbsolutePath() );
+
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+    }
+}
