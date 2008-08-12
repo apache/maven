0911fda7d208109191fb75d63ae63d71679599d0
John Dennis Casey <jdcasey@apache.org>
2008-08-12 03:19:23 +0000

    [MNG-3703] Calculate the concrete state of execution projects after a forked lifecycle completes, so paths, etc. are available to processing in the forking mojo. Including an integration test to verify this.
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@685027 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-integration-tests/src/test/resources/mng-3703-executionProjectRelativePaths/maven-mng3703-plugin/src/main/java/jar/AbstractCheckMojo.java
@@ -0,0 +1,116 @@
+package jar;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+
+import com.sun.tools.jdi.LinkedHashMap;
+
+public abstract class AbstractCheckMojo
+    extends AbstractMojo
+{
+    
+    protected static boolean forkHasRun = false;
+    
+    /**
+     * @parameter default-value="${project}"
+     * @required
+     * @readonly
+     */
+    private MavenProject project;
+    
+    /**
+     * @parameter default-value="${executedProject}"
+     * @required
+     * @readonly
+     */
+    private MavenProject executionProject;
+
+    public void execute()
+        throws MojoExecutionException
+    {
+        if ( getMainProject().getBasedir() == null )
+        {
+            throw new MojoExecutionException( "Basedir is null on the main project instance." );
+        }
+        
+        if ( getTestProject().getBasedir() == null )
+        {
+            throw new MojoExecutionException( "Basedir is null on the " + getTestProjectLabel() + " instance." );
+        }
+        
+        String executionBasedir = getTestProject().getBasedir().getAbsolutePath();
+        
+        Map failedPaths = new LinkedHashMap();
+        
+        checkListOfPaths( getTestProject().getCompileSourceRoots(), executionBasedir, "compileSourceRoots", failedPaths );
+        checkListOfPaths( getTestProject().getTestCompileSourceRoots(), executionBasedir, "testCompileSourceRoots", failedPaths );
+        checkListOfPaths( getTestProject().getScriptSourceRoots(), executionBasedir, "scriptSourceRoots", failedPaths );
+        
+        
+        if ( !failedPaths.isEmpty() )
+        {
+            StringBuffer buffer = new StringBuffer();
+            buffer.append( "The following paths were relative (should have been absolute):" );
+            for ( Iterator it = failedPaths.entrySet().iterator(); it.hasNext(); )
+            {
+                Map.Entry entry = (Map.Entry) it.next();
+                
+                buffer.append( "\n-  " ).append( entry.getKey() ).append( ": '" ).append( entry.getValue() ).append( "'" );
+            }
+            
+            throw new MojoExecutionException( buffer.toString() );
+        }
+        
+        forkHasRun = true;
+    }
+    
+    protected MavenProject getMainProject()
+    {
+        return project;
+    }
+    
+    protected MavenProject getExecutionProject()
+    {
+        return executionProject;
+    }
+    
+    protected abstract MavenProject getTestProject();
+    
+    protected abstract String getTestProjectLabel();
+
+    private void checkListOfPaths( List paths, String base, String label, Map failedPaths )
+    {
+        if ( paths != null && !paths.isEmpty() )
+        {
+            for ( int i = 0; i < paths.size(); i++ )
+            {
+                String root = (String) paths.get( i );
+                if ( !root.startsWith( base ) )
+                {
+                    failedPaths.put( label + "[" + i + "]", root );
+                }
+            }
+        }
+    }
+}
