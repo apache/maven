925c53b6b7ed4012e436fe972c27207ee232227c
John Dennis Casey <jdcasey@apache.org>
2008-08-25 17:13:39 +0000

    [MNG-3724] Adding integration test.
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@688780 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-integration-tests/src/test/resources/mng-3724-executionProjectSync/maven-mng3724-plugin/src/main/java/jar/AbstractMng2734Mojo.java
@@ -0,0 +1,23 @@
+package jar;
+
+import java.io.File;
+
+import org.apache.maven.plugin.AbstractMojo;
+
+public abstract class AbstractMng2734Mojo
+    extends AbstractMojo
+{
+
+    /**
+     * Location of the file.
+     * @parameter expression="${project.build.directory}/generated/src/main/java"
+     * @required
+     */
+    private File generatorTargetDir;
+    
+    protected final File getGeneratorTargetDir()
+    {
+        return generatorTargetDir;
+    }
+
+}
