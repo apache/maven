f2853d62524a297a9bcb12ad539ba3681b1127dc
Ralph Goers <rgoers@apache.org>
2008-08-29 05:25:50 +0000

    add test projects for MNG-0624
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@690110 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-integration-tests/src/test/resources/mng-0624/dependencyManagement/child/grandchild1/src/main/java/org/apache/maven/mng624/HelloWorld.java
@@ -0,0 +1,7 @@
+package org.apache.maven.mng624;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello " + World.getWorld());
+  }
+}
---
+++ its/core-integration-tests/src/test/resources/mng-0624/dependencyManagement/child/grandchild2/src/main/java/org/apache/maven/mng624/World.java
@@ -0,0 +1,5 @@
+package org.apache.maven.mng624;
+
+public class World {
+  public static String getWorld() { return "earth"; }
+}
