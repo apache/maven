bfd34180340ea64f6322df36725aa251aa10c803
Benjamin Bentmann <bentmann@apache.org>
2008-09-19 22:12:11 +0000

    o Decoupled it0060 from the Compiler Plugin
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@697267 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0060Test.java
+++ its/core-integration-tests/src/test/java/org/apache/maven/integrationtests/MavenIT0060Test.java
@@ -11,19 +11,16 @@ public class MavenIT0060Test
 
     /**
      * Test aggregation of list configuration items when using
-     * 'combine.children=append' attribute. Specifically, merge the list of
-     * excludes for the testCompile mojo.
+     * 'combine.children=append' attribute.
      */
     public void testit0060()
         throws Exception
     {
         File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/it0060" );
         Verifier verifier = new Verifier( testDir.getAbsolutePath() );
-        verifier.executeGoal( "test" );
-        verifier.assertFilePresent( "subproject/target/classes/org/apache/maven/it0060/Person.class" );
-        verifier.assertFilePresent( "subproject/target/test-classes/org/apache/maven/it0060/PersonTest.class" );
-        verifier.assertFileNotPresent( "subproject/target/test-classes/org/apache/maven/it0060/PersonTwoTest.class" );
-        verifier.assertFileNotPresent( "subproject/target/test-classes/org/apache/maven/it0060/PersonThreeTest.class" );
+        verifier.executeGoal( "initialize" );
+        verifier.assertFilePresent( "parent.txt" );
+        verifier.assertFilePresent( "child.txt" );
         verifier.verifyErrorFreeLog();
         verifier.resetStreams();
 
