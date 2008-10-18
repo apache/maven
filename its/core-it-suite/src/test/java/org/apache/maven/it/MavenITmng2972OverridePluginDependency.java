db3bb28ce6fbeb641d8a47a1af5238d740abc4f1
Benjamin Bentmann <bentmann@apache.org>
2008-10-18 18:01:46 +0000

    o Strengthened IT to check more aspects of dependency override
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@705900 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng2972OverridePluginDependency.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng2972OverridePluginDependency.java
@@ -19,104 +19,71 @@ package org.apache.maven.it;
  * under the License.
  */
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import junit.framework.Assert;
-
 import org.apache.maven.it.Verifier;
 import org.apache.maven.it.util.FileUtils;
 import org.apache.maven.it.util.ResourceExtractor;
 
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-2972">MNG-2972</a>.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
 public class MavenITmng2972OverridePluginDependency
     extends AbstractMavenIntegrationTestCase
 {
+
     public MavenITmng2972OverridePluginDependency()
     {
         super( "(2.0.8,)" );
     }
 
-    public void testitMNG2972()
+    /**
+     * Verify that a project-level plugin dependency replaces the original dependency from the plugin POM.
+     */
+    public void testitLifecycleInvocation()
         throws Exception
     {
-
-        // The testdir is computed from the location of this
-        // file.
-        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-2972-overridingPluginDependency" );
-
-        Verifier verifier;
-
-        /*
-         * We must first make sure that any artifact created by this test has been removed from the local repository.
-         * Failing to do this could cause unstable test results. Fortunately, the verifier makes it easy to do this.
-         */
-        verifier = new Verifier( testDir.getAbsolutePath() );
-        verifier.deleteArtifact( "org.apache.maven.its.mng2972", "user", "1.0", "jar" );
-        verifier.deleteArtifact( "org.apache.maven.its.mng2972", "mojo", "0.0.1-SNAPSHOT", "jar" );
-        verifier.deleteArtifact( "org.apache.maven.its.mng2972", "dep", "1.0", "jar" );
-        verifier.deleteArtifact( "org.apache.maven.its.mng2972", "dep", "2.0", "jar" );
-
-        verifier = new Verifier( new File( testDir.getAbsolutePath(), "dep1" ).getAbsolutePath() );
-        verifier.executeGoal( "install" );
-        verifier.verifyErrorFreeLog();
-
-        verifier = new Verifier( new File( testDir.getAbsolutePath(), "dep2" ).getAbsolutePath() );
-        verifier.executeGoal( "install" );
-        verifier.verifyErrorFreeLog();
-
-        verifier = new Verifier( new File( testDir.getAbsolutePath(), "mojo" ).getAbsolutePath() );
-        verifier.executeGoal( "install" );
-        verifier.verifyErrorFreeLog();
-
-        verifier = new Verifier( new File( testDir.getAbsolutePath(), "user" ).getAbsolutePath() );
-        verifier.executeGoal( "validate" );
-        verifier.verifyErrorFreeLog();
-
-        List lines =
-            verifier.loadFile( new File( testDir.getAbsolutePath(), "user" ).getAbsolutePath(), "log.txt", false );
-        int foundVersionOne = 0;
-        int foundVersionTwo = 0;
-        for ( Iterator i = lines.iterator(); i.hasNext(); )
-        {
-
-            String line = (String) i.next();
-            if ( line.indexOf( "MNG-2972-VERSION-1" ) != -1 )
-                foundVersionOne++;
-            if ( line.indexOf( "MNG-2972-VERSION-2" ) != -1 )
-                foundVersionTwo++;
-        }
-
-        verifier.resetStreams();
-
-        Assert.assertEquals( "Should not be using plugin dependency version 1", 0, foundVersionOne );
-        Assert.assertEquals( "Should be using plugin version 2 once.", 1, foundVersionTwo );
-
-        /**
-         * Now try to execute the plugin directly
-         */
-
-        verifier = new Verifier( new File( testDir.getAbsolutePath(), "user" ).getAbsolutePath() );
-        verifier.executeGoal( "org.apache.maven.its.mng2972:mojo:0.0.1-SNAPSHOT:test" );
-        verifier.verifyErrorFreeLog();
-
-        lines = verifier.loadFile( new File( testDir.getAbsolutePath(), "user" ).getAbsolutePath(), "log.txt", false );
-        foundVersionOne = 0;
-        foundVersionTwo = 0;
-        for ( Iterator i = lines.iterator(); i.hasNext(); )
-        {
-
-            String line = (String) i.next();
-            if ( line.indexOf( "MNG-2972-VERSION-1" ) != -1 )
-                foundVersionOne++;
-            if ( line.indexOf( "MNG-2972-VERSION-2" ) != -1 )
-                foundVersionTwo++;
-        }
-
-        verifier.resetStreams();
-
-        Assert.assertEquals( "Should not be using plugin dependency version 1", 0, foundVersionOne );
-        Assert.assertEquals( "Should be using plugin version 2 once.", 1, foundVersionTwo );
+        run( false );
     }
+
+    /**
+     * Verify that a project-level plugin dependency replaces the original dependency from the plugin POM.
+     */
+    public void testitCommandLineInvocation()
+        throws Exception
+    {
+        run( true );
+    }
+
+    private void run( boolean cli )
+        throws Exception
+    {
+        String propFile = cli ? "target/cli.properties" : "target/lifecycle.properties";
+
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-2972" );
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteArtifact( "org.apache.maven.its.plugins.class-loader", "dep-b", "0.2-mng-2972", "jar" );
+        new File( testDir, propFile ).delete();
+        Properties sysProps = new Properties();
+        sysProps.setProperty( "clsldr.pluginClassLoaderOutput", propFile );
+        verifier.setSystemProperties( sysProps );
+        verifier.executeGoal( cli ? "org.apache.maven.its.plugins:maven-it-plugin-class-loader:2.1-SNAPSHOT:load" : "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+        FileUtils.rename( new File( testDir, "log.txt"), new File( testDir, cli ? "log2.txt" : "log1.txt" ) );
+
+        Properties pclProps = verifier.loadProperties( propFile );
+        assertNotNull( pclProps.getProperty( "org.apache.maven.its.mng2972.MNG2972" ) );
+        assertNull( pclProps.getProperty( "org.apache.maven.plugin.coreit.ClassA" ) );
+        assertNull( pclProps.getProperty( "org.apache.maven.plugin.coreit.ClassB" ) );
+        assertEquals( "1", pclProps.getProperty( "org/apache/maven/its/mng2972/mng-2972.properties.count" ) );
+        assertEquals( "0", pclProps.getProperty( "org/apache/maven/plugin/coreit/a.properties.count" ) );
+        assertEquals( "0", pclProps.getProperty( "org/apache/maven/plugin/coreit/b.properties.count" ) );
+    }
+
 }
--- its/core-it-suite/src/test/resources/mng-2972-overridingPluginDependency/dep2/src/main/java/its/Dep.java
+++
@@ -1,9 +0,0 @@
-package its;
-
-public class Dep 
-{
-    public static void main()
-    {
-        System.out.println( "MNG-2972-VERSION-2" );
-    }
-}
