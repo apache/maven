64d40573a4638d0d577c8843f987476fd01011e4
Benjamin Bentmann <bentmann@apache.org>
2008-11-01 21:09:09 +0000

    o Decoupled it0083 from production plugins
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@709778 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-it-suite/src/test/java/org/apache/maven/it/MavenIT0083Test.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenIT0083Test.java
@@ -23,32 +23,39 @@ import org.apache.maven.it.Verifier;
 import org.apache.maven.it.util.ResourceExtractor;
 
 import java.io.File;
+import java.util.Collection;
 
 public class MavenIT0083Test
     extends AbstractMavenIntegrationTestCase
 {
 
     /**
-     * Verify that overriding a compile time dependency as provided in a WAR ensures it is not included.
+     * Verify that overriding a transitive compile time dependency as provided in a WAR ensures it is not included.
      */
     public void testit0083()
         throws Exception
     {
         File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/it0083" );
+
         Verifier verifier = new Verifier( testDir.getAbsolutePath() );
-        verifier.executeGoal( "package" );
-        verifier.assertFilePresent( "test-component-a/target/test-component-a-0.1.jar" );
-        verifier.assertFilePresent( "test-component-b/target/test-component-b-0.1.jar" );
-        verifier.assertFilePresent( "test-component-c/target/test-component-c-0.1.war" );
-        verifier.assertFilePresent(
-            "test-component-c/target/test-component-c-0.1.war!/WEB-INF/lib/test-component-b-0.1.jar" );
-        verifier.assertFileNotPresent(
-            "test-component-c/target/test-component-c-0.1/WEB-INF/lib/test-component-a-0.1.jar" );
-        verifier.assertFilePresent(
-            "test-component-c/target/test-component-c-0.1/WEB-INF/lib/test-component-b-0.1.jar" );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.deleteArtifacts( "org.apache.maven.its.it0083" );
+        verifier.executeGoal( "validate" );
         verifier.verifyErrorFreeLog();
         verifier.resetStreams();
 
-    }
-}
+        Collection compileArtifacts = verifier.loadLines( "target/compile.txt", "UTF-8" );
+        assertTrue( compileArtifacts.toString(), 
+            compileArtifacts.contains( "org.apache.maven.its.it0083:direct-dep:jar:0.1" ) );
+        assertTrue( compileArtifacts.toString(), 
+            compileArtifacts.contains( "org.apache.maven.its.it0083:trans-dep:jar:0.1" ) );
 
+        Collection runtimeArtifacts = verifier.loadLines( "target/runtime.txt", "UTF-8" );
+        assertTrue( runtimeArtifacts.toString(), 
+            runtimeArtifacts.contains( "org.apache.maven.its.it0083:direct-dep:jar:0.1" ) );
+        assertFalse( runtimeArtifacts.toString(), 
+            runtimeArtifacts.contains( "org.apache.maven.its.it0083:trans-dep:jar:0.1" ) );
+    }
+
+}
