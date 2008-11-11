132a1e76db4843e74729df870844af3315e9ec04
Benjamin Bentmann <bentmann@apache.org>
2008-11-11 19:35:12 +0000

    [MNG-3833] POM interpolation fails to fully interpolate chain of dependent properties
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@713126 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng3833PomInterpolationDataFlowChainTest.java
@@ -0,0 +1,67 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-3833">MNG-3833</a>.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class MavenITmng3833PomInterpolationDataFlowChainTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng3833PomInterpolationDataFlowChainTest()
+    {
+    }
+
+    /**
+     * Test that POM interpolation fully interpolates all properties in data flow chain, i.e. where property
+     * A depends on property B, and property B depends on property C and so on.
+     */
+    public void testitMNG3833()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-3833" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        Properties props = verifier.loadProperties( "target/interpolated.properties" );
+
+        for ( int i = 0; i < 24; i++ )
+        {
+            String index = ( ( i < 10 ) ? "0" : "" ) + i;
+            assertEquals( "PASSED", props.getProperty( "project.properties.property" + index ) );
+        }
+    }
+
+}
