d47d5249951e44bf7c318e2882e37849592c3445
Benjamin Bentmann <bentmann@apache.org>
2008-11-14 11:51:21 +0000

    [MNG-3839] XML parsing of POM does not reliably coalesce text data
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@713980 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng3839PomParsingCoalesceTextTest.java
@@ -0,0 +1,63 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-3839">MNG-3839</a>.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class MavenITmng3839PomParsingCoalesceTextTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng3839PomParsingCoalesceTextTest()
+    {
+    }
+
+    /**
+     * Test that POM parsing properly coalesces text data.
+     */
+    public void testitMNG3833()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-3839" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        Properties props = verifier.loadProperties( "target/xml.properties" );
+        assertEquals( "A  Test  Project Property", props.getProperty( "project.properties.prop0" ) );
+        assertEquals( "That's a test!", props.getProperty( "project.properties.prop1" ) );
+        assertEquals( 32 * 1024, props.getProperty( "project.properties.prop2" ).trim().replaceAll( "[\n\r]", "" ).length() );
+    }
+
+}
