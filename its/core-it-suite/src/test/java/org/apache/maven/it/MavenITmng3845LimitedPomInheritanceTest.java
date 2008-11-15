3a9dc16e26cc0c860efb6828f0c2ed3894c71eca
Benjamin Bentmann <bentmann@apache.org>
2008-11-15 13:26:47 +0000

    [MNG-3845] Unintended inheritance of parent elements overriden by children
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@714252 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng3845LimitedPomInheritanceTest.java
@@ -0,0 +1,70 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-3845">MNG-3845</a>.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class MavenITmng3845LimitedPomInheritanceTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng3845LimitedPomInheritanceTest()
+    {
+    }
+
+    /**
+     * Test that inheritance is all-or-nothing for certain sub-trees of the POM.
+     */
+    public void testitMNG3845()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-3845" );
+
+        Verifier verifier = new Verifier( new File( testDir, "child" ).getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        Properties props = verifier.loadProperties( "target/pom.properties" );
+        assertEquals( "", props.getProperty( "project.organization.url", "" ) );
+        assertEquals( "", props.getProperty( "project.issueManagement.system", "" ) );
+        assertEquals( "0", props.getProperty( "project.ciManagement.notifiers", "0" ) );
+        assertEquals( "", props.getProperty( "project.distributionManagement.repository.name", "" ) );
+        assertEquals( "true", props.getProperty( "project.distributionManagement.repository.uniqueVersion", "true" ) );
+        assertEquals( "default", props.getProperty( "project.distributionManagement.repository.layout", "default" ) );
+        assertEquals( "", props.getProperty( "project.distributionManagement.snapshotRepository.name", "" ) );
+        assertEquals( "true", props.getProperty( "project.distributionManagement.snapshotRepository.uniqueVersion", "true" ) );
+        assertEquals( "default", props.getProperty( "project.distributionManagement.snapshotRepository.layout", "default" ) );
+        assertEquals( "", props.getProperty( "project.distributionManagement.site.name", "" ) );
+    }
+
+}
