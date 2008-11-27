dac9668fbd45e94f144db961687653e3ab1e5601
Jason van Zyl <jvanzyl@apache.org>
2008-11-27 05:16:20 +0000

    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@721080 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-project-builder/src/test/java/EnforcerPomTest.java
@@ -0,0 +1,10 @@
+import org.apache.maven.shared.model.DomainModel;
+
+public class EnforcerPomTest
+{
+    public void testConstructionOfTheEnforcerPom()
+        throws Exception
+    {
+        DomainModel model = null;
+    }
+}
