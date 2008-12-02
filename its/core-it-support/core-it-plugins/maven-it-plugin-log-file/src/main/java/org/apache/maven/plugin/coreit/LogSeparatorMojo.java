3015a143c7d2eca2a8bf495df590fce2bfd60535
Benjamin Bentmann <bentmann@apache.org>
2008-12-02 17:05:12 +0000

    o Added new IT plugin to verify multi goal executions scenarios
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@722525 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-support/core-it-plugins/maven-it-plugin-log-file/src/main/java/org/apache/maven/plugin/coreit/LogSeparatorMojo.java
@@ -0,0 +1,60 @@
+package org.apache.maven.plugin.coreit;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * Appends a separator line to the log file.
+ * 
+ * @goal log-separator
+ * @phase initialize
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class LogSeparatorMojo
+    extends AbstractLogMojo
+{
+
+    /**
+     * The length of the separator line.
+     * 
+     * @parameter expression="${log.length}" default-value="80"
+     */
+    private int length;
+
+    /**
+     * Runs this mojo.
+     * 
+     * @throws MojoExecutionException If the output file could not be created.
+     */
+    public void execute()
+        throws MojoExecutionException
+    {
+        StringBuffer buffer = new StringBuffer( length );
+        for ( int i = 0; i < length; i++ )
+        {
+            buffer.append( '-' );
+        }
+        append( buffer.toString() );
+    }
+
+}
