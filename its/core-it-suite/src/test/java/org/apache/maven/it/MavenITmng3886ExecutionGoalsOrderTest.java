9666892a5ba38f951fe1f1718fdd1522cea9d8dc
Benjamin Bentmann <bentmann@apache.org>
2008-12-02 17:38:28 +0000

    [MNG-3886] Ordering of goals from a plugin execution is not respected if plugin management applies
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@722537 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng3886ExecutionGoalsOrderTest.java
@@ -0,0 +1,79 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-3886">MNG-3886</a>.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class MavenITmng3886ExecutionGoalsOrderTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng3886ExecutionGoalsOrderTest()
+    {
+    }
+
+    /**
+     * Test that the goals from a plugin execution are executed in the order given by the POM when no <pluginManagement>
+     * is involved.
+     */
+    public void testitWithoutPluginManagement()
+        throws Exception
+    {
+        testitMNG3886( "test-1" );
+    }
+
+    /**
+     * Test that the goals from a plugin execution are executed in the order given by the POM when <pluginManagement>
+     * is involved.
+     */
+    public void testitWithPluginManagement()
+        throws Exception
+    {
+        testitMNG3886( "test-2" );
+    }
+
+    private void testitMNG3886( String project )
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-3886" );
+
+        Verifier verifier = new Verifier( new File( testDir, project ).getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        List lines = verifier.loadLines( "target/it.log", "UTF-8" );
+        assertEquals( Arrays.asList( new String[] { "test", "----" } ), lines );
+    }
+
+}
