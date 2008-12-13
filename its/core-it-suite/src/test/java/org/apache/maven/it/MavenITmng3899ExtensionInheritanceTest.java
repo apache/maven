64823bb71b6318d6b0bee1e66465eb8c4dc827e9
Benjamin Bentmann <bentmann@apache.org>
2008-12-13 17:27:27 +0000

    [MNG-3899] Inheritance does not merge extensions with same gid and aid
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@726238 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng3899ExtensionInheritanceTest.java
@@ -0,0 +1,69 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-3899">MNG-3899</a>.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class MavenITmng3899ExtensionInheritanceTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng3899ExtensionInheritanceTest()
+    {
+        super( "(2.99,)" );
+    }
+
+    /**
+     * Test that build extensions are properly merged during inheritance.
+     */
+    public void testitMNG3899()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-3899" );
+
+        Verifier verifier = new Verifier( new File( testDir, "sub" ).getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.deleteArtifacts( "org.apache.maven.its.mng3899" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        Properties props = verifier.loadProperties( "target/extension.properties" );
+        assertEquals( "3", props.getProperty( "project.build.extensions" ) );
+        assertEquals( "b", props.getProperty( "project.build.extensions.0.artifactId" ) );
+        assertEquals( "0.1", props.getProperty( "project.build.extensions.0.version" ) );
+        assertEquals( "a", props.getProperty( "project.build.extensions.1.artifactId" ) );
+        assertEquals( "0.2", props.getProperty( "project.build.extensions.1.version" ) );
+        assertEquals( "c", props.getProperty( "project.build.extensions.2.artifactId" ) );
+        assertEquals( "0.1", props.getProperty( "project.build.extensions.2.version" ) );
+    }
+
+}
