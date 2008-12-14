c52bed50972c39e47a059aceb6716e938391e2b2
Benjamin Bentmann <bentmann@apache.org>
2008-12-14 13:20:02 +0000

    [MNG-3906] Project-level plugin dependencies are in random order after merging
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@726432 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng3906MergedPluginClassPathOrderingTest.java
@@ -0,0 +1,78 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-3906">MNG-3906</a>.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class MavenITmng3906MergedPluginClassPathOrderingTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng3906MergedPluginClassPathOrderingTest()
+    {
+        super( "(2.0.10,2.1.0-M1),(2.1.0-M1,)" );
+    }
+
+    /**
+     * Test that project-level plugin dependencies are properly merged during inheritance.
+     */
+    public void testitMNG3813()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-3906" );
+
+        Verifier verifier = new Verifier( new File( testDir, "sub" ).getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.deleteArtifacts( "org.apache.maven.its.mng3906" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        Properties pclProps = verifier.loadProperties( "target/pcl.properties" );
+
+        String className = "org.apache.maven.its.mng3906.SomeClass";
+        String resName = className.replace( '.', '/' ) + ".class";
+
+        assertEquals( "5", pclProps.getProperty( resName + ".count" ) );
+
+        assertTrue( pclProps.getProperty( resName + ".0" ), 
+            pclProps.getProperty( resName + ".0" ).endsWith( "/c-0.1.jar!/" + resName ) );
+        assertTrue( pclProps.getProperty( resName + ".1" ),
+            pclProps.getProperty( resName + ".1" ).endsWith( "/a-0.2.jar!/" + resName ) );
+        assertTrue( pclProps.getProperty( resName + ".2" ),
+            pclProps.getProperty( resName + ".2" ).endsWith( "/b-0.1.jar!/" + resName ) );
+        assertTrue( pclProps.getProperty( resName + ".3" ),
+            pclProps.getProperty( resName + ".3" ).endsWith( "/e-0.1.jar!/" + resName ) );
+        assertTrue( pclProps.getProperty( resName + ".4" ),
+            pclProps.getProperty( resName + ".4" ).endsWith( "/d-0.1.jar!/" + resName ) );
+    }
+
+}
