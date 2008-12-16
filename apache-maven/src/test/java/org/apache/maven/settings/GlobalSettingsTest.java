50bbec280b30b975b985c267fe46933c614349fc
Benjamin Bentmann <bentmann@apache.org>
2008-12-16 10:30:13 +0000

    o Added a unit test to smoke check the settings.xml shipped with the distro (merged from r727007)
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/branches/maven-2.1.x@727010 13f79535-47bb-0310-9956-ffa450edef68

---
+++ apache-maven/src/test/java/org/apache/maven/settings/GlobalSettingsTest.java
@@ -0,0 +1,60 @@
+package org.apache.maven.settings;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import org.apache.maven.settings.io.xpp3.SettingsXpp3Reader;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests that the global settings.xml shipped with the distribution is in good state.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class GlobalSettingsTest
+    extends TestCase
+{
+
+    public void testValidGlobalSettings()
+        throws Exception
+    {
+        String basedir = System.getProperty( "basedir", System.getProperty( "user.dir" ) );
+
+        File globalSettingsFile = new File( basedir, "src/conf/settings.xml" );
+        assertTrue( globalSettingsFile.getAbsolutePath(), globalSettingsFile.isFile() );
+
+        Reader reader = new InputStreamReader( new FileInputStream( globalSettingsFile ), "UTF-8" );
+        try
+        {
+            new SettingsXpp3Reader().read( reader );
+        }
+        finally
+        {
+            reader.close();
+        }
+    }
+
+}
