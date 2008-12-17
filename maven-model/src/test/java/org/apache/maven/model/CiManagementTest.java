f595fbc6fc0daac9cbf7f67dfda25848ef432909
Benjamin Bentmann <bentmann@apache.org>
2008-12-17 22:31:47 +0000

    [MNG-3921] Extensions are subject to NPEs in their hashCode() impl
    o Merged from r727548
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/branches/maven-2.1.x@727553 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-model/src/test/java/org/apache/maven/model/CiManagementTest.java
@@ -0,0 +1,55 @@
+package org.apache.maven.model;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import junit.framework.TestCase;
+
+/**
+ * Tests {@code CiManagement}.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class CiManagementTest
+    extends TestCase
+{
+
+    public void testHashCodeNullSafe()
+    {
+        new CiManagement().hashCode();
+    }
+
+    public void testEqualsNullSafe()
+    {
+        assertFalse( new CiManagement().equals( null ) );
+    }
+
+    public void testEqualsIdentity()
+    {
+        CiManagement thing = new CiManagement();
+        assertTrue( thing.equals( thing ) );
+    }
+
+    public void testToStringNullSafe()
+    {
+        assertNotNull( new CiManagement().toString() );
+    }
+
+}
---
+++ maven-model/src/test/java/org/apache/maven/model/RelocationTest.java
@@ -0,0 +1,55 @@
+package org.apache.maven.model;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import junit.framework.TestCase;
+
+/**
+ * Tests {@code Relocation}.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class RelocationTest
+    extends TestCase
+{
+
+    public void testHashCodeNullSafe()
+    {
+        new Relocation().hashCode();
+    }
+
+    public void testEqualsNullSafe()
+    {
+        assertFalse( new Relocation().equals( null ) );
+    }
+
+    public void testEqualsIdentity()
+    {
+        Relocation thing = new Relocation();
+        assertTrue( thing.equals( thing ) );
+    }
+
+    public void testToStringNullSafe()
+    {
+        assertNotNull( new Relocation().toString() );
+    }
+
+}
