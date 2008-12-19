2bd749bfc10c1ea52bd3f2efd64ef1709069e771
Brett Leslie Porter <brett@apache.org>
2008-12-19 00:42:48 +0000

    [MNG-1830] use ISO 8601 format (not combined for readability)
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/branches/maven-2.1.x@727868 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/main/java/org/apache/maven/cli/MavenCli.java
+++ maven-core/src/main/java/org/apache/maven/cli/MavenCli.java
@@ -63,8 +63,10 @@ import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
@@ -610,7 +612,12 @@ public class MavenCli
         {
             msg += " (";
             msg += ( rev != null ? "r" + rev : "" );
-            msg += ( timestamp != null ? ( rev != null ? "; " : "" ) + new java.util.Date( Long.valueOf( timestamp ).longValue() ) : "" );
+            if ( timestamp != null )
+            {
+                SimpleDateFormat fmt = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ssZ" );
+                String ts = fmt.format( new Date( Long.valueOf( timestamp ).longValue() ) );
+                msg += ( rev != null ? "; " : "" ) + ts;
+            }
             msg += ")";
         }
 
