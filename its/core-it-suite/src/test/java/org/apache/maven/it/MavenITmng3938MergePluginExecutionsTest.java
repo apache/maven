36696db2a060edf7521915718220c4eb7f95bef2
Benjamin Bentmann <bentmann@apache.org>
2008-12-22 18:59:19 +0000

    [MNG-3938] [regression] Plugin executions with default id are not always merged
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@728741 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng3938MergePluginExecutionsTest.java
@@ -0,0 +1,87 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-3938">MNG-3938</a>.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class MavenITmng3938MergePluginExecutionsTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng3938MergePluginExecutionsTest()
+    {
+        super( "(2.0.4,)" );
+    }
+
+    /**
+     * Test that plugin executions with the same id are merged during inheritance, especially executions using the
+     * default id, regardless whether the id is given explicitly by the user or implicitly assumed from defaults, when
+     * no <pluginManagement> is involved.
+     */
+    public void testitWithoutPluginManagement()
+        throws Exception
+    {
+        testitMNG3938( "test-1" );
+    }
+
+    /**
+     * Test that plugin executions with the same id are merged during inheritance, especially executions using the
+     * default id, regardless whether the id is given explicitly by the user or implicitly assumed from defaults, when
+     * <pluginManagement> is involved.
+     */
+    public void testitWithPluginManagement()
+        throws Exception
+    {
+        testitMNG3938( "test-2" );
+    }
+
+    private void testitMNG3938( String project )
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-3938" );
+
+        Verifier verifier = new Verifier( new File( new File( testDir, project ), "sub" ).getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        List lines = verifier.loadLines( "target/default.log", "UTF-8" );
+        assertEquals( Arrays.asList( new String[] { "child" } ), lines );
+
+        lines = verifier.loadLines( "target/non-default.log", "UTF-8" );
+        assertEquals( Arrays.asList( new String[] { "child" } ), lines );
+
+        verifier.assertFileNotPresent( "target/parent.log" );
+    }
+
+}
