8d845eaa55103d8bb004f7355b73c91c4d8facd0
Benjamin Bentmann <bentmann@apache.org>
2008-12-26 16:56:31 +0000

    o Created UT from MNG-3822
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@729511 13f79535-47bb-0310-9956-ffa450edef68

--- maven-project/src/test/java/org/apache/maven/project/builder/PomConstructionTest.java
+++ maven-project/src/test/java/org/apache/maven/project/builder/PomConstructionTest.java
@@ -390,6 +390,26 @@ public class PomConstructionTest
         assertTrue( pom.getValue( "properties/projectSiteOut" ).toString().endsWith( "doc" ) );
     }
 
+    public void testInterpolationWithBasedirAlignedDirectories()
+        throws Exception
+    {
+        PomTestWrapper pom = buildPom( "basedir-aligned-interpolation" );
+        assertEquals( new File( pom.getBasedir(), "src/main/java" ),
+                      new File( pom.getValue( "properties/buildMainSrc" ).toString() ) );
+        assertEquals( new File( pom.getBasedir(), "src/test/java" ),
+                      new File( pom.getValue( "properties/buildTestSrc" ).toString() ) );
+        assertEquals( new File( pom.getBasedir(), "src/main/scripts" ),
+                      new File( pom.getValue( "properties/buildScriptSrc" ).toString() ) );
+        assertEquals( new File( pom.getBasedir(), "target" ),
+                      new File( pom.getValue( "properties/buildOut" ).toString() ) );
+        assertEquals( new File( pom.getBasedir(), "target/classes" ),
+                      new File( pom.getValue( "properties/buildMainOut" ).toString() ) );
+        assertEquals( new File( pom.getBasedir(), "target/test-classes" ),
+                      new File( pom.getValue( "properties/buildTestOut" ).toString() ) );
+        assertEquals( new File( pom.getBasedir(), "target/site" ),
+                      new File( pom.getValue( "properties/siteOut" ).toString() ) );
+    }
+
     private PomArtifactResolver artifactResolver( String basedir )
     {
         return new FileBasedPomArtifactResolver( new File( BASE_POM_DIR, basedir ) );
