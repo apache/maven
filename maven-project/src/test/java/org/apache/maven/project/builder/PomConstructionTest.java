fd68f592503acc9c48164ddf33a068e7eb8843bc
Benjamin Bentmann <bentmann@apache.org>
2008-12-26 16:48:36 +0000

    o Created UT from MNG-3831
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@729508 13f79535-47bb-0310-9956-ffa450edef68

--- maven-project/src/test/java/org/apache/maven/project/builder/PomConstructionTest.java
+++ maven-project/src/test/java/org/apache/maven/project/builder/PomConstructionTest.java
@@ -357,6 +357,39 @@ public class PomConstructionTest
         }
     }
 
+    public void testInterpolationOfLegacyExpressionsThatDontIncludeTheProjectPrefix()
+        throws Exception
+    {
+        PomTestWrapper pom = buildPom( "unprefixed-expression-interpolation/child" );
+        assertEquals( pom.getBasedir(), new File( pom.getValue( "properties/projectDir" ).toString() ) );
+
+        assertEquals( "org.apache.maven.its.mng3831.child", pom.getValue( "properties/projectGroupId" ) );
+        assertEquals( "child", pom.getValue( "properties/projectArtifactId" ) );
+        assertEquals( "2.0-alpha-1", pom.getValue( "properties/projectVersion" ) );
+        assertEquals( "jar", pom.getValue( "properties/projectPackaging" ) );
+
+        assertEquals( "child-name", pom.getValue( "properties/projectName" ) );
+        assertEquals( "child-desc", pom.getValue( "properties/projectDesc" ) );
+        assertEquals( "http://child.org/", pom.getValue( "properties/projectUrl" ) );
+        assertEquals( "2008", pom.getValue( "properties/projectYear" ) );
+        assertEquals( "child-org-name", pom.getValue( "properties/projectOrgName" ) );
+
+        assertEquals( "2.0.0", pom.getValue( "properties/projectPrereqMvn" ) );
+        assertEquals( "http://scm.org/", pom.getValue( "properties/projectScmUrl" ) );
+        assertEquals( "http://issue.org/", pom.getValue( "properties/projectIssueUrl" ) );
+        assertEquals( "http://ci.org/", pom.getValue( "properties/projectCiUrl" ) );
+        assertEquals( "child-dist-repo", pom.getValue( "properties/projectDistRepoName" ) );
+        assertEquals( "http://dist.org/", pom.getValue( "properties/projectDistRepoUrl" ) );
+        assertEquals( "http://site.org/", pom.getValue( "properties/projectDistSiteUrl" ) );
+
+        assertEquals( "org.apache.maven.its.mng3831", pom.getValue( "properties/parentGroupId" ) );
+        assertEquals( "parent", pom.getValue( "properties/parentArtifactId" ) );
+        assertEquals( "1.0", pom.getValue( "properties/parentVersion" ) );
+
+        assertTrue( pom.getValue( "properties/projectBuildOut" ).toString().endsWith( "bin" ) );
+        assertTrue( pom.getValue( "properties/projectSiteOut" ).toString().endsWith( "doc" ) );
+    }
+
     private PomArtifactResolver artifactResolver( String basedir )
     {
         return new FileBasedPomArtifactResolver( new File( BASE_POM_DIR, basedir ) );
