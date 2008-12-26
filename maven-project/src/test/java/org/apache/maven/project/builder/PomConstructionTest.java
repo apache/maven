3dd7dcecbcfa3bb48ad39a2b50e90e6d8b1279f3
Benjamin Bentmann <bentmann@apache.org>
2008-12-26 16:11:47 +0000

    o Created UT from MNG-3846
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@729504 13f79535-47bb-0310-9956-ffa450edef68

--- maven-project/src/test/java/org/apache/maven/project/builder/PomConstructionTest.java
+++ maven-project/src/test/java/org/apache/maven/project/builder/PomConstructionTest.java
@@ -302,6 +302,24 @@ public class PomConstructionTest
                       new File( (String) pom.getValue( "properties/dir2" ) ) );
     }
 
+    public void testAppendArtifactIdOfChildToInheritedUrls()
+        throws Exception
+    {
+        PomTestWrapper pom = buildPom( "url-inheritance/sub" );
+        assertEquals( "http://parent.url/child", pom.getValue( "url" ) );
+        assertEquals( "http://parent.url/org/", pom.getValue( "organization/url" ) );
+        assertEquals( "http://parent.url/license.txt", pom.getValue( "licenses[1]/url" ) );
+        assertEquals( "http://parent.url/viewvc/child", pom.getValue( "scm/url" ) );
+        assertEquals( "http://parent.url/scm/child", pom.getValue( "scm/connection" ) );
+        assertEquals( "https://parent.url/scm/child", pom.getValue( "scm/developerConnection" ) );
+        assertEquals( "http://parent.url/issues", pom.getValue( "issueManagement/url" ) );
+        assertEquals( "http://parent.url/ci", pom.getValue( "ciManagement/url" ) );
+        assertEquals( "http://parent.url/dist", pom.getValue( "distributionManagement/repository/url" ) );
+        assertEquals( "http://parent.url/snaps", pom.getValue( "distributionManagement/snapshotRepository/url" ) );
+        assertEquals( "http://parent.url/site/child", pom.getValue( "distributionManagement/site/url" ) );
+        assertEquals( "http://parent.url/download", pom.getValue( "distributionManagement/downloadUrl" ) );
+    }
+
     private PomArtifactResolver artifactResolver( String basedir )
     {
         return new FileBasedPomArtifactResolver( new File( BASE_POM_DIR, basedir ) );
