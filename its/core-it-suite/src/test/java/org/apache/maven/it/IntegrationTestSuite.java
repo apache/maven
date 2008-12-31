f971b45d44014053333ba58ab879e928ba634062
Benjamin Bentmann <bentmann@apache.org>
2008-12-31 17:17:37 +0000

    [MNG-3586] [regression] jaxws mojo wsgen failure with maven 3.0 due to system-scope dependencies being excluded
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@730416 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
@@ -152,6 +152,7 @@ public class IntegrationTestSuite
         suite.addTestSuite( MavenITmng3652UserAgentHeaderTest.class );
         suite.addTestSuite( MavenITmng3642DynamicResourcesTest.class );
         suite.addTestSuite( MavenITmng3599useHttpProxyForWebDAVTest.class );
+        suite.addTestSuite( MavenITmng3586SystemScopePluginDependencyTest.class );
         suite.addTestSuite( MavenITmng3581PluginUsesWagonDependencyTest.class );
         suite.addTestSuite( MavenITmng3545ProfileDeactivationTest.class );
         suite.addTestSuite( MavenITmng3536AppendedAbsolutePathsTest.class );
