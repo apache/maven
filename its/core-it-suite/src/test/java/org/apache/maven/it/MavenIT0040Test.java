bd4bcb1ecb6ac03e5abd70431cdeb65d89125a38
Benjamin Bentmann <bentmann@apache.org>
2009-01-06 19:29:08 +0000

    o Decoupled it0040 from production plugins
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@732070 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-it-suite/src/test/java/org/apache/maven/it/MavenIT0040Test.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenIT0040Test.java
@@ -41,12 +41,15 @@ public class MavenIT0040Test
         throws Exception
     {
         File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/it0040" );
+
         Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
         verifier.executeGoal( "package" );
-        verifier.assertFilePresent( "target/maven-it-it0040-1.0-it.jar" );
         verifier.verifyErrorFreeLog();
         verifier.resetStreams();
 
+        verifier.assertFilePresent( "target/maven-it-it0040-1.0-it.jar" );
     }
-}
 
+}
