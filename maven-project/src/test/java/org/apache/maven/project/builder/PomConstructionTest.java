2e4285f616ae6fba25ac1d371085025704c6e590
Benjamin Bentmann <bentmann@apache.org>
2009-01-23 16:06:37 +0000

    o Created UT for MNG-3864
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@737066 13f79535-47bb-0310-9956-ffa450edef68

--- maven-project/src/test/java/org/apache/maven/project/builder/PomConstructionTest.java
+++ maven-project/src/test/java/org/apache/maven/project/builder/PomConstructionTest.java
@@ -492,6 +492,34 @@ public class PomConstructionTest
     }
     //*/
 
+    public void testOrderOfPluginExecutionConfigurationElementsWithoutPluginManagement()
+        throws Exception
+    {
+        PomTestWrapper pom = buildPom( "plugin-exec-config-order/wo-plugin-mngt" );
+        String prefix = "build/plugins[1]/executions[1]/configuration/";
+        assertEquals( "one", pom.getValue( prefix + "stringParams/stringParam[1]" ) );
+        assertEquals( "two", pom.getValue( prefix + "stringParams/stringParam[2]" ) );
+        assertEquals( "three", pom.getValue( prefix + "stringParams/stringParam[3]" ) );
+        assertEquals( "four", pom.getValue( prefix + "stringParams/stringParam[4]" ) );
+        assertEquals( "key1", pom.getValue( prefix + "propertiesParam/property[1]/name" ) );
+        assertEquals( "key2", pom.getValue( prefix + "propertiesParam/property[2]/name" ) );
+    }
+
+    /* FIXME: cf. MNG-3864
+    public void testOrderOfPluginExecutionConfigurationElementsWithPluginManagement()
+        throws Exception
+    {
+        PomTestWrapper pom = buildPom( "plugin-exec-config-order/w-plugin-mngt" );
+        String prefix = "build/plugins[1]/executions[1]/configuration/";
+        assertEquals( "one", pom.getValue( prefix + "stringParams/stringParam[1]" ) );
+        assertEquals( "two", pom.getValue( prefix + "stringParams/stringParam[2]" ) );
+        assertEquals( "three", pom.getValue( prefix + "stringParams/stringParam[3]" ) );
+        assertEquals( "four", pom.getValue( prefix + "stringParams/stringParam[4]" ) );
+        assertEquals( "key1", pom.getValue( prefix + "propertiesParam/property[1]/name" ) );
+        assertEquals( "key2", pom.getValue( prefix + "propertiesParam/property[2]/name" ) );
+    }
+    //*/
+
     private PomArtifactResolver artifactResolver( String basedir )
     {
         return new FileBasedPomArtifactResolver( new File( BASE_POM_DIR, basedir ) );
