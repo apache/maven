8bc517009b66cb13ce21756894ce8941029970b0
Benjamin Bentmann <bentmann@apache.org>
2009-01-23 16:44:14 +0000

    o Created UT for MNG-2591
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@737078 13f79535-47bb-0310-9956-ffa450edef68

--- maven-project/src/test/java/org/apache/maven/project/builder/PomConstructionTest.java
+++ maven-project/src/test/java/org/apache/maven/project/builder/PomConstructionTest.java
@@ -537,6 +537,31 @@ public class PomConstructionTest
     }
     //*/
 
+    /* FIXME: cf. MNG-2591
+    public void testAppendOfInheritedPluginConfiguration()
+        throws Exception
+    {
+        PomTestWrapper pom = buildPom( "plugin-config-append/subproject" );
+        String prefix = "build/plugins[1]/configuration/";
+        assertEquals( "PARENT-1", pom.getValue( prefix + "stringParams/stringParam[1]" ) );
+        assertEquals( "PARENT-3", pom.getValue( prefix + "stringParams/stringParam[2]" ) );
+        assertEquals( "PARENT-2", pom.getValue( prefix + "stringParams/stringParam[3]" ) );
+        assertEquals( "PARENT-4", pom.getValue( prefix + "stringParams/stringParam[4]" ) );
+        assertEquals( "CHILD-1", pom.getValue( prefix + "stringParams/stringParam[5]" ) );
+        assertEquals( "CHILD-3", pom.getValue( prefix + "stringParams/stringParam[6]" ) );
+        assertEquals( "CHILD-2", pom.getValue( prefix + "stringParams/stringParam[7]" ) );
+        assertEquals( "CHILD-4", pom.getValue( prefix + "stringParams/stringParam[8]" ) );
+        assertEquals( "PARENT-1", pom.getValue( prefix + "listParam/listParam[1]" ) );
+        assertEquals( "PARENT-3", pom.getValue( prefix + "listParam/listParam[2]" ) );
+        assertEquals( "PARENT-2", pom.getValue( prefix + "listParam/listParam[3]" ) );
+        assertEquals( "PARENT-4", pom.getValue( prefix + "listParam/listParam[4]" ) );
+        assertEquals( "CHILD-1", pom.getValue( prefix + "listParam/listParam[5]" ) );
+        assertEquals( "CHILD-3", pom.getValue( prefix + "listParam/listParam[6]" ) );
+        assertEquals( "CHILD-2", pom.getValue( prefix + "listParam/listParam[7]" ) );
+        assertEquals( "CHILD-4", pom.getValue( prefix + "listParam/listParam[8]" ) );
+    }
+    //*/
+
     private PomArtifactResolver artifactResolver( String basedir )
     {
         return new FileBasedPomArtifactResolver( new File( BASE_POM_DIR, basedir ) );
