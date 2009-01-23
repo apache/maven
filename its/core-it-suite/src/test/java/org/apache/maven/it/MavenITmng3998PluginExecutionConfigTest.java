f97345d9751640e35394e1ee345b5d5deb3508ee
Benjamin Bentmann <bentmann@apache.org>
2009-01-23 18:33:43 +0000

    [MNG-3998] Loss of execution configuration
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@737135 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng3998PluginExecutionConfigTest.java
@@ -0,0 +1,67 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-3998">MNG-3998</a>.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class MavenITmng3998PluginExecutionConfigTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng3998PluginExecutionConfigTest()
+    {
+        super();
+    }
+
+    /**
+     * Test that multiple plugin executions do not lose their configuration when plugin management is used.
+     */
+    public void testitMNG3998()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-3998" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        List executions = verifier.loadLines( "target/exec.log", "UTF-8" );
+        // NOTE: Ordering of executions is another issue (MNG-3887), so ignore/normalize order
+        Collections.sort( executions );
+        List expected = Arrays.asList( new String[] { "exec-1", "exec-2" } );
+        assertEquals( expected, executions );
+    }
+
+}
