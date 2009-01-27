e586f44948f10e0716bba0aeb03b60ca59ec6f17
Benjamin Bentmann <bentmann@apache.org>
2009-01-27 11:08:38 +0000

    [MNG-4007] [regression] Effective model contains paths with non-normalized file separators
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@738054 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4007PlatformFileSeparatorTest.java
@@ -0,0 +1,98 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-4007">MNG-4007</a>.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class MavenITmng4007PlatformFileSeparatorTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng4007PlatformFileSeparatorTest()
+    {
+        super();
+    }
+
+    /**
+     * Verify that paths to project directories use the platform-specific file separator.
+     */
+    public void testitMNG4007()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-4007" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        Properties modelProps = verifier.loadProperties( "target/model.properties" );
+
+        assertPath( modelProps.getProperty( "project.build.directory" ) );
+
+        assertPath( modelProps.getProperty( "project.build.outputDirectory" ) );
+
+        assertPath( modelProps.getProperty( "project.build.testOutputDirectory" ) );
+
+        assertPath( modelProps.getProperty( "project.build.sourceDirectory" ) );
+        assertPath( modelProps.getProperty( "project.compileSourceRoots.0" ) );
+
+        assertPath( modelProps.getProperty( "project.build.testSourceDirectory" ) );
+        assertPath( modelProps.getProperty( "project.testCompileSourceRoots.0" ) );
+
+        assertPath( modelProps.getProperty( "project.build.resources.0.directory" ) );
+
+        assertPath( modelProps.getProperty( "project.build.testResources.0.directory" ) );
+
+        assertPath( modelProps.getProperty( "project.build.filters.0" ) );
+
+        /*
+         * NOTE: The script source directory is deliberately excluded from the checks due to MNG-3741.
+         */
+
+        // FIXME: MNG-3877
+        if ( matchesVersionRange( "(4,)" ) )
+        {
+            assertPath( modelProps.getProperty( "project.reporting.outputDirectory" ) );
+        }
+    }
+
+    private void assertPath( String actual )
+    {
+        /*
+         * NOTE: Whether the path is absolute is another issue (MNG-3877), we are only interested in the proper
+         * file separator here.
+         */
+        assertEquals( new File( actual ).getPath(), actual );
+    }
+
+}
