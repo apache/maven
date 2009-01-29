6921cebb9ce15553bfabc7159977a6910fd6c720
Brett Porter <brett@apache.org>
2009-01-29 04:15:02 +0000

    [MNG-3920] add integration test for uses of plexus-component-api in plugins
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@738752 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-support/core-it-plugins/maven-it-plugin-plexus-component-api/src/main/java/org/apache/maven/its/plugins/VelocityMojo.java
@@ -0,0 +1,56 @@
+package org.apache.maven.its.plugins;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.codehaus.plexus.velocity.VelocityComponent;
+
+/**
+ * @goal velocity
+ * @phase validate
+ */
+public class VelocityMojo
+    extends AbstractMojo
+{
+    /**
+     * @component
+     */
+    protected VelocityComponent velocityComponent;
+
+    public void execute()
+        throws MojoExecutionException, MojoFailureException
+    {
+        // velocityComponent should not be null
+        velocityComponent.getEngine();
+
+        try
+        {
+            // velocityComponent engine should not be null
+            // this is the real test to check that we got the right Initializable interface in both Plexus and the component
+            velocityComponent.getEngine().getTemplate( "/template.vm" );
+        }
+        catch ( Exception e )
+        {
+            throw new MojoExecutionException( e.getMessage(), e );
+        }
+    }
+}
