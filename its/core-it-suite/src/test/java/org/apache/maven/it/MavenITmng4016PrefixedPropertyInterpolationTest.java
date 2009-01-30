b0e0acd544598ad8dd49a98de170699e3992a513
Benjamin Bentmann <bentmann@apache.org>
2009-01-30 16:09:55 +0000

    [MNG-4016] Properties with the prefix project/pom are not interpolated from the properties section
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@739321 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4016PrefixedPropertyInterpolationTest.java
@@ -0,0 +1,66 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-4016">MNG-4016</a>.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class MavenITmng4016PrefixedPropertyInterpolationTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng4016PrefixedPropertyInterpolationTest()
+    {
+        super( "(2.1.0-M1,)" );
+    }
+
+    /**
+     * Test that expressions with the special prefixes "project.", "pom." and "env." can be interpolated from
+     * properties that include the prefix.
+     */
+    public void testitMNG4016()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-4016" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        verifier.assertFilePresent( "target/model.properties" );
+        Properties props = verifier.loadProperties( "target/model.properties" );
+        assertEquals( "PASSED-1", props.getProperty( "project.properties.projectProperty" ) );
+        assertEquals( "PASSED-2", props.getProperty( "project.properties.pomProperty" ) );
+        assertEquals( "PASSED-3", props.getProperty( "project.properties.envProperty" ) );
+    }
+
+}
