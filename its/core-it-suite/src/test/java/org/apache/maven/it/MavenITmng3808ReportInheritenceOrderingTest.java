363cc4895f31e4d52727e372c7e0d365b4cefed2
Brett Porter <brett@apache.org>
2009-02-06 10:24:06 +0000

    [MNG-3808] integration test for report inheritence ordering in the POM
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@741508 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng3808ReportInheritenceOrderingTest.java
@@ -0,0 +1,67 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.util.List;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.maven.it.util.ResourceExtractor;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-3808">MNG-3808</a>.
+ * 
+ * @author Brett Porter
+ * @version $Id: MavenITmng4016PrefixedPropertyInterpolationTest.java 739321 2009-01-30 16:09:55Z bentmann $
+ */
+public class MavenITmng3808ReportInheritenceOrderingTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng3808ReportInheritenceOrderingTest()
+    {
+        super( "[2.0.11,2.1.0-M1),[2.1.0-M2,)" );
+    }
+
+    /**
+     * Test that 3 executions are run in the correct order.
+     */
+    public void testitMNG3719()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-3808" );
+        testDir = new File( testDir, "child" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        verifier.assertFilePresent( "target/model.properties" );
+        Properties props = verifier.loadProperties( "target/model.properties" );
+        assertEquals( "maven-it-plugin-file", props.getProperty( "project.reporting.plugins.0.artifactId" ) );
+        assertEquals( "maven-it-plugin-expression", props.getProperty( "project.reporting.plugins.1.artifactId" ) );
+        assertEquals( "maven-it-plugin-configuration", props.getProperty( "project.reporting.plugins.2.artifactId" ) );
+    }
+}
