47652c164d6a61ce64331f9f3aea4861ebcdd055
Brett Leslie Porter <brett@apache.org>
2009-02-10 09:29:46 +0000

    add test case for identity and hashcode behaviour
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/branches/maven-2.1.x@742911 13f79535-47bb-0310-9956-ffa450edef68

--- maven-model/src/test/java/org/apache/maven/model/ReportPluginTest.java
+++ maven-model/src/test/java/org/apache/maven/model/ReportPluginTest.java
@@ -41,6 +41,42 @@ public class ReportPluginTest
         assertFalse( new ReportPlugin().equals( null ) );
     }
 
+    public void testEqualsIsKey()
+    {
+        ReportPlugin thing = new ReportPlugin();
+        thing.setGroupId( "groupId" );
+        thing.setArtifactId( "artifactId" );
+        thing.setVersion( "1.0" );
+        ReportPlugin thing2 = new ReportPlugin();
+        thing2.setGroupId( "groupId" );
+        thing2.setArtifactId( "artifactId" );
+        thing2.setVersion( "2.0" );
+        assertEquals( thing2, thing );
+
+        ReportPlugin thing3 = new ReportPlugin();
+        thing3.setGroupId( "otherGroupId" );
+        thing3.setArtifactId( "artifactId" );
+        assertFalse( thing3.equals( thing ) );
+    }
+
+    public void testHashcodeIsId()
+    {
+        ReportPlugin thing = new ReportPlugin();
+        thing.setGroupId( "groupId" );
+        thing.setArtifactId( "artifactId" );
+        thing.setVersion( "1.0" );
+        ReportPlugin thing2 = new ReportPlugin();
+        thing2.setGroupId( "groupId" );
+        thing2.setArtifactId( "artifactId" );
+        thing2.setVersion( "2.0" );
+        assertEquals( thing2.hashCode(), thing.hashCode() );
+
+        ReportPlugin thing3 = new ReportPlugin();
+        thing3.setGroupId( "otherGroupId" );
+        thing3.setArtifactId( "artifactId" );
+        assertFalse( thing3.hashCode() == thing.hashCode() );
+    }
+
     public void testEqualsIdentity()
     {
         ReportPlugin thing = new ReportPlugin();
