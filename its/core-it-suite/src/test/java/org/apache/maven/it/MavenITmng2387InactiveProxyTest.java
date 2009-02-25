758934d4af390446a0f4ccc23c2367d23fbde30e
Brett Porter <brett@apache.org>
2009-02-25 07:13:13 +0000

    [MNG-2387] <active> on <proxy> in settings is misleading - integration test
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@747690 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng2387InactiveProxyTest.java
@@ -0,0 +1,105 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.net.InetAddress;
+import java.util.Properties;
+
+import org.apache.maven.it.util.ResourceExtractor;
+import org.mortbay.jetty.Handler;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.DefaultHandler;
+import org.mortbay.jetty.handler.HandlerList;
+import org.mortbay.jetty.handler.ResourceHandler;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-2387">MNG-2387</a>.
+ * 
+ * @author Brett Porter
+ * @version $Id$
+ */
+public class MavenITmng2387InactiveProxyTest
+    extends AbstractMavenIntegrationTestCase
+{
+    private Server server;
+
+    private int port;
+
+    private File testDir;
+
+    public MavenITmng2387InactiveProxyTest()
+    {
+        // TODO: re-instate feature in 3.0
+        super( "[2.0.11,2.1.0-M1),[2.1.0,)" ); // 2.0.11+, 2.1.0+
+    }
+
+    public void setUp()
+        throws Exception
+    {
+        testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-2387" );
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setResourceBase( new File( testDir, "repo" ).getAbsolutePath() );
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers( new Handler[] { resource_handler, new DefaultHandler() } );
+
+        server = new Server( 0 );
+        server.setHandler( handlers );
+        server.start();
+
+        port = server.getConnectors()[0].getLocalPort();
+    }
+
+    protected void tearDown()
+        throws Exception
+    {
+        super.tearDown();
+
+        server.stop();
+    }
+
+    /**
+     * Test that mirror definitions are properly evaluated. In particular, the first matching mirror definition
+     * from the settings should win, i.e. ordering of mirror definitions matters.
+     */
+    public void testitFirstMatchWins()
+        throws Exception
+    {
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        
+        Properties properties = verifier.newDefaultFilterProperties();        
+        properties.setProperty( "@host@", InetAddress.getLocalHost().getCanonicalHostName() );
+        properties.setProperty( "@port@", Integer.toString( port ) );
+        verifier.filterFile( "pom-unfiltered.xml", "pom.xml", "UTF-8", properties );
+
+        verifier.setAutoclean( false );
+        verifier.deleteArtifacts( "org.apache.maven.its.mng2387" );
+        verifier.getCliOptions().add( "--settings" );
+        verifier.getCliOptions().add( "settings.xml" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        verifier.assertArtifactPresent( "org.apache.maven.its.mng2387", "a", "0.1", "jar" );
+    }
+
+}
