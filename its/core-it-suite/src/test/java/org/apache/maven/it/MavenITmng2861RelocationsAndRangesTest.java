bbbf2daa0cd8d0ae64e8e8508ad4ee1cb6649ab8
Benjamin Bentmann <bentmann@apache.org>
2009-03-01 19:53:41 +0000

    o Moved IT artifacts into group id for issue nubmer
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@749080 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng2861RelocationsAndRangesTest.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng2861RelocationsAndRangesTest.java
@@ -24,42 +24,24 @@ import org.apache.maven.it.util.ResourceExtractor;
 
 import java.io.File;
 
-
 public class MavenITmng2861RelocationsAndRangesTest
     extends AbstractMavenIntegrationTestCase
 {
+
     public MavenITmng2861RelocationsAndRangesTest()
     {
         super( "(2.0.8,)" );
     }
 
-    public void testitMNG2861 ()
+    public void testitMNG2861()
         throws Exception
     {
-
-
-        // The testdir is computed from the location of this
-        // file.
         File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-2861" );
 
-        Verifier verifier;
-
-        /*
-         * We must first make sure that any artifact created
-         * by this test has been removed from the local
-         * repository. Failing to do this could cause
-         * unstable test results. Fortunately, the verifier
-         * makes it easy to do this.
-         */
-        verifier = new Verifier( new File(testDir,"MNG-2861").getAbsolutePath() );
-        verifier.deleteArtifact( "org.apache.maven.its.mng2861", "MNG-2861", "1.0-SNAPSHOT", "pom" );
-        verifier.deleteArtifact( "org.apache.maven.its.mng2861", "A", "1.0-SNAPSHOT", "jar" );
-        verifier.deleteArtifact( "org.apache.maven.its.mng2861", "B", "1.0-SNAPSHOT", "jar" );
-        verifier.deleteArtifact( "org.apache.maven.its.mng2861", "C", "1.0-SNAPSHOT", "jar" );
-
+        Verifier verifier = new Verifier( new File( testDir, "MNG-2861" ).getAbsolutePath() );
+        verifier.deleteArtifacts( "org.apache.maven.its.mng2861" );
         verifier.executeGoal( "install" );
-
         verifier.verifyErrorFreeLog();
-
     }
+
 }
