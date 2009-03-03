c9514a39f52861445ccdb25cbf4852174ae0401d
Benjamin Bentmann <bentmann@apache.org>
2009-03-03 12:25:22 +0000

    o Fixed ConcurrentModificationException
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@749595 13f79535-47bb-0310-9956-ffa450edef68

--- maven-compat/src/main/java/org/apache/maven/artifact/resolver/DefaultArtifactResolver.java
+++ maven-compat/src/main/java/org/apache/maven/artifact/resolver/DefaultArtifactResolver.java
@@ -408,8 +408,6 @@ public class DefaultArtifactResolver
                 try
                 {
                     resolve( artifact, remoteRepositories, localRepository );
-
-                    result.addArtifact( artifact );
                 }
                 catch ( ArtifactNotFoundException anfe )
                 {
