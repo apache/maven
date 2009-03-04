023a96155bef4114fbc8e7aae10ecb9ef244368d
Britton Isbell <sisbell@apache.org>
2009-03-04 18:50:28 +0000

    Support for jdk activation ranges.
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@750099 13f79535-47bb-0310-9956-ffa450edef68

--- maven-project-builder/src/test/java/org/apache/maven/project/builder/profile/JdkMatcherTest.java
+++ maven-project-builder/src/test/java/org/apache/maven/project/builder/profile/JdkMatcherTest.java
@@ -83,7 +83,118 @@ public class JdkMatcherTest {
         JdkMatcher matcher = new JdkMatcher();
         assertFalse(matcher.isMatch(modelContainer, props));
     }
+    
+    @org.junit.Test
+    public void jdkVersionRange_ClosedEdge()  {
+        List<ModelProperty> modelProperties = new ArrayList<ModelProperty>();
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.xUri, null));
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.Activation.xUri , null));
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.Activation.jdk , "[1.5,"));
 
+        ModelContainer modelContainer = new DefaultModelContainer(modelProperties);
+
+        List<InterpolatorProperty> props = new ArrayList<InterpolatorProperty>();
+        props.add(new InterpolatorProperty("${java.specification.version}" , "1.5"));
+
+        JdkMatcher matcher = new JdkMatcher();
+        assertTrue(matcher.isMatch(modelContainer, props));
+    } 
+    
+    @org.junit.Test
+    public void jdkVersionRange_OpenEdge()  {
+        List<ModelProperty> modelProperties = new ArrayList<ModelProperty>();
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.xUri, null));
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.Activation.xUri , null));
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.Activation.jdk , "(1.5,"));
+
+        ModelContainer modelContainer = new DefaultModelContainer(modelProperties);
+
+        List<InterpolatorProperty> props = new ArrayList<InterpolatorProperty>();
+        props.add(new InterpolatorProperty("${java.specification.version}" , "1.5"));
+
+        JdkMatcher matcher = new JdkMatcher();
+        assertFalse(matcher.isMatch(modelContainer, props));
+    }  
+    
+    @org.junit.Test
+    public void jdkVersionRange_OpenEdge2()  {
+        List<ModelProperty> modelProperties = new ArrayList<ModelProperty>();
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.xUri, null));
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.Activation.xUri , null));
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.Activation.jdk , "(1.4,"));
+
+        ModelContainer modelContainer = new DefaultModelContainer(modelProperties);
+
+        List<InterpolatorProperty> props = new ArrayList<InterpolatorProperty>();
+        props.add(new InterpolatorProperty("${java.specification.version}" , "1.5"));
+
+        JdkMatcher matcher = new JdkMatcher();
+        assertTrue(matcher.isMatch(modelContainer, props));
+    }   
+    
+    @org.junit.Test
+    public void jdkVersionRange_OpenEdgeWithPadding()  {
+        List<ModelProperty> modelProperties = new ArrayList<ModelProperty>();
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.xUri, null));
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.Activation.xUri , null));
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.Activation.jdk , "(1.5.0,"));
+
+        ModelContainer modelContainer = new DefaultModelContainer(modelProperties);
+
+        List<InterpolatorProperty> props = new ArrayList<InterpolatorProperty>();
+        props.add(new InterpolatorProperty("${java.specification.version}" , "1.5"));
+
+        JdkMatcher matcher = new JdkMatcher();
+        assertTrue(matcher.isMatch(modelContainer, props));
+    }  
+    @org.junit.Test
+    public void jdkVersionRange_OpenRightEdge()  {
+        List<ModelProperty> modelProperties = new ArrayList<ModelProperty>();
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.xUri, null));
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.Activation.xUri , null));
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.Activation.jdk , ", 1.6)"));
+
+        ModelContainer modelContainer = new DefaultModelContainer(modelProperties);
+
+        List<InterpolatorProperty> props = new ArrayList<InterpolatorProperty>();
+        props.add(new InterpolatorProperty("${java.specification.version}" , "1.5"));
+
+        JdkMatcher matcher = new JdkMatcher();
+        assertTrue(matcher.isMatch(modelContainer, props));
+    }
+    
+    @org.junit.Test
+    public void jdkVersionRange_OpenRightEdge2()  {
+        List<ModelProperty> modelProperties = new ArrayList<ModelProperty>();
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.xUri, null));
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.Activation.xUri , null));
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.Activation.jdk , ",1.5)"));
+
+        ModelContainer modelContainer = new DefaultModelContainer(modelProperties);
+
+        List<InterpolatorProperty> props = new ArrayList<InterpolatorProperty>();
+        props.add(new InterpolatorProperty("${java.specification.version}" , "1.5"));
+
+        JdkMatcher matcher = new JdkMatcher();
+        assertFalse(matcher.isMatch(modelContainer, props));
+    }  
+    
+    @org.junit.Test
+    public void jdkVersionRange_OpenRightEdgeWithWhiteSpace()  {
+        List<ModelProperty> modelProperties = new ArrayList<ModelProperty>();
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.xUri, null));
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.Activation.xUri , null));
+        modelProperties.add(new ModelProperty(ProjectUri.Profiles.Profile.Activation.jdk , ", 1.5)"));
+
+        ModelContainer modelContainer = new DefaultModelContainer(modelProperties);
+
+        List<InterpolatorProperty> props = new ArrayList<InterpolatorProperty>();
+        props.add(new InterpolatorProperty("${java.specification.version}" , "1.5"));
+
+        JdkMatcher matcher = new JdkMatcher();
+        assertFalse(matcher.isMatch(modelContainer, props));
+    } 
+    
     @org.junit.Test
     public void jdkVersionNotFound()  {
         List<ModelProperty> modelProperties = new ArrayList<ModelProperty>();
