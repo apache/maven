df550f14b16ce4826454beaf05d09808d6f4e0bf
Benjamin Bentmann <bentmann@apache.org>
2009-03-05 23:22:12 +0000

    o Moved repository declarations to settings
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@750673 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng0956ComponentInjectionViaProjectLevelPluginDepTest.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng0956ComponentInjectionViaProjectLevelPluginDepTest.java
@@ -50,6 +50,9 @@ public class MavenITmng0956ComponentInjectionViaProjectLevelPluginDepTest
         verifier.setAutoclean( false );
         verifier.deleteDirectory( "target" );
         verifier.deleteArtifacts( "org.apache.maven.its.it0081" );
+        verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", verifier.newDefaultFilterProperties() );
+        verifier.getCliOptions().add( "--settings" );
+        verifier.getCliOptions().add( "settings.xml" );
         verifier.executeGoal( "validate" );
         verifier.verifyErrorFreeLog();
         verifier.resetStreams();
