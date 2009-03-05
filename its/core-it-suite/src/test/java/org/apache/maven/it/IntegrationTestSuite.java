df41e8f53536be3e479e0f9dd569fb3df3bea60a
Benjamin Bentmann <bentmann@apache.org>
2009-03-05 21:44:15 +0000

    [MNG-4068] [regression] Use proper id for mirrored repository
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@750609 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
@@ -91,6 +91,7 @@ public class IntegrationTestSuite
         // suite.addTestSuite( MavenIT0109ReleaseUpdateTest.class );
         // suite.addTestSuite( MavenIT0108SnapshotUpdateTest.class ); -- MNG-3137
 
+        suite.addTestSuite( MavenITmng4068AuthenticatedMirrorTest.class );
         suite.addTestSuite( MavenITmng4056ClassifierBasedDepResolutionFromReactorTest.class );
         suite.addTestSuite( MavenITmng4053PluginConfigAttributesTest.class );
         suite.addTestSuite( MavenITmng4040ProfileInjectedModulesTest.class );
---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4068AuthenticatedMirrorTest.java
@@ -0,0 +1,131 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.util.Properties;
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.DefaultHandler;
+import org.mortbay.jetty.handler.HandlerList;
+import org.mortbay.jetty.handler.ResourceHandler;
+import org.mortbay.jetty.security.Constraint;
+import org.mortbay.jetty.security.ConstraintMapping;
+import org.mortbay.jetty.security.HashUserRealm;
+import org.mortbay.jetty.security.SecurityHandler;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-4068">MNG-4068</a>.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class MavenITmng4068AuthenticatedMirrorTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    private File testDir;
+
+    private Server server;
+
+    private int port;
+
+    public MavenITmng4068AuthenticatedMirrorTest()
+    {
+        super( ALL_MAVEN_VERSIONS );
+    }
+
+    public void setUp()
+        throws Exception
+    {
+        super.setUp();
+
+        testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-4068" );
+
+        Constraint constraint = new Constraint();
+        constraint.setName( Constraint.__BASIC_AUTH );
+        constraint.setRoles( new String[] { "user" } );
+        constraint.setAuthenticate( true );
+
+        ConstraintMapping constraintMapping = new ConstraintMapping();
+        constraintMapping.setConstraint( constraint );
+        constraintMapping.setPathSpec( "/*" );
+
+        HashUserRealm userRealm = new HashUserRealm( "TestRealm" );
+        userRealm.put( "testuser", "testtest" );
+        userRealm.addUserToRole( "testuser", "user" );
+
+        SecurityHandler securityHandler = new SecurityHandler();
+        securityHandler.setUserRealm( userRealm );
+        securityHandler.setConstraintMappings( new ConstraintMapping[] { constraintMapping } );
+
+        ResourceHandler repoHandler = new ResourceHandler();
+        repoHandler.setResourceBase( new File( testDir, "repo" ).getAbsolutePath() );
+
+        HandlerList handlerList = new HandlerList();
+        handlerList.addHandler( securityHandler );
+        handlerList.addHandler( repoHandler );
+        handlerList.addHandler( new DefaultHandler() );
+
+        server = new Server( 0 );
+        server.setHandler( handlerList );
+        server.start();
+
+        port = server.getConnectors()[0].getLocalPort();
+    }
+
+    protected void tearDown()
+        throws Exception
+    {
+        if ( server != null )
+        {
+            server.stop();
+        }
+
+        super.tearDown();
+    }
+
+    /**
+     * Test that downloading of release/snapshot artifacts from an authenticated mirror works. This basically boils down
+     * to using the proper id for the mirrored repository when looking up the credentials.
+     */
+    public void testit()
+        throws Exception
+    {
+        Properties filterProps = new Properties();
+        filterProps.setProperty( "@mirrorPort@", Integer.toString( port ) );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", filterProps );
+        verifier.setAutoclean( false );
+        verifier.deleteArtifacts( "org.apache.maven.its.mng4068" );
+        verifier.getCliOptions().add( "--settings" );
+        verifier.getCliOptions().add( "settings.xml" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        verifier.assertArtifactPresent( "org.apache.maven.its.mng4068", "a", "0.1", "jar" );
+        verifier.assertArtifactPresent( "org.apache.maven.its.mng4068", "b", "0.1-SNAPSHOT", "jar" );
+    }
+
+}
