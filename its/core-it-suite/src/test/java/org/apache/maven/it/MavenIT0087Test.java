c3f2a06650ca4a6d3941d5485825049fc15777d4
Benjamin Bentmann <bentmann@apache.org>
2009-03-05 22:45:59 +0000

    o Moved repository declarations to settings
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@750647 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-it-suite/src/test/java/org/apache/maven/it/MavenIT0087Test.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenIT0087Test.java
@@ -52,6 +52,9 @@ public class MavenIT0087Test
         verifier.setAutoclean( false );
         verifier.deleteDirectory( "target" );
         verifier.deleteArtifacts( "org.apache.maven.its.it0087" );
+        verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", verifier.newDefaultFilterProperties() );
+        verifier.getCliOptions().add( "--settings" );
+        verifier.getCliOptions().add( "settings.xml" );
         verifier.executeGoal( "validate" );
         verifier.verifyErrorFreeLog();
         verifier.resetStreams();
