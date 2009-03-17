2e0ff48d0c54d47b6b4a9188ab49c7ac8c6ba848
Brett Porter <brett@apache.org>
2009-03-17 02:28:08 +0000

    [MNG-4091] add test for bad plugin descriptor handling
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@755085 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4091BadPluginDescriptorTest.java
@@ -0,0 +1,82 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.maven.it.util.ResourceExtractor;
+
+/**
+ * MNG-4091 - Bad plugin descriptor error handling
+ */
+public class MavenITmng4091BadPluginDescriptorTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng4091BadPluginDescriptorTest()
+    {
+        super( "[2.1.0,)" ); // only test in 2.1.0+
+    }
+
+    public void testitMNG4091()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-4091" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+
+        try
+        {
+            verifier.executeGoal( "validate" );
+
+            fail( "should throw an error during execution." );
+        }
+        catch ( VerificationException e )
+        {
+            // expected...it'd be nice if we could get the specifics of the exception right here...
+        }
+        finally
+        {
+            verifier.resetStreams();
+        }
+
+
+        List logFile = verifier.loadFile( verifier.getBasedir(), verifier.getLogFileName(), false );
+
+        String msg = "Plugin's descriptor contains the wrong version: 2.0-SNAPSHOT";
+
+        boolean foundMessage = false;
+        for ( Iterator it = logFile.iterator(); it.hasNext(); )
+        {
+            String line = (String) it.next();
+            if ( line.indexOf( msg ) > -1 )
+            {
+                foundMessage = true;
+                break;
+            }
+        }
+
+        assertTrue( "User-friendly message was not found in output.", foundMessage );
+    }
+}
+
