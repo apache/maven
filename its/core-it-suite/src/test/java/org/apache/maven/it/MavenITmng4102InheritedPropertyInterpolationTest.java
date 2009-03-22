79d44ad7bc971fc87379187e5c97aea7691c196d
Benjamin Bentmann <bentmann@apache.org>
2009-03-22 22:00:45 +0000

    [MNG-4102] [regression] Properties used for interpolation of inherited properties can't be customized by child
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@757262 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4102InheritedPropertyInterpolationTest.java
@@ -0,0 +1,64 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-4102">MNG-4102</a>.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class MavenITmng4102InheritedPropertyInterpolationTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng4102InheritedPropertyInterpolationTest()
+    {
+        super( ALL_MAVEN_VERSIONS );
+    }
+
+    /**
+     * Verify that the effective value of an inherited property reflects the values of any nested property
+     * as defined by the child. This boils down to the order of inheritance and (parent) interpolation.
+     */
+    public void testitMNG4102()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-4102" );
+
+        Verifier verifier = new Verifier( new File( testDir, "sub" ).getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        Properties props = verifier.loadProperties( "target/pom.properties" );
+        assertEquals( "CHILD", props.getProperty( "project.properties.overridden" ) );
+        assertEquals( "CHILD", props.getProperty( "project.properties.interpolated" ) );
+    }
+
+}
