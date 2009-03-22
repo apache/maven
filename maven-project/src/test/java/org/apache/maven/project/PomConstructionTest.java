ca9d6e96b98ae32d225ba9ee085b8f43b2f90379
Benjamin Bentmann <bentmann@apache.org>
2009-03-22 10:37:56 +0000

    [MNG-3846] [regression] Inherited URLs are not automatically extended with the child's artifactId
    o Extended UT to test deeper inheritance hierarchy
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@757168 13f79535-47bb-0310-9956-ffa450edef68

--- maven-project/src/test/java/org/apache/maven/project/PomConstructionTest.java
+++ maven-project/src/test/java/org/apache/maven/project/PomConstructionTest.java
@@ -563,6 +563,25 @@ public class PomConstructionTest
         assertEquals( "http://parent.url/download", pom.getValue( "distributionManagement/downloadUrl" ) );
     }
 
+    /* FIXME: MNG-3846
+    public void testAppendArtifactIdOfParentAndChildToInheritedUrls()
+        throws Exception
+    {
+        PomTestWrapper pom = buildPom( "url-inheritance/another-parent/sub" );
+        assertEquals( "http://parent.url/ap/child", pom.getValue( "url" ) );
+        assertEquals( "http://parent.url/org/", pom.getValue( "organization/url" ) );
+        assertEquals( "http://parent.url/license.txt", pom.getValue( "licenses[1]/url" ) );
+        assertEquals( "http://parent.url/viewvc/ap/child", pom.getValue( "scm/url" ) );
+        assertEquals( "http://parent.url/scm/ap/child", pom.getValue( "scm/connection" ) );
+        assertEquals( "https://parent.url/scm/ap/child", pom.getValue( "scm/developerConnection" ) );
+        assertEquals( "http://parent.url/issues", pom.getValue( "issueManagement/url" ) );
+        assertEquals( "http://parent.url/ci", pom.getValue( "ciManagement/url" ) );
+        assertEquals( "http://parent.url/dist", pom.getValue( "distributionManagement/repository/url" ) );
+        assertEquals( "http://parent.url/snaps", pom.getValue( "distributionManagement/snapshotRepository/url" ) );
+        assertEquals( "http://parent.url/site/ap/child", pom.getValue( "distributionManagement/site/url" ) );
+        assertEquals( "http://parent.url/download", pom.getValue( "distributionManagement/downloadUrl" ) );
+    }
+    //*/
 
     public void testNonInheritedElementsInSubtreesOverriddenByChild()
         throws Exception
