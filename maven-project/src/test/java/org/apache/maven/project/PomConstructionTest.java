f5167ad05a992517f276647614fbb32c995ad88d
Britton Isbell <sisbell@apache.org>
2009-03-25 01:41:14 +0000

    Unit test was incorrect. Fixed it.
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@758122 13f79535-47bb-0310-9956-ffa450edef68

--- maven-project/src/test/java/org/apache/maven/project/PomConstructionTest.java
+++ maven-project/src/test/java/org/apache/maven/project/PomConstructionTest.java
@@ -287,17 +287,17 @@ public class PomConstructionTest
 } 
 */
 
-    /* FIX - REGRESSION
+
     public void testSingleConfigurationInheritance()
         throws Exception
     {
         PomTestWrapper pom = buildPom( "single-configuration-inheritance" );
-        System.out.println(pom.getDomainModel().asString());
+
         assertEquals( 2, ( (List<?>) pom.getValue( "build/plugins[1]/executions[1]/configuration[1]/rules" ) ).size() );
         assertEquals("2.0.6", pom.getValue( "build/plugins[1]/executions[1]/configuration[1]/rules[1]/requireMavenVersion[1]/version" ) );
-        assertEquals("[2.0.6,)", pom.getValue( "build/plugins[1]/executions[1]/configuration[1]/rules[2]/requireMavenVersion[1]/version" ) );
+        assertEquals("[1.4,)", pom.getValue( "build/plugins[1]/executions[1]/configuration[1]/rules[1]/requireJavaVersion[1]/version" ) );
     }
-*/
+
     public void testConfigWithPluginManagement()
         throws Exception
     {
