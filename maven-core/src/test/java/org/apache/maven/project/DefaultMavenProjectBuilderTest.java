e6fa6d26b4b1b797684239ef76ab59cdc7ee2dd8
Benjamin Bentmann <bentmann@apache.org>
2009-05-12 12:48:41 +0000

    o Extended UT to capture MNG-3719 as well
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/branches/MNG-2766@773870 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/test/java/org/apache/maven/project/DefaultMavenProjectBuilderTest.java
+++ maven-core/src/test/java/org/apache/maven/project/DefaultMavenProjectBuilderTest.java
@@ -101,7 +101,9 @@ public class DefaultMavenProjectBuilderTest
         File f1 = getTestFile( "src/test/resources/projects/duplicate-plugins-merged-pom.xml" );
 
         MavenProject project = getProject( f1 );
-        assertEquals( 2, ( (Plugin) project.getBuildPlugins().get( 0 ) ).getDependencies().size() );
+        assertEquals( 2, project.getBuildPlugins().get( 0 ).getDependencies().size() );
+        assertEquals( 2, project.getBuildPlugins().get( 0 ).getExecutions().size() );
+        assertEquals( "first", project.getBuildPlugins().get( 0 ).getExecutions().get( 0 ).getId() );
     }
 
 
