c4560558838b320604eb814560125235b76e6bc0
Benjamin Bentmann <bentmann@apache.org>
2009-05-14 21:02:10 +0000

    o Restored basedir alignment
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/branches/MNG-2766@774925 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/main/java/org/apache/maven/plugin/PluginParameterExpressionEvaluator.java
+++ maven-core/src/main/java/org/apache/maven/plugin/PluginParameterExpressionEvaluator.java
@@ -344,6 +344,26 @@ public class PluginParameterExpressionEvaluator
 
     public File alignToBaseDirectory( File file )
     {
+        // TODO: Copied from the DefaultInterpolator. We likely want to resurrect the PathTranslator or at least a
+        // similar component for re-usage
+        if ( file != null )
+        {
+            if ( file.isAbsolute() )
+            {
+                // path was already absolute, just normalize file separator and we're done
+            }
+            else if ( file.getPath().startsWith( File.separator ) )
+            {
+                // drive-relative Windows path, don't align with project directory but with drive root
+                file = file.getAbsoluteFile();
+            }
+            else
+            {
+                // an ordinary relative path, align with project directory
+                file = new File( new File( basedir, file.getPath() ).toURI().normalize() ).getAbsoluteFile();
+            }
+        }
         return file;
     }
+
 }
