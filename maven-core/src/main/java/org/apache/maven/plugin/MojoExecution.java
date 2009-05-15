7612c0e0202250f8b703d361a3050ccfbaec3aa3
Benjamin Bentmann <bentmann@apache.org>
2009-05-15 20:20:32 +0000

    o Added toString() to ease debugging
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/branches/MNG-2766@775311 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/main/java/org/apache/maven/plugin/MojoExecution.java
+++ maven-core/src/main/java/org/apache/maven/plugin/MojoExecution.java
@@ -102,4 +102,17 @@ public class MojoExecution
     {
         this.lifecyclePhase = lifecyclePhase;
     }        
+
+    @Override
+    public String toString()
+    {
+        StringBuilder buffer = new StringBuilder( 128 );
+        if ( mojoDescriptor != null )
+        {
+            buffer.append( mojoDescriptor.getId() );
+        }
+        buffer.append( " {execution: " ).append( executionId ).append( "}" );
+        return buffer.toString();
+    }
+
 }
