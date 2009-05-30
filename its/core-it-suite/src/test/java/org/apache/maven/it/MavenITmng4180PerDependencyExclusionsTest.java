6ed90963b1f5682c22a9334f480768242cb0667d
Benjamin Bentmann <bentmann@apache.org>
2009-05-30 12:00:12 +0000

    [MNG-4180] [regression] Dependency exclusions are applied globally
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@780248 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4180PerDependencyExclusionsTest.java
@@ -0,0 +1,75 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-4180">MNG-4180</a>.
+ * 
+ * @author Benjamin Bentmann
+ */
+public class MavenITmng4180PerDependencyExclusionsTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng4180PerDependencyExclusionsTest()
+    {
+        super( "[2.0.5,)" );
+    }
+
+    /**
+     * Test that dependency exclusions are not applied globally but are limited to the sub tree that is rooted at the
+     * dependency they are declared on.
+     */
+    public void testit()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-4180" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.deleteArtifacts( "org.apache.maven.its.mng4180" );
+        verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", verifier.newDefaultFilterProperties() );
+        verifier.getCliOptions().add( "-s" );
+        verifier.getCliOptions().add( "settings.xml" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        List artifacts = verifier.loadLines( "target/artifacts.txt", "UTF-8" );
+        Collections.sort( artifacts );
+
+        List expected = new ArrayList();
+        expected.add( "org.apache.maven.its.mng4180:a:jar:0.1" );
+        expected.add( "org.apache.maven.its.mng4180:b:jar:0.1" );
+        expected.add( "org.apache.maven.its.mng4180:c:jar:0.1" );
+
+        assertEquals( expected, artifacts );
+    }
+
+}
