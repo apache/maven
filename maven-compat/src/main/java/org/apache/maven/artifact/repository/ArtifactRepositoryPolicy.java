cdf805d662f31d3c6ed7d1a739baf8d237358b57
Benjamin Bentmann <bentmann@apache.org>
2009-06-12 17:28:22 +0000

    o Added toString() to ease debugging
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@784207 13f79535-47bb-0310-9956-ffa450edef68

--- maven-compat/src/main/java/org/apache/maven/artifact/repository/ArtifactRepositoryPolicy.java
+++ maven-compat/src/main/java/org/apache/maven/artifact/repository/ArtifactRepositoryPolicy.java
@@ -146,4 +146,19 @@ public class ArtifactRepositoryPolicy
         // else assume "never"
         return checkForUpdates;
     }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder buffer = new StringBuilder( 64 );
+        buffer.append( "{enabled=" );
+        buffer.append( enabled );
+        buffer.append( ", checksums=" );
+        buffer.append( checksumPolicy );
+        buffer.append( ", updates=" );
+        buffer.append( updatePolicy );
+        buffer.append( "}" );
+        return buffer.toString();
+    }
+
 }
