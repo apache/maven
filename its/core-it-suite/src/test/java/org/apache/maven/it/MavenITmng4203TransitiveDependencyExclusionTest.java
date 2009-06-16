a405737512d4c7d2d49ac5698de1831a3f0d9664
Benjamin Bentmann <bentmann@apache.org>
2009-06-16 19:46:07 +0000

    [MNG-4203] [regression] Dependency exclusions are not applied transitively
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@785359 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4203TransitiveDependencyExclusionTest.java
@@ -0,0 +1,76 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-4203">MNG-4203</a>.
+ * 
+ * @author Benjamin Bentmann
+ */
+public class MavenITmng4203TransitiveDependencyExclusionTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng4203TransitiveDependencyExclusionTest()
+    {
+        super( ALL_MAVEN_VERSIONS );
+    }
+
+    /**
+     * Test that exclusions defined on a dependency apply to its transitive dependencies as well.
+     */
+    public void testit()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-4203" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.deleteArtifacts( "org.apache.maven.its.mng4203" );
+        verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", verifier.newDefaultFilterProperties() );
+        verifier.getCliOptions().add( "-s" );
+        verifier.getCliOptions().add( "settings.xml" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        List artifacts = verifier.loadLines( "target/artifacts.txt", "UTF-8" );
+        Collections.sort( artifacts );
+
+        List expected = new ArrayList();
+        expected.add( "org.apache.maven.its.mng4203:b:jar:0.1" );
+        expected.add( "org.apache.maven.its.mng4203:c:jar:0.1" );
+
+        assertEquals( expected, artifacts );
+
+        verifier.assertArtifactNotPresent( "org.apache.maven.its.mng4203", "a", "0.1", "pom" );
+        verifier.assertArtifactNotPresent( "org.apache.maven.its.mng4203", "a", "0.1", "jar" );
+    }
+
+}
