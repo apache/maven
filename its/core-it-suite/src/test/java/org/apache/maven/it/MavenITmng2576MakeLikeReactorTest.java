8d7e199b3367d124c9b435056fae23abc7f884a9
Benjamin Bentmann <bentmann@apache.org>
2009-07-08 17:19:27 +0000

    o Fixed typo
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@792234 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng2576MakeLikeReactorTest.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng2576MakeLikeReactorTest.java
@@ -25,7 +25,7 @@ import org.apache.maven.it.Verifier;
 import org.apache.maven.it.util.ResourceExtractor;
 
 /**
- * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-4193">MNG-4193</a>.
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-2576">MNG-2576</a>.
  * 
  * @author Benjamin Bentmann
  */
