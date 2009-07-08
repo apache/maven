9577a2072103fd9b6eb371a5b18cef8894b97f01
Benjamin Bentmann <bentmann@apache.org>
2009-07-08 19:56:55 +0000

    [MNG-4231] Snapshots are not checked for updates
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@792283 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
@@ -91,6 +91,7 @@ public class IntegrationTestSuite
         // suite.addTestSuite( MavenIT0109ReleaseUpdateTest.class );
         // suite.addTestSuite( MavenIT0108SnapshotUpdateTest.class ); -- MNG-3137
 
+        suite.addTestSuite( MavenITmng4231SnapshotUpdatePolicyTest.class );
         suite.addTestSuite( MavenITmng4214MirroredParentSearchReposTest.class );
         suite.addTestSuite( MavenITmng4208InterpolationPrefersCliOverProjectPropsTest.class );
         suite.addTestSuite( MavenITmng4207PluginWithLog4JTest.class );
---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4231SnapshotUpdatePolicyTest.java
@@ -0,0 +1,119 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-4231">MNG-4231</a>.
+ * 
+ * @author Benjamin Bentmann
+ */
+public class MavenITmng4231SnapshotUpdatePolicyTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng4231SnapshotUpdatePolicyTest()
+    {
+        super( ALL_MAVEN_VERSIONS );
+    }
+
+    /**
+     * Test the update policy "always" for snapshot dependencies is respected.
+     */
+    public void testitAlways()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-4231" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteArtifacts( "org.apache.maven.its.mng4231" );
+        verifier.getCliOptions().add( "-s" );
+        verifier.getCliOptions().add( "settings.xml" );
+
+        Properties filterProps = verifier.newDefaultFilterProperties();
+        filterProps.setProperty( "@updates@", "always" );
+
+        filterProps.setProperty( "@repo@", "repo-1" );
+        verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", filterProps );
+        verifier.setLogFileName( "log-always-1.txt" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+
+        filterProps.setProperty( "@repo@", "repo-2" );
+        verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", filterProps );
+        verifier.setLogFileName( "log-always-2.txt" );
+        verifier.deleteDirectory( "target" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+
+        verifier.resetStreams();
+
+        Properties checksums = verifier.loadProperties( "target/checksum.properties" );
+        assertChecksum( "db3f17644e813af768ae6e82a6d0a2f29aef8988", checksums );
+    }
+
+    /**
+     * Test the update policy "never" for snapshot dependencies is respected.
+     */
+    public void testitNever()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-4231" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteArtifacts( "org.apache.maven.its.mng4231" );
+        verifier.getCliOptions().add( "-s" );
+        verifier.getCliOptions().add( "settings.xml" );
+
+        Properties filterProps = verifier.newDefaultFilterProperties();
+        filterProps.setProperty( "@updates@", "never" );
+
+        filterProps.setProperty( "@repo@", "repo-1" );
+        verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", filterProps );
+        verifier.setLogFileName( "log-never-1.txt" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+
+        filterProps.setProperty( "@repo@", "repo-2" );
+        verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", filterProps );
+        verifier.setLogFileName( "log-never-2.txt" );
+        verifier.deleteDirectory( "target" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+
+        verifier.resetStreams();
+
+        Properties checksums = verifier.loadProperties( "target/checksum.properties" );
+        assertChecksum( "97c4e1a857d8957594ccbd2ac19d51885478ec80", checksums );
+    }
+
+    private void assertChecksum( String checksum, Properties checksums )
+    {
+        assertEquals( checksum, checksums.getProperty( "a-0.1-SNAPSHOT.jar" ).toLowerCase( java.util.Locale.ENGLISH ) );
+    }
+
+}
