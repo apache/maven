03c8b58235443055de8c7301c009476bbeb186fe
Benjamin Bentmann <bentmann@apache.org>
2009-07-22 10:03:53 +0000

    o Fixed resolution of plugin default config. The project building request provides only the base set of repos, not the final set. In addition to the base set, the final repo list contains POM repos and repos from external profiles. Therefore, the MavenProject instance (which holds the final repo set) has to be queried for the repos.
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@796657 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/main/java/org/apache/maven/project/DefaultProjectBuilder.java
+++ maven-core/src/main/java/org/apache/maven/project/DefaultProjectBuilder.java
@@ -140,8 +140,6 @@ public class DefaultProjectBuilder
 
             project.setOriginalModel( result.getRawModel() );
 
-//            project.setRemoteArtifactRepositories( configuration.getRemoteRepositories() );
-//            project.setPluginArtifactRepositories( configuration.getPluginArtifactRepositories() );
             project.setRemoteArtifactRepositories( listener.getRemoteRepositories() );
             project.setPluginArtifactRepositories( listener.getPluginRepositories() );
 
@@ -153,7 +151,7 @@ public class DefaultProjectBuilder
                 {
                     lifecycle.populateDefaultConfigurationForPlugins( model.getBuild().getPlugins(),
                                                                       configuration.getLocalRepository(),
-                                                                      configuration.getPluginArtifactRepositories() );
+                                                                      project.getPluginArtifactRepositories() );
                 }
             }
             catch ( LifecycleExecutionException e )
