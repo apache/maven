b2a9420e63387eb5ce2cb211ae6a80a607e4651e
Olivier Lamy <olamy@apache.org>
2009-07-25 19:42:15 +0000

    remove unused imports. fix license headers. remove empty class.
    git-svn-id: https://svn.apache.org/repos/asf/maven/components/trunk@797828 13f79535-47bb-0310-9956-ffa450edef68

--- maven-compat/src/main/java/org/apache/maven/artifact/manager/DefaultWagonManager.java
+++ maven-compat/src/main/java/org/apache/maven/artifact/manager/DefaultWagonManager.java
@@ -1,6 +1,24 @@
 package org.apache.maven.artifact.manager;
 
-import org.apache.maven.artifact.handler.ArtifactHandler;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import org.codehaus.plexus.component.annotations.Component;
 
 @Component(role=WagonManager.class) 
@@ -8,4 +26,5 @@ public class DefaultWagonManager
     extends org.apache.maven.repository.legacy.DefaultWagonManager
     implements WagonManager
 {
+    // nothing to do 
 }
--- maven-compat/src/main/java/org/apache/maven/artifact/manager/WagonManager.java
+++ maven-compat/src/main/java/org/apache/maven/artifact/manager/WagonManager.java
@@ -19,25 +19,6 @@ package org.apache.maven.artifact.manager;
  * under the License.
  */
 
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.metadata.ArtifactMetadata;
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.repository.legacy.WagonConfigurationException;
-import org.apache.maven.wagon.ResourceDoesNotExistException;
-import org.apache.maven.wagon.TransferFailedException;
-import org.apache.maven.wagon.UnsupportedProtocolException;
-import org.apache.maven.wagon.Wagon;
-import org.apache.maven.wagon.authentication.AuthenticationInfo;
-import org.apache.maven.wagon.events.TransferListener;
-import org.apache.maven.wagon.proxy.ProxyInfo;
-import org.apache.maven.wagon.repository.Repository;
-import org.apache.maven.wagon.repository.RepositoryPermissions;
-import org.codehaus.plexus.PlexusContainer;
-import org.codehaus.plexus.util.xml.Xpp3Dom;
-
-import java.io.File;
-import java.util.Collection;
-import java.util.List;
 
 /**
  * Manages <a href="http://maven.apache.org/wagon">Wagon</a> related operations in Maven.
@@ -49,4 +30,5 @@ import java.util.List;
 public interface WagonManager
     extends org.apache.maven.repository.legacy.WagonManager
 {
+    // nothing to do 
 }
