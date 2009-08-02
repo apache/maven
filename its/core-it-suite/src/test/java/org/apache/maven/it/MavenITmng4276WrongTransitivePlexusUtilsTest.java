a1a0bb1339923abfc9a4dcbd65e3bfb16fb1e62c
Benjamin Bentmann <bentmann@apache.org>
2009-08-02 11:01:52 +0000

    [MNG-4276] Plugins with indirect dependency on plexus-utils are stuck with plexus-utils:1.1
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@800027 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4276WrongTransitivePlexusUtilsTest.java
@@ -0,0 +1,68 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-4276">MNG-4276</a>.
+ *
+ * @author Benjamin Bentmann
+ */
+public class MavenITmng4276WrongTransitivePlexusUtilsTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng4276WrongTransitivePlexusUtilsTest()
+    {
+        super( "[3.0-alpha-3,)" );
+    }
+
+    /**
+     * Verify that plugins that have a *transitive* dependency on plexus-utils:x.y get that version and not a random
+     * version injected by the core like 1.1.
+     */
+    public void testit()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-4276" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.deleteArtifacts( "org.apache.maven.its.mng4276" );
+        verifier.deleteArtifact( "org.codehaus.plexus", "plexus-utils", "1.1.4276", "jar" );
+        verifier.deleteArtifact( "org.codehaus.plexus", "plexus-utils", "1.1.4276", "pom" );
+        verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", verifier.newDefaultFilterProperties() );
+        verifier.getCliOptions().add( "-s" );
+        verifier.getCliOptions().add( "settings.xml" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        Properties props = verifier.loadProperties( "target/class.properties" );
+        assertNotNull( props.getProperty( "org.apache.maven.its.mng4276.CoreIt" ) );
+    }
+
+}
