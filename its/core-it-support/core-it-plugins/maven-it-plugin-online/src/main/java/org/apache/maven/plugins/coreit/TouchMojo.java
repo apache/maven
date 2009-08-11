bb004bd928c900e028144bf8f3ad7a5307f0e45e
Benjamin Bentmann <bentmann@apache.org>
2009-08-11 13:54:45 +0000

    o Added IT plugin to test @requiresOnline
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@803123 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-support/core-it-plugins/maven-it-plugin-online/src/main/java/org/apache/maven/plugins/coreit/TouchMojo.java
@@ -0,0 +1,71 @@
+package org.apache.maven.plugin.coreit;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Creates an empty file with a user-specified name. NOTE: This mojo requires online mode.
+ * 
+ * @goal touch
+ * @phase validate
+ * @requiresOnline true
+ * 
+ * @author Benjamin Bentmann
+ */
+public class TouchMojo
+    extends AbstractMojo
+{
+
+    /**
+     * The path to the output file.
+     * 
+     * @parameter expression="${touch.outputFile}" default-value="target/touch.txt"
+     */
+    private File outputFile;
+
+    /**
+     * Runs this mojo.
+     * 
+     * @throws MojoExecutionException If the output file could not be created.
+     */
+    public void execute()
+        throws MojoExecutionException
+    {
+        getLog().info( "[MAVEN-CORE-IT-LOG] Creating output file: " + outputFile );
+
+        try
+        {
+            outputFile.getParentFile().mkdirs();
+            outputFile.createNewFile();
+        }
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException( "Output file could not be created: " + outputFile, e );
+        }
+
+        getLog().info( "[MAVEN-CORE-IT-LOG] Created output file: " + outputFile );
+    }
+
+}
