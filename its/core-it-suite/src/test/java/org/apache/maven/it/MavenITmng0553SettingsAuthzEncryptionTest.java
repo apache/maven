7ccdc164290114cebfebaa4d7140d0abcf899060
Benjamin Bentmann <bentmann@apache.org>
2009-08-12 13:36:16 +0000

    o Decoupled IT from core API details like wagon manager and tested effective repo access instead
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@803503 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng0553SettingsAuthzEncryptionTest.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng0553SettingsAuthzEncryptionTest.java
@@ -23,18 +23,88 @@ import org.apache.maven.it.Verifier;
 import org.apache.maven.it.util.ResourceExtractor;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Properties;
 
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.DefaultHandler;
+import org.mortbay.jetty.handler.HandlerList;
+import org.mortbay.jetty.handler.ResourceHandler;
+import org.mortbay.jetty.security.Constraint;
+import org.mortbay.jetty.security.ConstraintMapping;
+import org.mortbay.jetty.security.HashUserRealm;
+import org.mortbay.jetty.security.SecurityHandler;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-553">MNG-553</a>.
+ * 
+ * @author Benjamin Bentmann
+ */
 public class MavenITmng0553SettingsAuthzEncryptionTest
     extends AbstractMavenIntegrationTestCase
 {
+
+    private File testDir;
+
+    private Server server;
+
+    private int port;
+
     public MavenITmng0553SettingsAuthzEncryptionTest()
     {
         // TODO: reintroduce for 3.0
         super( "(2.1.0-M1,3.0-alpha-1)" ); // 2.1.0-M2+
+    }
+
+    public void setUp()
+        throws Exception
+    {
+        super.setUp();
+
+        testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-0553" );
+
+        Constraint constraint = new Constraint();
+        constraint.setName( Constraint.__BASIC_AUTH );
+        constraint.setRoles( new String[] { "user" } );
+        constraint.setAuthenticate( true );
+
+        ConstraintMapping constraintMapping = new ConstraintMapping();
+        constraintMapping.setConstraint( constraint );
+        constraintMapping.setPathSpec( "/*" );
+
+        HashUserRealm userRealm = new HashUserRealm( "TestRealm" );
+        userRealm.put( "testuser", "testtest" );
+        userRealm.addUserToRole( "testuser", "user" );
+
+        SecurityHandler securityHandler = new SecurityHandler();
+        securityHandler.setUserRealm( userRealm );
+        securityHandler.setConstraintMappings( new ConstraintMapping[] { constraintMapping } );
+
+        ResourceHandler repoHandler = new ResourceHandler();
+        repoHandler.setResourceBase( new File( testDir, "repo" ).getAbsolutePath() );
+
+        HandlerList handlerList = new HandlerList();
+        handlerList.addHandler( securityHandler );
+        handlerList.addHandler( repoHandler );
+        handlerList.addHandler( new DefaultHandler() );
+
+        server = new Server( 0 );
+        server.setHandler( handlerList );
+        server.start();
+
+        port = server.getConnectors()[0].getLocalPort();
+    }
+
+    protected void tearDown()
+        throws Exception
+    {
+        if ( server != null )
+        {
+            server.stop();
+            server = null;
+        }
+
+        super.tearDown();
     }
 
     /**
@@ -43,23 +113,22 @@ public class MavenITmng0553SettingsAuthzEncryptionTest
     public void testitBasic()
         throws Exception
     {
-        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-0553/test-1" );
+        Properties filterProps = new Properties();
+        filterProps.setProperty( "@port@", Integer.toString( port ) );
 
-        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        Verifier verifier = new Verifier( new File( testDir, "test-1" ).getAbsolutePath() );
         verifier.setAutoclean( false );
-        verifier.deleteDirectory( "target" );
-        List cliOptions = new ArrayList();
-        cliOptions.add( "--settings" );
-        cliOptions.add( "settings.xml" );
+        verifier.deleteArtifacts( "org.apache.maven.its.mng0553" );
+        verifier.assertArtifactNotPresent( "org.apache.maven.its.mng0553", "a", "0.1-SNAPSHOT", "jar" );
+        verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", filterProps );
         verifier.getSystemProperties().setProperty( "settings.security", "settings-security.xml" );
-        verifier.setCliOptions( cliOptions );
+        verifier.getCliOptions().add( "--settings" );
+        verifier.getCliOptions().add( "settings.xml" );
         verifier.executeGoal( "validate" );
         verifier.verifyErrorFreeLog();
         verifier.resetStreams();
 
-        Properties props = verifier.loadProperties( "target/auth.properties" );
-        assertEquals( "testuser", props.getProperty( "test.username" ) );
-        assertEquals( "testtest", props.getProperty( "test.password" ) );
+        verifier.assertArtifactPresent( "org.apache.maven.its.mng0553", "a", "0.1-SNAPSHOT", "jar" );
     }
 
     /**
@@ -69,32 +138,30 @@ public class MavenITmng0553SettingsAuthzEncryptionTest
     public void testitRelocation()
         throws Exception
     {
-        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-0553/test-2" );
-
-        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
-        verifier.setAutoclean( false );
-        verifier.deleteDirectory( "target" );
-
-        // NOTE: The upper-case scheme name is essential part of the test
-        String secUrl = "FILE://" + new File( testDir, "relocated-settings-security.xml" ).toURI().getRawPath();
         Properties filterProps = new Properties();
+        filterProps.setProperty( "@port@", Integer.toString( port ) );
+        // NOTE: The upper-case scheme name is essential part of the test
+        String secUrl = "FILE://" + new File( testDir, "test-2/relocated-settings-security.xml" ).toURI().getRawPath();
         filterProps.setProperty( "@relocation@", secUrl );
+
+        Verifier verifier = new Verifier( new File( testDir, "test-2" ).getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteArtifacts( "org.apache.maven.its.mng0553" );
+        verifier.assertArtifactNotPresent( "org.apache.maven.its.mng0553", "a", "0.1-SNAPSHOT", "jar" );
+
         // NOTE: The tilde ~ in the file name is essential part of the test
         verifier.filterFile( "security-template.xml", "settings~security.xml", "UTF-8", filterProps );
+        verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", filterProps );
 
-        List cliOptions = new ArrayList();
-        cliOptions.add( "--settings" );
-        cliOptions.add( "settings.xml" );
         verifier.getSystemProperties().setProperty( "settings.security", "settings~security.xml" );
-        verifier.setCliOptions( cliOptions );
+        verifier.getCliOptions().add( "--settings" );
+        verifier.getCliOptions().add( "settings.xml" );
         // NOTE: The selection of the Turkish language for the JVM locale is essential part of the test
         verifier.executeGoal( "validate", Collections.singletonMap( "MAVEN_OPTS", "-Duser.language=tr" ) );
         verifier.verifyErrorFreeLog();
         verifier.resetStreams();
 
-        Properties props = verifier.loadProperties( "target/auth.properties" );
-        assertEquals( "testuser", props.getProperty( "test.username" ) );
-        assertEquals( "testtest", props.getProperty( "test.password" ) );
+        verifier.assertArtifactPresent( "org.apache.maven.its.mng0553", "a", "0.1-SNAPSHOT", "jar" );
     }
 
 }
