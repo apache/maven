724131c8be4f7cb73e45ccf6ac733803b4b0e2ce
Benjamin Bentmann <bentmann@apache.org>
2009-08-12 10:43:41 +0000

    o Enabled IT for 3.x
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@803439 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng3535SelfReferentialPropertiesTest.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng3535SelfReferentialPropertiesTest.java
@@ -32,7 +32,7 @@ public class MavenITmng3535SelfReferentialPropertiesTest
 
     public MavenITmng3535SelfReferentialPropertiesTest()
     {
-        super( "[2.1.0-M1,3.0-alpha-1)" ); // handled differently in 3.0+
+        super( "[2.1.0-M1,3.0-alpha-1),[3.0-alpha-3,)" );
     }
 
     public void testitMNG3535_ShouldSucceed()
