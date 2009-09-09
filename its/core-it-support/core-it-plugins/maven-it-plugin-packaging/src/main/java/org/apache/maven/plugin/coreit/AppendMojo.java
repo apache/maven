df5a0d668ca20fef09fbd53e27bc2f3258a9d017
Benjamin Bentmann <bentmann@apache.org>
2009-09-09 21:46:34 +0000

    [MNG-4350] [regression] Multiple goals bound to the same phase in a lifecycle mapping get executed out of order
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@813129 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-support/core-it-plugins/maven-it-plugin-packaging/src/main/java/org/apache/maven/plugin/coreit/AppendMojo.java
@@ -0,0 +1,80 @@
+package org.apache.maven.plugin.coreit;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+/**
+ * Appends a message to an UTF-8 encoded plain text file.
+ * 
+ * @author Benjamin Bentmann
+ * @goal append
+ */
+public class AppendMojo
+    extends AbstractMojo
+{
+
+    /**
+     * @parameter expression="${append.message}"
+     */
+    private String message;
+
+    /**
+     * @parameter default-value="${project.build.directory}/log.txt"
+     */
+    private File outputFile;
+
+    public void execute()
+        throws MojoExecutionException
+    {
+        getLog().info( "[MAVEN-CORE-IT-LOG] Creating output file: " + outputFile );
+
+        try
+        {
+            outputFile.getParentFile().mkdirs();
+
+            getLog().info( "[MAVEN-CORE-IT-LOG]   " + message );
+
+            OutputStreamWriter writer = new OutputStreamWriter( new FileOutputStream( outputFile, true ), "UTF-8" );
+            try
+            {
+                writer.write( message );
+                writer.write( "\n" );
+            }
+            finally
+            {
+                writer.close();
+            }
+        }
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException( "Output file could not be created: " + outputFile, e );
+        }
+
+        getLog().info( "[MAVEN-CORE-IT-LOG] Created output file: " + outputFile );
+    }
+
+}
