ba912743fa4449644fd8821673c927de6ec6a67e
Herve Boutemy <hboutemy@apache.org>
2009-09-10 17:48:55 +0000

    fixed errors reported by Checkstyle
    git-svn-id: https://svn.apache.org/repos/asf/maven/maven-2/branches/maven-2.2.x@813527 13f79535-47bb-0310-9956-ffa450edef68

--- maven-compat/src/main/java/org/apache/maven/cli/compat/CompatibleMain.java
+++ maven-compat/src/main/java/org/apache/maven/cli/compat/CompatibleMain.java
@@ -24,7 +24,7 @@ import org.codehaus.classworlds.ClassWorld;
 
 /**
  * Main class used to shield the user from the rest of Maven in the event the user is using JDK < 1.5.
- * 
+ *
  * @since 2.2.0
  */
 public class CompatibleMain
@@ -47,16 +47,16 @@ public class CompatibleMain
         // ----------------------------------------------------------------------
         // Setup the command line parser
         // ----------------------------------------------------------------------
-        
+
         String javaVersion = System.getProperty( "java.specification.version", "1.5" );
-        if ( "1.4".equals( javaVersion ) || "1.3".equals( javaVersion ) 
+        if ( "1.4".equals( javaVersion ) || "1.3".equals( javaVersion )
              || "1.2".equals( javaVersion )  || "1.1".equals( javaVersion ) )
         {
-	        System.out.println( "Java specification version: " + javaVersion );
+            System.out.println( "Java specification version: " + javaVersion );
             System.err.println( "This release of Maven requires Java version 1.5 or greater." );
             return 1;
         }
-        
+
         return MavenCli.main( args, classWorld );
     }
 }
