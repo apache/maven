c2f7650c7bb431609774391f42c75f0f78a7dc5d
Herve Boutemy <hboutemy@apache.org>
2009-09-10 13:18:03 +0000

    use Java 5 generics (merged ftom maven-3 r804537)
    git-svn-id: https://svn.apache.org/repos/asf/maven/maven-2/branches/maven-2.2.x@813421 13f79535-47bb-0310-9956-ffa450edef68

--- maven-settings/src/main/java/org/apache/maven/settings/DefaultMavenSettingsBuilder.java
+++ maven-settings/src/main/java/org/apache/maven/settings/DefaultMavenSettingsBuilder.java
@@ -187,7 +187,7 @@ public class DefaultMavenSettingsBuilder
 
     private void activateDefaultProfiles( Settings settings )
     {
-        List activeProfiles = settings.getActiveProfiles();
+        List<String> activeProfiles = settings.getActiveProfiles();
 
         for ( Iterator profiles = settings.getProfiles().iterator(); profiles.hasNext(); )
         {
@@ -241,13 +241,13 @@ public class DefaultMavenSettingsBuilder
         // -------------------------------------------------------------------------------------
         // Alright, here's the justification for all the regexp wizardry below...
         //
-        // Continuum and other server-like apps may need to locate the user-level and 
+        // Continuum and other server-like apps may need to locate the user-level and
         // global-level settings somewhere other than ${user.home} and ${maven.home},
         // respectively. Using a simple replacement of these patterns will allow them
         // to specify the absolute path to these files in a customized components.xml
         // file. Ideally, we'd do full pattern-evaluation against the sysprops, but this
         // is a first step. There are several replacements below, in order to normalize
-        // the path character before we operate on the string as a regex input, and 
+        // the path character before we operate on the string as a regex input, and
         // in order to avoid surprises with the File construction...
         // -------------------------------------------------------------------------------------
 
@@ -270,7 +270,7 @@ public class DefaultMavenSettingsBuilder
             path = path.replaceAll( "\\\\", "/" );
             // ---------------------------------------------------------------------------------
             // I'm not sure if this last regexp was really intended to disallow the usage of
-            // network paths as user.home directory. Unfortunately it did. I removed it and 
+            // network paths as user.home directory. Unfortunately it did. I removed it and
             // have not detected any problems yet.
             // ---------------------------------------------------------------------------------
             // path = path.replaceAll( "//", "/" );
