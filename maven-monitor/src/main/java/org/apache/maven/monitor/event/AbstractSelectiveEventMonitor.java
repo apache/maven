26cb31404176b4f6aeaa9803969946f2bd9d3d89
Herve Boutemy <hboutemy@apache.org>
2009-09-10 18:12:57 +0000

    fixed errors reported by Checkstyle
    git-svn-id: https://svn.apache.org/repos/asf/maven/maven-2/branches/maven-2.2.x@813535 13f79535-47bb-0310-9956-ffa450edef68

--- maven-monitor/src/main/java/org/apache/maven/monitor/event/AbstractSelectiveEventMonitor.java
+++ maven-monitor/src/main/java/org/apache/maven/monitor/event/AbstractSelectiveEventMonitor.java
@@ -28,35 +28,35 @@ import java.util.List;
 public abstract class AbstractSelectiveEventMonitor
     implements EventMonitor
 {
-    
+
     private List boundStartEvents;
     private List boundErrorEvents;
     private List boundEndEvents;
 
-    protected AbstractSelectiveEventMonitor(String[] startEvents, String[] endEvents, String[] errorEvents)
+    protected AbstractSelectiveEventMonitor( String[] startEvents, String[] endEvents, String[] errorEvents )
     {
         this.boundStartEvents = Arrays.asList( startEvents );
-        
+
         this.boundEndEvents = Arrays.asList( endEvents );
-        
+
         this.boundErrorEvents = Arrays.asList( errorEvents );
     }
 
     public final void startEvent( String eventName, String target, long timestamp )
     {
-        if( boundStartEvents.contains( eventName ) )
+        if ( boundStartEvents.contains( eventName ) )
         {
             doStartEvent( eventName, target, timestamp );
         }
     }
-    
+
     protected void doStartEvent( String eventName, String target, long timestamp )
     {
     }
 
     public final void endEvent( String eventName, String target, long timestamp )
     {
-        if( boundEndEvents.contains( eventName ) )
+        if ( boundEndEvents.contains( eventName ) )
         {
             doEndEvent( eventName, target, timestamp );
         }
@@ -68,7 +68,7 @@ public abstract class AbstractSelectiveEventMonitor
 
     public final void errorEvent( String eventName, String target, long timestamp, Throwable cause )
     {
-        if( boundErrorEvents.contains( eventName ) )
+        if ( boundErrorEvents.contains( eventName ) )
         {
             doErrorEvent( eventName, target, timestamp, cause );
         }
