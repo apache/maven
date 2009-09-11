9c939c940063207df03aa3df156f6b9f4483f93e
Benjamin Bentmann <bentmann@apache.org>
2009-09-11 12:28:15 +0000

    o Enabled IT for Maven 3.x
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@813802 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng3052DepRepoAggregationTest.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng3052DepRepoAggregationTest.java
@@ -48,8 +48,7 @@ public class MavenITmng3052DepRepoAggregationTest
 
     public MavenITmng3052DepRepoAggregationTest()
     {
-        // TODO: fate of POM repositories in 3.x is unclear, disabled for now
-        super( "(2.0.9,3.0-alpha-1)" ); // only test in 2.0.10+
+        super( "(2.0.9,)" ); // only test in 2.0.10+
     }
 
     public void testitMNG3052 ()
