3fd317923a76c244c7d891f6910a66faa7caa559
John Dennis Casey <jdcasey@apache.org>
2009-09-11 14:05:08 +0000

    forgot to transfer the logger over in the copy constructor.
    git-svn-id: https://svn.apache.org/repos/asf/maven/maven-2/branches/maven-2.2.x@813837 13f79535-47bb-0310-9956-ffa450edef68

--- maven-project/src/main/java/org/apache/maven/project/artifact/MavenMetadataSource.java
+++ maven-project/src/main/java/org/apache/maven/project/artifact/MavenMetadataSource.java
@@ -100,6 +100,7 @@ public class MavenMetadataSource
         
         projectBuilderConfig = configuration;
         projectBuilderConfig.setMetadataSource( this );
+        enableLogging( getLogger() );
     }
     
     /**
