66f8e092750c2db6f5ab2077180a4838bb25fa85
John Dennis Casey <jdcasey@apache.org>
2009-09-11 14:10:27 +0000

    oops, gotta pull the logger from the metadata source PARAMETER...duh
    git-svn-id: https://svn.apache.org/repos/asf/maven/maven-2/branches/maven-2.2.x@813839 13f79535-47bb-0310-9956-ffa450edef68

--- maven-project/src/main/java/org/apache/maven/project/artifact/MavenMetadataSource.java
+++ maven-project/src/main/java/org/apache/maven/project/artifact/MavenMetadataSource.java
@@ -100,7 +100,7 @@ public class MavenMetadataSource
         
         projectBuilderConfig = configuration;
         projectBuilderConfig.setMetadataSource( this );
-        enableLogging( getLogger() );
+        enableLogging( mms.getLogger() );
     }
     
     /**
