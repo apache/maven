01a18ac944622601eaa3eb978b13613b8a2d3a7b
Benjamin Bentmann <bentmann@apache.org>
2009-09-14 15:19:31 +0000

    [MNG-4357] [regression] Custom packagings from build extensions are not reliably loaded during a reactor build
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@814691 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4357LifecycleMappingDiscoveryInReactorTest.java
@@ -0,0 +1,72 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-4357">MNG-4357</a>.
+ * 
+ * @author Benjamin Bentmann
+ */
+public class MavenITmng4357LifecycleMappingDiscoveryInReactorTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng4357LifecycleMappingDiscoveryInReactorTest()
+    {
+        super( "[2.1.0,)" );
+    }
+
+    /**
+     * Test that different projects in a reactor build can use different versions of the same extension.
+     * This should still hold true if the two versions of the extension provide the same set of components.
+     */
+    public void testit()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-4357" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.deleteDirectory( "mod-a/target" );
+        verifier.deleteDirectory( "mod-b/target" );
+        verifier.deleteArtifacts( "org.apache.maven.its.mng4357" );
+        Properties filterProps = verifier.newDefaultFilterProperties();
+        verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", filterProps );
+        verifier.getCliOptions().add( "--settings" );
+        verifier.getCliOptions().add( "settings.xml" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        verifier.assertFilePresent( "mod-a/target/old.txt" );
+        verifier.assertFileNotPresent( "mod-a/target/new.txt" );
+
+        verifier.assertFileNotPresent( "mod-b/target/old.txt" );
+        verifier.assertFilePresent( "mod-b/target/new.txt" );
+    }
+
+}
