00fa4413d2b6b3579b505af59e9172afb967f482
Benjamin Bentmann <bentmann@apache.org>
2009-10-11 21:22:35 +0000

    o Added parser-neutral facades for the Modello generated settings reader/writer
    git-svn-id: https://svn.apache.org/repos/asf/maven/maven-3/trunk@824168 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-core/src/main/java/org/apache/maven/settings/io/DefaultSettingsReader.java
@@ -0,0 +1,110 @@
+package org.apache.maven.settings.io;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.util.Map;
+
+import org.apache.maven.settings.Settings;
+import org.apache.maven.settings.io.xpp3.SettingsXpp3Reader;
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.util.IOUtil;
+import org.codehaus.plexus.util.ReaderFactory;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+
+/**
+ * Handles deserialization of settings from some kind of textual format like XML.
+ * 
+ * @author Benjamin Bentmann
+ */
+@Component( role = SettingsReader.class )
+public class DefaultSettingsReader
+    implements SettingsReader
+{
+
+    public Settings read( File input, Map<String, ?> options )
+        throws IOException
+    {
+        if ( input == null )
+        {
+            throw new IllegalArgumentException( "input file missing" );
+        }
+
+        Settings settings = read( ReaderFactory.newXmlReader( input ), options );
+
+        return settings;
+    }
+
+    public Settings read( Reader input, Map<String, ?> options )
+        throws IOException
+    {
+        if ( input == null )
+        {
+            throw new IllegalArgumentException( "input reader missing" );
+        }
+
+        try
+        {
+            SettingsXpp3Reader r = new SettingsXpp3Reader();
+            return r.read( input, isStrict( options ) );
+        }
+        catch ( XmlPullParserException e )
+        {
+            throw new SettingsParseException( e.getMessage(), e.getLineNumber(), e.getColumnNumber(), e );
+        }
+        finally
+        {
+            IOUtil.close( input );
+        }
+    }
+
+    public Settings read( InputStream input, Map<String, ?> options )
+        throws IOException
+    {
+        if ( input == null )
+        {
+            throw new IllegalArgumentException( "input stream missing" );
+        }
+
+        try
+        {
+            SettingsXpp3Reader r = new SettingsXpp3Reader();
+            return r.read( input, isStrict( options ) );
+        }
+        catch ( XmlPullParserException e )
+        {
+            throw new SettingsParseException( e.getMessage(), e.getLineNumber(), e.getColumnNumber(), e );
+        }
+        finally
+        {
+            IOUtil.close( input );
+        }
+    }
+
+    private boolean isStrict( Map<String, ?> options )
+    {
+        Object value = ( options != null ) ? options.get( IS_STRICT ) : null;
+        return value == null || Boolean.parseBoolean( value.toString() );
+    }
+
+}
---
+++ maven-core/src/main/java/org/apache/maven/settings/io/DefaultSettingsWriter.java
@@ -0,0 +1,115 @@
+package org.apache.maven.settings.io;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.Map;
+
+import org.apache.maven.settings.Settings;
+import org.apache.maven.settings.io.xpp3.SettingsXpp3Writer;
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.util.IOUtil;
+import org.codehaus.plexus.util.WriterFactory;
+
+/**
+ * Handles serialization of settings into some kind of textual format like XML.
+ * 
+ * @author Benjamin Bentmann
+ */
+@Component( role = SettingsWriter.class )
+public class DefaultSettingsWriter
+    implements SettingsWriter
+{
+
+    public void write( File output, Map<String, Object> options, Settings settings )
+        throws IOException
+    {
+        if ( output == null )
+        {
+            throw new IllegalArgumentException( "output file missing" );
+        }
+
+        if ( settings == null )
+        {
+            throw new IllegalArgumentException( "settings missing" );
+        }
+
+        output.getParentFile().mkdirs();
+
+        write( WriterFactory.newXmlWriter( output ), options, settings );
+    }
+
+    public void write( Writer output, Map<String, Object> options, Settings settings )
+        throws IOException
+    {
+        if ( output == null )
+        {
+            throw new IllegalArgumentException( "output writer missing" );
+        }
+
+        if ( settings == null )
+        {
+            throw new IllegalArgumentException( "settings missing" );
+        }
+
+        try
+        {
+            SettingsXpp3Writer w = new SettingsXpp3Writer();
+            w.write( output, settings );
+        }
+        finally
+        {
+            IOUtil.close( output );
+        }
+    }
+
+    public void write( OutputStream output, Map<String, Object> options, Settings settings )
+        throws IOException
+    {
+        if ( output == null )
+        {
+            throw new IllegalArgumentException( "output stream missing" );
+        }
+
+        if ( settings == null )
+        {
+            throw new IllegalArgumentException( "settings missing" );
+        }
+
+        try
+        {
+            String encoding = settings.getModelEncoding();
+            if ( encoding == null || encoding.length() <= 0 )
+            {
+                encoding = "UTF-8";
+            }
+            write( new OutputStreamWriter( output, encoding ), options, settings );
+        }
+        finally
+        {
+            IOUtil.close( output );
+        }
+    }
+
+}
