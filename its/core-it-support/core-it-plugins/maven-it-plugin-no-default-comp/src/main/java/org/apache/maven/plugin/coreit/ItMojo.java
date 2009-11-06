7d3a1d5d0cf4c3a42a7bbde9656fe947f60453ba
Benjamin Bentmann <bentmann@apache.org>
2009-11-06 11:45:38 +0000

    [MNG-4429] [regression] jspc plugin works in Maven 2.2.1, fails in 3.0
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@833357 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-support/core-it-plugins/maven-it-plugin-no-default-comp/src/main/java/org/apache/maven/plugin/coreit/ItMojo.java
@@ -0,0 +1,93 @@
+package org.apache.maven.plugin.coreit;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * Requires a component with a non-default role hint and dumps this hint to a properties file.
+ * 
+ * @goal it
+ * @phase initialize
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class ItMojo
+    extends AbstractMojo
+{
+
+    /**
+     * The path to the output file.
+     * 
+     * @parameter expression="${touch.outputFile}" default-value="target/comp.properties"
+     */
+    private File outputFile;
+
+    /**
+     * NOTE: We don't specify a role hint here!
+     * 
+     * @component
+     */
+    private Component component;
+
+    /**
+     * Runs this mojo.
+     * 
+     * @throws MojoExecutionException If the output file could not be created.
+     */
+    public void execute()
+        throws MojoExecutionException
+    {
+        getLog().info( "[MAVEN-CORE-IT-LOG] Using component: " + component );
+
+        Properties props = new Properties();
+        props.setProperty( "id", component.getId() );
+
+        getLog().info( "[MAVEN-CORE-IT-LOG] Creating output file: " + outputFile );
+
+        try
+        {
+            outputFile.getParentFile().mkdirs();
+            FileOutputStream os = new FileOutputStream( outputFile );
+            try
+            {
+                props.store( os, "MAVEN-CORE-IT-LOG" );
+            }
+            finally
+            {
+                os.close();
+            }
+        }
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException( "Output file could not be created: " + outputFile, e );
+        }
+
+        getLog().info( "[MAVEN-CORE-IT-LOG] Created output file: " + outputFile );
+    }
+
+}
