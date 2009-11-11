7e578bd047bc4f771a7fe0236f157e1c03cc8ad1
Benjamin Bentmann <bentmann@apache.org>
2009-11-11 12:23:29 +0000

    [MNG-4436] [regression] Singleton components can get instantianted multiple times
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@834849 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-support/core-it-plugins/maven-it-plugin-singleton-component/src/main/java/org/apache/maven/plugin/coreit/DefaultComponent.java
@@ -0,0 +1,45 @@
+package org.apache.maven.plugin.coreit;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * A stateful singleton.
+ * 
+ * @plexus.component role="org.apache.maven.plugin.coreit.Component" role-hint="default"
+ * 
+ * @author Benjamin Bentmann
+ */
+public class DefaultComponent
+    implements Component
+{
+
+    private final String id;
+
+    public DefaultComponent()
+    {
+        id = Integer.toString( System.identityHashCode( this ) );
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+}
