0cf9b1ae8c4f3ccea80d9135dc8e7f475599ae6d
Benjamin Bentmann <bentmann@apache.org>
2009-11-25 16:22:54 +0000

    [MNG-4469] [regression] deploy:deploy-file fails with secured repository
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@884174 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4469AuthenticatedDeploymentToCustomRepoTest.java
@@ -0,0 +1,155 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+import org.mortbay.jetty.Handler;
+import org.mortbay.jetty.Request;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.AbstractHandler;
+import org.mortbay.jetty.handler.HandlerList;
+import org.mortbay.jetty.security.Constraint;
+import org.mortbay.jetty.security.ConstraintMapping;
+import org.mortbay.jetty.security.HashUserRealm;
+import org.mortbay.jetty.security.SecurityHandler;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-4469">MNG-4469</a>.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class MavenITmng4469AuthenticatedDeploymentToCustomRepoTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    private Server server;
+
+    private int port;
+
+    private boolean deployed;
+
+    public MavenITmng4469AuthenticatedDeploymentToCustomRepoTest()
+    {
+        super( "[2.0.3,3.0-alpha-3),[3.0-alpha-6,)" );
+    }
+
+    public void setUp()
+        throws Exception
+    {
+        super.setUp();
+
+        Handler repoHandler = new AbstractHandler()
+        {
+            public void handle( String target, HttpServletRequest request, HttpServletResponse response, int dispatch )
+                throws IOException, ServletException
+            {
+                System.out.println( "Handling " + request.getMethod() + " " + request.getRequestURL() );
+
+                if ( "PUT".equalsIgnoreCase( request.getMethod() ) )
+                {
+                    response.setStatus( HttpServletResponse.SC_OK );
+                    deployed = true;
+                }
+                else
+                {
+                    response.setStatus( HttpServletResponse.SC_NOT_FOUND );
+                }
+
+                ( (Request) request ).setHandled( true );
+            }
+        };
+
+        Constraint constraint = new Constraint();
+        constraint.setName( Constraint.__BASIC_AUTH );
+        constraint.setRoles( new String[] { "deployer" } );
+        constraint.setAuthenticate( true );
+
+        ConstraintMapping constraintMapping = new ConstraintMapping();
+        constraintMapping.setConstraint( constraint );
+        constraintMapping.setPathSpec( "/*" );
+
+        HashUserRealm userRealm = new HashUserRealm( "TestRealm" );
+        userRealm.put( "testuser", "testtest" );
+        userRealm.addUserToRole( "testuser", "deployer" );
+
+        SecurityHandler securityHandler = new SecurityHandler();
+        securityHandler.setUserRealm( userRealm );
+        securityHandler.setConstraintMappings( new ConstraintMapping[] { constraintMapping } );
+
+        HandlerList handlerList = new HandlerList();
+        handlerList.addHandler( securityHandler );
+        handlerList.addHandler( repoHandler );
+
+        server = new Server( 0 );
+        server.setHandler( handlerList );
+        server.start();
+
+        port = server.getConnectors()[0].getLocalPort();
+
+        deployed = false;
+    }
+
+    protected void tearDown()
+        throws Exception
+    {
+        if ( server != null )
+        {
+            server.stop();
+            server = null;
+        }
+
+        super.tearDown();
+    }
+
+    /**
+     * Test that deployment to a custom repository (i.e. created by a plugin) that requires authentification works.
+     */
+    public void testit()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-4469" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.getCliOptions().add( "--settings" );
+        verifier.getCliOptions().add( "settings.xml" );
+        verifier.setSystemProperty( "file", "settings.xml" );
+        verifier.setSystemProperty( "groupId", "org.apache.maven.its.mng4469" );
+        verifier.setSystemProperty( "artifactId", "it" );
+        verifier.setSystemProperty( "version", "0.1" );
+        verifier.setSystemProperty( "repositoryId", "mng4469" );
+        verifier.setSystemProperty( "repositoryUrl", "http://localhost:" + port + "/repo" );
+        verifier.executeGoal( "org.apache.maven.its.plugins:maven-it-plugin-artifact:2.1-SNAPSHOT:deploy-file" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        assertTrue( deployed );
+    }
+
+}
---
+++ its/core-it-support/core-it-plugins/maven-it-plugin-artifact/src/main/java/org/apache/maven/plugin/coreit/DeployFileMojo.java
@@ -0,0 +1,152 @@
+package org.apache.maven.plugin.coreit;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.deployer.ArtifactDeployer;
+import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.repository.ArtifactRepositoryFactory;
+import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+import java.io.File;
+
+/**
+ * Deploys a user-supplied file to some repository. This mimics part of the Maven Deploy Plugin.
+ * 
+ * @goal deploy-file
+ * @requiresProject false
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class DeployFileMojo
+    extends AbstractMojo
+{
+
+    /**
+     * The file of the artifact to deploy.
+     * 
+     * @parameter expression="${file}"
+     */
+    private File file;
+
+    /**
+     * The group id of the artifact.
+     * 
+     * @parameter expression="${groupId}"
+     */
+    private String groupId;
+
+    /**
+     * The artifact id of the artifact.
+     * 
+     * @parameter expression="${artifactId}"
+     */
+    private String artifactId;
+
+    /**
+     * The version of the artifact.
+     * 
+     * @parameter expression="${version}"
+     */
+    private String version;
+
+    /**
+     * The URL of the repository to deploy to.
+     * 
+     * @parameter expression="${repositoryUrl}"
+     */
+    private String repositoryUrl;
+
+    /**
+     * The ID of the repository to deploy to.
+     * 
+     * @parameter expression="${repositoryId}"
+     */
+    private String repositoryId;
+
+    /**
+     * The repository factory.
+     * 
+     * @component
+     */
+    private ArtifactRepositoryFactory repositoryFactory;
+
+    /**
+     * The repository layout.
+     * 
+     * @component roleHint="default"
+     */
+    private ArtifactRepositoryLayout repositoryLayout;
+
+    /**
+     * The artifact factory.
+     * 
+     * @component
+     */
+    private ArtifactFactory artifactFactory;
+
+    /**
+     * The artifact deployer.
+     * 
+     * @component
+     */
+    private ArtifactDeployer deployer;
+
+    /**
+     * The local repository.
+     * 
+     * @parameter default-value="${localRepository}"
+     * @readonly
+     * @required
+     */
+    private ArtifactRepository localRepository;
+
+    /**
+     * Runs this mojo.
+     * 
+     * @throws MojoExecutionException If any artifact could not be deployed.
+     */
+    public void execute()
+        throws MojoExecutionException, MojoFailureException
+    {
+        getLog().info( "[MAVEN-CORE-IT-LOG] Deploying artifacts" );
+
+        try
+        {
+            ArtifactRepository repository =
+                repositoryFactory.createDeploymentArtifactRepository( repositoryId, repositoryUrl, repositoryLayout,
+                                                                      true );
+
+            Artifact artifact = artifactFactory.createArtifact( groupId, artifactId, version, null, "jar" );
+
+            deployer.deploy( file, artifact, repository, localRepository );
+        }
+        catch ( Exception e )
+        {
+            throw new MojoExecutionException( "Failed to deploy artifacts: " + e.getMessage(), e );
+        }
+    }
+
+}
