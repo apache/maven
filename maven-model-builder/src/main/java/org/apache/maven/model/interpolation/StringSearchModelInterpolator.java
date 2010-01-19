apache/maven

commit 03a383e30e4e3a866bc2a7983d1b58fbd05f0fe5

[MNG-4529] maven fails on IBM JDK 1.5.0 with exception IllegalAccessException: Field is final
git-svn-id: https://svn.apache.org/repos/asf/maven/maven-3/trunk@900021 13f79535-47bb-0310-9956-ffa450edef68

maven-model-builder/src/main/java/org/apache/maven/model/interpolation/StringSearchModelInterpolator.java
@@ -32,6 +32,7 @@ import org.codehaus.plexus.interpolation.ValueSource;
 import java.io.File;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.util.ArrayList;
@@ -325,6 +326,11 @@ public class StringSearchModelInterpolator
             }
 
             if ( "parent".equals( field.getName() ) )
+            {
+                return false;
+            }
+
+            if ( Modifier.isFinal( field.getModifiers() ) )
             {
                 return false;
             }

maven-model-builder/src/test/java/org/apache/maven/model/interpolation/StringSearchModelInterpolatorTest.java
@@ -1,0 +1,60 @@
+package org.apache.maven.model.interpolation;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.Properties;
+
+import org.apache.maven.model.Model;
+import org.apache.maven.model.building.DefaultModelBuildingRequest;
+import org.apache.maven.model.building.SimpleProblemCollector;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Benjamin Bentmann
+ */
+public class StringSearchModelInterpolatorTest
+    extends TestCase
+{
+
+    public void testFinalFieldsExcludedFromInterpolation()
+    {
+        Properties props = new Properties();
+        props.setProperty( "expression", "value" );
+        DefaultModelBuildingRequest request = new DefaultModelBuildingRequest();
+        request.setUserProperties( props );
+
+        SimpleProblemCollector problems = new SimpleProblemCollector();
+        StringSearchModelInterpolator interpolator = new StringSearchModelInterpolator();
+        interpolator.interpolateObject( new ClassWithFinalField(), new Model(), null, request, problems );
+
+        assertTrue( problems.getFatals().toString(), problems.getFatals().isEmpty() );
+        assertTrue( problems.getErrors().toString(), problems.getErrors().isEmpty() );
+        assertTrue( problems.getWarnings().toString(), problems.getWarnings().isEmpty() );
+    }
+
+    static class ClassWithFinalField
+    {
+
+        public static final String CONSTANT = "${expression}";
+
+    }
+
+}

commit 9168e662225c9b8ef8698c3eaf1efc9cc88a60bc

o Fixed regression introduced by r900021
git-svn-id: https://svn.apache.org/repos/asf/maven/maven-3/trunk@900965 13f79535-47bb-0310-9956-ffa450edef68

maven-model-builder/src/main/java/org/apache/maven/model/interpolation/StringSearchModelInterpolator.java
@@ -156,7 +156,7 @@ public class StringSearchModelInterpolator
                                 if ( String.class == type )
                                 {
                                     String value = (String) fields[i].get( target );
-                                    if ( value != null )
+                                    if ( value != null && !Modifier.isFinal( fields[i].getModifiers() ) )
                                     {
                                         String interpolated =
                                             modelInterpolator.interpolateInternal( value, valueSources, postProcessors,
@@ -326,11 +326,6 @@ public class StringSearchModelInterpolator
             }
 
             if ( "parent".equals( field.getName() ) )
-            {
-                return false;
-            }
-
-            if ( Modifier.isFinal( field.getModifiers() ) )
             {
                 return false;
             }