a0df903c89fd14cc8343254180a8317f038d4140
Benjamin Bentmann <bentmann@apache.org>
2010-03-19 15:03:51 +0000

    [MNG-2577] Allow interpolation of Properties in settings.xml
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@925255 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
@@ -393,6 +393,7 @@ public class IntegrationTestSuite
         suite.addTestSuite( MavenITmng2668UsePluginDependenciesForSortingTest.class );
         suite.addTestSuite( MavenITmng2605BogusProfileActivationTest.class );
         suite.addTestSuite( MavenITmng2591MergeInheritedPluginConfigTest.class );
+        suite.addTestSuite( MavenITmng2577SettingsXmlInterpolationTest.class );
         suite.addTestSuite( MavenITmng2576MakeLikeReactorTest.class );
         suite.addTestSuite( MavenITmng2562TimestampTest.class );
         suite.addTestSuite( MavenITmng2539PluginDependenciesComeFromPluginReposTest.class );
---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng2577SettingsXmlInterpolationTest.java
@@ -0,0 +1,91 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-2577">MNG-2577</a>.
+ * 
+ * @author Benjamin Bentmann
+ */
+public class MavenITmng2577SettingsXmlInterpolationTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng2577SettingsXmlInterpolationTest()
+    {
+        super( "[2.0.3,)" );
+    }
+
+    /**
+     * Verify that the settings.xml can be interpolated using environment variables.
+     */
+    public void testitEnvVars()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-2577" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.getCliOptions().add( "--settings" );
+        verifier.getCliOptions().add( "settings-env.xml" );
+        verifier.setEnvironmentVariable( "MNGIT", "env-var-test" );
+        verifier.setLogFileName( "log-env.txt" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        Properties props = verifier.loadProperties( "target/settings.properties" );
+        assertEquals( "env-var-test", props.getProperty( "settings.servers.0.username" ) );
+    }
+
+    /**
+     * Verify that the settings.xml can be interpolated using system properties.
+     */
+    public void testitSystemProps()
+        throws Exception
+    {
+        requiresMavenVersion( "[3.0-alpha-1,)" );
+
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-2577" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.getCliOptions().add( "--settings" );
+        verifier.getCliOptions().add( "settings-sys.xml" );
+        verifier.setSystemProperty( "usr.MNGIT", "usr-prop-test" );
+        verifier.setLogFileName( "log-sys.txt" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        Properties props = verifier.loadProperties( "target/settings.properties" );
+        assertEquals( "usr-prop-test", props.getProperty( "settings.servers.0.username" ) );
+        assertEquals( File.separator, props.getProperty( "settings.servers.0.password" ) );
+    }
+
+}
