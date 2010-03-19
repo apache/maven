75ce32ab48a9695ad23d4b39cfaff7f9e927b8e8
Benjamin Bentmann <bentmann@apache.org>
2010-03-19 23:19:58 +0000

    [MNG-4600] [regression] Optional flag from dependency management applied to dependencies
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@925480 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
@@ -85,6 +85,7 @@ public class IntegrationTestSuite
         // suite.addTestSuite( MavenIT0109ReleaseUpdateTest.class );
         // suite.addTestSuite( MavenIT0108SnapshotUpdateTest.class ); -- MNG-3137
 
+        suite.addTestSuite( MavenITmng4600DependencyOptionalFlagManagementTest.class );
         suite.addTestSuite( MavenITmng4590ImportedPomUsesSystemPropertiesTest.class );
         suite.addTestSuite( MavenITmng4586PluginPrefixResolutionFromVersionlessPluginMngtTest.class );
         suite.addTestSuite( MavenITmng4580ProjectLevelPluginDepUsedForCliInvocInReactorTest.class );
---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4600DependencyOptionalFlagManagementTest.java
@@ -0,0 +1,89 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-4600">MNG-4600</a>.
+ * 
+ * @author Benjamin Bentmann
+ */
+public class MavenITmng4600DependencyOptionalFlagManagementTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng4600DependencyOptionalFlagManagementTest()
+    {
+        super( "[2.0.3,3.0-alpha-1),[3.0-alpha-8,)" );
+    }
+
+    /**
+     * Verify that a dependency's optional flag is not subject to dependency management. This part of the test checks
+     * the effective model.
+     */
+    public void testitModel()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-4600/model" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        Properties props = verifier.loadProperties( "target/pom.properties" );
+        assertEquals( "dep", props.getProperty( "project.dependencies.0.artifactId" ) );
+        assertEquals( "false", props.getProperty( "project.dependencies.0.optional" ) );
+    }
+
+    /**
+     * Verify that a transitive dependency's optional flag is not subject to dependency management of the root artifat.
+     * This part of the test checks the artifact collector.
+     */
+    public void testitResolution()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-4600/resolution" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.deleteArtifacts( "org.apache.maven.its.mng4600" );
+        verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", verifier.newDefaultFilterProperties() );
+        verifier.getCliOptions().add( "--settings" );
+        verifier.getCliOptions().add( "settings.xml" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        List classpath = verifier.loadLines( "target/classpath.txt", "UTF-8" );
+        assertTrue( classpath.toString(), classpath.contains( "direct-0.2.jar" ) );
+        assertTrue( classpath.toString(), classpath.contains( "transitive-0.1.jar" ) );
+    }
+
+}
