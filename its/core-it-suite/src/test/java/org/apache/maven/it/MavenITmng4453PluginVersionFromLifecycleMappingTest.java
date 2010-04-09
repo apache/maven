c2066127f9f0ea1c5fa4226516f2603bbcd813c4
Benjamin Bentmann <bentmann@apache.org>
2010-04-09 20:52:26 +0000

    [MNG-4453] [regression] Plugin versions defined in a lifecycle mapping are not respected
    o Enabled IT for 3.0-beta-1
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@932604 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4453PluginVersionFromLifecycleMappingTest.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4453PluginVersionFromLifecycleMappingTest.java
@@ -35,7 +35,7 @@ public class MavenITmng4453PluginVersionFromLifecycleMappingTest
 
     public MavenITmng4453PluginVersionFromLifecycleMappingTest()
     {
-        super( "[2.0.3,3.0-alpha-1)" );
+        super( "[2.0.3,3.0-alpha-1),[3.0-beta-1,)" );
     }
 
     /**
