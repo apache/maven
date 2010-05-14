7f42e485cae5ccdcb25515b042167e596672ef4c
Olivier Lamy <olamy@apache.org>
2010-05-14 20:59:30 +0000

    restore more backward comp.
    git-svn-id: https://svn.apache.org/repos/asf/maven/maven-3/trunk@944465 13f79535-47bb-0310-9956-ffa450edef68

--- maven-compat/src/main/java/org/apache/maven/repository/legacy/DefaultWagonManager.java
+++ maven-compat/src/main/java/org/apache/maven/repository/legacy/DefaultWagonManager.java
@@ -31,6 +31,8 @@ import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.metadata.ArtifactMetadata;
 import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.apache.maven.artifact.repository.ArtifactRepositoryPolicy;
+import org.apache.maven.repository.MirrorSelector;
+import org.apache.maven.settings.Mirror;
 import org.apache.maven.wagon.ConnectionException;
 import org.apache.maven.wagon.ResourceDoesNotExistException;
 import org.apache.maven.wagon.TransferFailedException;
@@ -71,7 +73,8 @@ public class DefaultWagonManager
 
     @Requirement
     private UpdateCheckManager updateCheckManager;
-
+   
+    
     //
     // Retriever
     //   
@@ -758,4 +761,5 @@ public class DefaultWagonManager
 
         return wagon;
     }
+   
 }
