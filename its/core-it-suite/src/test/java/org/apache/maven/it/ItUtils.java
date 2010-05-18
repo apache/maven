c31ca6323ba1d2a7e522375d8a02c168394c1517
Benjamin Bentmann <bentmann@apache.org>
2010-05-18 15:48:31 +0000

    [MNG-4679] [regression] command line option "-update-snapshots" does not work for dependency:copy-dependencies
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@945714 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/ItUtils.java
@@ -0,0 +1,72 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+
+/**
+ * @author Benjamin Bentmann
+ */
+class ItUtils
+{
+
+    public static String calcHash( File file, String algo )
+        throws Exception
+    {
+        MessageDigest digester = MessageDigest.getInstance( algo );
+
+        FileInputStream is = new FileInputStream( file );
+        try
+        {
+            DigestInputStream dis = new DigestInputStream( is, digester );
+
+            for ( byte[] buffer = new byte[1024 * 4]; dis.read( buffer ) >= 0; )
+            {
+                // just read it
+            }
+        }
+        finally
+        {
+            is.close();
+        }
+
+        byte[] digest = digester.digest();
+
+        StringBuffer hash = new StringBuffer( digest.length * 2 );
+
+        for ( int i = 0; i < digest.length; i++ )
+        {
+            int b = digest[i] & 0xFF;
+
+            if ( b < 0x10 )
+            {
+                hash.append( '0' );
+            }
+
+            hash.append( Integer.toHexString( b ) );
+        }
+
+        return hash.toString();
+    }
+
+}
---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4679SnapshotUpdateInPluginTest.java
@@ -0,0 +1,90 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+import java.util.Properties;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-4679">MNG-4679</a>.
+ * 
+ * @author Benjamin Bentmann
+ */
+public class MavenITmng4679SnapshotUpdateInPluginTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng4679SnapshotUpdateInPluginTest()
+    {
+        super( "[2.0.3,3.0-alpha-1),[3.0-beta-2,)" );
+    }
+
+    /**
+     * Verify that plugins using the 2.x style artifact resolver/collector directly are subject to the snapshot update
+     * mode of the current Maven session.
+     */
+    public void testit()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-4679" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteArtifacts( "org.apache.maven.its.mng4679" );
+        verifier.getCliOptions().add( "-s" );
+        verifier.getCliOptions().add( "settings.xml" );
+
+        Properties filterProps = verifier.newDefaultFilterProperties();
+
+        filterProps.setProperty( "@repo@", "repo-1" );
+        verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", filterProps );
+        verifier.setLogFileName( "log-force-1.txt" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+
+        assertChecksum( verifier, "jar", "cc7df75103468dd798397a81d0162d70faf92455" );
+        assertChecksum( verifier, "pom", "7de357a948a8bb2357759c7c585adb504e579bad" );
+
+        filterProps.setProperty( "@repo@", "repo-2" );
+        verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", filterProps );
+        verifier.setLogFileName( "log-force-2.txt" );
+        verifier.deleteDirectory( "target" );
+        verifier.getCliOptions().add( "-U" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+
+        verifier.resetStreams();
+
+        assertChecksum( verifier, "jar", "f3d46277c2ab45ff9bbd97605c942bed7fc27f97" );
+        assertChecksum( verifier, "pom", "8f17048dee72cc6ec33e9ab30fa00a910e1d6997" );
+    }
+
+    private void assertChecksum( Verifier verifier, String ext, String checksum )
+        throws Exception
+    {
+        String path = verifier.getArtifactPath( "org.apache.maven.its.mng4679", "dep", "0.1-SNAPSHOT", ext );
+        String actual = ItUtils.calcHash( new File( path ), "SHA-1" );
+        assertEquals( checksum, actual );
+    }
+
+}
