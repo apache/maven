d3965e9cbf8e1f818a74a704e8c8616a4cc32a74
Benjamin Bentmann <bentmann@apache.org>
2010-07-06 09:34:43 +0000

    [MNG-4718] [regression] Missing Class in 3.0-beta-1: org.apache.maven.project.artifact.AttachedArtifact
    git-svn-id: https://svn.apache.org/repos/asf/maven/maven-3/trunk@960855 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/main/java/org/apache/maven/project/DefaultMavenProjectHelper.java
+++ maven-core/src/main/java/org/apache/maven/project/DefaultMavenProjectHelper.java
@@ -20,23 +20,18 @@ package org.apache.maven.project;
  */
 
 import java.io.File;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.DefaultArtifact;
-import org.apache.maven.artifact.InvalidArtifactRTException;
 import org.apache.maven.artifact.handler.ArtifactHandler;
 import org.apache.maven.artifact.handler.manager.ArtifactHandlerManager;
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.artifact.versioning.VersionRange;
 import org.apache.maven.model.Resource;
-import org.apache.maven.repository.legacy.metadata.ArtifactMetadata;
+import org.apache.maven.project.artifact.AttachedArtifact;
 import org.codehaus.plexus.component.annotations.Component;
 import org.codehaus.plexus.component.annotations.Requirement;
 import org.codehaus.plexus.logging.AbstractLogEnabled;
 
+@SuppressWarnings( "deprecation" )
 @Component( role = MavenProjectHelper.class )
 public class DefaultMavenProjectHelper
     extends AbstractLogEnabled
@@ -129,143 +124,6 @@ public class DefaultMavenProjectHelper
         resource.setExcludes( excludes );
 
         project.addTestResource( resource );
-    }
-
-    private static class AttachedArtifact
-        extends DefaultArtifact
-    {
-
-        private final Artifact parent;
-
-        public AttachedArtifact( Artifact parent, String type, String classifier, ArtifactHandler artifactHandler )
-        {
-            super( parent.getGroupId(), parent.getArtifactId(), parent.getVersionRange(), parent.getScope(), type,
-                   classifier, artifactHandler, parent.isOptional() );
-
-            setDependencyTrail( Collections.singletonList( parent.getId() ) );
-
-            this.parent = parent;
-
-            if ( getId().equals( parent.getId() ) )
-            {
-                throw new InvalidArtifactRTException( parent.getGroupId(), parent.getArtifactId(), parent.getVersion(),
-                                                      parent.getType(),
-                                                      "An attached artifact must have a different ID than "
-                                                          + "its corresponding main artifact." );
-            }
-        }
-
-        public AttachedArtifact( Artifact parent, String type, ArtifactHandler artifactHandler )
-        {
-            this( parent, type, null, artifactHandler );
-        }
-
-        public void setArtifactId( String artifactId )
-        {
-            // ignore this. We should ALWAYS use the information from the parent artifact.
-        }
-
-        public List getAvailableVersions()
-        {
-            return parent.getAvailableVersions();
-        }
-
-        public void setAvailableVersions( List availableVersions )
-        {
-            // ignore this. We should ALWAYS use the information from the parent artifact.
-        }
-
-        public String getBaseVersion()
-        {
-            return parent.getBaseVersion();
-        }
-
-        public void setBaseVersion( String baseVersion )
-        {
-            // ignore this. We should ALWAYS use the information from the parent artifact.
-        }
-
-        public String getDownloadUrl()
-        {
-            return parent.getDownloadUrl();
-        }
-
-        public void setDownloadUrl( String downloadUrl )
-        {
-            // ignore this. We should ALWAYS use the information from the parent artifact.
-        }
-
-        public void setGroupId( String groupId )
-        {
-            // ignore this. We should ALWAYS use the information from the parent artifact.
-        }
-
-        public ArtifactRepository getRepository()
-        {
-            return parent.getRepository();
-        }
-
-        public void setRepository( ArtifactRepository repository )
-        {
-            // ignore this. We should ALWAYS use the information from the parent artifact.
-        }
-
-        public String getScope()
-        {
-            return parent.getScope();
-        }
-
-        public void setScope( String scope )
-        {
-            // ignore this. We should ALWAYS use the information from the parent artifact.
-        }
-
-        public String getVersion()
-        {
-            return parent.getVersion();
-        }
-
-        public void setVersion( String version )
-        {
-            // ignore this. We should ALWAYS use the information from the parent artifact.
-        }
-
-        public VersionRange getVersionRange()
-        {
-            return parent.getVersionRange();
-        }
-
-        public void setVersionRange( VersionRange range )
-        {
-            // ignore this. We should ALWAYS use the information from the parent artifact.
-        }
-
-        public boolean isRelease()
-        {
-            return parent.isRelease();
-        }
-
-        public void setRelease( boolean release )
-        {
-            // ignore this. We should ALWAYS use the information from the parent artifact.
-        }
-
-        public boolean isSnapshot()
-        {
-            return parent.isSnapshot();
-        }
-
-        public void addMetadata( ArtifactMetadata metadata )
-        {
-            // ignore. The parent artifact will handle metadata.
-            // we must fail silently here to avoid problems with the artifact transformers.
-        }
-
-        public Collection getMetadataList()
-        {
-            return Collections.EMPTY_LIST;
-        }
-
     }
 
 }
