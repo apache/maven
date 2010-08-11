957a4e288388816db0575ba917067db4c0be24b1
Benjamin Bentmann <bentmann@apache.org>
2010-08-11 19:19:29 +0000

    o Stabilized test
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@984541 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4761PluginLevelDependencyScopesTest.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4761PluginLevelDependencyScopesTest.java
@@ -52,6 +52,7 @@ public class MavenITmng4761PluginLevelDependencyScopesTest
         final Verifier verifier = newVerifier( testDir.getAbsolutePath() );
         verifier.setAutoclean( false );
         verifier.deleteDirectory( "target" );
+        verifier.deleteArtifacts( "org.apache.maven.its.mng4761" );
         verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", verifier.newDefaultFilterProperties() );
         verifier.getCliOptions().add( "-s" );
         verifier.getCliOptions().add( "settings.xml" );
