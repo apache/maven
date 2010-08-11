b2fd85fa8a4a187f319b2506b764aede8d1a254f
John Dennis Casey <jdcasey@apache.org>
2010-08-11 18:55:44 +0000

    [MNG-4761] Force plugin-level dependencies to use runtime scope, so conflict resolution doesn't make a determination based on dependency scoping.
    git-svn-id: https://svn.apache.org/repos/asf/maven/maven-3/trunk@984534 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/main/java/org/apache/maven/plugin/internal/DefaultPluginDependenciesResolver.java
+++ maven-core/src/main/java/org/apache/maven/plugin/internal/DefaultPluginDependenciesResolver.java
@@ -98,6 +98,7 @@ public class DefaultPluginDependenciesResolver
         Set<Artifact> overrideArtifacts = new LinkedHashSet<Artifact>();
         for ( Dependency dependency : plugin.getDependencies() )
         {
+            dependency.setScope( Artifact.SCOPE_RUNTIME );
             overrideArtifacts.add( repositorySystem.createDependencyArtifact( dependency ) );
         }
 
