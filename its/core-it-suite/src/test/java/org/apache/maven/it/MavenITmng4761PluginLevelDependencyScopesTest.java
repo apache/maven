c08498f3af6ed19504c718e9008de7f0bc19a756
John Dennis Casey <jdcasey@apache.org>
2010-08-11 18:53:11 +0000

    [MNG-4761] Adding integration test
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@984531 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4761PluginLevelDependencyScopesTest.java
@@ -0,0 +1,63 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-4761">MNG-4761</a>.
+ *
+ * @author jdcasey
+ */
+public class MavenITmng4761PluginLevelDependencyScopesTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng4761PluginLevelDependencyScopesTest()
+    {
+        super( "(3.0-beta-2,)" );
+    }
+
+    /**
+     * Verify that plugin-level dependencies specified in a user's pom.xml DO NOT use compile scope.
+     * Using any scope other than runtime for plugin dependencies may favor them and their transitive 
+     * dependencies inappropriately, leading to unpredictable results.
+     * 
+     * Plugin-dependency scope should be DISREGARDED, and runtime scope should be forced.
+     */
+    public void testit()
+        throws Exception
+    {
+        final File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-4761" );
+
+        final Verifier verifier = newVerifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.filterFile( "settings-template.xml", "settings.xml", "UTF-8", verifier.newDefaultFilterProperties() );
+        verifier.getCliOptions().add( "-s" );
+        verifier.getCliOptions().add( "settings.xml" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+    }
+
+}
