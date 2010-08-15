621d58347c39de2bb2f513972e45e3dc4864e5eb
Benjamin Bentmann <bentmann@apache.org>
2010-08-15 14:53:25 +0000

    o Fixed test order
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@985684 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
@@ -238,8 +238,8 @@ public class IntegrationTestSuite
         suite.addTestSuite( MavenITmng4070WhitespaceTrimmingTest.class );
         suite.addTestSuite( MavenITmng4068AuthenticatedMirrorTest.class );
         suite.addTestSuite( MavenITmng4056ClassifierBasedDepResolutionFromReactorTest.class );
-        suite.addTestSuite( MavenITmng4052ReactorAwareImportScopeTest.class );
         suite.addTestSuite( MavenITmng4053PluginConfigAttributesTest.class );
+        suite.addTestSuite( MavenITmng4052ReactorAwareImportScopeTest.class );
         suite.addTestSuite( MavenITmng4048VersionRangeReactorResolutionTest.class );
         suite.addTestSuite( MavenITmng4040ProfileInjectedModulesTest.class );
         suite.addTestSuite( MavenITmng4036ParentResolutionFromSettingsRepoTest.class );
