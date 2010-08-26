9a739a395b4e8fa9db6c9c9a901023cdff5d1fdd
Benjamin Bentmann <bentmann@apache.org>
2010-08-26 12:19:57 +0000

    [MNG-4779] NullPointerException thrown during dependency resolution when dependency with range occurs more than once in the dirty tree
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@989638 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4779MultipleDepsWithVersionRangeFromLocalRepoTest.java
@@ -0,0 +1,64 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.Verifier;
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-4779">MNG-4779</a>.
+ * 
+ * @author Benjamin Bentmann
+ */
+public class MavenITmng4779MultipleDepsWithVersionRangeFromLocalRepoTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng4779MultipleDepsWithVersionRangeFromLocalRepoTest()
+    {
+        super( "[2.0.3,)" );
+    }
+
+    /**
+     * Test that dependency resolution doesn't error out when a dependency with a range satisfied from the local repo
+     * is seen more than once during the collection.
+     */
+    public void testit()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-4779" );
+
+        Verifier verifier = newVerifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "test/target" );
+        verifier.deleteArtifacts( "org.apache.maven.its.mng4779" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        List classpath = verifier.loadLines( "test/target/classpath.txt", "UTF-8" );
+
+        assertEquals( classpath.toString(), 4, classpath.size() );
+    }
+
+}
