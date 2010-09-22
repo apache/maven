137b64e837d56f6aa1571bdde765b85dc256a8df
Benjamin Bentmann <bentmann@apache.org>
2010-09-22 10:23:18 +0000

    o Fixed glitch in daily update policy which uses wrong timezone
    git-svn-id: https://svn.apache.org/repos/asf/maven/maven-3/trunk@999860 13f79535-47bb-0310-9956-ffa450edef68

--- maven-artifact/src/main/java/org/apache/maven/artifact/repository/ArtifactRepositoryPolicy.java
+++ maven-artifact/src/main/java/org/apache/maven/artifact/repository/ArtifactRepositoryPolicy.java
@@ -124,8 +124,8 @@ public class ArtifactRepositoryPolicy
         }
         else if ( UPDATE_POLICY_DAILY.equals( updatePolicy ) )
         {
-            // Get midnight boundary
-            Calendar cal = Calendar.getInstance( TimeZone.getTimeZone( "UTC" ) );
+            // Get local midnight boundary
+            Calendar cal = Calendar.getInstance();
 
             cal.set( Calendar.HOUR_OF_DAY, 0 );
             cal.set( Calendar.MINUTE, 0 );
