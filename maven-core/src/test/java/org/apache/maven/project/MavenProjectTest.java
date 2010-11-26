990f4519fe0fcf59d0488d81aeb0cddcfe6b29e1
Benjamin Bentmann <bentmann@apache.org>
2010-11-26 12:24:25 +0000

    o Used generics
    git-svn-id: https://svn.apache.org/repos/asf/maven/maven-3/trunk@1039329 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/test/java/org/apache/maven/project/MavenProjectTest.java
+++ maven-core/src/test/java/org/apache/maven/project/MavenProjectTest.java
@@ -96,7 +96,7 @@ public class MavenProjectTest
 
         MavenProject clonedProject = (MavenProject) projectToClone.clone();
         assertEquals( "maven-core", clonedProject.getArtifactId() );
-        Map clonedMap = clonedProject.getManagedVersionMap();
+        Map<?, ?> clonedMap = clonedProject.getManagedVersionMap();
         assertNotNull( "ManagedVersionMap not copied", clonedMap );
         assertTrue( "ManagedVersionMap is not empty", clonedMap.isEmpty() );
     }
@@ -108,17 +108,17 @@ public class MavenProjectTest
         MavenProject projectToClone = getProjectWithDependencies( f );
         DependencyManagement dep = projectToClone.getDependencyManagement();
         assertNotNull( "No dependencyManagement", dep );
-        List list = dep.getDependencies();
+        List<?> list = dep.getDependencies();
         assertNotNull( "No dependencies", list );
         assertTrue( "Empty dependency list", !list.isEmpty() );
 
-        Map map = projectToClone.getManagedVersionMap();
+        Map<?, ?> map = projectToClone.getManagedVersionMap();
         assertNotNull( "No ManagedVersionMap", map );
         assertTrue( "ManagedVersionMap is empty", !map.isEmpty() );
 
         MavenProject clonedProject = (MavenProject) projectToClone.clone();
         assertEquals( "maven-core", clonedProject.getArtifactId() );
-        Map clonedMap = clonedProject.getManagedVersionMap();
+        Map<?, ?> clonedMap = clonedProject.getManagedVersionMap();
         assertNotNull( "ManagedVersionMap not copied", clonedMap );
         assertTrue( "ManagedVersionMap is empty", !clonedMap.isEmpty() );
         assertTrue( "ManagedVersionMap does not contain test key",
