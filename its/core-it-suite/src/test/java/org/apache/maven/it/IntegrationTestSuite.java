8eb6ab7ee0f77303853d1a4038feeddda2ea0c26
Benjamin Bentmann <bentmann@apache.org>
2011-01-15 05:21:16 +0000

    [MNG-4973] [regression] Build extensions are invisible to plugins in multi module build
    o Added IT
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@1059264 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/IntegrationTestSuite.java
@@ -84,6 +84,7 @@ public class IntegrationTestSuite
         // suite.addTestSuite( MavenIT0108SnapshotUpdateTest.class ); -- MNG-3137
 
         suite.addTestSuite( MavenITmng4975ProfileInjectedPluginExecutionOrderTest.class );
+        suite.addTestSuite( MavenITmng4973ExtensionVisibleToPluginInReactorTest.class );
         suite.addTestSuite( MavenITmng4966AbnormalUrlPreservationTest.class );
         suite.addTestSuite( MavenITmng4960MakeLikeReactorResumeTest.class );
         suite.addTestSuite( MavenITmng4955LocalVsRemoteSnapshotResolutionTest.class );
