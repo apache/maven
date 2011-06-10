2feabb84bcefd75cc585fc41098a7176f07e5651
Olivier Lamy <olamy@apache.org>
2011-06-10 10:20:39 +0000

    try something different to fix MavenITmng4233ReactorResolutionForManuallyCreatedArtifactTest with symlink on jenkins asf
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@1134267 13f79535-47bb-0310-9956-ffa450edef68

--- its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4233ReactorResolutionForManuallyCreatedArtifactTest.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng4233ReactorResolutionForManuallyCreatedArtifactTest.java
@@ -57,7 +57,7 @@ public class MavenITmng4233ReactorResolutionForManuallyCreatedArtifactTest
         verifier.resetStreams();
 
         Properties props = verifier.loadProperties( "consumer/target/artifact.properties" );
-        assertEquals( new File( testDir, "producer/pom.xml" ).getCanonicalFile(), new File(
+        assertEquals( new File( testDir.getCanonicalFile(), "producer/pom.xml" ), new File(
             props.getProperty( "org.apache.maven.its.mng4233:producer:jar:1.0-SNAPSHOT" ) ).getCanonicalFile() );
     }
 
