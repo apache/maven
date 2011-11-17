c15263a465ce44e45627f689d2931953ec7d565b
Olivier Lamy <olamy@apache.org>
2011-11-17 13:34:33 +0000

    [MNG-5201] upgrade aether to 1.13 and sisu to 2.3.0 .
    git-svn-id: https://svn.apache.org/repos/asf/maven/maven-3/trunk@1203182 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/test/java/org/apache/maven/project/LegacyLocalRepositoryManager.java
+++ maven-core/src/test/java/org/apache/maven/project/LegacyLocalRepositoryManager.java
@@ -21,8 +21,11 @@ package org.apache.maven.project;
 
 import java.io.File;
 
+import org.sonatype.aether.RepositorySystemSession;
 import org.sonatype.aether.artifact.Artifact;
 import org.sonatype.aether.impl.internal.SimpleLocalRepositoryManager;
+import org.sonatype.aether.repository.LocalArtifactRequest;
+import org.sonatype.aether.repository.LocalArtifactResult;
 
 /**
  * @author Benjamin Bentmann
@@ -55,5 +58,18 @@ public class LegacyLocalRepositoryManager
 
         return path.toString();
     }
+
+    public LocalArtifactResult find( RepositorySystemSession session, LocalArtifactRequest request )
+    {
+ 	    String path = getPathForLocalArtifact( request.getArtifact() );
+ 	    File file = new File( getRepository().getBasedir(), path );
+        LocalArtifactResult result = new LocalArtifactResult( request );
+ 	    if ( file.isFile() )
+        {
+ 	        result.setFile( file );
+ 	        result.setAvailable( true );
+ 	    }
+ 	    return result;
+ 	}
 
 }
