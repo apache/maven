75a424452ca6a297ff993519fd140f1723cb2877
Benson Margulies <bimargulies@apache.org>
2011-12-10 21:00:40 +0000

    MNG-5214: Dependency resolution substitutes g:a:v:jar for j:a:v:something-else when something-else isn't in the reactor o Add an integration test for the new behavior, conditioned for 3.0.5 and after. o Add another mojo to maven-it-plugin-dependency-resolution to support the test. Note that I had to tweak dependencies on the plugin to add this mojo; if anyone really wants to run against 2.0 with the trunk of the tests this will need something, I'm not sure what.
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@1212870 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5214DontMapWsdlToJar.java
@@ -0,0 +1,76 @@
+package org.apache.maven.it;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.maven.it.util.ResourceExtractor;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+public class MavenITmng5214DontMapWsdlToJar extends AbstractMavenIntegrationTestCase
+{
+    public MavenITmng5214DontMapWsdlToJar()
+    {
+        super( "[3.0-5,)" );
+    }
+    
+    /**
+     * Test that the code that allows test-jar and ejb-client dependencies to resolve to the 
+     * target/classes or target/test-class is *not* applies to other types, e.g. wsdl.
+     */
+    public void testitTestPhase()
+        throws Exception
+    {
+        File setupDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-5214/dependency" );
+        
+        Verifier setupVerifier = newVerifier( setupDir.getAbsolutePath() );
+        setupVerifier.setAutoclean( false );
+        setupVerifier.setMavenDebug( true );
+        setupVerifier.deleteDirectory( "target" );
+        setupVerifier.deleteArtifacts( "org.apache.maven.its.mng5214" );
+        setupVerifier.setLogFileName( "log-setup.txt" );
+        setupVerifier.addCliOption( "-PcreateWsdl" );
+        setupVerifier.executeGoal( "generate-resources" );
+
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-5214" );
+        
+        Verifier verifier = newVerifier( testDir.getAbsolutePath() );
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "consumer/target" );
+        verifier.deleteDirectory( "dependency/target" );
+        verifier.setLogFileName( "log-test.txt" );
+        verifier.executeGoal( "test" );
+        verifier.verifyErrorFreeLog();
+        List lines = verifier.loadFile( verifier.getBasedir(), verifier.getLogFileName(), false );
+        Iterator lineIt = lines.iterator();
+        // RESOLVE-ONE-DEPENDENCY org.apache.maven.its.mng5214:dependency:wsdl:1.0-SNAPSHOT $ /tmp/it.repo/org/apache/maven/its/mng5214/dependency/1.0-SNAPSHOT/dependency-1.0-SNAPSHOT.wsdl
+        while ( lineIt.hasNext() )
+        {
+            String line = (String) lineIt.next();
+            if ( line.contains( "RESOLVE-ONE-DEPENDENCY org.apache.maven.its.mng5214:dependency:wsdl:1.0-SNAPSHOT"  ) )
+            {
+                assertFalse( line.contains( "classes-main" ) );
+                assertTrue( line.endsWith( ".wsdl" ) );
+            }
+        }
+    }
+}
---
+++ its/core-it-support/core-it-plugins/maven-it-plugin-dependency-resolution/src/main/java/org/apache/maven/plugin/coreit/ResolveOneDependencyMojo.java
@@ -0,0 +1,199 @@
+package org.apache.maven.plugin.coreit;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.metadata.ArtifactMetadataSource;
+import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
+import org.apache.maven.artifact.resolver.ArtifactResolutionException;
+import org.apache.maven.artifact.resolver.ArtifactResolutionResult;
+import org.apache.maven.artifact.resolver.ArtifactResolver;
+import org.apache.maven.artifact.resolver.filter.ScopeArtifactFilter;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.project.MavenProject;
+
+/**
+ * Attempts to resolve a single artifact from dependencies, and logs the results for the Verifier to look at.
+ * 
+ * @goal resolve-one-dependency
+ * @requiresDependencyResolution runtime
+ * @author bimargulies
+ * @version $Id$
+ */
+public class ResolveOneDependencyMojo
+    extends AbstractDependencyMojo
+{
+
+    /**
+     * Group ID of the artifact to resolve.
+     * 
+     * @parameter
+     * @required
+     */
+    private String groupId;
+    /**
+     * Artifact ID of the artifact to resolve.
+     * 
+     * @parameter
+     * @required
+     */
+    private String artifactId;
+    /**
+     * Version  of the artifact to resolve.
+     * 
+     * @parameter
+     * @required
+     */
+    private String version;
+    /**
+     * Type of the artifact to resolve.
+     * 
+     * @parameter
+     * @required
+     */
+    private String type;
+    /**
+     * Classifier of the artifact to resolve.
+     * 
+     * @parameter
+     */
+    private String classifier;
+
+    /**
+     * The scope to resolve for.
+     * 
+     * @parameter
+     * @required
+     */
+    private String scope;
+
+    /**
+     * @parameter expression="${project}"
+     * @required
+     */
+    MavenProject project;
+
+    /**
+     * @component
+     */
+    private ArtifactResolver resolver;
+
+    /**
+     * @component
+     * @readonly
+     * @required
+     */
+    private ArtifactFactory artifactFactory;
+
+    /**
+     * The Maven session.
+     * 
+     * @parameter expression="${session}"
+     * @readonly
+     * @required
+     */
+    private MavenSession session;
+
+    /**
+     * Metadata source object.
+     * 
+     * @component
+     */
+    private ArtifactMetadataSource metadataSource;
+
+    /**
+     * Runs this mojo.
+     * 
+     * @throws MojoExecutionException If the output file could not be created or any dependency could not be resolved.
+     */
+    public void execute()
+        throws MojoExecutionException
+    {
+
+        Artifact projectArtifact = project.getArtifact();
+        if ( projectArtifact == null )
+        {
+            projectArtifact =
+                artifactFactory.createProjectArtifact( project.getGroupId(), project.getArtifactId(),
+                                                       project.getVersion() );
+        }
+
+        Set depArtifacts = new HashSet();
+        Artifact artifact = artifactFactory.createArtifactWithClassifier( groupId, artifactId, version, type, classifier );
+        depArtifacts.add( artifact );
+
+        ScopeArtifactFilter scopeFilter = new ScopeArtifactFilter( scope );
+
+        ArtifactResolutionResult result;
+        try
+        {
+            result =
+                resolver.resolveTransitively( depArtifacts, projectArtifact, project.getManagedVersionMap(),
+                                              session.getLocalRepository(), project.getRemoteArtifactRepositories(),
+                                              metadataSource, scopeFilter );
+        }
+        catch ( ArtifactResolutionException e )
+        {
+            throw new MojoExecutionException( "RESOLVE-ONE-DEPENDENCY ArtifactResolutionException exception ", e );
+        }
+        catch ( ArtifactNotFoundException e )
+        {
+            throw new MojoExecutionException( "RESOLVE-ONE-DEPENDENCY ArtifactNotFoundException exception ", e );
+        }
+
+        if ( result == null )
+        {
+            getLog().info( "RESOLVE-ONE-DEPENDENCY null result" );
+        }
+        else
+        {
+            Set resolvedArtifacts = result.getArtifacts();
+            Iterator it = resolvedArtifacts.iterator();
+            /*
+             * Assume that the user of this is not interested in transitive deps and such, just report the one.
+             */
+            while ( it.hasNext() )
+            {
+                Artifact a = (Artifact) it.next();
+                if ( a.equals( artifact ) )
+                {
+                    File file = a.getFile();
+                    if ( file == null )
+                    {
+                        getLog().info( " RESOLVE-ONE-DEPENDENCY " + a.toString() + " $ NO-FILE" );
+                    }
+                    else
+                    {
+                        getLog().info( " RESOLVE-ONE-DEPENDENCY " + a.toString() + " $ " + file.getAbsolutePath() );
+                    }
+                    return;
+                }
+            }
+            getLog().info(" RESOLVE-ONE-DEPENDENCY " + artifact.toString() + " $ NOT-RESOLVED" );
+        }
+    }
+}
