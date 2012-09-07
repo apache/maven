9c722441d861608dc956e0a078a2a651087f8a84
Olivier Lamy <olamy@apache.org>
2012-09-07 16:02:57 +0000

    add it test for MNG-5338 missed to add files
    git-svn-id: https://svn.apache.org/repos/asf/maven/core-integration-testing/trunk@1382081 13f79535-47bb-0310-9956-ffa450edef68

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5338FileOptionToDirectory.java
@@ -0,0 +1,77 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+
+/**
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-5338">MNG-5338</a>.
+ *
+ * @author Olivier Lamy
+ */
+public class MavenITmng5338FileOptionToDirectory
+    extends AbstractMavenIntegrationTestCase
+{
+
+    private File testDir;
+
+    public MavenITmng5338FileOptionToDirectory()
+    {
+        super( "[3.0.5,)" );
+    }
+
+    public void setUp()
+        throws Exception
+    {
+        super.setUp();
+
+        testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-5338" );
+
+    }
+
+    protected void tearDown()
+        throws Exception
+    {
+
+        super.tearDown();
+    }
+
+    /**
+     * Verify that the repositories are used in the reversed order of definition in settings.xml.
+     */
+    public void testFileOptionToADirectory()
+        throws Exception
+    {
+        Verifier verifier = newVerifier( testDir.getAbsolutePath() );
+
+        verifier.setAutoclean( false );
+        verifier.deleteDirectory( "target" );
+        verifier.deleteArtifacts( "org.apache.maven.its.mng5338" );
+
+        verifier.getCliOptions().add( "-f project" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+    }
+
+}
---
+++ its/core-it-suite/src/test/resources/mng-5338/project/src/main/java/org/apache/maven/its/mng5338/App.java
@@ -0,0 +1,13 @@
+package org.apache.maven.its.mng5338;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
