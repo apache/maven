5f92449d5d628f3c40f8a4082bcedc369e96bd2d
Herve Boutemy <hboutemy@apache.org>
2012-10-20 21:44:11 +0000

    first step at component unit-tests
    git-svn-id: https://svn.apache.org/repos/asf/maven/maven-3/trunk@1400523 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-aether-provider/src/test/java/org/apache/maven/repository/internal/RepositorySystemTest.java
@@ -0,0 +1,98 @@
+package org.apache.maven.repository.internal;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.net.MalformedURLException;
+
+import org.apache.maven.repository.internal.util.ConsoleRepositoryListener;
+import org.apache.maven.repository.internal.util.ConsoleTransferListener;
+import org.codehaus.plexus.PlexusTestCase;
+import org.sonatype.aether.artifact.Artifact;
+import org.sonatype.aether.collection.CollectRequest;
+import org.sonatype.aether.collection.CollectResult;
+import org.sonatype.aether.graph.Dependency;
+import org.sonatype.aether.repository.LocalRepository;
+import org.sonatype.aether.repository.RemoteRepository;
+import org.sonatype.aether.util.artifact.DefaultArtifact;
+import org.sonatype.aether.RepositorySystem;
+import org.sonatype.aether.RepositorySystemSession;
+
+public class RepositorySystemTest
+    extends PlexusTestCase
+{
+    protected RepositorySystem system;
+
+    protected RepositorySystemSession session;
+
+    @Override
+    protected void setUp()
+        throws Exception
+    {
+        system = lookup( RepositorySystem.class );
+        session = newMavenRepositorySystemSession( system );
+    }
+
+    @Override
+    protected void tearDown()
+        throws Exception
+    {
+        session = null;
+        system = null;
+        super.tearDown();
+    }
+
+    public void testCollectDependencies()
+        throws Exception
+    {
+        String artifactCoords = "ut.simple:artifact:1.0"; // TODO test extension:classifier
+        Artifact artifact = new DefaultArtifact( artifactCoords );
+
+        CollectRequest collectRequest = new CollectRequest();
+        collectRequest.setRoot( new Dependency( artifact, "" ) );
+        collectRequest.addRepository( newTestRepository() );
+
+        CollectResult collectResult = system.collectDependencies( session, collectRequest );
+
+        assertEquals( 1, collectResult.getRoot().getChildren().size() );
+        Artifact dep = collectResult.getRoot().getChildren().get( 0 ).getDependency().getArtifact();
+        assertEquals( "ut.simple", dep.getGroupId() );
+        assertEquals( "dependency", dep.getArtifactId() );
+        assertEquals( "1.0", dep.getVersion() );
+    }
+
+    public static RepositorySystemSession newMavenRepositorySystemSession( RepositorySystem system )
+    {
+        MavenRepositorySystemSession session = new MavenRepositorySystemSession();
+
+        LocalRepository localRepo = new LocalRepository( "target/local-repo" );
+        session.setLocalRepositoryManager( system.newLocalRepositoryManager( localRepo ) );
+
+        session.setTransferListener( new ConsoleTransferListener() );
+        session.setRepositoryListener( new ConsoleRepositoryListener() );
+
+        return session;
+    }
+
+    public static RemoteRepository newTestRepository()
+        throws MalformedURLException
+    {
+        return new RemoteRepository( "repo", "default", getTestFile( "target/test-classes/repo" ).toURI().toURL().toString() );
+    }
+}
