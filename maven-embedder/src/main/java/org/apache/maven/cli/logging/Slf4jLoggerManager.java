72df73f9e8308115974c3898af074ad3ad77c91d
Olivier Lamy <olamy@apache.org>
2012-11-15 10:18:06 +0000

    code format and add @since
    git-svn-id: https://svn.apache.org/repos/asf/maven/maven-3/trunk@1409718 13f79535-47bb-0310-9956-ffa450edef68

--- maven-embedder/src/main/java/org/apache/maven/cli/logging/Slf4jLoggerManager.java
+++ maven-embedder/src/main/java/org/apache/maven/cli/logging/Slf4jLoggerManager.java
@@ -7,46 +7,58 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Use an SLF4J {@link org.slf4j.ILoggerFactory} as a backing for a Plexus {@link org.codehaus.plexus.logging.LoggerManager}.
- * 
+ *
  * @author Jason van Zyl
+ * @since 3.1
  */
-public class Slf4jLoggerManager implements LoggerManager {
+public class Slf4jLoggerManager
+    implements LoggerManager
+{
 
-  private ILoggerFactory loggerFactory;
+    private ILoggerFactory loggerFactory;
 
-  public Slf4jLoggerManager() {
-    loggerFactory = LoggerFactory.getILoggerFactory();
-  }
+    public Slf4jLoggerManager()
+    {
+        loggerFactory = LoggerFactory.getILoggerFactory();
+    }
 
-  public Logger getLoggerForComponent(String role) {
-    return new Slf4jLogger(loggerFactory.getLogger(role));
-  }
+    public Logger getLoggerForComponent( String role )
+    {
+        return new Slf4jLogger( loggerFactory.getLogger( role ) );
+    }
 
-  public Logger getLoggerForComponent(String role, String hint) {
-    return new Slf4jLogger(loggerFactory.getLogger(role + "." + hint));
-  }
+    public Logger getLoggerForComponent( String role, String hint )
+    {
+        return new Slf4jLogger( loggerFactory.getLogger( role + "." + hint ) );
+    }
 
-  //
-  // Trying to give loggers back is a bad idea. Ceki said so :-)
-  //
-  public void returnComponentLogger(String role) {
-  }
+    //
+    // Trying to give loggers back is a bad idea. Ceki said so :-)
+    //
+    public void returnComponentLogger( String role )
+    {
+    }
 
-  public void returnComponentLogger(String role, String hint) {
-  }
+    public void returnComponentLogger( String role, String hint )
+    {
+    }
 
-  public int getThreshold() {
-    return 0;
-  }
+    public int getThreshold()
+    {
+        return 0;
+    }
 
-  public void setThreshold(int threshold) {
-  }
+    public void setThreshold( int threshold )
+    {
+    }
 
-  public void setThresholds(int threshold) {
-  }
+    public void setThresholds( int threshold )
+    {
+    }
 
-  public int getActiveLoggerCount() {
-    return 0;
-  }
+    public int getActiveLoggerCount()
+    {
+        return 0;
+    }
 
 }
