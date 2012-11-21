4e52800a1bf5ac0e7d326b069520df53c6ce06e2
Herve Boutemy <hboutemy@apache.org>
2012-11-21 21:36:19 +0000

    [MNG-5380] check that whitespace can be preserved in Maven plugin configuration when building MavenProject (still need to check if content is not trimmed when injected into plugin)
    git-svn-id: https://svn.apache.org/repos/asf/maven/maven-3/trunk@1412310 13f79535-47bb-0310-9956-ffa450edef68

--- maven-core/src/test/java/org/apache/maven/project/PomConstructionTest.java
+++ maven-core/src/test/java/org/apache/maven/project/PomConstructionTest.java
@@ -1083,9 +1083,10 @@ public class PomConstructionTest
     {
         PomTestWrapper pom = buildPom( "reporting-plugin-config/sub" );
 
-        assertEquals( 2, ( (List<?>) pom.getValue( "reporting/plugins[1]/configuration/stringParams" ) ).size() );
+        assertEquals( 3, ( (List<?>) pom.getValue( "reporting/plugins[1]/configuration/stringParams" ) ).size() );
         assertEquals( "parentParam", pom.getValue( "reporting/plugins[1]/configuration/stringParams[1]/stringParam[1]" ) );
         assertEquals( "childParam", pom.getValue( "reporting/plugins[1]/configuration/stringParams[1]/stringParam[2]" ) );
+        assertEquals( "  preserve space  ", pom.getValue( "reporting/plugins[1]/configuration/stringParams[1]/stringParam[3]" ) );
         assertEquals( "true", pom.getValue( "reporting/plugins[1]/configuration/booleanParam" ) );
     }    
     
