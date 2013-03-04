df3fdc9d5ca51822756923e2106249047b3c04a8
bdemers <bdemers@apache.org>
2013-03-04 11:21:40 -0500

    A copy of the gradle wrapper ported for use with maven Lightly touched to better mesh with current maven builds. (some of which my not have been necessary in hind sight)
    Test it out with this build, run: "./mvnw clean install" at the root of this repo
    This will download, install, and run maven 3.0.5 (as configured in maven/wrapper/maven-wrapper.properties)
    
    NOTE: windows batch file is NOT tested
    
    
    TODO: 
    * Possibly extract these bits out into something both gradle and maven (and others) can use
    * Port gradle IT's to this build
    * See if anyone is interested in this project

---
+++ maven-wrapper/src/test/java/org/apache/maven/wrapper/DownloaderTest.java
@@ -0,0 +1,49 @@
+package org.apache.maven.wrapper;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.net.URI;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+public class DownloaderTest
+{
+
+    private DefaultDownloader download;
+
+    private File testDir;
+
+    private File downloadFile;
+
+    private File rootDir;
+
+    private URI sourceRoot;
+
+    private File remoteFile;
+
+    @Before
+    public void setUp()
+        throws Exception
+    {
+        download = new DefaultDownloader( "mvnw", "aVersion" );
+        testDir = new File( "target/test-files/DownloadTest" );
+        rootDir = new File( testDir, "root" );
+        downloadFile = new File( rootDir, "file" );
+        remoteFile = new File( testDir, "remoteFile" );
+        FileUtils.write( remoteFile, "sometext" );
+        sourceRoot = remoteFile.toURI();
+    }
+
+    @Test
+    public void testDownload()
+        throws Exception
+    {
+        assert !downloadFile.exists();
+        download.download( sourceRoot, downloadFile );
+        assert downloadFile.exists();
+        assertEquals( "sometext", FileUtils.readFileToString( downloadFile ) );
+    }
+}
