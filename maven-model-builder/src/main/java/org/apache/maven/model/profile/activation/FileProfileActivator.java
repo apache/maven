c6529932f9e3efdfc86ed73f59a307a8f8b6ea5f
Mysterion <pericov.ko@gmail.com>
2014-05-27 00:18:08 +0400

    Add solution to http://jira.codehaus.org/browse/MNG-4565
    Signed-off-by: Jason van Zyl <jason@tesla.io>

--- maven-model-builder/src/main/java/org/apache/maven/model/profile/activation/FileProfileActivator.java
+++ maven-model-builder/src/main/java/org/apache/maven/model/profile/activation/FileProfileActivator.java
@@ -167,4 +167,23 @@ public class FileProfileActivator
         return missing ? !fileExists : fileExists;
     }
 
+    @Override
+    public boolean presentInConfig( Profile profile, ProfileActivationContext context, ModelProblemCollector problems )
+    {
+        Activation activation = profile.getActivation();
+
+        if ( activation == null )
+        {
+            return false;
+        }
+
+        ActivationFile file = activation.getFile();
+
+        if ( file == null )
+        {
+            return false;
+        }
+        return true;
+    }
+
 }
