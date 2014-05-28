493b23d61300ebc381b34482d41e9b86fdf9bc06
Mark Ingram <mark.ingram@trafigura.com>
2014-05-28 17:36:31 +0100

    Added guidance for setting proxy if required
    Signed-off-by: Jason van Zyl <jason@tesla.io>

--- its/run-its.bat
+++ its/run-its.bat
@@ -1,6 +1,7 @@
 @REM How JvZ runs the ITs from a clean slate if it would be on Windows
 
-mvn clean install -Prun-its,embedded -Dmaven.repo.local=%cd%/repo
+mvn clean install -Prun-its,embedded -Dmaven.repo.local=%cd%/repo 
 
+@REM If behind a proxy try this..
 
-suite.addTestSuite( MavenITmng5572ReactorPluginExtensionsTest.class  );
+@REM mvn clean install -Prun-its,embedded -Dmaven.repo.local=%cd%/repo -Dproxy.active=true -Dproxy.type=http -Dproxy.host=<host> -Dproxy.port=<port> -Dproxy.user= -Dproxy.pass=
--- its/run-its.sh
+++ its/run-its.sh
@@ -3,3 +3,7 @@
 # How I run the ITs from a clean slate. Though I do this with a primed Nexus instance. JvZ.
 
 mvn clean install -Prun-its,embedded -Dmaven.repo.local=`pwd`/repo
+
+# If behind a proxy try this
+
+# mvn clean install -Prun-its,embedded -Dmaven.repo.local=`pwd`/repo -Dproxy.active=true -Dproxy.type=http -Dproxy.host=<host> -Dproxy.port=<port> -Dproxy.user= -Dproxy.pass=
