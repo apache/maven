84b9a832e90235f8e9b16b7d1d2b1de81de2b49a
Jason van Zyl <jason@tesla.io>
2014-05-29 13:51:50 -0400

    Rename as previous ITs have been named

--- its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5608ProfileActivationWarningTest.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5608ProfileActivationWarningTest.java
@@ -43,7 +43,7 @@ public class MavenITmng5608ProfileActivationWarningTest
     public void testitMNG5608()
         throws Exception
     {
-        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-5608" );
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-5608-profile-activation-warning" );
 
         Verifier verifier = newVerifier( testDir.getAbsolutePath() );
         verifier.executeGoal( "validate" );
