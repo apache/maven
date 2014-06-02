9a128aa7c1fcf1bd99577ef83720dcc3f95de4f0
Tamas Cservenak <tamas@cservenak.net>
2014-06-02 15:41:19 +0200

    MNG-5640: IT that verifies that lifecycle participant afterSessionEnd is invoked even with build failure
    Signed-off-by: Jason van Zyl <jason@tesla.io>

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5640LifecycleParticipantAfterSessionEnd.java
@@ -0,0 +1,66 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+
+import org.apache.maven.it.util.ResourceExtractor;
+
+/**
+ * IT that verifies that lifecycle participant afterSessionEnd
+ * method is invoked even with build failure.
+ */
+public class MavenITmng5640LifecycleParticipantAfterSessionEnd
+    extends AbstractMavenIntegrationTestCase
+{
+    public MavenITmng5640LifecycleParticipantAfterSessionEnd()
+    {
+        super( "[3.2.2,)" );
+    }
+
+    public void test()
+        throws Exception
+    {
+        File testDir =
+            ResourceExtractor.simpleExtractResources( getClass(), "/mng-5640-lifecycleParticipant-afterSession" );
+        File extensionDir = new File( testDir, "extension" );
+        File projectDir = new File( testDir, "basic" );
+
+        Verifier verifier;
+
+        // install the test plugin
+        verifier = newVerifier( extensionDir.getAbsolutePath(), "remote" );
+        verifier.executeGoal( "install" );
+        verifier.resetStreams();
+        verifier.verifyErrorFreeLog();
+
+        // build the test project
+        verifier = newVerifier( projectDir.getAbsolutePath(), "remote" );
+        try {
+            verifier.executeGoal("package");
+        } catch (VerificationException e) {
+            // expected, as the build will fail due to always failing UT
+        }
+        verifier.resetStreams();
+        verifier.verifyTextInLog("testApp(org.apache.maven.its.mng5640.FailingTest)");
+
+        verifier.assertFilePresent( "target/afterSessionEnd.txt" );
+    }
+}
---
+++ its/core-it-suite/src/test/resources/mng-5640-lifecycleParticipant-afterSession/basic/src/test/java/org/apache/maven/its/mng5640/FailingTest.java
@@ -0,0 +1,18 @@
+package org.apache.maven.its.mng5640;
+
+import junit.framework.TestCase;
+
+/**
+ * Always failing UT.
+ */
+public class FailingTest
+    extends TestCase
+{
+  /**
+   * Rigourous Test :-)
+   */
+  public void testApp()
+  {
+    assertTrue( false );
+  }
+}
