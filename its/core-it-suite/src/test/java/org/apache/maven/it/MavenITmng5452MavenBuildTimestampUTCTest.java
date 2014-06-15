810e73202e6ed22ab36dce9786e8f8f092c9e0ad
Michael Osipov <michaelo@apache.org>
2014-06-15 22:31:32 +0200

    Fix href label for IT MNG-5452.

--- its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5452MavenBuildTimestampUTCTest.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5452MavenBuildTimestampUTCTest.java
@@ -26,7 +26,7 @@ import java.util.Properties;
 import org.apache.maven.it.util.ResourceExtractor;
 
 /**
- * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-5452">MNG-4091</a>
+ * This is a test set for <a href="http://jira.codehaus.org/browse/MNG-5452">MNG-5452</a>
  * Make sure that the maven.build.timestamp is in UTC.
  */
 public class MavenITmng5452MavenBuildTimestampUTCTest
