ce6fc6237a5ea3d3f534b751b54c236b948336cf
Jason van Zyl <jason@tesla.io>
2014-07-05 16:05:06 -0400

    Adding a note about the deprecation

--- maven-core/src/main/java/org/apache/maven/ProjectDependenciesResolver.java
+++ maven-core/src/main/java/org/apache/maven/ProjectDependenciesResolver.java
@@ -29,6 +29,11 @@ import org.apache.maven.execution.MavenSession;
 import org.apache.maven.project.MavenProject;
 
 @Deprecated
+/**
+ * @deprecated As of 3.2.2, and there is no direct replacement. This is an internal class which was not marked as such, but should have been.
+ * @author jvanzyl
+ *
+ */
 public interface ProjectDependenciesResolver
 {
 
