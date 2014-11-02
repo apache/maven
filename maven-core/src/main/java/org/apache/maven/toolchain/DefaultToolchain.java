2d086fde2520f806d411e561c40deaee18512ef7
Robert Scholte <rfscholte@codehaus.org>
2014-11-02 19:00:39 +0100

    remove unused import

--- maven-core/src/main/java/org/apache/maven/toolchain/DefaultToolchain.java
+++ maven-core/src/main/java/org/apache/maven/toolchain/DefaultToolchain.java
@@ -24,7 +24,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.maven.toolchain.java.DefaultJavaToolChain;
 import org.apache.maven.toolchain.model.ToolchainModel;
 import org.codehaus.plexus.logging.Logger;
 import org.codehaus.plexus.util.xml.Xpp3Dom;
