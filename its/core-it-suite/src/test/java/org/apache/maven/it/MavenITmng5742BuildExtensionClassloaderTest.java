4a0c98864045c3169b9344cab660d860120a9192
Igor Fedorenko <ifedorenko@apache.org>
2014-12-25 13:20:11 -0500

    MNG-5742 fixed duplicate plugin realms when extensions=true
    Signed-off-by: Igor Fedorenko <ifedorenko@apache.org>

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5742BuildExtensionClassloaderTest.java
@@ -0,0 +1,61 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+
+import org.apache.maven.it.util.ResourceExtractor;
+import org.apache.maven.shared.utils.io.FileUtils;
+
+public class MavenITmng5742BuildExtensionClassloaderTest
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng5742BuildExtensionClassloaderTest()
+    {
+        super( "[3.2.6-SNAPSHOT,)" );
+    }
+
+    public void testBuildExtensionClassloader()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-5742-build-extension-classloader" );
+        File pluginDir = new File( testDir, "plugin" );
+        File projectDir = new File( testDir, "project" );
+
+        Verifier verifier;
+
+        // install the test plugin
+        verifier = newVerifier( pluginDir.getAbsolutePath(), "remote" );
+        verifier.executeGoal( "install" );
+        verifier.resetStreams();
+        verifier.verifyErrorFreeLog();
+
+        // build the test project
+        verifier = newVerifier( projectDir.getAbsolutePath(), "remote" );
+        verifier.executeGoal( "validate" );
+        verifier.resetStreams();
+        verifier.verifyErrorFreeLog();
+        verifier.assertFilePresent( "target/execution-success.txt" );
+
+        String actual = FileUtils.fileRead( new File( projectDir, "target/execution-success.txt" ) );
+        assertEquals( "executed", actual );
+    }
+}
