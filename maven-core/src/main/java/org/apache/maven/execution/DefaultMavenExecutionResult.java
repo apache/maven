ed40dffdb5ad6efb61a5bef4b0aa3f977e91bfee
Tibor17 <tibor17@lycos.com>
2015-01-02 21:44:39 +0100

    [MAVEN] removed unused import

--- maven-core/src/main/java/org/apache/maven/execution/DefaultMavenExecutionResult.java
+++ maven-core/src/main/java/org/apache/maven/execution/DefaultMavenExecutionResult.java
@@ -23,7 +23,6 @@ import java.util.Collections;
 import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.apache.maven.project.DependencyResolutionResult;
