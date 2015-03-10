ae42f093bed3267fbf42749a775a2a4bc2dd7be9
Igor Fedorenko <ifedorenko@apache.org>
2015-03-10 10:37:41 -0400

    MNG-5783 test plugin.artifacts can include slf4j
    Signed-off-by: Igor Fedorenko <ifedorenko@apache.org>

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5783PluginDependencyFiltering.java
@@ -0,0 +1,40 @@
+package org.apache.maven.it;
+
+import java.io.File;
+import java.util.List;
+
+import org.apache.maven.it.util.ResourceExtractor;
+
+public class MavenITmng5783PluginDependencyFiltering
+    extends AbstractMavenIntegrationTestCase
+{
+
+    public MavenITmng5783PluginDependencyFiltering()
+    {
+        super( "[3.0,)" );
+    }
+
+    public void testSLF4j()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-5783-plugin-dependency-filtering" );
+        Verifier verifier = newVerifier( new File( testDir, "plugin" ).getAbsolutePath() );
+        verifier.executeGoal( "install" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        verifier = newVerifier( new File( testDir, "slf4j" ).getAbsolutePath(), "remote" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        // Note that plugin dependencies always include plugin itself and plexus-utils
+
+        List<String> dependencies = verifier.loadLines( "target/dependencies.txt", "UTF-8" );
+        assertEquals( 3, dependencies.size() );
+        assertEquals( "mng-5783-plugin-dependency-filtering:mng-5783-plugin-dependency-filtering-plugin:maven-plugin:0.1",
+                      dependencies.get( 0 ) );
+        assertEquals( "org.slf4j:slf4j-api:jar:1.7.5", dependencies.get( 1 ) );
+        assertEquals( "org.codehaus.plexus:plexus-utils:jar:1.1", dependencies.get( 2 ) );
+    }
+}
---
+++ its/core-it-suite/src/test/resources/mng-5783-plugin-dependency-filtering/plugin/src/main/java/org/apache/maven/its/mng5783/plugin/TestMojo.java
@@ -0,0 +1,72 @@
+package org.apache.maven.its.mng5783.plugin;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.List;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * @goal test
+ */
+public class TestMojo
+    extends AbstractMojo
+{
+    /** @parameter property="project.build.directory" */
+    private File outputDirectory;
+
+    /** @parameter property="plugin.artifacts" */
+    private List<Artifact> artifacts;
+
+    public void execute()
+        throws MojoExecutionException
+    {
+        try
+        {
+            File file = new File( outputDirectory, "dependencies.txt" );
+            file.getParentFile().mkdirs();
+            BufferedWriter w = new BufferedWriter( new OutputStreamWriter( new FileOutputStream( file ), "UTF-8" ) );
+            try
+            {
+                for ( Artifact artifact : artifacts )
+                {
+                    w.write( artifact.getId() );
+                    w.newLine();
+                }
+            }
+            finally
+            {
+                w.close();
+            }
+        }
+        catch ( IOException e )
+        {
+            throw new MojoExecutionException( e.getMessage(), e );
+        }
+
+    }
+}
