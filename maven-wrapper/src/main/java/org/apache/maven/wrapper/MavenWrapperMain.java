fcd0a69a15c33def668d27b2b359f2b80a46b9ac
Jason van Zyl <jason@tesla.io>
2015-04-03 00:55:27 -0400

    Update location for searching for the version

--- maven-wrapper/src/main/java/org/apache/maven/wrapper/MavenWrapperMain.java
+++ maven-wrapper/src/main/java/org/apache/maven/wrapper/MavenWrapperMain.java
@@ -86,7 +86,7 @@ public class MavenWrapperMain {
 
   static String wrapperVersion() {
     try {
-      InputStream resourceAsStream = MavenWrapperMain.class.getResourceAsStream("/META-INF/maven/org.apache.maven/maven-wapper/pom.properties");
+      InputStream resourceAsStream = MavenWrapperMain.class.getResourceAsStream("/META-INF/maven/io.takari/maven-wapper/pom.properties");
       if (resourceAsStream == null) {
         throw new RuntimeException("No maven properties found.");
       }
