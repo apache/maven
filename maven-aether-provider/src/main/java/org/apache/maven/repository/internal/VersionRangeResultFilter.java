ce9759d04de96720ccf9e2320dfdddbe8cae55c6
barthel <barthel@users.noreply.github.com>
2015-04-30 14:37:52 +0200

    MNG-6049: Adds version range result filter behaviour
    The DefaultVersionRangeResolver will be extends with a filter for
    version range results.
    
    This commit adds a new interfaces VersionRangeResultFilter and a
    non filtering DefaultVersionRangeResultFilter.

---
+++ maven-aether-provider/src/main/java/org/apache/maven/repository/internal/VersionRangeResultFilter.java
@@ -0,0 +1,45 @@
+package org.apache.maven.repository.internal;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.eclipse.aether.resolution.VersionRangeResolutionException;
+import org.eclipse.aether.resolution.VersionRangeResult;
+
+/**
+ * Filters the resolved versions provided by {@link VersionRangeResult#getVersions()}.
+ *
+ * @author barthel
+ * @since 3.4.0
+ */
+public interface VersionRangeResultFilter
+{
+
+    /**
+     *
+     * @param versionRangeResult The version range result, must not be {@code null}
+     * @return A filtered version range result, never {@code null}
+     * @throws VersionRangeResolutionException If the requested range could not be parsed. Note that an empty range does
+     * not raise an exception.
+     * @see VersionRangeResult#getVersions()
+     */
+    VersionRangeResult filterVersionRangeResult( VersionRangeResult versionRangeResult )
+            throws VersionRangeResolutionException;
+
+}
