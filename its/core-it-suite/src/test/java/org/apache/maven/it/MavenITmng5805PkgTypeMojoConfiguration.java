e8de3ad0a162ab329f5ef8ccb996161948a57878
Kristian Rosenvold <krosenvold@apache.org>
2015-06-23 19:00:50 +0200

    Moved test extension and support code to the proper place. Hopefully this will make test more reliable

--- its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5805PkgTypeMojoConfiguration.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5805PkgTypeMojoConfiguration.java
@@ -20,22 +20,8 @@ public class MavenITmng5805PkgTypeMojoConfiguration
         
         Verifier verifier;
         
-        verifier = newVerifier( new File( testDir, "extension" ).getAbsolutePath() );
-        verifier.executeGoal( "install" );
-        verifier.verifyErrorFreeLog();
-        verifier.resetStreams();
-        
-        verifier = newVerifier( new File( testDir, "plugin" ).getAbsolutePath() );
-        verifier.executeGoal( "install" );
-        verifier.verifyErrorFreeLog();
-        verifier.resetStreams();
-        
-        verifier = newVerifier( new File( testDir, "plugin-dep" ).getAbsolutePath() );
-        verifier.executeGoal( "install" );
-        verifier.verifyErrorFreeLog();
-        verifier.resetStreams();
-        
-        verifier = newVerifier( new File( testDir, "project" ).getAbsolutePath() );
+
+        verifier = newVerifier( testDir.getAbsolutePath() );
         verifier.executeGoal( "validate" );
         verifier.verifyErrorFreeLog();
         verifier.verifyTextInLog( "CLASS_NAME=org.apache.maven.its.mng5805.TestClass1" );
--- its/core-it-suite/src/test/resources/mng-5805-pkg-type-mojo-configuration/plugin-dep/src/main/java/org/apache/maven/its/mng5805/TestClass1.java
+++
@@ -1,5 +0,0 @@
-package org.apache.maven.its.mng5805;
-
-public class TestClass1 {
-
-}
