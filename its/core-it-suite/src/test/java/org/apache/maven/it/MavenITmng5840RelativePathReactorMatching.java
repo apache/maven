26e16b613debc5d8792c9ca47f56de568455ad7a
Stephen Connolly <stephen.alan.connolly@gmail.com>
2015-06-24 14:44:08 +0100

    [MNG-5840] Adding a test case to ensure regression does not repeat
    - Confirmed that this test case passes on 3.0.4, 3.2.5 and current 3.3.4-SNAPSHOT after fix merged while failing on 3.3.3

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5840RelativePathReactorMatching.java
@@ -0,0 +1,31 @@
+package org.apache.maven.it;
+
+import org.apache.maven.it.util.ResourceExtractor;
+
+import java.io.File;
+
+public class MavenITmng5840RelativePathReactorMatching
+    extends AbstractMavenIntegrationTestCase
+{
+    public MavenITmng5840RelativePathReactorMatching()
+    {
+        super( ALL_MAVEN_VERSIONS );
+    }
+    
+    public void testPkgTypeMojoConfiguration()
+        throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-5840-relative-path-reactor-matching" );
+        
+        Verifier verifier = newVerifier( new File( testDir, "parent-1" ).getAbsolutePath(), "remote" );
+        verifier.executeGoal( "install" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+
+        verifier = newVerifier( new File( testDir, "child" ).getAbsolutePath(), "remote" );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+    }
+    
+}
