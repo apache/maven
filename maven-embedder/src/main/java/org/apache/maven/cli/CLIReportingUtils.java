d556da84348b0d2de04839ad18ec75ba7231ea8c
Michael Osipov <michaelo@apache.org>
2015-10-10 19:52:56 +0200

    Capitalize names in missing -v output

--- maven-embedder/src/main/java/org/apache/maven/cli/CLIReportingUtils.java
+++ maven-embedder/src/main/java/org/apache/maven/cli/CLIReportingUtils.java
@@ -58,14 +58,14 @@ public final class CLIReportingUtils
         version.append( createMavenVersionString( properties ) ).append( ls );
         version.append( reduce(
             properties.getProperty( "distributionShortName" ) + " home: " + System.getProperty( "maven.home",
-                                                                                                "<unknown maven "
+                                                                                                "<unknown Maven "
                                                                                                     + "home>" ) ) )
             .append(
             ls );
         version.append( "Java version: " ).append(
-            System.getProperty( "java.version", "<unknown java version>" ) ).append( ", vendor: " ).append(
+            System.getProperty( "java.version", "<unknown Java version>" ) ).append( ", vendor: " ).append(
             System.getProperty( "java.vendor", "<unknown vendor>" ) ).append( ls );
-        version.append( "Java home: " ).append( System.getProperty( "java.home", "<unknown java home>" ) ).append( ls );
+        version.append( "Java home: " ).append( System.getProperty( "java.home", "<unknown Java home>" ) ).append( ls );
         version.append( "Default locale: " ).append( Locale.getDefault() ).append( ", platform encoding: " ).append(
             System.getProperty( "file.encoding", "<unknown encoding>" ) ).append( ls );
         version.append( "OS name: \"" ).append( Os.OS_NAME ).append( "\", version: \"" ).append( Os.OS_VERSION ).append(
