6518f1f364d9085d21a711ef31e2d43cdf4fa443
Michael Osipov <michaelo@apache.org>
2016-02-12 23:49:14 +0100

    Added some docs in CLIReporting Utils

--- maven-embedder/src/main/java/org/apache/maven/cli/CLIReportingUtils.java
+++ maven-embedder/src/main/java/org/apache/maven/cli/CLIReportingUtils.java
@@ -169,18 +169,22 @@ public final class CLIReportingUtils
         String format;
         if ( d > 0 )
         {
+            // Length 11+ chars
             format = "%d d %02d:%02d h";
         }
         else if ( h > 0 )
         {
+            // Length 7 chars
             format = "%2$02d:%3$02d h";
         }
         else if ( m > 0 )
         {
+            // Length 9 chars
             format = "%3$02d:%4$02d min";
         }
         else
         {
+            // Length 7-8 chars
             format = "%4$d.%5$03d s";
         }
 
