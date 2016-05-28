c7a4079de6f99307c559e6ab67cd2c4d51209638
Hervé Boutemy <hboutemy@apache.org>
2016-05-28 13:28:23 +0200

    updated urls (https, plexus) and license header formatting

--- maven-core/src/main/java/org/apache/maven/AbstractMavenLifecycleParticipant.java
+++ maven-core/src/main/java/org/apache/maven/AbstractMavenLifecycleParticipant.java
@@ -27,7 +27,7 @@ import org.apache.maven.execution.MavenSession;
  * All callback methods (will) follow beforeXXX/afterXXX naming pattern to
  * indicate at what lifecycle point it is being called.
  *
- * @see <a href="http://maven.apache.org/examples/maven-3-lifecycle-extensions.html">example</a>
+ * @see <a href="https://maven.apache.org/examples/maven-3-lifecycle-extensions.html">example</a>
  * @see <a href="https://issues.apache.org/jira/browse/MNG-4224">MNG-4224</a>
  * @since 3.0-alpha-3
  */
