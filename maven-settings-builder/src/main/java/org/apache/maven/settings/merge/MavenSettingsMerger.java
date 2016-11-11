84e546cb38846e216747149dcb82b5ae2e896890
Christian Schulte <schulte@apache.org>
2016-11-11 21:41:46 +0100

    [MNG-4645] Move Central repo definition out of Maven's core so it can be more easily changed

--- maven-settings-builder/src/main/java/org/apache/maven/settings/merge/MavenSettingsMerger.java
+++ maven-settings-builder/src/main/java/org/apache/maven/settings/merge/MavenSettingsMerger.java
@@ -88,6 +88,8 @@ public class MavenSettingsMerger {
         shallowMergeById(dominant.getServers(), recessive.getServers(), recessiveSourceLevel);
         shallowMergeById(dominant.getProxies(), recessive.getProxies(), recessiveSourceLevel);
         shallowMergeById(dominant.getProfiles(), recessive.getProfiles(), recessiveSourceLevel);
+        shallowMergeById(dominant.getRepositories(), recessive.getRepositories(), recessiveSourceLevel);
+        shallowMergeById(dominant.getPluginRepositories(), recessive.getPluginRepositories(), recessiveSourceLevel);
     }
 
     /**
