2385484a813edc157ba151c9daa0625eaf8902f9
Christian Schulte <schulte@apache.org>
2016-12-18 04:24:02 +0100

    Revert "[MNG-5457] Show repository id when downloading or uploading from/to a remote repository"
    This reverts commit 8a1b6359f31178ce804018cfb524e72f1178028a.

--- maven-embedder/src/main/java/org/apache/maven/cli/transfer/AbstractMavenTransferListener.java
+++ maven-embedder/src/main/java/org/apache/maven/cli/transfer/AbstractMavenTransferListener.java
@@ -23,6 +23,7 @@ import java.io.PrintStream;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
+
 import org.apache.commons.lang3.Validate;
 import org.eclipse.aether.transfer.AbstractTransferListener;
 import org.eclipse.aether.transfer.TransferCancelledException;
@@ -216,15 +217,9 @@ public abstract class AbstractMavenTransferListener
     public void transferInitiated( TransferEvent event )
     {
         String type = event.getRequestType() == TransferEvent.RequestType.PUT ? "Uploading" : "Downloading";
-        String direction = event.getRequestType() == TransferEvent.RequestType.PUT ? "to" : "from";
 
         TransferResource resource = event.getResource();
-        StringBuilder message = new StringBuilder();
-        message.append( type ).append( ' ' ).append( direction ).append( ' ' ).append( resource.getRepositoryId() );
-        message.append( ": " );
-        message.append( resource.getRepositoryUrl() ).append( resource.getResourceName() );
-
-        out.println( message.toString() );
+        out.println( type + ": " + resource.getRepositoryUrl() + resource.getResourceName() );
     }
 
     @Override
@@ -232,35 +227,30 @@ public abstract class AbstractMavenTransferListener
         throws TransferCancelledException
     {
         TransferResource resource = event.getResource();
-        out.println( "[WARNING] " + event.getException().getMessage() + " from " + resource.getRepositoryId() + " for "
-            + resource.getRepositoryUrl() + resource.getResourceName() );
+        out.println( "[WARNING] " + event.getException().getMessage() + " for " + resource.getRepositoryUrl()
+            + resource.getResourceName() );
     }
 
     @Override
     public void transferSucceeded( TransferEvent event )
     {
-        String type = ( event.getRequestType() == TransferEvent.RequestType.PUT ? "Uploaded" : "Downloaded" );
-        String direction = event.getRequestType() == TransferEvent.RequestType.PUT ? "to" : "from";
-
         TransferResource resource = event.getResource();
         long contentLength = event.getTransferredBytes();
+
         FileSizeFormat format = new FileSizeFormat( Locale.ENGLISH );
+        String type = ( event.getRequestType() == TransferEvent.RequestType.PUT ? "Uploaded" : "Downloaded" );
+        String len = format.format( contentLength );
 
-        StringBuilder message = new StringBuilder();
-        message.append( type ).append( ' ' ).append( direction ).append( ' ' ).append( resource.getRepositoryId() );
-        message.append( ": " );
-        message.append( resource.getRepositoryUrl() ).append( resource.getResourceName() );
-        message.append( " (" ).append( format.format( contentLength ) );
-
+        String throughput = "";
         long duration = System.currentTimeMillis() - resource.getTransferStartTime();
         if ( duration > 0L )
         {
             double bytesPerSecond = contentLength / ( duration / 1000.0 );
-            message.append( " at " ).append( format.format( (long) bytesPerSecond ) ).append( "/s" );
+            throughput = " at " + format.format( (long) bytesPerSecond ) + "/s";
         }
 
-        message.append( ')' );
-        out.println( message.toString() );
+        out.println( type + ": " + resource.getRepositoryUrl() + resource.getResourceName() + " (" + len
+            + throughput + ")" );
     }
 
 }
