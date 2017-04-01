d9c9a8551d31849e00cd8e47dde6258f7fac769a
Michael Osipov <michaelo@apache.org>
2017-04-01 22:10:35 +0200

    [MNG-6202] Cannot pass nonProxyHosts to ITs making remote tests lock up when proxy rejects to proxy internal hosts
    Set default value 'localhost' of 'proxy.nonProxyHosts' and allow users to
    override if necessary.

--- its/run-its.bat
+++ its/run-its.bat
@@ -19,8 +19,8 @@
 
 @REM How JvZ runs the ITs from a clean slate if it would be on Windows
 
-mvn clean install -Prun-its,embedded -Dmaven.repo.local=%cd%/repo 
+mvn clean install -Prun-its,embedded -Dmaven.repo.local=%cd%\repo
 
 @REM If behind a proxy try this..
 
-@REM mvn clean install -Prun-its,embedded -Dmaven.repo.local=%cd%/repo -Dproxy.active=true -Dproxy.type=http -Dproxy.host=<host> -Dproxy.port=<port> -Dproxy.user= -Dproxy.pass=
+@REM mvn clean install -Prun-its,embedded -Dmaven.repo.local=%cd%\repo -Dproxy.host=<host> -Dproxy.port=<port> -Dproxy.user= -Dproxy.pass= -Dproxy.nonProxyHosts=<hosts>
--- its/run-its.sh
+++ its/run-its.sh
@@ -25,4 +25,4 @@ mvn clean install -Prun-its,embedded -Dmaven.repo.local=`pwd`/repo
 
 # If behind a proxy try this
 
-# mvn clean install -Prun-its,embedded -Dmaven.repo.local=`pwd`/repo -Dproxy.active=true -Dproxy.type=http -Dproxy.host=<host> -Dproxy.port=<port> -Dproxy.user= -Dproxy.pass=
+# mvn clean install -Prun-its,embedded -Dmaven.repo.local=`pwd`/repo -Dproxy.host=<host> -Dproxy.port=<port> -Dproxy.user= -Dproxy.pass= -Dproxy.nonProxyHosts=<hosts>
