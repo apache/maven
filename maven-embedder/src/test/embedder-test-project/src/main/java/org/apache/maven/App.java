ba07c836221a740e4e3999cf735d71855f2fe00d
Sharabaddin <general1308@gmail.com>
2017-07-09 00:14:51 +0300

    Update App.java
    correct code style(most standart)
    and change correct history "Hello, World!"

--- maven-embedder/src/test/embedder-test-project/src/main/java/org/apache/maven/App.java
+++ maven-embedder/src/test/embedder-test-project/src/main/java/org/apache/maven/App.java
@@ -4,10 +4,8 @@ package org.apache.maven;
  * Hello world!
  *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }
