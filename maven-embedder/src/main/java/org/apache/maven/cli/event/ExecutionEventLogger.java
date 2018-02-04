99e66229aca82e12292d0e481d8e71158cc52ca4
Karl Heinz Marbaise <khmarbaise@apache.org>
2018-02-04 11:27:33 +0100

    [MNG-6340] [Performance]To make System.gc() call configurable in target summary code  o Just removed the System.gc() call.

--- maven-embedder/src/main/java/org/apache/maven/cli/event/ExecutionEventLogger.java
+++ maven-embedder/src/main/java/org/apache/maven/cli/event/ExecutionEventLogger.java
@@ -233,14 +233,6 @@ public class ExecutionEventLogger
         logger.info( "Total time: " + formatDuration( time ) + wallClock );
 
         logger.info( "Finished at: " + formatTimestamp( finish ) );
-
-        System.gc();
-
-        Runtime r = Runtime.getRuntime();
-
-        long mb = 1024 * 1024;
-
-        logger.info( "Final Memory: " + ( r.totalMemory() - r.freeMemory() ) / mb + "M/" + r.totalMemory() / mb + "M" );
     }
 
     @Override
