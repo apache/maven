f5674713a6b309881cd25aef64b4c31de94900d0
Hervé Boutemy <hboutemy@apache.org>
2018-08-12 00:01:17 +0200

    improved documentation

--- maven-artifact/src/main/java/org/apache/maven/artifact/handler/ArtifactHandler.java
+++ maven-artifact/src/main/java/org/apache/maven/artifact/handler/ArtifactHandler.java
@@ -20,16 +20,32 @@ package org.apache.maven.artifact.handler;
  */
 
 /**
+ * An artifact handler defies for one type (defined as Plexus role):<ul>
+ * <li>extension and classifier to be able to download the file,</li>
+ * <li>information on how to use the artifact: whether to add it to the classpath, or to take into account its
+ * dependencies.</li>
+ * </ul>
+ * 
  * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
  */
 public interface ArtifactHandler
 {
     String ROLE = ArtifactHandler.class.getName();
 
+    /**
+     * Get the file extension associated to the type of artifact.
+     *
+     * @return the extension
+     */
     String getExtension();
 
     String getDirectory();
 
+    /**
+     * Get the classifier associated to the type of artifact.
+     * 
+     * @return the classifier
+     */
     String getClassifier();
 
     String getPackaging();
