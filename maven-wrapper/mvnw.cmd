e5c51489e942c1a261cd8548220774c290854a47
Julian Hyde <jhyde@apache.org>
2018-08-31 09:54:11 -0700

    On Windows, use TLS 1.2
    Github no longer supports TLS 1.0 or 1.1, but TLS 1.2 is not the default
    version of TLS for PowerShell.
    See https://github.com/cretueusebiu/valet-windows/issues/78#issuecomment-369824766
    
    Also convert a couple of tabs to spaces, and remove a trailing space.

--- maven-wrapper/mvnw.cmd
+++ maven-wrapper/mvnw.cmd
@@ -122,7 +122,7 @@ set WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain
 
 set DOWNLOAD_URL="https://repo.maven.apache.org/maven2/io/takari/maven-wrapper/0.4.2/maven-wrapper-0.4.2.jar"
 FOR /F "tokens=1,2 delims==" %%A IN (%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.properties) DO (
-	IF "%%A"=="wrapperUrl" SET DOWNLOAD_URL=%%B 
+    IF "%%A"=="wrapperUrl" SET DOWNLOAD_URL=%%B
 )
 
 @REM Extension to allow automatically downloading the maven-wrapper.jar from Maven-central
