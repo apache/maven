790be24342237632552bba8e7a9dd290c560da7f
Reinhold Degenfellner <reinhold.jun@degenfellner.com>
2018-12-18 20:58:17 +0100

    use MVNW_USER and MVNW_PASSWORD instead of system properties
    fix distribution naming
    add readme entry

--- maven-wrapper/src/main/java/org/apache/maven/wrapper/MavenWrapperMain.java
+++ maven-wrapper/src/main/java/org/apache/maven/wrapper/MavenWrapperMain.java
@@ -37,7 +37,10 @@ public class MavenWrapperMain {
   public static final String MAVEN_USER_HOME_ENV_KEY = "MAVEN_USER_HOME";
 
   public static final String MVNW_VERBOSE = "MVNW_VERBOSE";
+  public static final String MVNW_USER = "MVNW_USER";
+  public static final String MVNW_PASSWORD = "MVNW_PASSWORD";
 
+  
   public static void main(String[] args) throws Exception {
     File wrapperJar = wrapperJar();
     File propertiesFile = wrapperProperties(wrapperJar);
