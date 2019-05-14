65a5a1bbaaf205fa2614d0725291a4bbc63f9633
rfscholte <rfscholte@apache.org>
2019-05-14 21:26:07 +0200

    [MNG-6656] Introduce base for build/consumer pom

---
+++ maven-core/src/main/java/org/apache/maven/xml/filter/ConsumerPomXMLFilter.java
@@ -0,0 +1,73 @@
+package org.apache.maven.xml.filter;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLFilter;
+import org.xml.sax.XMLReader;
+
+import org.xml.sax.helpers.XMLFilterImpl;
+
+/**
+ * XML Filter to transform pom.xml to consumer pom.
+ * This often means stripping of build-specific information.
+ * 
+ * This filter is used at 2 locations:
+ * - {@link org.apache.maven.internal.aether.DefaultRepositorySystemSessionFactory} when publishing pom files.
+ * - TODO ???Class when a reactor module is used as dependency. This ensures consistency of dependency handling
+ * 
+ * @author Robert Scholte
+ * @since 3.7.0
+ */
+public class ConsumerPomXMLFilter extends XMLFilterImpl
+{
+    private final XMLFilter rootFilter;
+
+    public ConsumerPomXMLFilter() throws SAXException, ParserConfigurationException
+    {
+        this( SAXParserFactory.newInstance().newSAXParser().getXMLReader() );
+    }
+
+    public ConsumerPomXMLFilter( XMLReader parent )
+    {
+        rootFilter = new BuildPomXMLFilter( parent );
+        
+        // Ensure that xs:any elements aren't touched by next filters
+        XMLFilter filter = new FastForwardFilter( rootFilter );
+        
+        // Strip modules
+        filter = new ModulesXMLFilter( filter );
+        // Adjust relativePath
+        filter = new RelativePathXMLFilter( filter );
+        
+        // maybe more to follow
+        
+        super.setParent( filter );
+    }
+    
+    @Override
+    public void setParent( XMLReader parent )
+    {
+        rootFilter.setParent( parent );
+    }
+}
---
+++ maven-core/src/main/java/org/apache/maven/xml/filter/ModulesXMLFilter.java
@@ -0,0 +1,102 @@
+package org.apache.maven.xml.filter;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLFilterImpl;
+
+/**
+ * Remove all modules, this is just buildtime information
+ * 
+ * @author Robert Scholte
+ * @since 3.7.0
+ */
+class ModulesXMLFilter
+    extends XMLFilterImpl
+{
+    /**
+     * Using 3 to also remove whitespace-block after closing tag
+     * -1 none
+     *  1 started
+     *  2 ended
+     */
+    int modulesStatus = -1;
+    
+    ModulesXMLFilter()
+    {
+        super();
+    }
+
+    ModulesXMLFilter( XMLReader parent )
+    {
+        super( parent );
+    }
+
+    @Override
+    public void startElement( String uri, String localName, String qName, Attributes atts )
+        throws SAXException
+    {
+        if ( modulesStatus == -1 && "modules".equals( localName ) )
+        {
+            modulesStatus = 1;
+        }
+        else if ( modulesStatus == 2 )
+        {
+            modulesStatus = -1;
+        }
+
+        if ( modulesStatus != 1 )
+        {
+            super.startElement( uri, localName, qName, atts );
+        }
+    }
+
+    @Override
+    public void endElement( String uri, String localName, String qName )
+        throws SAXException
+    {
+        if ( modulesStatus == 1 && "modules".equals( localName ) )
+        {
+            modulesStatus = 2;
+        }
+        else if ( modulesStatus == 2 )
+        {
+            modulesStatus = -1;
+        }
+        
+        if ( modulesStatus == -1 )
+        {
+            super.endElement( uri, localName, qName );
+        }
+    }
+
+    @Override
+    public void characters( char[] ch, int start, int length )
+        throws SAXException
+    {
+        if ( modulesStatus == -1 )
+        {
+            super.characters( ch, start, length );
+        }
+    }
+
+}
