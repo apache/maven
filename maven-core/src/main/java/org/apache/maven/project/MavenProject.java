daec61693d5a1fd5951d4f8d2d3cd832ae9dacaa
Guillaume Nodet <gnodet@gmail.com>
2019-06-28 23:34:24 +0200

    [MNG-6698] Improve the ManagedVersionMap
    This map is not used by maven and the computation can be quite heavy, so lazy compute it.  In addition, do not clone the map, as it's an unmodifiable one.

--- maven-core/src/main/java/org/apache/maven/project/MavenProject.java
+++ maven-core/src/main/java/org/apache/maven/project/MavenProject.java
@@ -1300,7 +1300,7 @@ public class MavenProject
 
         if ( project.getManagedVersionMap() != null )
         {
-            setManagedVersionMap( new HashMap<>( project.getManagedVersionMap() ) );
+            setManagedVersionMap( project.getManagedVersionMap() );
         }
 
         lifecyclePhases.addAll( project.lifecyclePhases );
