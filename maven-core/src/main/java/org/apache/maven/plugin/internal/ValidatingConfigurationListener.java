b3b8176b898c005783802950674d5e75e6a5f647
Gabriel Belingueres <belingueres@gmail.com>
2019-09-09 00:36:13 -0300

    [MNG-5222] - Maven 3 no longer logs warnings about deprecated plugin parameters
    - Added warning when setting deprecated parameter with value different
    than the default.
    - Changed Logger to SLF4J.

--- maven-core/src/main/java/org/apache/maven/plugin/internal/ValidatingConfigurationListener.java
+++ maven-core/src/main/java/org/apache/maven/plugin/internal/ValidatingConfigurationListener.java
@@ -19,13 +19,18 @@ package org.apache.maven.plugin.internal;
  * under the License.
  */
 
+import java.lang.reflect.Array;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.StringJoiner;
 
 import org.apache.maven.plugin.descriptor.MojoDescriptor;
 import org.apache.maven.plugin.descriptor.Parameter;
+import org.apache.maven.shared.utils.logging.MessageUtils;
 import org.codehaus.plexus.component.configurator.ConfigurationListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A configuration listener to help validate the plugin configuration. For instance, check for required but missing
@@ -41,12 +46,17 @@ class ValidatingConfigurationListener
 
     private final ConfigurationListener delegate;
 
+    private final MojoDescriptor mojoDescriptor;
+
+    private final Logger logger = LoggerFactory.getLogger( getClass() );
+
     private final Map<String, Parameter> missingParameters;
 
     ValidatingConfigurationListener( Object mojo, MojoDescriptor mojoDescriptor, ConfigurationListener delegate )
     {
         this.mojo = mojo;
         this.delegate = delegate;
+        this.mojoDescriptor = mojoDescriptor;
         this.missingParameters = new HashMap<>();
 
         if ( mojoDescriptor.getParameters() != null )
@@ -92,6 +102,60 @@ class ValidatingConfigurationListener
         {
             missingParameters.remove( fieldName );
         }
+
+        if ( logger.isWarnEnabled() )
+        {
+            warnDeprecated( fieldName, value );
+        }
     }
 
+    private void warnDeprecated( String fieldName, Object value )
+    {
+        Parameter parameter = mojoDescriptor.getParameterMap().get( fieldName );
+        String deprecated = parameter.getDeprecated();
+        if ( deprecated != null && !deprecated.isEmpty() )
+        {
+            if ( !toString( value ).equals( toString( parameter.getDefaultValue() ) ) )
+            {
+                StringBuilder sb = new StringBuilder( "  Parameter '" );
+                sb.append( fieldName ).append( '\'' );
+                if ( parameter.getExpression() != null )
+                {
+                    String userProperty = parameter.getExpression().replace( "${", "'" ).replace( '}', '\'' );
+                    sb.append( " (User Property " ).append( userProperty ).append( ")" );
+                }
+                sb.append( " is deprecated. " ).append( deprecated );
+
+                logger.warn( MessageUtils.buffer().warning( sb.toString() ).toString() );
+            }
+        }
+    }
+
+    /**
+     * Creates a human-friendly string representation of the specified object.
+     *
+     * @param obj The object to create a string representation for, may be <code>null</code>.
+     * @return The string representation, never <code>null</code>.
+     */
+    private static String toString( Object obj )
+    {
+        String str;
+        if ( obj != null && obj.getClass().isArray() )
+        {
+            int n = Array.getLength( obj );
+            StringBuilder buf = new StringBuilder( 256 );
+            StringJoiner sj = new StringJoiner( ", ", "[", "]" );
+            for ( int i = 0; i < n; i++ )
+            {
+                sj.add( String.valueOf( Array.get( obj, i ) ) );
+            }
+            buf.append( sj.toString() );
+            str = buf.toString();
+        }
+        else
+        {
+            str = String.valueOf( obj );
+        }
+        return str;
+    }
 }
