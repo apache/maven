95ac87fd5b11bc137434cb4465007e7113f0ff3f
Maarten Mulders <maartenm@infosupport.com>
2020-05-23 21:37:41 +0200

    Immediately set recursive

--- maven-embedder/src/main/java/org/apache/maven/cli/MavenCli.java
+++ maven-embedder/src/main/java/org/apache/maven/cli/MavenCli.java
@@ -1372,7 +1372,6 @@ public class MavenCli
 
         List<String> goals = commandLine.getArgList();
 
-        boolean recursive = true;
 
         // this is the default behavior.
         String reactorFailureBehaviour = MavenExecutionRequest.REACTOR_FAIL_FAST;
@@ -1381,7 +1380,7 @@ public class MavenCli
 
         if ( commandLine.hasOption( CLIManager.NON_RECURSIVE ) )
         {
-            recursive = false;
+            request.setRecursive( false ); // default: true
         }
 
         if ( commandLine.hasOption( CLIManager.FAIL_FAST ) )
@@ -1494,7 +1493,6 @@ public class MavenCli
         request.setBaseDirectory( baseDirectory ).setGoals( goals ).setSystemProperties(
             cliRequest.systemProperties ).setUserProperties( cliRequest.userProperties ).setReactorFailureBehavior(
             reactorFailureBehaviour ) // default: fail fast
-            .setRecursive( recursive ) // default: true
             .setShowErrors( showErrors ) // default: false
             .addActiveProfiles( activeProfiles ) // optional
             .addInactiveProfiles( inactiveProfiles ) // optional
