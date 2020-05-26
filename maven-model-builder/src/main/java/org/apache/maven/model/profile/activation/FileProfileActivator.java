f5ceadc4f309f5384048c8f0cdab5183112e9835
Ravil Galeyev <dehasi@yandex.ru>
2020-05-26 18:17:36 +0200

    [MNG-6802] Switch to commons lang3 StringUtils

--- maven-model-builder/src/main/java/org/apache/maven/model/profile/activation/FileProfileActivator.java
+++ maven-model-builder/src/main/java/org/apache/maven/model/profile/activation/FileProfileActivator.java
@@ -25,6 +25,7 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.model.Activation;
 import org.apache.maven.model.ActivationFile;
 import org.apache.maven.model.Profile;
@@ -35,7 +36,6 @@ import org.apache.maven.model.building.ModelProblemCollectorRequest;
 import org.apache.maven.model.path.ProfileActivationFilePathInterpolator;
 import org.apache.maven.model.profile.ProfileActivationContext;
 import org.codehaus.plexus.interpolation.InterpolationException;
-import org.codehaus.plexus.util.StringUtils;
 
 /**
  * Determines profile activation based on the existence/absence of some file.
