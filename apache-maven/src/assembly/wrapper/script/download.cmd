6cf17fd8fa0dba2bc59ee2d1fe59071c9c99b6f9
rfscholte <rfscholte@apache.org>
2020-05-27 22:57:34 +0200

    [MNG-6919] drop wrapperUrl from maven-wrapper.properties Ensure maven-wrapper can handle spaced paths

--- apache-maven/src/assembly/wrapper/script/download.cmd
+++ apache-maven/src/assembly/wrapper/script/download.cmd
@@ -8,10 +8,6 @@ goto endWrapper
 :downloadWrapper
 set DOWNLOAD_URL="https://repo.maven.apache.org/maven2/org/apache/maven/maven-wrapper/${project.version}/maven-wrapper-${project.version}.jar"
 
-FOR /F "tokens=1,2 delims==" %%A IN ("%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.properties") DO (
-    IF "%%A"=="wrapperUrl" SET DOWNLOAD_URL=%%B
-)
-
 if not "%MVNW_REPOURL%" == "" (
     SET DOWNLOAD_URL="%MVNW_REPOURL%/org/apache/maven/maven-wrapper/${project.version}/maven-wrapper-${project.version}.jar"
 )
