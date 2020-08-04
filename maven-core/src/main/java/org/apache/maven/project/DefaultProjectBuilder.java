b6acc3ae58f3b15e7b526fb219f3f4664a15de21
Martin Kanters <martinkanters@apache.org>
2020-08-04 08:21:05 +0200

    [MNG-6118] Reverting changes in DefaultProjectBuilder.

--- maven-core/src/main/java/org/apache/maven/project/DefaultProjectBuilder.java
+++ maven-core/src/main/java/org/apache/maven/project/DefaultProjectBuilder.java
@@ -426,7 +426,7 @@ public class DefaultProjectBuilder
         {
             noErrors =
                 build( results, new ArrayList<>(), projectIndex, interimResults, request,
-                        new HashMap<>(), config.session, false ) && noErrors;
+                        new HashMap<>(), config.session ) && noErrors;
         }
         finally
         {
@@ -639,11 +639,10 @@ public class DefaultProjectBuilder
 
     }
 
-    @SuppressWarnings( "checkstyle:parameternumber" )
     private boolean build( List<ProjectBuildingResult> results, List<MavenProject> projects,
                            Map<String, MavenProject> projectIndex, List<InterimResult> interimResults,
                            ProjectBuildingRequest request, Map<File, Boolean> profilesXmls,
-                           RepositorySystemSession session, boolean isPhaseTwoBuildGuaranteed )
+                           RepositorySystemSession session )
     {
         boolean noErrors = true;
 
@@ -652,16 +651,7 @@ public class DefaultProjectBuilder
             MavenProject project = interimResult.listener.getProject();
             try
             {
-                ModelBuildingResult result = interimResult.result;
-
-                // We should only invoke the build on projects that the user wants to build.
-                // Other projects could be important for inter-module dependency resolution.
-                boolean shouldInvokeBuildPhaseTwo = isPhaseTwoBuildGuaranteed
-                        || request.getBuildPom().equals( project.getFile() );
-                if ( shouldInvokeBuildPhaseTwo )
-                {
-                    result = modelBuilder.build( interimResult.request, interimResult.result );
-                }
+                ModelBuildingResult result = modelBuilder.build( interimResult.request, interimResult.result );
 
                 // 2nd pass of initialization: resolve and build parent if necessary
                 try
@@ -676,8 +666,7 @@ public class DefaultProjectBuilder
 
                 List<MavenProject> modules = new ArrayList<>();
                 noErrors =
-                    build( results, modules, projectIndex, interimResult.modules, request, profilesXmls, session,
-                            shouldInvokeBuildPhaseTwo )
+                    build( results, modules, projectIndex, interimResult.modules, request, profilesXmls, session )
                     && noErrors;
 
                 projects.addAll( modules );
