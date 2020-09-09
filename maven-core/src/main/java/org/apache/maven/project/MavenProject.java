d459873343a5105f332ff3c2e5adb24dacb20dd5
Martin Kanters <martinkanters@apache.org>
2020-09-09 13:54:16 +0200

    [MNG-6118] Removed dead code.

--- maven-core/src/main/java/org/apache/maven/project/MavenProject.java
+++ maven-core/src/main/java/org/apache/maven/project/MavenProject.java
@@ -284,11 +284,6 @@ public class MavenProject
         getModel().setDependencies( dependencies );
     }
 
-    public void setModules( List<String> modules )
-    {
-        getModel().setModules( modules );
-    }
-
     public List<Dependency> getDependencies()
     {
         return getModel().getDependencies();
