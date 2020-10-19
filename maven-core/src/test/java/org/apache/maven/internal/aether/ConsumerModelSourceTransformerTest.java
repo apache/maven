6d6bdefa515d3b5a8ed2eb76f1ab304f36b2687b
rfscholte <rfscholte@apache.org>
2020-10-19 21:34:24 +0200

    Add unittest to verify transform result of ConsumerModelSourceTransformer

---
+++ maven-core/src/test/java/org/apache/maven/internal/aether/ConsumerModelSourceTransformerTest.java
@@ -0,0 +1,75 @@
+package org.apache.maven.internal.aether;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
+import org.apache.maven.model.Model;
+import org.apache.maven.model.building.TransformerContext;
+import org.junit.Test;
+import org.xmlunit.assertj.XmlAssert;
+
+public class ConsumerModelSourceTransformerTest
+{
+    private ConsumerModelSourceTransformer transformer = new ConsumerModelSourceTransformer();
+
+    @Test
+    public void test() throws Exception
+    {
+        Path pomFile = Paths.get( "src/test/resources/projects/transform.pom").toAbsolutePath();
+        InputStream in = transformer.transform( pomFile, new NoTransformerContext() );
+        
+        String text =
+            new BufferedReader( new InputStreamReader( in,
+                                                       StandardCharsets.UTF_8 ) ).lines().collect( Collectors.joining( "\n" ) );
+
+        XmlAssert.assertThat( in ).and( Files.newInputStream( pomFile ) ).areIdentical();
+    }
+
+    private static class NoTransformerContext implements TransformerContext
+    {
+        @Override
+        public String getUserProperty( String key )
+        {
+            return null;
+        }
+        
+        @Override
+        public Model getRawModel( String groupId, String artifactId )
+            throws IllegalStateException
+        {
+            return null;
+        }
+        
+        @Override
+        public Model getRawModel( Path p )
+        {
+            return null;
+        }
+    }
+}
