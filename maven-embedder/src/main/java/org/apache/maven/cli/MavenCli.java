3755caeb9bae5928d3ab09b855332ab1cb336691
Maarten Mulders <mthmulders@apache.org>
2021-02-12 17:10:55 +0100

    Remove unused import

--- maven-embedder/src/main/java/org/apache/maven/cli/MavenCli.java
+++ maven-embedder/src/main/java/org/apache/maven/cli/MavenCli.java
@@ -114,7 +114,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
 import java.util.Set;
-import java.util.StringTokenizer;
 import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
