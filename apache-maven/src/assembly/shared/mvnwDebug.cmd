9ba84aabe26c74db171d877b80130d5f15344008
Guillaume Dufour <guillaume.duff@gmail.com>
2021-04-25 21:10:31 +0200

    [MNG-7149] Introduce MAVEN_DEBUG_ADDRESS in mvnDebug scripts
    mvnDebug socket now binds by default on localhost:8000. set MAVEN_DEBUG_ADDRESS
    environment variable to choose another address (host:port).
    
    This closes #469

--- apache-maven/src/assembly/shared/mvnwDebug.cmd
+++ apache-maven/src/assembly/shared/mvnwDebug.cmd
@@ -20,14 +20,25 @@
 @REM
 @REM Environment Variable Prerequisites
 @REM
-@REM   JAVA_HOME          Must point at your Java Development Kit installation.
-@REM   MAVEN_BATCH_ECHO  (Optional) Set to 'on' to enable the echoing of the batch commands.
-@REM   MAVEN_BATCH_PAUSE (Optional) set to 'on' to wait for a key stroke before ending.
-@REM   MAVEN_OPTS        (Optional) Java runtime options used when Maven is executed.
-@REM   MAVEN_SKIP_RC     (Optional) Flag to disable loading of mavenrc files.
+@REM   JAVA_HOME           Must point at your Java Development Kit installation.
+@REM   MAVEN_BATCH_ECHO    (Optional) Set to 'on' to enable the echoing of the batch commands.
+@REM   MAVEN_BATCH_PAUSE   (Optional) set to 'on' to wait for a key stroke before ending.
+@REM   MAVEN_OPTS          (Optional) Java runtime options used when Maven is executed.
+@REM   MAVEN_SKIP_RC       (Optional) Flag to disable loading of mavenrc files.
+@REM   MAVEN_DEBUG_ADDRESS (Optional) Set the debug address. Default value is localhost:8000
 @REM -----------------------------------------------------------------------------
 
+@REM Begin all REM lines with '@' in case MAVEN_BATCH_ECHO is 'on'
+@echo off
+@REM set title of command window
+title %0
+@REM enable echoing by setting MAVEN_BATCH_ECHO to 'on'
+@if "%MAVEN_BATCH_ECHO%"=="on" echo %MAVEN_BATCH_ECHO%
+
 @setlocal
-@set MAVEN_DEBUG_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=0.0.0.0:8000
+
+IF "%MAVEN_DEBUG_ADDRESS%"=="" @set MAVEN_DEBUG_ADDRESS=localhost:8000
+
+@set MAVEN_DEBUG_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=%MAVEN_DEBUG_ADDRESS%
 
 @call "%~dp0"mvnw.cmd %*
