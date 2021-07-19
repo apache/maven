e9a54c6d0272b79c69ef60195b41c26d331ecef9
Michael Osipov <michaelo@apache.org>
2021-07-19 23:45:50 +0200

    Use proper term: directory

--- apache-maven/src/assembly/shared/init.cmd
+++ apache-maven/src/assembly/shared/init.cmd
@@ -4,7 +4,7 @@
 
 set MAVEN_CMD_LINE_ARGS=%*
 
-@REM Find the project basedir, i.e., the directory that contains the folder ".mvn".
+@REM Find the project basedir, i.e., the directory that contains the directory ".mvn".
 @REM Fallback to current working directory if not found.
 
 set "MAVEN_PROJECTBASEDIR=%MAVEN_BASEDIR%"
