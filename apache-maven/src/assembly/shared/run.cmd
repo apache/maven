6c67c9d0a6b7a717a2e5718a770638b9fcc0fbe3
Michael Osipov <michaelo@apache.org>
2021-07-25 14:55:39 +0200

    [MNG-7197] Simplify exit code handling in Windows command startup script
    This closes #512

--- apache-maven/src/assembly/shared/run.cmd
+++ apache-maven/src/assembly/shared/run.cmd
@@ -24,6 +24,4 @@ if exist "%USERPROFILE%\mavenrc_post.cmd" call "%USERPROFILE%\mavenrc_post.cmd"
 @REM pause the script if MAVEN_BATCH_PAUSE is set to 'on'
 if "%MAVEN_BATCH_PAUSE%"=="on" pause
 
-if "%MAVEN_TERMINATE_CMD%"=="on" exit %ERROR_CODE%
-
-cmd /C exit /B %ERROR_CODE%
+exit /b %ERROR_CODE%
