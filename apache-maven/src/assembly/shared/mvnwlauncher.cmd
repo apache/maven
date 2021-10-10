bf59026554f954296bbedaa21359afc0a9c3e4b2
Hervé Boutemy <hboutemy@apache.org>
2021-10-10 20:07:36 +0200

    [MNG-6914] rename MAVEN_LAUNCHER to LAUNCHER_CLASS (from LAUNCHER_JAR)

--- apache-maven/src/assembly/shared/mvnwlauncher.cmd
+++ apache-maven/src/assembly/shared/mvnwlauncher.cmd
@@ -1,3 +1,3 @@
 set LAUNCHER_JAR="%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar"
-set MAVEN_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain
+set LAUNCHER_CLASS=org.apache.maven.wrapper.MavenWrapperMain
 
