c62e3fe9c9a922b9afd050da65ab1daacf18337a
Guillaume Nodet <gnodet@gmail.com>
2021-11-17 10:26:47 +0100

    [MNG-7129] Fix lifecycles

--- maven-core/src/main/java/org/apache/maven/lifecycle/DefaultLifecycles.java
+++ maven-core/src/main/java/org/apache/maven/lifecycle/DefaultLifecycles.java
@@ -45,7 +45,7 @@ import org.slf4j.LoggerFactory;
 @Singleton
 public class DefaultLifecycles
 {
-    public static final String[] STANDARD_LIFECYCLES = { "default", "clean", "site", "wrapper" };
+    public static final String[] STANDARD_LIFECYCLES = { "clean", "default", "site", "wrapper" };
 
     private final Logger logger = LoggerFactory.getLogger( getClass() );
 
