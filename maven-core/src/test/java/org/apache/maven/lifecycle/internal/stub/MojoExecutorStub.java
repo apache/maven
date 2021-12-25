48339b9eebba5dc73bf7287464423f86202a12dd
Michael Osipov <michaelo@apache.org>
2021-12-25 12:28:47 +0100

    [MNG-5561] Plugin relocation loses configuration
    Previously, to locate plugin configuration in a project the plugin descriptor
    was read first and then the GA were extracted. This always worked because the
    GA from the model and the GA from plugin descriptor (plugin.xml) were identical.
    When a plugin is relocated the target artifact is read, thus its plugin
    descriptor as well. Naturally, the GA of new (relocated) does not correspond to
    the old (static) one in the model. Therefore, the configuration is not found.
    New approach is to use the original plugin GA to locate the configuration in
    the model regardless of the relocation.
    
    This closes #642

--- maven-core/src/test/java/org/apache/maven/lifecycle/internal/stub/MojoExecutorStub.java
+++ maven-core/src/test/java/org/apache/maven/lifecycle/internal/stub/MojoExecutorStub.java
@@ -25,6 +25,7 @@ import org.apache.maven.lifecycle.internal.PhaseRecorder;
 import org.apache.maven.lifecycle.internal.ProjectIndex;
 import org.apache.maven.plugin.BuildPluginManager;
 import org.apache.maven.plugin.MavenPluginManager;
+import org.apache.maven.model.Plugin;
 import org.apache.maven.plugin.MojoExecution;
 import org.apache.maven.plugin.descriptor.MojoDescriptor;
 import org.apache.maven.plugin.descriptor.PluginDescriptor;
@@ -67,12 +68,14 @@ public class MojoExecutorStub
     }
 
 
-    public static MojoDescriptor createMojoDescriptor( String mojoDescription )
+    public static MojoDescriptor createMojoDescriptor( Plugin plugin )
     {
         final PluginDescriptor descriptor = new PluginDescriptor();
-        descriptor.setArtifactId( mojoDescription );
+        descriptor.setGroupId( plugin.getGroupId() );
+        descriptor.setArtifactId( plugin.getArtifactId() );
+        descriptor.setPlugin( plugin );
+        descriptor.setVersion( plugin.getVersion() );
         final MojoDescriptor mojoDescriptor = new MojoDescriptor();
-        mojoDescriptor.setDescription( mojoDescription );
         mojoDescriptor.setPluginDescriptor( descriptor );
         return mojoDescriptor;
     }
