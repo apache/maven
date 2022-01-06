bcd3d4d5ffe5c72e0ffaa9844619bbc4e45a71b5
Hervé Boutemy <hboutemy@apache.org>
2022-01-06 08:17:26 +0100

    [MNG-7264] refactor packaging bindings providers to ease maintenance

---
+++ maven-core/src/main/java/org/apache/maven/lifecycle/providers/packaging/PomLifecycleMappingProvider.java
@@ -0,0 +1,45 @@
+package org.apache.maven.lifecycle.providers.packaging;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+/**
+ * {@code pom} packaging plugins bindings provider for {@code default} lifecycle.
+ */
+@Named( "pom" )
+@Singleton
+public final class PomLifecycleMappingProvider
+    extends AbstractLifecycleMappingProvider
+{
+    private static final String[] BINDINGS =
+    {
+        "install", "org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install",
+        "deploy",  "org.apache.maven.plugins:maven-deploy-plugin:3.0.0-M1:deploy"
+    };
+    
+    @Inject
+    public PomLifecycleMappingProvider()
+    {
+        super( BINDINGS );
+    }
+}
