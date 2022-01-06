aff754b43bf655daa9a6cb9e19b976c633786bb8
Guillaume Nodet <gnodet@gmail.com>
2022-01-06 10:05:31 +0100

    Add a specific exception

--- maven-embedder/src/main/java/org/apache/maven/cli/internal/BootstrapCoreExtensionManager.java
+++ maven-embedder/src/main/java/org/apache/maven/cli/internal/BootstrapCoreExtensionManager.java
@@ -128,19 +128,27 @@ public class BootstrapCoreExtensionManager
 
     private List<Artifact> resolveExtension( CoreExtension extension, RepositorySystemSession repoSession,
                                              List<RemoteRepository> repositories, DependencyFilter dependencyFilter )
-        throws PluginResolutionException
+        throws ExtensionResolutionException
     {
-        Plugin plugin = new Plugin();
-        plugin.setGroupId( extension.getGroupId() );
-        plugin.setArtifactId( extension.getArtifactId() );
-        plugin.setVersion( extension.getVersion() );
+        try
+        {
+            Plugin plugin = new Plugin();
+            plugin.setGroupId( extension.getGroupId() );
+            plugin.setArtifactId( extension.getArtifactId() );
+            plugin.setVersion( extension.getVersion() );
 
-        DependencyNode root =
-            pluginDependenciesResolver.resolveCoreExtension( plugin, dependencyFilter, repositories, repoSession );
-        PreorderNodeListGenerator nlg = new PreorderNodeListGenerator();
-        root.accept( nlg );
-        List<Artifact> artifacts = nlg.getArtifacts( false );
+            DependencyNode root = pluginDependenciesResolver
+                    .resolveCoreExtension( plugin, dependencyFilter, repositories, repoSession );
+            PreorderNodeListGenerator nlg = new PreorderNodeListGenerator();
+            root.accept( nlg );
+            List<Artifact> artifacts = nlg.getArtifacts( false );
 
-        return artifacts;
+            return artifacts;
+        }
+        catch ( PluginResolutionException e )
+        {
+            throw new ExtensionResolutionException( extension, e.getCause() );
+        }
     }
+
 }
