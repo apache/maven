89237dd74dfef4f2ece8549662e93454f9c7e19e
Guillaume Nodet <gnodet@gmail.com>
2022-05-09 09:53:33 +0200

    Revert "[MNG-7347] SessionScoped beans should be singletons for a given session (#653)" (#715)
    This reverts commit b4518b5fe416a552a59e5201b4569a9bc0af3153.

--- maven-core/src/main/java/org/apache/maven/lifecycle/internal/LifecycleModuleBuilder.java
+++ maven-core/src/main/java/org/apache/maven/lifecycle/internal/LifecycleModuleBuilder.java
@@ -90,12 +90,8 @@ public class LifecycleModuleBuilder
 
         // session may be different from rootSession seeded in DefaultMaven
         // explicitly seed the right session here to make sure it is used by Guice
-        final boolean scoped = session != rootSession;
-        if ( scoped )
-        {
-            sessionScope.enter();
-            sessionScope.seed( MavenSession.class, session );
-        }
+        sessionScope.enter( reactorContext.getSessionScopeMemento() );
+        sessionScope.seed( MavenSession.class, session );
         try
         {
 
@@ -149,10 +145,7 @@ public class LifecycleModuleBuilder
         }
         finally
         {
-            if ( scoped )
-            {
-                sessionScope.exit();
-            }
+            sessionScope.exit();
 
             session.setCurrentProject( null );
 
