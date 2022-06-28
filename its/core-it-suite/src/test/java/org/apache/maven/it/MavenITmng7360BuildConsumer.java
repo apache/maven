34fe4f415ab2a86c8909733bcd9181307b88c52d
Guillaume Nodet <gnodet@apache.org>
2022-06-28 08:42:57 +0200

    [MNG-7360] Add an IT for build consumer with tags in plugin configs (#167)

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng7360BuildConsumer.java
@@ -0,0 +1,51 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+
+import org.apache.maven.it.util.ResourceExtractor;
+
+/**
+ * This test suite tests whether the consumer pom feature can load a project with a {@code parent} tag 
+ * inside the plugin configuration.
+ * Related JIRA issue: <a href="https://issues.apache.org/jira/browse/MNG-7360">MNG-7360</a>.
+ *
+ * @author Guillaume Nodet
+ */
+public class MavenITmng7360BuildConsumer extends AbstractMavenIntegrationTestCase
+{
+
+    private static final String PROJECT_PATH = "/mng-7360-build-consumer";
+
+    public MavenITmng7360BuildConsumer()
+    {
+        super( "[4.0.0-alpha-1,)" );
+    }
+
+    public void testSelectModuleByCoordinate() throws Exception
+    {
+        final File projectDir = ResourceExtractor.simpleExtractResources( getClass(), PROJECT_PATH );
+        final Verifier verifier = newVerifier( projectDir.getAbsolutePath() );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+    }
+
+}
