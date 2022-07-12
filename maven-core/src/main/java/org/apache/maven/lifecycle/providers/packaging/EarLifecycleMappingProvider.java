f05397a88dc4178fff7ac7d307797cd54db3bef9
Tamas Cservenak <tamas@cservenak.net>
2022-07-12 15:13:55 +0200

    Pull out lifecycle plugin versions (#766)
    To lessen mistake (copy-paste vs forgotten update) but also just to
    make things less redundant. This also makes possible "experimenting"
    much more easier.
    
    This is merely a refactoring that does not takes or gives anything, just tidies up.

--- maven-core/src/main/java/org/apache/maven/lifecycle/providers/packaging/EarLifecycleMappingProvider.java
+++ maven-core/src/main/java/org/apache/maven/lifecycle/providers/packaging/EarLifecycleMappingProvider.java
@@ -32,13 +32,14 @@ public final class EarLifecycleMappingProvider
     extends AbstractLifecycleMappingProvider
 {
     // START SNIPPET: ear
+    @SuppressWarnings( "checkstyle:linelength" )
     private static final String[] BINDINGS =
     {
-        "generate-resources", "org.apache.maven.plugins:maven-ear-plugin:3.1.2:generate-application-xml",
-        "process-resources",  "org.apache.maven.plugins:maven-resources-plugin:3.2.0:resources",
-        "package",            "org.apache.maven.plugins:maven-ear-plugin:3.1.2:ear",
-        "install",            "org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install",
-        "deploy",             "org.apache.maven.plugins:maven-deploy-plugin:3.0.0-M1:deploy"
+        "generate-resources", "org.apache.maven.plugins:maven-ear-plugin:" + EAR_PLUGIN_VERSION + ":generate-application-xml",
+        "process-resources",  "org.apache.maven.plugins:maven-resources-plugin:" + RESOURCES_PLUGIN_VERSION + ":resources",
+        "package",            "org.apache.maven.plugins:maven-ear-plugin:" + EAR_PLUGIN_VERSION + ":ear",
+        "install",            "org.apache.maven.plugins:maven-install-plugin:" + INSTALL_PLUGIN_VERSION + ":install",
+        "deploy",             "org.apache.maven.plugins:maven-deploy-plugin:" + DEPLOY_PLUGIN_VERSION + ":deploy"
     };
     // END SNIPPET: ear
 
