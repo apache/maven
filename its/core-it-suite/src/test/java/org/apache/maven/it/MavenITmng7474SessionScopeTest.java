b9eae39894f88e945aefc6463df60f1274fc1548
Guillaume Nodet <gnodet@apache.org>
2022-07-25 15:21:08 +0200

    [MNG-7474] SessionScoped beans should be singletons for a given session (#177)
    * [MNG-7474] SessionScoped beans should be singletons for a given session
    * Upgrade Maven Resources Plugin to 3.2.0 in MNG-6720/MNG7335 to avoid
      ConcurrentModificationException
    * Clean up IT a bit
    
    This closes #177

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng7474SessionScopeTest.java
@@ -0,0 +1,49 @@
+package org.apache.maven.it;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+
+import org.apache.maven.it.util.ResourceExtractor;
+
+/**
+ * This is a test set for <a href="https://issues.apache.org/jira/browse/MNG-7474">MNG-7474</a>:
+ * check that Session scope beans are actually singletons for the session.
+ */
+public class MavenITmng7474SessionScopeTest
+        extends AbstractMavenIntegrationTestCase
+{
+    public MavenITmng7474SessionScopeTest()
+    {
+        super( "[3.9.0,)" );
+    }
+
+    public void testSessionScope()
+            throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(), "/mng-7474-session-scope" );
+
+        Verifier verifier = newVerifier( testDir.getAbsolutePath() );
+        verifier.executeGoal( "validate" );
+        verifier.verifyErrorFreeLog();
+        verifier.resetStreams();
+    }
+
+}
