93823185eb9bbe2f9bcd9f96d1d87f73974a17de
Luyao Xiong <you@example.com>
2022-08-01 18:19:39 +0800

    format testcase

--- maven-resolver-provider/src/test/java/org/apache/maven/repository/internal/ArtifactDescriptorReaderDelegateTest.java
+++ maven-resolver-provider/src/test/java/org/apache/maven/repository/internal/ArtifactDescriptorReaderDelegateTest.java
@@ -26,54 +26,59 @@ import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.resolution.ArtifactDescriptorRequest;
 import org.eclipse.aether.resolution.ArtifactDescriptorResult;
 
-public class ArtifactDescriptorReaderDelegateTest extends AbstractRepositoryTestCase{
+public class ArtifactDescriptorReaderDelegateTest extends AbstractRepositoryTestCase
+{
     public void testMNG7509()
-            throws Exception{
+            throws Exception
+    {
 
         ArtifactDescriptorReaderDelegate delegate1 = new ArtifactDescriptorReaderDelegate();
-        ArtifactDescriptorResult result1 = mockResult("org.apache.maven.its", "dep-mng-1","0.0.1-SNAPSHOT");
-        Model model1 = mockModelWithDepMgmt("org.apache.maven.its", "depMgmt-1", "0.0.1-SNAPSHOT");
-        delegate1.populateResult(session, result1, model1);
+        ArtifactDescriptorResult result1 = mockResult( "org.apache.maven.its", "dep-mng-1", "0.0.1-SNAPSHOT" );
+        Model model1 = mockModelWithDepMgmt( "org.apache.maven.its", "depMgmt-1", "0.0.1-SNAPSHOT" );
+        delegate1.populateResult( session, result1, model1 );
 
         ArtifactDescriptorReaderDelegate delegate2 = new ArtifactDescriptorReaderDelegate();
-        ArtifactDescriptorResult result2 = mockResult("org.apache.maven.its", "dep-mng-2","0.0.2-SNAPSHOT");
-        Model model2 = mockModelWithDepMgmt("org.apache.maven.its", "depMgmt-1", "0.0.1-SNAPSHOT");
-        delegate2.populateResult(session, result2, model2);
+        ArtifactDescriptorResult result2 = mockResult( "org.apache.maven.its", "dep-mng-2", "0.0.2-SNAPSHOT" );
+        Model model2 = mockModelWithDepMgmt( "org.apache.maven.its", "depMgmt-1", "0.0.1-SNAPSHOT" );
+        delegate2.populateResult( session, result2, model2 );
 
         ArtifactDescriptorReaderDelegate delegate3 = new ArtifactDescriptorReaderDelegate();
-        ArtifactDescriptorResult result3 = mockResult("org.apache.maven.its", "dep-mng-3","0.0.2-SNAPSHOT");
-        Model model3 = mockModelWithDepMgmt("org.apache.maven.its", "depMgmt-2", "0.0.2-SNAPSHOT");
-        delegate3.populateResult(session, result3, model3);
+        ArtifactDescriptorResult result3 = mockResult( "org.apache.maven.its", "dep-mng-3", "0.0.2-SNAPSHOT" );
+        Model model3 = mockModelWithDepMgmt( "org.apache.maven.its", "depMgmt-2", "0.0.2-SNAPSHOT" );
+        delegate3.populateResult( session, result3, model3 );
 
         // When dependencyManagement in different Model include the same dependency,
         // they should refer to the same dependency instance
-        assertNotSame(model1.getDependencyManagement().getDependencies(), model2.getDependencyManagement().getDependencies().get(0));
-        assertSame(result1.getManagedDependencies().get(0), result2.getManagedDependencies().get(0));
+        assertNotSame( model1.getDependencyManagement().getDependencies(),
+                model2.getDependencyManagement().getDependencies().get( 0 ) );
+        assertSame( result1.getManagedDependencies().get( 0 ), result2.getManagedDependencies().get( 0 ) );
 
-        assertNotSame(model2.getDependencyManagement().getDependencies(), model3.getDependencyManagement().getDependencies().get(0));
-        assertNotSame(result2.getManagedDependencies().get(0), result3.getManagedDependencies().get(0));
+        assertNotSame( model2.getDependencyManagement().getDependencies(),
+                model3.getDependencyManagement().getDependencies().get( 0 ) );
+        assertNotSame( result2.getManagedDependencies().get( 0 ), result3.getManagedDependencies().get( 0 ) );
 
     }
 
-    private Model mockModelWithDepMgmt(String groupId, String artifactId, String version) {
+    private Model mockModelWithDepMgmt( String groupId, String artifactId, String version )
+    {
         Model model = new Model();
-        model.setDependencyManagement(new DependencyManagement());
+        model.setDependencyManagement( new DependencyManagement() );
         Dependency dep = new Dependency();
-        dep.setGroupId(groupId);
-        dep.setArtifactId(artifactId);
-        dep.setVersion(version);
-        model.getDependencyManagement().addDependency(dep);
+        dep.setGroupId( groupId );
+        dep.setArtifactId( artifactId );
+        dep.setVersion( version );
+        model.getDependencyManagement().addDependency( dep );
 
         return model;
     }
 
-    private ArtifactDescriptorResult mockResult(String groupId, String artifactId, String version) throws Exception{
+    private ArtifactDescriptorResult mockResult( String groupId, String artifactId, String version ) throws Exception
+    {
         ArtifactDescriptorRequest request = new ArtifactDescriptorRequest();
         request.addRepository( newTestRepository() );
-        request.setArtifact( new DefaultArtifact( groupId, artifactId, "jar", version) );
+        request.setArtifact( new DefaultArtifact( groupId, artifactId, "jar", version ) );
         return new ArtifactDescriptorResult( request );
     }
-
 
 
 }
