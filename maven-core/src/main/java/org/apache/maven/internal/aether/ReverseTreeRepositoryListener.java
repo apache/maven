115f485918d5a0135f3396c0fdba5badd2c8bf1e
Tamas Cservenak <tamas@cservenak.net>
2022-12-04 13:29:51 +0100

    PR comments.

--- maven-core/src/main/java/org/apache/maven/internal/aether/ReverseTreeRepositoryListener.java
+++ maven-core/src/main/java/org/apache/maven/internal/aether/ReverseTreeRepositoryListener.java
@@ -30,6 +30,7 @@ import java.util.ListIterator;
 import java.util.Objects;
 import org.eclipse.aether.AbstractRepositoryListener;
 import org.eclipse.aether.RepositoryEvent;
+import org.eclipse.aether.RepositorySystemSession;
 import org.eclipse.aether.RequestTrace;
 import org.eclipse.aether.artifact.Artifact;
 import org.eclipse.aether.collection.CollectStepData;
@@ -47,22 +48,11 @@ class ReverseTreeRepositoryListener extends AbstractRepositoryListener {
     public void artifactResolved(RepositoryEvent event) {
         requireNonNull(event, "event cannot be null");
 
-        if (!event.getArtifact()
-                .getFile()
-                .getPath()
-                .startsWith(event.getSession().getLocalRepository().getBasedir().getPath())) {
-            return; // reactor artifact
-        }
-        RequestTrace trace = event.getTrace();
-        CollectStepData collectStepTrace = null;
-        while (trace != null) {
-            if (trace.getData() instanceof CollectStepData) {
-                collectStepTrace = (CollectStepData) trace.getData();
-                break;
-            }
-            trace = trace.getParent();
+        if (!isLocalRepositoryArtifact(event.getSession(), event.getArtifact())) {
+            return;
         }
 
+        CollectStepData collectStepTrace = lookupCollectStepData(event.getTrace());
         if (collectStepTrace == null) {
             return;
         }
@@ -101,12 +91,44 @@ class ReverseTreeRepositoryListener extends AbstractRepositoryListener {
     }
 
     /**
+     * Returns {@code true} if passed in artifact is originating from local repository. In other words, we want
+     * to process and store tracking information ONLY into local repository, not to any other place. This method
+     * filters out currently built artifacts, as events are fired for them as well, but their resolved artifact
+     * file would point to checked out source-tree, not the local repository.
+     * <p>
+     * Visible for testing.
+     */
+    boolean isLocalRepositoryArtifact(RepositorySystemSession session, Artifact artifact) {
+        return artifact.getFile()
+                .getPath()
+                .startsWith(session.getLocalRepository().getBasedir().getPath());
+    }
+
+    /**
+     * Unravels trace tree (going upwards from current node), looking for {@link CollectStepData} trace data.
+     * This method may return {@code null} if no collect step data found in passed trace data or it's parents.
+     * <p>
+     * Visible for testing.
+     */
+    CollectStepData lookupCollectStepData(RequestTrace trace) {
+        CollectStepData collectStepTrace = null;
+        while (trace != null) {
+            if (trace.getData() instanceof CollectStepData) {
+                collectStepTrace = (CollectStepData) trace.getData();
+                break;
+            }
+            trace = trace.getParent();
+        }
+        return collectStepTrace;
+    }
+
+    /**
      * The event "artifact resolved" if fired WHENEVER an artifact is resolved, BUT it happens also when an artifact
      * descriptor (model, the POM) is being built, and parent (and parent of parent...) is being asked for. Hence, this
      * method "filters" out in WHICH artifact are we interested in, but it intentionally neglects extension as
      * ArtifactDescriptorReader modifies extension to "pom" during collect. So all we have to rely on is GAV only.
      */
-    private boolean isInScope(Artifact artifact, Artifact nodeArtifact) {
+    boolean isInScope(Artifact artifact, Artifact nodeArtifact) {
         return Objects.equals(artifact.getGroupId(), nodeArtifact.getGroupId())
                 && Objects.equals(artifact.getArtifactId(), nodeArtifact.getArtifactId())
                 && Objects.equals(artifact.getVersion(), nodeArtifact.getVersion());
---
+++ maven-core/src/test/java/org/apache/maven/internal/aether/ReverseTreeRepositoryListenerTest.java
@@ -0,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.maven.internal.aether;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.RequestTrace;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.collection.CollectStepData;
+import org.eclipse.aether.repository.LocalRepository;
+import org.junit.Test;
+
+/**
+ * UT for {@link ReverseTreeRepositoryListener}.
+ */
+public class ReverseTreeRepositoryListenerTest {
+    private final ReverseTreeRepositoryListener subject = new ReverseTreeRepositoryListener();
+
+    @Test
+    public void isLocalRepositoryArtifactTest() {
+        File baseDir = new File("local/repository");
+        LocalRepository localRepository = new LocalRepository(baseDir);
+        RepositorySystemSession session = mock(RepositorySystemSession.class);
+        when(session.getLocalRepository()).thenReturn(localRepository);
+
+        Artifact localRepositoryArtifact = mock(Artifact.class);
+        when(localRepositoryArtifact.getFile()).thenReturn(new File(baseDir, "some/path/within"));
+
+        Artifact nonLocalReposioryArtifact = mock(Artifact.class);
+        when(nonLocalReposioryArtifact.getFile()).thenReturn(new File("something/completely/different"));
+
+        assertThat(subject.isLocalRepositoryArtifact(session, localRepositoryArtifact), equalTo(true));
+        assertThat(subject.isLocalRepositoryArtifact(session, nonLocalReposioryArtifact), equalTo(false));
+    }
+
+    @Test
+    public void lookupCollectStepDataTest() {
+        RequestTrace doesNotHaveIt =
+                RequestTrace.newChild(null, "foo").newChild("bar").newChild("baz");
+        assertThat(subject.lookupCollectStepData(doesNotHaveIt), nullValue());
+
+        final CollectStepData data = mock(CollectStepData.class);
+
+        RequestTrace haveItFirst = RequestTrace.newChild(null, data)
+                .newChild("foo")
+                .newChild("bar")
+                .newChild("baz");
+        assertThat(subject.lookupCollectStepData(haveItFirst), sameInstance(data));
+
+        RequestTrace haveItLast = RequestTrace.newChild(null, "foo")
+                .newChild("bar")
+                .newChild("baz")
+                .newChild(data);
+        assertThat(subject.lookupCollectStepData(haveItLast), sameInstance(data));
+
+        RequestTrace haveIt = RequestTrace.newChild(null, "foo")
+                .newChild("bar")
+                .newChild(data)
+                .newChild("baz");
+        assertThat(subject.lookupCollectStepData(haveIt), sameInstance(data));
+    }
+}
