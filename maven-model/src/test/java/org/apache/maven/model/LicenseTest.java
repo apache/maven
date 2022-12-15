9f1459e1b37db355a23ae453ab2e00f10dd99d60
Piotrek Żygieło <pzygielo@users.noreply.github.com>
2022-12-15 22:01:46 +0100

    Follow toString format in License

--- maven-model/src/test/java/org/apache/maven/model/LicenseTest.java
+++ maven-model/src/test/java/org/apache/maven/model/LicenseTest.java
@@ -61,6 +61,6 @@ public class LicenseTest
 
         String s = license.toString();
 
-        assert "License(name=Unlicense, url=http://lic.localdomain)".equals( s ) : s;
+        assert "License {name=Unlicense, url=http://lic.localdomain}".equals( s ) : s;
     }
 }
