538a6b40a557790b150b0855d9d047fa8a275dd4
Michael Osipov <michaelo@apache.org>
2022-12-23 09:16:13 +0100

    [MNG-7352] org.apache.maven.toolchain.java.JavaToolchainImpl should be public
    This closes #934

--- maven-core/src/main/java/org/apache/maven/toolchain/java/JavaToolchainImpl.java
+++ maven-core/src/main/java/org/apache/maven/toolchain/java/JavaToolchainImpl.java
@@ -31,7 +31,7 @@ import org.slf4j.Logger;
  * @author Milos Kleint
  * @since 2.0.9, renamed from DefaultJavaToolChain in 3.2.4
  */
-class JavaToolchainImpl extends DefaultToolchain implements JavaToolchain {
+public class JavaToolchainImpl extends DefaultToolchain implements JavaToolchain {
     private String javaHome;
 
     public static final String KEY_JAVAHOME = "jdkHome"; // NOI18N
