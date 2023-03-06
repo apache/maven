366e3f5f8661339cc562153aed5e07fe871ceaa6
Tamas Cservenak <tamas@cservenak.net>
2023-03-06 10:42:55 +0100

    [MNG-7715] Update to resolver 1.9.6 (#1025)
    Update and apply some adjustments to UTs as
    error message did change.
    
    ---
    
    https://issues.apache.org/jira/browse/MNG-7715

--- maven-resolver-provider/src/test/java/org/apache/maven/repository/internal/DefaultModelResolverTest.java
+++ maven-resolver-provider/src/test/java/org/apache/maven/repository/internal/DefaultModelResolverTest.java
@@ -56,7 +56,7 @@ public final class DefaultModelResolverTest extends AbstractRepositoryTestCase {
             fail("Expected 'UnresolvableModelException' not thrown.");
         } catch (final UnresolvableModelException e) {
             assertNotNull(e.getMessage());
-            assertTrue(e.getMessage().startsWith("Could not find artifact ut.simple:artifact:pom:0 in repo"));
+            assertTrue(e.getMessage().contains("Could not find artifact ut.simple:artifact:pom:0 in repo"));
         }
     }
 
@@ -119,7 +119,7 @@ public final class DefaultModelResolverTest extends AbstractRepositoryTestCase {
             fail("Expected 'UnresolvableModelException' not thrown.");
         } catch (final UnresolvableModelException e) {
             assertNotNull(e.getMessage());
-            assertTrue(e.getMessage().startsWith("Could not find artifact ut.simple:artifact:pom:0 in repo"));
+            assertTrue(e.getMessage().contains("Could not find artifact ut.simple:artifact:pom:0 in repo"));
         }
     }
 
