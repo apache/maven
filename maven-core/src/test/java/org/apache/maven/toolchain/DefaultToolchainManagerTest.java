2e1cc89031e7e76d266344ef0e3185beda47bc0c
Tamas Cservenak <tamas@cservenak.net>
2023-04-13 21:39:39 +0200

    [MNG-7670] Update misc dependencies
    And do align them. Max Level is Java 8, but
    we had several conflicts, like HttpClient
    (used by Wagon but also by Resolver-Transport-Http).
    
    ---
    
    https://issues.apache.org/jira/browse/MNG-7670

--- maven-core/src/test/java/org/apache/maven/toolchain/DefaultToolchainManagerTest.java
+++ maven-core/src/test/java/org/apache/maven/toolchain/DefaultToolchainManagerTest.java
@@ -37,7 +37,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.isA;
+import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
