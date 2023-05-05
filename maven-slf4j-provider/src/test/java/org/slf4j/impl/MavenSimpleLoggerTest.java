be160878f02628c13166b213212f226b22d7a750
Tamas Cservenak <tamas@cservenak.net>
2023-05-05 15:18:11 +0200

    Reformat

--- maven-slf4j-provider/src/test/java/org/slf4j/impl/MavenSimpleLoggerTest.java
+++ maven-slf4j-provider/src/test/java/org/slf4j/impl/MavenSimpleLoggerTest.java
@@ -1,13 +1,31 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.slf4j.impl;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.assertLinesMatch;
@@ -18,7 +36,8 @@ class MavenSimpleLoggerTest {
     void includesCauseAndSuppressedExceptionsWhenWritingThrowables(TestInfo testInfo) throws Exception {
         Exception causeOfSuppressed = new NoSuchElementException("cause of suppressed");
         Exception suppressed = new IllegalStateException("suppressed", causeOfSuppressed);
-        suppressed.addSuppressed(new IllegalArgumentException("suppressed suppressed", new ArrayIndexOutOfBoundsException("suppressed suppressed cause")));
+        suppressed.addSuppressed(new IllegalArgumentException(
+                "suppressed suppressed", new ArrayIndexOutOfBoundsException("suppressed suppressed cause")));
         Exception cause = new IllegalArgumentException("cause");
         cause.addSuppressed(suppressed);
         Exception throwable = new RuntimeException("top-level", cause);
@@ -32,7 +51,8 @@ class MavenSimpleLoggerTest {
 
         Class<?> testClass = testInfo.getTestClass().get();
         String testMethodName = testInfo.getTestMethod().get().getName();
-        String testClassStackTraceLinePattern = "at " + testClass.getName() + "." + testMethodName + " \\(" + testClass.getSimpleName() + ".java:\\d+\\)";
+        String testClassStackTraceLinePattern = "at " + testClass.getName() + "." + testMethodName + " \\("
+                + testClass.getSimpleName() + ".java:\\d+\\)";
         List<String> expectedLines = Arrays.asList(
                 "java.lang.RuntimeException: top-level",
                 "    " + testClassStackTraceLinePattern,
@@ -51,8 +71,7 @@ class MavenSimpleLoggerTest {
                 ">> stacktrace >>",
                 "    Caused by: java.util.NoSuchElementException: cause of suppressed",
                 "        " + testClassStackTraceLinePattern,
-                ">> stacktrace >>"
-        );
+                ">> stacktrace >>");
 
         assertLinesMatch(expectedLines, actualLines);
     }
