1d3d305b268116f114dced49578a10f5afb81ebd
Guillaume Nodet <gnodet@gmail.com>
2023-05-30 09:07:10 +0200

    Fix possible NPE

--- maven-core/src/main/java/org/apache/maven/toolchain/DefaultToolchain.java
+++ maven-core/src/main/java/org/apache/maven/toolchain/DefaultToolchain.java
@@ -83,18 +83,20 @@ implements Toolchain, ToolchainPrivate {
 
     @Override
     public boolean matchesRequirements(Map<String, String> requirements) {
-        for (Map.Entry<String, String> requirement : requirements.entrySet()) {
-            String key = requirement.getKey();
+        if (requirements != null) {
+            for (Map.Entry<String, String> requirement : requirements.entrySet()) {
+                String key = requirement.getKey();
 
-            RequirementMatcher matcher = provides.get(key);
+                RequirementMatcher matcher = provides.get(key);
 
-            if (matcher == null) {
-                getLog().debug("Toolchain " + this + " is missing required property: " + key);
-                return false;
-            }
-            if (!matcher.matches(requirement.getValue())) {
-                getLog().debug("Toolchain " + this + " doesn't match required property: " + key);
-                return false;
+                if (matcher == null) {
+                    getLog().debug("Toolchain " + this + " is missing required property: " + key);
+                    return false;
+                }
+                if (!matcher.matches(requirement.getValue())) {
+                    getLog().debug("Toolchain " + this + " doesn't match required property: " + key);
+                    return false;
+                }
             }
         }
         return true;
