4480f05273c6cd6669eac73e145c224d14eb47fc
Guillaume Nodet <gnodet@gmail.com>
2023-06-01 17:11:49 +0200

    [MNG-7585] Remove duplicate classes

--- src/mdo/java/WrapperList.java
+++ src/mdo/java/WrapperList.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.maven.model;
+package ${package};
 
 import java.util.AbstractList;
 import java.util.ArrayList;
