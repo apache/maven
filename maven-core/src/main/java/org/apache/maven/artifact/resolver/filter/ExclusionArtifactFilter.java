3ef12a8fb43e85814f23bd524fea0c7e2d950bb7
Guillaume Nodet <gnodet@gmail.com>
2023-07-15 17:44:51 +0200

    Use JDK glob path matcher

--- maven-core/src/main/java/org/apache/maven/artifact/resolver/filter/ExclusionArtifactFilter.java
+++ maven-core/src/main/java/org/apache/maven/artifact/resolver/filter/ExclusionArtifactFilter.java
@@ -18,9 +18,12 @@
  */
 package org.apache.maven.artifact.resolver.filter;
 
+import java.lang.reflect.Proxy;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import org.apache.maven.artifact.Artifact;
@@ -46,104 +49,26 @@ public class ExclusionArtifactFilter implements ArtifactFilter {
     }
 
     private static Predicate<Artifact> toPredicate(Exclusion exclusion) {
-        Pattern groupId = Pattern.compile(convertGlobToRegex(exclusion.getGroupId()));
-        Pattern artifactId = Pattern.compile(convertGlobToRegex(exclusion.getArtifactId()));
-        Predicate<Artifact> predGroupId = a -> groupId.matcher(a.getGroupId()).matches();
-        Predicate<Artifact> predArtifactId =
-                a -> artifactId.matcher(a.getArtifactId()).matches();
+        PathMatcher groupId = FileSystems.getDefault().getPathMatcher("glob:" + exclusion.getGroupId());
+        PathMatcher artifactId = FileSystems.getDefault().getPathMatcher("glob:" + exclusion.getArtifactId());
+        Predicate<Artifact> predGroupId = a -> groupId.matches(createPathProxy(a.getGroupId()));
+        Predicate<Artifact> predArtifactId = a -> artifactId.matches(createPathProxy(a.getArtifactId()));
         return predGroupId.and(predArtifactId);
     }
 
     /**
-     * Converts a standard POSIX Shell globbing pattern into a regular expression
-     * pattern. The result can be used with the standard {@link java.util.regex} API to
-     * recognize strings which match the glob pattern.
-     * <p/>
-     * See also, the POSIX Shell language:
-     * http://pubs.opengroup.org/onlinepubs/009695399/utilities/xcu_chap02.html#tag_02_13_01
-     *
-     * @param pattern A glob pattern.
-     * @return A regex pattern to recognize the given glob pattern.
+     * In order to reuse the glob matcher from the filesystem, we need
+     * to create Path instances.  Those are only used with the toString method.
+     * This hack works because the only system-dependent thing is the path
+     * separator which should not be part of the groupId or artifactId.
      */
-    private static String convertGlobToRegex(String pattern) {
-        StringBuilder sb = new StringBuilder(pattern.length());
-        int inGroup = 0;
-        int inClass = 0;
-        int firstIndexInClass = -1;
-        char[] arr = pattern.toCharArray();
-        for (int i = 0; i < arr.length; i++) {
-            char ch = arr[i];
-            switch (ch) {
-                case '\\':
-                    if (++i >= arr.length) {
-                        sb.append('\\');
-                    } else {
-                        char next = arr[i];
-                        switch (next) {
-                            case ',':
-                                // escape not needed
-                                break;
-                            case 'Q':
-                            case 'E':
-                                // extra escape needed
-                                sb.append('\\');
-                                sb.append('\\');
-                                break;
-                            default:
-                                sb.append('\\');
-                                break;
-                        }
-                        sb.append(next);
+    private static Path createPathProxy(String value) {
+        return (Path) Proxy.newProxyInstance(
+                ExclusionArtifactFilter.class.getClassLoader(), new Class[] {Path.class}, (proxy1, method, args) -> {
+                    if ("toString".equals(method.getName())) {
+                        return value;
                     }
-                    break;
-                case '*':
-                    sb.append(inClass == 0 ? ".*" : "*");
-                    break;
-                case '?':
-                    sb.append(inClass == 0 ? "." : "?");
-                    break;
-                case '[':
-                    inClass++;
-                    firstIndexInClass = i + 1;
-                    sb.append('[');
-                    break;
-                case ']':
-                    inClass--;
-                    sb.append(']');
-                    break;
-                case '.':
-                case '(':
-                case ')':
-                case '+':
-                case '|':
-                case '^':
-                case '$':
-                case '@':
-                case '%':
-                    if (inClass == 0 || (firstIndexInClass == i && ch == '^')) {
-                        sb.append('\\');
-                    }
-                    sb.append(ch);
-                    break;
-                case '!':
-                    sb.append(firstIndexInClass == i ? '^' : '!');
-                    break;
-                case '{':
-                    inGroup++;
-                    sb.append('(');
-                    break;
-                case '}':
-                    inGroup--;
-                    sb.append(')');
-                    break;
-                case ',':
-                    sb.append(inGroup > 0 ? '|' : ',');
-                    break;
-                default:
-                    sb.append(ch);
-                    break;
-            }
-        }
-        return sb.toString();
+                    throw new UnsupportedOperationException();
+                });
     }
 }
