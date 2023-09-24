d60a1cce4a0d60f309519b7e8b6f57d678d72b09
Slawomir Jaranowski <s.jaranowski@gmail.com>
2023-09-24 21:41:59 +0200

    [MNG-7895] Support ${project.basedir} in file profile activation - fix ITs

--- its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5608ProfileActivationWarningTest.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng5608ProfileActivationWarningTest.java
@@ -34,7 +34,9 @@ import org.junit.jupiter.api.Test;
 public class MavenITmng5608ProfileActivationWarningTest extends AbstractMavenIntegrationTestCase {
 
     public MavenITmng5608ProfileActivationWarningTest() {
-        super("(3.2.1,)");
+        // changed in https://issues.apache.org/jira/browse/MNG-7895
+        // TODO - consider a separate test
+        super("(3.2.1,4.0.0-alpha-8)");
     }
 
     @Test
