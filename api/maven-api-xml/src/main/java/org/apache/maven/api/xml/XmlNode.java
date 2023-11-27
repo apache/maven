05fcf5b2d3b529385563619d0693eb4a2b7847d0
Guillaume Nodet <gnodet@gmail.com>
2023-11-28 00:11:34 +0100

    [MNG-6036] Add namespace to XmlNode (#1318)

--- api/maven-api-xml/src/main/java/org/apache/maven/api/xml/XmlNode.java
+++ api/maven-api-xml/src/main/java/org/apache/maven/api/xml/XmlNode.java
@@ -30,8 +30,6 @@ import org.apache.maven.api.annotations.ThreadSafe;
 /**
  * An immutable xml node.
  *
- * TODO: v4: add support for namespaces
- *
  * @since 4.0.0
  */
 @Experimental
@@ -82,6 +80,12 @@ public interface XmlNode {
     @Nonnull
     String getName();
 
+    @Nonnull
+    String getNamespaceUri();
+
+    @Nonnull
+    String getPrefix();
+
     @Nullable
     String getValue();
 
@@ -105,8 +109,6 @@ public interface XmlNode {
     }
 
     XmlNode merge(@Nullable XmlNode source, @Nullable Boolean childMergeOverride);
-
-    XmlNode clone();
 
     /**
      * Merge recessive into dominant and return either {@code dominant}
