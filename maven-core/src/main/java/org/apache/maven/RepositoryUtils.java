0fca451db13731cb17ce461b111460b5212e6c9f
Tamas Cservenak <tamas@cservenak.net>
2023-12-06 17:49:08 +0100

    Unroll imports

--- maven-core/src/main/java/org/apache/maven/RepositoryUtils.java
+++ maven-core/src/main/java/org/apache/maven/RepositoryUtils.java
@@ -19,7 +19,14 @@
 package org.apache.maven;
 
 import java.io.File;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.apache.maven.artifact.handler.ArtifactHandler;
