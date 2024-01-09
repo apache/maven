076b346c0c149a4ebe27a9149581293560a4277f
Tamas Cservenak <tamas@cservenak.net>
2024-01-09 21:57:17 +0100

    [MNG-7998] Minor cleanup regarding "tasks" (#1366)
    Just a minor cleanup regarding internals of "tasks".
    
    ---
    
    https://issues.apache.org/jira/browse/MNG-7998

--- maven-core/src/main/java/org/apache/maven/lifecycle/internal/Task.java
+++ maven-core/src/main/java/org/apache/maven/lifecycle/internal/Task.java
@@ -18,15 +18,46 @@
  */
 package org.apache.maven.lifecycle.internal;
 
+import java.util.Objects;
+
 /**
- * A task that is a lifecycle.
+ * A Maven task, at this level is merely just an opaque string.
  * <p>
  * <strong>NOTE:</strong> This class is not part of any public api and can be changed or deleted without prior notice.
  *
- * @since 3.0
+ * @since 4.0.0
  */
-public final class LifecycleTask extends Task {
-    public LifecycleTask(String lifecyclePhase) {
-        super(lifecyclePhase);
+public abstract class Task {
+
+    private final String value;
+
+    public Task(String value) {
+        this.value = Objects.requireNonNull(value, "value");
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Task)) {
+            return false;
+        }
+        Task task = (Task) o;
+        return Objects.equals(getClass(), task.getClass()) && Objects.equals(value, task.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getClass(), value);
+    }
+
+    @Override
+    public String toString() {
+        return value;
     }
 }
