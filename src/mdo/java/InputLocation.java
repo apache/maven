c88afd470f985526282ca34c6c1682b55c721d5e
Guillaume Nodet <gnodet@gmail.com>
2024-02-07 22:28:01 +0100

    Clean mdo schemas (#1365)
    * fix namespace location
    * remove unneeded CDATA
    * code formatting
    * use {@code xxx}
    * generate location tracking classes
    * add location tracking to toolchain model
    * Fix javadoc

--- src/mdo/java/InputLocation.java
+++ src/mdo/java/InputLocation.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.maven.api.settings;
+package ${package};
 
 import java.io.Serializable;
 import java.util.Collection;
