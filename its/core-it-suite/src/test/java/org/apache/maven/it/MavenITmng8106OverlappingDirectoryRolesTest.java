aa92202f3cd3242f2aabf200a44a338c65b75c6b
Tamas Cservenak <tamas@cservenak.net>
2024-04-25 12:00:51 +0200

    [MNG-8106] IT (#336)
    Created IT that does "wrong" (as same directory has once G and once A role), and verify that both level metadata is present. In other words, but none is dropped.
    
    ---
    
    https://issues.apache.org/jira/browse/MNG-8106

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITmng8106OverlappingDirectoryRolesTest.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.maven.it;
+
+import java.io.File;
+import java.io.FileReader;
+
+import org.apache.maven.shared.verifier.Verifier;
+import org.apache.maven.shared.verifier.util.ResourceExtractor;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.codehaus.plexus.util.xml.Xpp3DomBuilder;
+import org.junit.jupiter.api.Test;
+
+public class MavenITmng8106OverlappingDirectoryRolesTest extends AbstractMavenIntegrationTestCase {
+    public MavenITmng8106OverlappingDirectoryRolesTest() {
+        // Broken in: 3.9.0..3.9.6 && 4.0.0-alpha-1..4.0.0-alpha-13
+        super("[,3.9.0),(3.9.6,3.999.999],[4.0.0-beta-1,)");
+    }
+
+    @Test
+    public void testDirectoryOverlap() throws Exception {
+        File testDir = ResourceExtractor.simpleExtractResources(getClass(), "/mng-8106");
+
+        Verifier verifier = newVerifier(new File(testDir, "plugin").getAbsolutePath());
+        verifier.addCliArgument("install");
+        verifier.execute();
+        verifier.verifyErrorFreeLog();
+
+        verifier = newVerifier(new File(testDir, "jar").getAbsolutePath());
+        verifier.addCliArgument("install");
+        verifier.execute();
+        verifier.verifyErrorFreeLog();
+
+        File metadataFile = new File(new File(verifier.getLocalRepository()), "mng-8106/it/maven-metadata-local.xml");
+        Xpp3Dom dom;
+        try (FileReader reader = new FileReader(metadataFile)) {
+            dom = Xpp3DomBuilder.build(reader);
+        }
+        assertTrue("metadata missing A level data", dom.getChild("versioning") != null);
+        assertTrue("metadata missing G level data", dom.getChild("plugins") != null);
+    }
+}
--- its/core-it-suite/src/test/java/org/apache/maven/it/TestSuiteOrdering.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/TestSuiteOrdering.java
@@ -120,6 +120,7 @@ public class TestSuiteOrdering implements ClassOrderer {
          * the tests are to finishing. Newer tests are also more likely to fail, so this is
          * a fail fast technique as well.
          */
+        suite.addTestSuite(MavenITmng8106OverlappingDirectoryRolesTest.class);
         suite.addTestSuite(MavenITmng8005IdeWorkspaceReaderUsedTest.class);
         suite.addTestSuite(MavenITmng7967ArtifactHandlerLanguageTest.class);
         suite.addTestSuite(MavenITmng7965PomDuplicateTagsTest.class);
