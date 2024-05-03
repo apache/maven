e6b7fe37f3e95e9bebd9b3f5b473b1f8e7d3a1b4
Guillaume Nodet <gnodet@gmail.com>
2024-05-03 09:06:17 +0200

    Tone down to debug

--- maven-core/src/main/java/org/apache/maven/internal/transformation/impl/DefaultConsumerPomBuilder.java
+++ maven-core/src/main/java/org/apache/maven/internal/transformation/impl/DefaultConsumerPomBuilder.java
@@ -216,8 +216,8 @@ class DefaultConsumerPomBuilder implements ConsumerPomBuilder {
                     .sorted()
                     .distinct()
                     .toList();
-            logger.info("ModelCache contains " + paths.size());
-            paths.forEach(s -> logger.info("    " + s));
+            logger.debug("ModelCache contains " + paths.size());
+            paths.forEach(s -> logger.debug("    " + s));
         }
         return modelBuilder.build(request.build());
     }
