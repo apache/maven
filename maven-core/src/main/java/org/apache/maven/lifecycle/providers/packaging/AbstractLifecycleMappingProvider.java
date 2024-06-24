codewithteja/html

Upgrade lifecycle plugins to 4.x

maven-core/src/main/java/org/apache/maven/lifecycle/providers/packaging/AbstractLifecycleMappingProvider.java
@@ -35,15 +35,15 @@ import static java.util.Objects.requireNonNull;
  */
 public abstract class AbstractLifecycleMappingProvider implements Provider<LifecycleMapping> {
     // START SNIPPET: versions
-    protected static final String RESOURCES_PLUGIN_VERSION = "3.3.0";
+    protected static final String RESOURCES_PLUGIN_VERSION = "4.0.0-SNAPSHOT";
 
     protected static final String COMPILER_PLUGIN_VERSION = "3.10.1";
 
     protected static final String SUREFIRE_PLUGIN_VERSION = "3.0.0-M8";
 
-    protected static final String INSTALL_PLUGIN_VERSION = "3.1.0";
+    protected static final String INSTALL_PLUGIN_VERSION = "4.0.0-SNAPSHOT";
 
-    protected static final String DEPLOY_PLUGIN_VERSION = "3.1.0";
+    protected static final String DEPLOY_PLUGIN_VERSION = "4.0.0-SNAPSHOT";
 
     // packaging
 

apache/maven

maven-core/src/main/java/org/apache/maven/lifecycle/providers/packaging/AbstractLifecycleMappingProvider.java
@@ -35,25 +35,25 @@ import static java.util.Objects.requireNonNull;
  */
 public abstract class AbstractLifecycleMappingProvider implements Provider<LifecycleMapping> {
     // START SNIPPET: versions
-    protected static final String RESOURCES_PLUGIN_VERSION = "3.3.1";
+    protected static final String RESOURCES_PLUGIN_VERSION = "4.0.0-SNAPSHOT";
 
-    protected static final String COMPILER_PLUGIN_VERSION = "3.13.0";
+    protected static final String COMPILER_PLUGIN_VERSION = "4.0.0-SNAPSHOT";
 
     protected static final String SUREFIRE_PLUGIN_VERSION = "3.2.5";
 
-    protected static final String INSTALL_PLUGIN_VERSION = "3.1.1";
+    protected static final String INSTALL_PLUGIN_VERSION = "4.0.0-SNAPSHOT";
 
     protected static final String DEPLOY_PLUGIN_VERSION = "3.1.1";
 
     // packaging
 
-    protected static final String JAR_PLUGIN_VERSION = "3.4.1";
+    protected static final String JAR_PLUGIN_VERSION = "4.0.0-SNAPSHOT";
 
     protected static final String EAR_PLUGIN_VERSION = "3.3.0";
 
     protected static final String EJB_PLUGIN_VERSION = "3.2.1";
 
-    protected static final String PLUGIN_PLUGIN_VERSION = "3.12.0";
+    protected static final String PLUGIN_PLUGIN_VERSION = "4.0.0-SNAPSHOT";
 
     protected static final String RAR_PLUGIN_VERSION = "3.0.0";
 
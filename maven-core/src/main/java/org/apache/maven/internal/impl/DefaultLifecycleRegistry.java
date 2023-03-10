9bb0663bba64dce6e308f4ffd204a1870534b4e8
Guillaume Nodet <gnodet@gmail.com>
2023-03-10 08:54:03 +0100

    Upgrade lifecycle plugins to 4.x

--- maven-core/src/main/java/org/apache/maven/internal/impl/DefaultLifecycleRegistry.java
+++ maven-core/src/main/java/org/apache/maven/internal/impl/DefaultLifecycleRegistry.java
@@ -219,7 +219,7 @@ public class DefaultLifecycleRegistry implements LifecycleRegistry {
 
     static class CleanLifecycle implements Lifecycle {
 
-        private static final String MAVEN_CLEAN_PLUGIN_VERSION = "3.2.0";
+        private static final String MAVEN_CLEAN_PLUGIN_VERSION = "4.0.0-SNAPSHOT";
 
         @Override
         public String id() {
