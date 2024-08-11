da7c2110e495ae32a8e7b431a410ccc188bf9609
Tamas Cservenak <tamas@cservenak.net>
2024-08-11 18:56:11 +0200

    [MNG-8180] Handle NPE due non-existent tags (#1639)
    There was an NPE possibility when plugin.xml had no expected tags present.
    
    Also: maven-compat has plugin.xml (!) w/o "name" tag, it NPEd and failed build. This was NOT picked up by CI as "rebuild itself" step does not install (just verify).
    
    ---
    
    https://issues.apache.org/jira/browse/MNG-8180

--- maven-resolver-provider/src/main/java/org/apache/maven/repository/internal/PluginsMetadataGenerator.java
+++ maven-resolver-provider/src/main/java/org/apache/maven/repository/internal/PluginsMetadataGenerator.java
@@ -130,13 +130,14 @@ class PluginsMetadataGenerator implements MetadataGenerator {
                             // - maven-plugin-api (for model)
                             // - Plexus Container (for model supporting classes and exceptions)
                             XmlNode root = XmlNodeBuilder.build(is, null);
-                            String groupId = root.getChild("groupId").getValue();
-                            String artifactId = root.getChild("artifactId").getValue();
-                            String goalPrefix = root.getChild("goalPrefix").getValue();
-                            String name = root.getChild("name").getValue();
+                            String groupId = mayGetChild(root, "groupId");
+                            String artifactId = mayGetChild(root, "artifactId");
+                            String goalPrefix = mayGetChild(root, "goalPrefix");
+                            String name = mayGetChild(root, "name");
                             // sanity check: plugin descriptor extracted from artifact must have same GA
                             if (Objects.equals(artifact.getGroupId(), groupId)
                                     && Objects.equals(artifact.getArtifactId(), artifactId)) {
+                                // here groupId and artifactId cannot be null
                                 return new PluginInfo(groupId, artifactId, goalPrefix, name);
                             } else {
                                 throw new InvalidArtifactPluginMetadataException(
@@ -149,12 +150,21 @@ class PluginsMetadataGenerator implements MetadataGenerator {
                             }
                         }
                     }
-                } catch (RuntimeException e) {
-                    throw e;
                 } catch (Exception e) {
+                    if (e instanceof InvalidArtifactPluginMetadataException iapme) {
+                        throw iapme;
+                    }
                     // here we can have: IO. ZIP or Plexus Conf Ex: but we should not interfere with user intent
                 }
             }
+        }
+        return null;
+    }
+
+    private static String mayGetChild(XmlNode node, String child) {
+        XmlNode c = node.getChild(child);
+        if (c != null) {
+            return c.getValue();
         }
         return null;
     }
