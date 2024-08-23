26a346a5385143ed67170777f1060b2474736b37
Tamas Cservenak <tamas@cservenak.net>
2024-08-23 15:40:34 +0200

    Roll on changes

---
+++ maven-api-impl/src/main/java/org/apache/maven/internal/impl/DefaultProtoSession.java
@@ -0,0 +1,158 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.maven.internal.impl;
+
+import java.nio.file.Path;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.maven.api.ProtoSession;
+import org.apache.maven.api.Version;
+
+import static java.util.Objects.requireNonNull;
+
+public class DefaultProtoSession implements ProtoSession {
+    private final Version mavenVersion;
+    private final Instant startTime;
+    private final Map<String, String> systemProperties;
+    private final Map<String, String> userProperties;
+    private final Path topDirectory;
+    private final Path rootDirectory;
+
+    private DefaultProtoSession(
+            Version mavenVersion,
+            Instant startTime,
+            Map<String, String> systemProperties,
+            Map<String, String> userProperties,
+            Path topDirectory,
+            Path rootDirectory) {
+        this.mavenVersion = requireNonNull(mavenVersion);
+        this.startTime = requireNonNull(startTime);
+        this.systemProperties = Map.copyOf(requireNonNull(systemProperties));
+        this.userProperties = Map.copyOf(requireNonNull(userProperties));
+        this.topDirectory = requireNonNull(topDirectory);
+        this.rootDirectory = requireNonNull(rootDirectory);
+    }
+
+    @Override
+    public Version getMavenVersion() {
+        return mavenVersion;
+    }
+
+    @Override
+    public Instant getStartTime() {
+        return startTime;
+    }
+
+    @Override
+    public Map<String, String> getUserProperties() {
+        return userProperties;
+    }
+
+    @Override
+    public Map<String, String> getSystemProperties() {
+        return systemProperties;
+    }
+
+    @Override
+    public Path getTopDirectory() {
+        return topDirectory;
+    }
+
+    @Override
+    public Path getRootDirectory() {
+        return rootDirectory;
+    }
+
+    public Builder toBuilder() {
+        return new Builder(mavenVersion, startTime, systemProperties, userProperties, topDirectory, rootDirectory);
+    }
+
+    public static class Builder {
+        private Version mavenVersion;
+        private Instant startTime;
+        private Map<String, String> systemProperties;
+        private Map<String, String> userProperties;
+        private Path topDirectory;
+        private Path rootDirectory;
+
+        public Builder() {
+            this(null, Instant.now(), new HashMap<>(), new HashMap<>(), null, null);
+        }
+
+        public Builder(
+                Version mavenVersion,
+                Instant startTime,
+                Map<String, String> systemProperties,
+                Map<String, String> userProperties,
+                Path topDirectory,
+                Path rootDirectory) {
+            this.mavenVersion = mavenVersion;
+            this.startTime = startTime;
+            this.systemProperties = systemProperties;
+            this.userProperties = userProperties;
+            this.topDirectory = topDirectory;
+            this.rootDirectory = rootDirectory;
+        }
+
+        public Builder withMavenVersion(Version mavenVersion) {
+            this.mavenVersion = mavenVersion;
+            return this;
+        }
+
+        public Builder withStartTime(Instant startTime) {
+            this.startTime = startTime;
+            return this;
+        }
+
+        public Builder withSystemProperties(Map<String, String> systemProperties) {
+            this.systemProperties = systemProperties;
+            return this;
+        }
+
+        public Builder withUserProperties(Map<String, String> userProperties) {
+            this.userProperties = userProperties;
+            return this;
+        }
+
+        public Builder putAllUserProperties(Map<String, String> userProperties) {
+            requireNonNull(userProperties);
+            this.userProperties = new HashMap<>(this.userProperties);
+            this.userProperties.putAll(userProperties);
+            this.userProperties = userProperties;
+            return this;
+        }
+
+        public Builder withTopDirectory(Path topDirectory) {
+            this.topDirectory = topDirectory;
+            return this;
+        }
+
+        public Builder withRootDirectory(Path rootDirectory) {
+            this.rootDirectory = rootDirectory;
+            return this;
+        }
+
+        public DefaultProtoSession build() {
+            return new DefaultProtoSession(
+                    mavenVersion, startTime, systemProperties, userProperties, topDirectory, rootDirectory);
+        }
+    }
+}
--- maven-core/src/main/java/org/apache/maven/internal/aether/PropertyContributorExtender.java
+++ maven-core/src/main/java/org/apache/maven/internal/aether/PropertyContributorExtender.java
@@ -22,18 +22,14 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 
-import java.nio.file.Path;
-import java.time.Instant;
 import java.util.Map;
 import java.util.Properties;
 
-import org.apache.maven.api.ProtoSession;
-import org.apache.maven.api.Version;
 import org.apache.maven.api.services.Lookup;
-import org.apache.maven.api.services.VersionParser;
 import org.apache.maven.api.spi.PropertyContributor;
 import org.apache.maven.execution.MavenExecutionRequest;
-import org.apache.maven.rtinfo.RuntimeInformation;
+import org.apache.maven.internal.impl.DefaultProtoSession;
+import org.apache.maven.internal.impl.DefaultProtoSessionFactory;
 
 /**
  * Extender that manages {@link PropertyContributor}.
@@ -44,68 +40,35 @@ import org.apache.maven.rtinfo.RuntimeInformation;
 @Singleton
 class PropertyContributorExtender implements MavenExecutionRequestExtender {
     private final Lookup lookup;
+    private final DefaultProtoSessionFactory protoSessionFactory;
 
     @Inject
-    PropertyContributorExtender(Lookup lookup) {
+    PropertyContributorExtender(Lookup lookup, DefaultProtoSessionFactory protoSessionFactory) {
         this.lookup = lookup;
+        this.protoSessionFactory = protoSessionFactory;
     }
 
-    @SuppressWarnings({"rawtypes", "unchecked"})
     @Override
     public void extend(MavenExecutionRequest mavenExecutionRequest) {
         Map<String, PropertyContributor> effectivePropertyContributors = lookup.lookupMap(PropertyContributor.class);
         if (!effectivePropertyContributors.isEmpty()) {
-            final Version mavenVersion = lookup.lookup(VersionParser.class)
-                    .parseVersion(lookup.lookup(RuntimeInformation.class).getMavenVersion());
-            final Map<String, String> systemPropertiesMap =
-                    Map.copyOf((Map) mavenExecutionRequest.getSystemProperties());
-            final Map<String, String> userPropertiesMap = Map.copyOf((Map) mavenExecutionRequest.getUserProperties());
-            final ProtoSession protoSession = new ProtoSession() {
-                @Override
-                public Version getMavenVersion() {
-                    return mavenVersion;
-                }
+            DefaultProtoSession protoSession = protoSessionFactory.from(mavenExecutionRequest);
 
-                @Override
-                public Instant getStartTime() {
-                    return mavenExecutionRequest.getStartTime().toInstant();
-                }
-
-                @Override
-                public Map<String, String> getUserProperties() {
-                    return userPropertiesMap;
-                }
-
-                @Override
-                public Map<String, String> getSystemProperties() {
-                    return systemPropertiesMap;
-                }
-
-                @Override
-                public Path getTopDirectory() {
-                    return mavenExecutionRequest.getTopDirectory();
-                }
-
-                @Override
-                public Path getRootDirectory() {
-                    return mavenExecutionRequest.getRootDirectory();
-                }
-            };
-
-            final Properties newProperties = new Properties();
-
+            int contributedProperties = 0;
             for (PropertyContributor contributor : effectivePropertyContributors.values()) {
                 Map<String, String> contribution = contributor.contribute(protoSession);
                 if (contribution != null && !contribution.isEmpty()) {
-                    newProperties.putAll(contribution);
+                    contributedProperties += contribution.size();
+                    protoSession = protoSession.toBuilder()
+                            .putAllUserProperties(contribution)
+                            .build();
                 }
             }
 
-            if (!newProperties.isEmpty()) {
-                Properties newUserProperties = new Properties();
-                newUserProperties.putAll(userPropertiesMap);
-                newUserProperties.putAll(newProperties);
-                mavenExecutionRequest.setUserProperties(newUserProperties);
+            if (contributedProperties > 0) {
+                Properties userProperties = new Properties();
+                userProperties.putAll(protoSession.getUserProperties());
+                mavenExecutionRequest.setUserProperties(userProperties);
             }
         }
     }
