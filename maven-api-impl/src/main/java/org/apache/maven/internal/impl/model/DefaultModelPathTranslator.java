691cecd61b9a78c1726baaeb798cd964e4caf7af
Tamas Cservenak <tamas@cservenak.net>
2024-09-21 17:58:25 +0200

    Same change in new API

--- maven-api-impl/src/main/java/org/apache/maven/internal/impl/model/DefaultModelPathTranslator.java
+++ maven-api-impl/src/main/java/org/apache/maven/internal/impl/model/DefaultModelPathTranslator.java
@@ -107,7 +107,7 @@ public class DefaultModelPathTranslator implements ModelPathTranslator {
 
     private Resource alignToBaseDirectory(Resource resource, Path basedir) {
         if (resource != null) {
-            String newDir = alignToBaseDirectory(resource.getDirectory(), basedir);
+            String newDir = mayAlignToBaseDirectoryOrNull(resource.getDirectory(), basedir);
             if (newDir != null) {
                 return resource.withDirectory(newDir);
             }
@@ -116,6 +116,17 @@ public class DefaultModelPathTranslator implements ModelPathTranslator {
     }
 
     private String alignToBaseDirectory(String path, Path basedir) {
+        String newPath = mayAlignToBaseDirectoryOrNull(path, basedir);
+        if (newPath != null) {
+            return newPath;
+        }
+        return path;
+    }
+
+    /**
+     * Returns aligned path or {@code null} if no need for change.
+     */
+    private String mayAlignToBaseDirectoryOrNull(String path, Path basedir) {
         String newPath = pathTranslator.alignToBaseDirectory(path, basedir);
         return Objects.equals(path, newPath) ? null : newPath;
     }
