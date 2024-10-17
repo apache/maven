3bad2f76731dd6c0f765976c861f2d5c87c58b0b
Francois Marot <fmarot@users.noreply.github.com>
2024-10-17 11:27:53 +0200

    [MNG-8315] Fix MAVEN_PROJECTBASEDIR in mvn.cmd when .mvn is at drive root (#1806)
    fixes https://issues.apache.org/jira/browse/MNG-8315

--- apache-maven/src/bin/mvn.cmd
+++ apache-maven/src/bin/mvn.cmd
@@ -166,6 +166,9 @@ for /F "usebackq delims=" %%a in ("%MAVEN_PROJECTBASEDIR%\.mvn\jvm.config") do s
 
 :endReadAdditionalConfig
 
+@REM do not let MAVEN_PROJECTBASEDIR end with a single backslash which would escape the double quote. This happens when .mvn at drive root.
+if "_%MAVEN_PROJECTBASEDIR:~-1%"=="_\" set "MAVEN_PROJECTBASEDIR=%MAVEN_PROJECTBASEDIR%\"
+
 for %%i in ("%MAVEN_HOME%"\boot\plexus-classworlds-*) do set CLASSWORLDS_JAR="%%i"
 set CLASSWORLDS_LAUNCHER=org.codehaus.plexus.classworlds.launcher.Launcher
 
