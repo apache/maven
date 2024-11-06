e0fab69c047cfff7febde4159752e7a2cec351a1
Tamas Cservenak <tamas@cservenak.net>
2024-11-06 17:07:45 +0100

    Reformat

--- impl/maven-core/src/main/java/org/apache/maven/extension/internal/CoreExtensionEntry.java
+++ impl/maven-core/src/main/java/org/apache/maven/extension/internal/CoreExtensionEntry.java
@@ -22,7 +22,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.nio.file.Path;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Enumeration;
