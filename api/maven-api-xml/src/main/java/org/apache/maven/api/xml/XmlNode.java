43277a648e39b57aed8768860117529b8507f63d
Elliotte Rusty Harold <elharo@ibiblio.org>
2025-01-04 07:14:46 -0500

    Clean up model description and API doc

--- api/maven-api-xml/src/main/java/org/apache/maven/api/xml/XmlNode.java
+++ api/maven-api-xml/src/main/java/org/apache/maven/api/xml/XmlNode.java
@@ -28,7 +28,7 @@ import org.apache.maven.api.annotations.Nullable;
 import org.apache.maven.api.annotations.ThreadSafe;
 
 /**
- * An immutable xml node.
+ * An immutable XML node.
  *
  * @since 4.0.0
  */
@@ -38,8 +38,6 @@ import org.apache.maven.api.annotations.ThreadSafe;
 public interface XmlNode {
 
     String CHILDREN_COMBINATION_MODE_ATTRIBUTE = "combine.children";
-
-    String CHILDREN_COMBINATION_MERGE = "merge";
 
     String CHILDREN_COMBINATION_APPEND = "append";
 
@@ -53,8 +51,6 @@ public interface XmlNode {
     String SELF_COMBINATION_MODE_ATTRIBUTE = "combine.self";
 
     String SELF_COMBINATION_OVERRIDE = "override";
-
-    String SELF_COMBINATION_MERGE = "merge";
 
     String SELF_COMBINATION_REMOVE = "remove";
 
