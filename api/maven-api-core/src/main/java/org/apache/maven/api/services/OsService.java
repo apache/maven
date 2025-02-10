3cd347b5d45c2d3ddcaf3525ea35a9e29ea21d16
Tamas Cservenak <tamas@cservenak.net>
2025-02-10 13:01:45 +0100

    Reformat

--- api/maven-api-core/src/main/java/org/apache/maven/api/services/OsService.java
+++ api/maven-api-core/src/main/java/org/apache/maven/api/services/OsService.java
@@ -19,12 +19,8 @@
 package org.apache.maven.api.services;
 
 import org.apache.maven.api.Service;
-import org.apache.maven.api.Session;
 import org.apache.maven.api.annotations.Experimental;
 import org.apache.maven.api.annotations.Nonnull;
-import org.apache.maven.api.settings.Settings;
-
-import java.nio.file.Path;
 
 /**
  * OS detection service.
