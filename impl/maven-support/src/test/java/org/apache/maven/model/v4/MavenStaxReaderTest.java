f89443f12357c14faff2f5d7cffdc45206a373b3
Guillaume Nodet <gnodet@gmail.com>
2025-03-21 08:00:24 +0100

    Avoid star imports

--- impl/maven-support/src/test/java/org/apache/maven/model/v4/MavenStaxReaderTest.java
+++ impl/maven-support/src/test/java/org/apache/maven/model/v4/MavenStaxReaderTest.java
@@ -25,7 +25,10 @@ import java.io.StringReader;
 import org.apache.maven.api.model.Model;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MavenStaxReaderTest {
 
