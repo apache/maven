db9b858cda8226b47b4896d7d6bdd160531a73df
Guillaume Nodet <gnodet@gmail.com>
2025-03-21 08:04:26 +0100

    Avoid star imports

--- impl/maven-impl/src/test/java/org/apache/maven/impl/DefaultModelXmlFactoryTest.java
+++ impl/maven-impl/src/test/java/org/apache/maven/impl/DefaultModelXmlFactoryTest.java
@@ -26,7 +26,9 @@ import org.apache.maven.api.services.xml.XmlReaderRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DefaultModelXmlFactoryTest {
 
