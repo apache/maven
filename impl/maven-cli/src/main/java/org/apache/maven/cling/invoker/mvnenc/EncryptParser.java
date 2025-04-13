b1466d866cc4b113a13eb539773edcb398d05db3
Tamas Cservenak <tamas@cservenak.net>
2025-04-13 20:50:07 +0200

    [MNG-8685] Better support for CI systems (#2254)
    Improve CI detection of Maven, make possible to propagate some options from CI to Maven.
    
    Changes:
    * `InvokerRequest` carries new info: `Optional<CIInfo>`: if present, we run on CI, if empty, not.
    * `Parser` (creating `InvokerRequest`) already inspects env and assembles invoker request, so make it detect CI as well
    * `CIDetector` is a Java Service (this all happens early, no DI yet) that can be extended (like adding CI specific detectors). Core had one implementation that became the "generic" that is what Maven 4 had so far.
    * Added "jenkins", "github", "teamcity", "circle" and "travis" support.
    
    ---
    
    https://issues.apache.org/jira/browse/MNG-8685

--- impl/maven-cli/src/main/java/org/apache/maven/cling/invoker/mvnenc/EncryptParser.java
+++ impl/maven-cli/src/main/java/org/apache/maven/cling/invoker/mvnenc/EncryptParser.java
@@ -50,6 +50,7 @@ public class EncryptParser extends BaseParser {
                 context.topDirectory,
                 context.rootDirectory,
                 context.extensions,
+                context.ciInfo,
                 (EncryptOptions) context.options);
     }
 
