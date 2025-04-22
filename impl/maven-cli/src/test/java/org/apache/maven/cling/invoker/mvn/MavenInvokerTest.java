1c01fa0284afe819aa935f4d6bc5e81c8f878fd2
Tamas Cservenak <tamas@cservenak.net>
2025-04-22 15:16:11 +0200

    Update Maveniverse stuff (#2269)
    Changes:
    * mimir 0.5.0
    * bom-builder 1.0.4
    * toolbox 0.7.4

--- impl/maven-cli/src/test/java/org/apache/maven/cling/invoker/mvn/MavenInvokerTest.java
+++ impl/maven-cli/src/test/java/org/apache/maven/cling/invoker/mvn/MavenInvokerTest.java
@@ -218,10 +218,10 @@ public class MavenInvokerTest extends MavenInvokerTestSupport {
         Map<String, String> logs = invoke(
                 cwd,
                 userHome,
-                List.of("eu.maveniverse.maven.plugins:toolbox:0.6.2:help"),
+                List.of("eu.maveniverse.maven.plugins:toolbox:0.7.4:help"),
                 List.of("--force-interactive"));
 
-        String log = logs.get("eu.maveniverse.maven.plugins:toolbox:0.6.2:help");
+        String log = logs.get("eu.maveniverse.maven.plugins:toolbox:0.7.4:help");
         assertTrue(log.contains("https://repo1.maven.org/maven2"), log);
         assertFalse(log.contains("https://repo.maven.apache.org/maven2"), log);
     }
