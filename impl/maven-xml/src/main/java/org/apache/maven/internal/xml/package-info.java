2fbe94687cc2f9c127f157c9040d72d0a67e00b7
Vincent Potucek <vpotucek@me.com>
2025-05-09 22:26:46 +0200

    fix folders

--- impl/maven-xml/src/main/java/org/apache/maven/internal/xml/package-info.java
+++
@@ -1,5 +0,0 @@
-// CHECKSTYLE_OFF: RegexpHeader
-/**
- * Contains implementation of the {@link org.apache.maven.api.xml.XmlNode} interface and related classes.
- */
-package org.apache.maven.internal.xml;
