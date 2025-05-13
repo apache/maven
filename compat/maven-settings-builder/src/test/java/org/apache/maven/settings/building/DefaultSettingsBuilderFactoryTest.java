e4dd54d7eff5da7a3bb95627a677f7c2ac1e64f4
Slawomir Jaranowski <s.jaranowski@gmail.com>
2025-05-13 21:23:43 +0200

    [MNG-5913] Allow defining aliases for existing server configurations in settings.xml
    Add the next tag ids to the server in settings.xml
    
    Additional server will be created in memory by SettingsBuilder,
    so the generated configuration should be transparent to other
    as Settings#getServers() will return complete list.

--- compat/maven-settings-builder/src/test/java/org/apache/maven/settings/building/DefaultSettingsBuilderFactoryTest.java
+++ compat/maven-settings-builder/src/test/java/org/apache/maven/settings/building/DefaultSettingsBuilderFactoryTest.java
@@ -19,10 +19,15 @@
 package org.apache.maven.settings.building;
 
 import java.io.File;
+import java.util.List;
+import java.util.Properties;
 
+import org.apache.maven.api.settings.Server;
+import org.apache.maven.settings.Settings;
+import org.assertj.core.api.recursive.comparison.RecursiveComparisonConfiguration;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  */
@@ -32,17 +37,114 @@ class DefaultSettingsBuilderFactoryTest {
         return new File("src/test/resources/settings/factory/" + name + ".xml").getAbsoluteFile();
     }
 
-    @Test
-    void testCompleteWiring() throws Exception {
+    private org.apache.maven.settings.Server asServer(Server delegate) {
+        return new org.apache.maven.settings.Server(delegate);
+    }
+
+    SettingsBuildingResult execute(String settingsName) throws Exception {
+        Properties properties = new Properties();
+        properties.setProperty("user.home", "/home/user");
+
         SettingsBuilder builder = new DefaultSettingsBuilderFactory().newInstance();
-        assertNotNull(builder);
+        assertThat(builder).isNotNull();
 
         DefaultSettingsBuildingRequest request = new DefaultSettingsBuildingRequest();
-        request.setSystemProperties(System.getProperties());
-        request.setUserSettingsFile(getSettings("simple"));
+        request.setSystemProperties(properties);
+        request.setUserSettingsFile(getSettings(settingsName));
 
         SettingsBuildingResult result = builder.build(request);
-        assertNotNull(result);
-        assertNotNull(result.getEffectiveSettings());
+        return assertThat(result).isNotNull().actual();
+    }
+
+    @Test
+    void testCompleteWiring() throws Exception {
+        Settings settings = assertThat(execute("simple"))
+                .extracting(SettingsBuildingResult::getEffectiveSettings)
+                .actual();
+
+        assertThat(settings.getLocalRepository())
+                .satisfiesAnyOf(
+                        repo -> assertThat(repo).isEqualTo("/home/user/.m2/repository"),
+                        repo -> assertThat(repo).endsWith("\\home\\user\\.m2\\repository"));
+    }
+
+    @Test
+    void testSettingsWithServers() throws Exception {
+        Settings settings = assertThat(execute("settings-servers-1"))
+                .extracting(SettingsBuildingResult::getEffectiveSettings)
+                .actual();
+
+        assertThat(settings.getDelegate().getServers())
+                .hasSize(2)
+                .usingRecursiveFieldByFieldElementComparator(RecursiveComparisonConfiguration.builder()
+                        .withIgnoredFields("locations")
+                        .build())
+                .containsExactlyInAnyOrder(
+                        Server.newBuilder()
+                                .id("server-1")
+                                .username("username1")
+                                .password("password1")
+                                .build(),
+                        Server.newBuilder()
+                                .id("server-2")
+                                .username("username2")
+                                .password("password2")
+                                .build());
+    }
+
+    @Test
+    void testSettingsWithServersAndAliases() throws Exception {
+        Settings settings = assertThat(execute("settings-servers-2"))
+                .extracting(SettingsBuildingResult::getEffectiveSettings)
+                .actual();
+
+        assertThat(settings.getDelegate().getServers())
+                .hasSize(6)
+                .usingRecursiveFieldByFieldElementComparator(RecursiveComparisonConfiguration.builder()
+                        .withIgnoredFields("locations")
+                        .build())
+                .containsExactlyInAnyOrder(
+                        Server.newBuilder()
+                                .id("server-1")
+                                .username("username1")
+                                .password("password1")
+                                .ids(List.of("server-11", "server-12"))
+                                .build(),
+                        Server.newBuilder()
+                                .id("server-11")
+                                .username("username1")
+                                .password("password1")
+                                .build(),
+                        Server.newBuilder()
+                                .id("server-12")
+                                .username("username1")
+                                .password("password1")
+                                .build(),
+                        Server.newBuilder()
+                                .id("server-2")
+                                .username("username2")
+                                .password("password2")
+                                .ids(List.of("server-21"))
+                                .build(),
+                        Server.newBuilder()
+                                .id("server-21")
+                                .username("username2")
+                                .password("password2")
+                                .build(),
+                        Server.newBuilder()
+                                .id("server-3")
+                                .username("username3")
+                                .password("password3")
+                                .build());
+    }
+
+    @Test
+    void testSettingsWithDuplicateServersIds() throws Exception {
+        SettingsBuildingResult result = execute("settings-servers-3");
+
+        assertThat(result.getProblems())
+                .hasSize(1)
+                .extracting(SettingsProblem::getMessage)
+                .containsExactly("'servers.server.id' must be unique but found duplicate server with id server-2");
     }
 }
