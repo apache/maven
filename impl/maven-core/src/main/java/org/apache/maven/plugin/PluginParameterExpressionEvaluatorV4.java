7f4fed0f52f3b44069ff37248d6da8c50148cfca
Vincent Potucek <vpotucek@me.com>
2025-05-21 21:33:47 +0200

    EmptyControlStatement: PluginParameterExpressionEvaluator

--- impl/maven-core/src/main/java/org/apache/maven/plugin/PluginParameterExpressionEvaluatorV4.java
+++ impl/maven-core/src/main/java/org/apache/maven/plugin/PluginParameterExpressionEvaluatorV4.java
@@ -227,9 +227,7 @@ public class PluginParameterExpressionEvaluatorV4 implements TypeAwareExpression
         // TODO Copied from the DefaultInterpolator. We likely want to resurrect the PathTranslator or at least a
         // similar component for re-usage
         if (file != null) {
-            if (file.isAbsolute()) {
-                // path was already absolute, just normalize file separator and we're done
-            } else if (file.getPath().startsWith(File.separator)) {
+            if (file.getPath().startsWith(File.separator)) {
                 // drive-relative Windows path, don't align with project directory but with drive root
                 file = file.getAbsoluteFile();
             } else {
