dacb81a4d18c7ed91bbcfaa890a6bda00f3b5e39
Pankraz76 <8830888+Pankraz76@users.noreply.github.com>
2025-06-05 12:25:33 +0200

    use try-with-resources statement in LookupWagonMojo (#2426)
    Co-authored-by: Vincent Potucek <vpotucek@me.com>

--- its/core-it-support/core-it-plugins/maven-it-plugin-uses-wagon/src/main/java/org/apache/maven/plugin/coreit/LookupWagonMojo.java
+++ its/core-it-support/core-it-plugins/maven-it-plugin-uses-wagon/src/main/java/org/apache/maven/plugin/coreit/LookupWagonMojo.java
@@ -92,21 +92,11 @@ public class LookupWagonMojo extends AbstractMojo {
 
         getLog().info("[MAVEN-CORE-IT-LOG] Creating output file " + outputFile);
 
-        OutputStream out = null;
-        try {
-            outputFile.getParentFile().mkdirs();
-            out = new FileOutputStream(outputFile);
+        outputFile.getParentFile().mkdirs();
+        try (OutputStream out = new FileOutputStream(outputFile)) {
             loaderProperties.store(out, "MAVEN-CORE-IT-LOG");
         } catch (IOException e) {
-            throw new MojoExecutionException("Output file could not be created: " + outputFile, e);
-        } finally {
-            if (out != null) {
-                try {
-                    out.close();
-                } catch (IOException e) {
-                    // just ignore
-                }
-            }
+            throw new MojoExecutionException(e);
         }
 
         getLog().info("[MAVEN-CORE-IT-LOG] Created output file " + outputFile);
