6eedb0de40be34b54e9056b7b85278e8784d49dd
Guillaume Nodet <gnodet@gmail.com>
2025-09-02 11:19:20 +0000

    gh-11086: Add failing IT for dependency POM with <url>${project.url}</url> causing recursive interpolation failure in Maven 4
    - Add MavenITgh11086DependencyPomSelfUrlTest asserting current failure
    - Add IT resources under its/core-it-suite/src/test/resources/gh-11086 with a dependency POM containing <url>${project.url}</url>
    - Revert earlier local tweak to DefaultModelInterpolator (no behavior changes in this PR)
    - Remove previous draft mng-8138 IT assets
    
    This PR documents the regression vs Maven 3 and will guide the compatibility fix.

---
+++ its/core-it-suite/src/test/java/org/apache/maven/it/MavenITgh11086DependencyPomSelfUrlTest.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.maven.it;
+
+import java.io.File;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Reproduces Maven 4 failure when a dependency POM has <url>${project.url}</url>.
+ * The IT currently asserts failure to document the regression (see gh-11086).
+ */
+public class MavenITgh11086DependencyPomSelfUrlTest extends AbstractMavenIntegrationTestCase {
+    public MavenITgh11086DependencyPomSelfUrlTest() {
+        super(ALL_MAVEN_VERSIONS);
+    }
+
+    @Test
+    public void testSelfReferentialUrlInDependencyPom() throws Exception {
+        File testDir = extractResources("/gh-11086");
+
+        Verifier verifier = newVerifier(testDir.getAbsolutePath());
+        verifier.setAutoclean(false);
+        verifier.filterFile("settings-template.xml", "settings.xml");
+        verifier.deleteDirectory("target");
+        verifier.deleteArtifacts("org.apache.maven.its.gh11086");
+        verifier.addCliArgument("-s");
+        verifier.addCliArgument("settings.xml");
+        verifier.addCliArgument("-X");
+        verifier.addCliArgument("validate");
+
+        assertThrows(Exception.class, () -> {
+            verifier.execute();
+            verifier.verifyErrorFreeLog();
+        });
+    }
+}
