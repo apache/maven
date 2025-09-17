740347e03f3857069f9e7b3f00cf398133ba773c
Tamas Cservenak <tamas@cservenak.net>
2025-09-17 16:43:41 +0200

    GH-10210: fix to eager decrypt of legacy passwords
    Accept ONLY documented forms of them.
    
    Fixes #10210

--- its/core-it-suite/src/test/java/org/apache/maven/it/TestSuiteOrdering.java
+++ its/core-it-suite/src/test/java/org/apache/maven/it/TestSuiteOrdering.java
@@ -104,6 +104,7 @@ public class TestSuiteOrdering implements ClassOrderer {
          * a fail fast technique as well.
          */
         suite.addTestSuite(MavenITgh11084ReactorReaderPreferConsumerPomTest.class);
+        suite.addTestSuite(MavenITgh10210SettingsXmlDecryptTest.class);
         suite.addTestSuite(MavenITgh10312TerminallyDeprecatedMethodInGuiceTest.class);
         suite.addTestSuite(MavenITgh10937QuotedPipesInMavenOptsTest.class);
         suite.addTestSuite(MavenITgh2532DuplicateDependencyEffectiveModelTest.class);
