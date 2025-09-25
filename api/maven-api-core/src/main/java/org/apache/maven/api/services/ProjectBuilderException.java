f3a86a93af5fe66a7b0273a41d185465191bd01f
Jay <39220950+vijaykriishna@users.noreply.github.com>
2025-09-26 03:27:56 +0530

    Removing all serialVersionUID (#11174, fixes #10155)

--- api/maven-api-core/src/main/java/org/apache/maven/api/services/ProjectBuilderException.java
+++ api/maven-api-core/src/main/java/org/apache/maven/api/services/ProjectBuilderException.java
@@ -18,8 +18,6 @@
  */
 package org.apache.maven.api.services;
 
-import java.io.Serial;
-
 import org.apache.maven.api.annotations.Experimental;
 
 /**
@@ -29,9 +27,6 @@ import org.apache.maven.api.annotations.Experimental;
  */
 @Experimental
 public class ProjectBuilderException extends MavenException {
-
-    @Serial
-    private static final long serialVersionUID = -7629871850875943799L;
 
     /**
      * @param message the message to give
--- api/maven-api-core/src/main/java/org/apache/maven/api/services/SettingsBuilderException.java
+++ api/maven-api-core/src/main/java/org/apache/maven/api/services/SettingsBuilderException.java
@@ -18,8 +18,6 @@
  */
 package org.apache.maven.api.services;
 
-import java.io.Serial;
-
 import org.apache.maven.api.annotations.Experimental;
 
 /**
@@ -29,9 +27,6 @@ import org.apache.maven.api.annotations.Experimental;
  */
 @Experimental
 public class SettingsBuilderException extends MavenBuilderException {
-
-    @Serial
-    private static final long serialVersionUID = 4714858598345418083L;
 
     /**
      * @param message the message to give
