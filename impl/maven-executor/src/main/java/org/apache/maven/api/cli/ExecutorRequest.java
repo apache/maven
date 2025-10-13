91e3d5c51b8290cce070fc8a27ff655f482a0a75
Tamas Cservenak <tamas@cservenak.net>
2025-10-13 21:25:09 +0200

    Remove use of toRealPath (#11250) (#11257)
    As this makes us "escape" from paths that are symbolic links, and also causes inconsistencies among paths (like maven home, and system settings and system toolchains).
    
    Backport of 92d53cb7075d4ff62fff0a05c7bc6b1d51566db8

--- impl/maven-executor/src/main/java/org/apache/maven/api/cli/ExecutorRequest.java
+++ impl/maven-executor/src/main/java/org/apache/maven/api/cli/ExecutorRequest.java
@@ -18,7 +18,6 @@
  */
 package org.apache.maven.api.cli;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Path;
@@ -401,9 +400,13 @@ public interface ExecutorRequest {
                 this.cwd = getCanonicalPath(requireNonNull(cwd));
                 this.installationDirectory = getCanonicalPath(requireNonNull(installationDirectory));
                 this.userHomeDirectory = getCanonicalPath(requireNonNull(userHomeDirectory));
-                this.jvmSystemProperties = jvmSystemProperties != null ? Map.copyOf(jvmSystemProperties) : null;
-                this.environmentVariables = environmentVariables != null ? Map.copyOf(environmentVariables) : null;
-                this.jvmArguments = jvmArguments != null ? List.copyOf(jvmArguments) : null;
+                this.jvmSystemProperties = jvmSystemProperties != null && !jvmSystemProperties.isEmpty()
+                        ? Map.copyOf(jvmSystemProperties)
+                        : null;
+                this.environmentVariables = environmentVariables != null && !environmentVariables.isEmpty()
+                        ? Map.copyOf(environmentVariables)
+                        : null;
+                this.jvmArguments = jvmArguments != null && !jvmArguments.isEmpty() ? List.copyOf(jvmArguments) : null;
                 this.stdIn = stdIn;
                 this.stdOut = stdOut;
                 this.stdErr = stdErr;
@@ -510,10 +513,6 @@ public interface ExecutorRequest {
     @Nonnull
     static Path getCanonicalPath(Path path) {
         requireNonNull(path, "path");
-        try {
-            return path.toRealPath();
-        } catch (IOException e) {
-            return getCanonicalPath(path.getParent()).resolve(path.getFileName());
-        }
+        return path.toAbsolutePath().normalize();
     }
 }
