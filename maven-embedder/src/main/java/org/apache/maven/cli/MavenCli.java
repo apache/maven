ed6cde0d8598ad2e40479d3ab9feccddae1e7b9a
Tamas Cservenak <tamas@cservenak.net>
2025-10-14 12:27:58 +0200

    Make maven.config use UTF8
    Maven embedder uses system default encoding, making builds non portable.
    
    Fixes #11258
    
    This MUST be emphasized in release notes, as issue describes, people
    may circumvented this by using `-Dfile.encoding=...` that will
    break if they did not use UTF8.

--- maven-embedder/src/main/java/org/apache/maven/cli/MavenCli.java
+++ maven-embedder/src/main/java/org/apache/maven/cli/MavenCli.java
@@ -27,7 +27,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -383,7 +383,7 @@ public class MavenCli {
             File configFile = new File(cliRequest.multiModuleProjectDirectory, MVN_MAVEN_CONFIG);
 
             if (configFile.isFile()) {
-                try (Stream<String> lines = Files.lines(configFile.toPath(), Charset.defaultCharset())) {
+                try (Stream<String> lines = Files.lines(configFile.toPath(), StandardCharsets.UTF_8)) {
                     String[] args = lines.filter(arg -> !arg.isEmpty() && !arg.startsWith("#"))
                             .toArray(String[]::new);
                     mavenConfig = cliManager.parse(args);
