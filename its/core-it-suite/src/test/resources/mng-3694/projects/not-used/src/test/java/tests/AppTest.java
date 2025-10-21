bd82a2dc6d668f380be498411ad5f7dbcd42b699
Sandra Parsick <sandra@parsick.dev>
2025-10-21 11:43:22 +0200

    chore: delete junit3 reference
    - updated test to junit5
    - delete dead code

--- its/core-it-suite/src/test/resources/mng-3694/projects/not-used/src/test/java/tests/AppTest.java
+++ its/core-it-suite/src/test/resources/mng-3694/projects/not-used/src/test/java/tests/AppTest.java
@@ -18,33 +18,19 @@
  */
 package tests;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
+public class AppTest {
 
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp() {
         assertTrue(true);
     }
