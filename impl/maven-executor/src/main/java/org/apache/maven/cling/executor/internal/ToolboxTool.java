f1436135d4b54c0691e429cd677c5f9b2b1f1cc2
Tamas Cservenak <tamas@cservenak.net>
2025-10-21 12:57:21 +0200

    Check stdout only
    As stderr will contain warnings on higher Java versions
    (guava and other; terminally deprecated method...)

--- impl/maven-executor/src/main/java/org/apache/maven/cling/executor/internal/ToolboxTool.java
+++ impl/maven-executor/src/main/java/org/apache/maven/cling/executor/internal/ToolboxTool.java
@@ -159,7 +159,7 @@ public class ToolboxTool implements ExecutorTool {
             result = result.replace("\n", "").replace("\r", "");
         }
         // sanity checks: stderr has any OR result is empty string (no method should emit empty string)
-        if (stderr.size() > 0 || result.trim().isEmpty()) {
+        if (result.trim().isEmpty()) {
             // see bug https://github.com/apache/maven/pull/11303 Fail in this case
             // tl;dr We NEVER expect empty string as output from this tool; so fail here instead to chase ghosts
             throw new IllegalStateException("Empty output from Toolbox; stdout[" + stdout.size() + "]=" + stdout
