db76418924390789475197de9b35c510ae65d80e
TheRealHaui <michael.hausegger@hausegger.tech>
2025-12-01 18:06:48 +0100

    Removed added star import

--- impl/maven-core/src/test/java/org/apache/maven/plugin/CacheUtilsTest.java
+++ impl/maven-core/src/test/java/org/apache/maven/plugin/CacheUtilsTest.java
@@ -23,7 +23,9 @@ import org.apache.maven.model.Plugin;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 class CacheUtilsTest {
 
