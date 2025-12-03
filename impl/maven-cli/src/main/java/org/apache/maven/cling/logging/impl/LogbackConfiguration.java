e5e6b60e7eb278cad489d214b17fd5bc2147e3a5
Alioth Null <alioth4j@proton.me>
2025-12-03 16:23:13 +0800

    Fix multiple SLF4J providers

--- impl/maven-cli/src/main/java/org/apache/maven/cling/logging/impl/LogbackConfiguration.java
+++ impl/maven-cli/src/main/java/org/apache/maven/cling/logging/impl/LogbackConfiguration.java
@@ -18,6 +18,10 @@
  */
 package org.apache.maven.cling.logging.impl;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.apache.maven.cling.logging.BaseSlf4jConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,15 +32,28 @@ import org.slf4j.LoggerFactory;
  * @since 3.1.0
  */
 public class LogbackConfiguration extends BaseSlf4jConfiguration {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogbackConfiguration.class);
+
     @Override
     public void setRootLoggerLevel(Level level) {
-        ch.qos.logback.classic.Level value =
-                switch (level) {
-                    case DEBUG -> ch.qos.logback.classic.Level.DEBUG;
-                    case INFO -> ch.qos.logback.classic.Level.INFO;
-                    default -> ch.qos.logback.classic.Level.ERROR;
-                };
-        ((ch.qos.logback.classic.Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME)).setLevel(value);
+        try {
+            Class<?> levelClass = Class.forName("ch.qos.logback.classic.Level");
+            Field levelField =
+                    switch (level) {
+                        case DEBUG -> levelClass.getField("DEBUG");
+                        case INFO -> levelClass.getField("INFO");
+                        default -> levelClass.getField("ERROR");
+                    };
+
+            Method setLevelMethod = levelClass.getMethod("setLevel", levelClass);
+            setLevelMethod.invoke(LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME), levelField.get(null));
+        } catch (ClassNotFoundException
+                | NoSuchFieldException
+                | NoSuchMethodException
+                | IllegalAccessException
+                | InvocationTargetException e) {
+            LOGGER.error("Failed to configure logback", e);
+        }
     }
 
     @Override
