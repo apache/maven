8c002684e4ed1163b0c5f12b3bc6583781e40be9
Tamas Cservenak <tamas@cservenak.net>
2025-12-18 17:30:02 +0100

    Parameterized test

--- maven-core/src/test/java/org/apache/maven/plugin/internal/MavenPluginJavaPrerequisiteCheckerTest.java
+++ maven-core/src/test/java/org/apache/maven/plugin/internal/MavenPluginJavaPrerequisiteCheckerTest.java
@@ -19,37 +19,68 @@
 package org.apache.maven.plugin.internal;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
 
+@RunWith(Parameterized.class)
 public class MavenPluginJavaPrerequisiteCheckerTest {
 
-    @Test
-    public void testMatchesVersion() {
-        MavenPluginJavaPrerequisiteChecker checker = new MavenPluginJavaPrerequisiteChecker();
-        assertTrue(checker.matchesVersion("8", "1.8"));
-        assertTrue(checker.matchesVersion("[8,)", "1.8"));
-        assertTrue(checker.matchesVersion("(,8]", "1.8"));
-        assertFalse(checker.matchesVersion("(,8)", "1.8"));
-        assertTrue(checker.matchesVersion("8", "9.0.1+11"));
-        assertTrue(checker.matchesVersion("[8,)", "9.0.1+11"));
-        assertFalse(checker.matchesVersion("(,8]", "9.0.1+11"));
-        assertFalse(checker.matchesVersion("(,8)", "9.0.1+11"));
-        assertTrue(checker.matchesVersion("1.8", "1.8"));
-        assertTrue(checker.matchesVersion("[1.8,)", "1.8"));
-        assertTrue(checker.matchesVersion("(,1.8]", "1.8"));
-        assertFalse(checker.matchesVersion("(,1.8)", "1.8"));
-        assertTrue(checker.matchesVersion("1.8", "9.0.1+11"));
-        assertTrue(checker.matchesVersion("[1.8,)", "9.0.1+11"));
-        assertFalse(checker.matchesVersion("(,1.8]", "9.0.1+11"));
-        assertFalse(checker.matchesVersion("(,1.8)", "9.0.1+11"));
+    private static final String JAVA_8_VERSION = "1.8";
+    private static final String JAVA_9_VERSION = "9.0.1+11";
 
-        assertTrue(checker.matchesVersion("1.0", "1.8"));
-        assertTrue(checker.matchesVersion("1.8", "9.0.1+11"));
-        assertFalse(checker.matchesVersion("[1.0,2],[3,4]", "2.1"));
-        assertTrue(checker.matchesVersion("[1.0,2],[3,4]", "3.1"));
-        assertThrows(IllegalArgumentException.class, () -> checker.matchesVersion("(1.0,0)", "11"));
+    @Parameterized.Parameters
+    public static Object[][] parameters() {
+        // requiredVersion, currentVersion, expected, throwable
+        return new Object[][] {
+            {"8", JAVA_8_VERSION, true},
+            {"[8,)", JAVA_8_VERSION, true},
+            {"(,8]", JAVA_8_VERSION, true},
+            {"(,8)", JAVA_8_VERSION, false},
+            {"1.8", JAVA_8_VERSION, true},
+            {"[1.8,)", JAVA_8_VERSION, true},
+            {"(,1.8]", JAVA_8_VERSION, true},
+            {"(,1.8)", JAVA_8_VERSION, false},
+            {"8", JAVA_9_VERSION, true},
+            {"[8,)", JAVA_9_VERSION, true},
+            {"(,8]", JAVA_9_VERSION, false},
+            {"(,8)", JAVA_9_VERSION, false},
+            {"1.8", JAVA_9_VERSION, true},
+            {"[1.8,)", JAVA_9_VERSION, true},
+            {"(,1.8]", JAVA_9_VERSION, false},
+            {"(,1.8)", JAVA_9_VERSION, false},
+
+            // various
+            {"1.0", JAVA_8_VERSION, true},
+            {"1.8", JAVA_9_VERSION, true},
+            {"[1.0,2],[3,4]", "2.1", false},
+            {"[1.0,2],[3,4]", "3.1", true},
+            {"(1.0,0)", "11", null},
+        };
+    }
+
+    private final MavenPluginJavaPrerequisiteChecker subject = new MavenPluginJavaPrerequisiteChecker();
+
+    @Parameterized.Parameter(0)
+    public String requiredVersion;
+
+    @Parameterized.Parameter(1)
+    public String currentVersion;
+
+    @Parameterized.Parameter(2)
+    public Boolean expected;
+
+    @Test
+    public void doTest() {
+        if (expected != null) {
+            assertEquals(
+                    requiredVersion + ":" + currentVersion + " -> " + expected,
+                    expected,
+                    subject.matchesVersion(requiredVersion, currentVersion));
+        } else {
+            assertThrows(IllegalArgumentException.class, () -> subject.matchesVersion(requiredVersion, currentVersion));
+        }
     }
 }
