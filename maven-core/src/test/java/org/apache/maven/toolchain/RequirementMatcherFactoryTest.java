b8e39bf924059ac89cc96f725c0b3bbdd6edc448
Tamas Cservenak <tamas@cservenak.net>
2026-03-11 16:39:27 +0100

    Feat: Improve matching of Toolchains versions
    This feature was totally off from what it was documented.

--- maven-core/src/test/java/org/apache/maven/toolchain/RequirementMatcherFactoryTest.java
+++ maven-core/src/test/java/org/apache/maven/toolchain/RequirementMatcherFactoryTest.java
@@ -50,13 +50,21 @@ public class RequirementMatcherFactoryTest {
     public void testCreateVersionMatcher() {
         RequirementMatcher matcher;
         matcher = RequirementMatcherFactory.createVersionMatcher("1.5.2");
-        assertFalse(matcher.matches("1.5"));
+        assertTrue(matcher.matches("1"));
+        assertTrue(matcher.matches("1.5"));
         assertTrue(matcher.matches("1.5.2"));
         assertFalse(matcher.matches("[1.4,1.5)"));
         assertFalse(matcher.matches("[1.5,1.5.2)"));
         assertFalse(matcher.matches("(1.5.2,1.6)"));
         assertTrue(matcher.matches("(1.4,1.5.2]"));
         assertTrue(matcher.matches("(1.5,)"));
+
+        assertTrue(matcher.matches("1.5+"));
+        assertFalse(matcher.matches("1.5-"));
+
+        assertFalse(matcher.matches("1.6+"));
+        assertTrue(matcher.matches("1.6-"));
+
         assertEquals("1.5.2", matcher.toString());
 
         // Ensure it is not printed as 1.5.0
