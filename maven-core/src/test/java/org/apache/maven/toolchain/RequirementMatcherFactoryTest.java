31bfd36986337551619ffa715a46763cef093f9a
Tamas Cservenak <tamas@cservenak.net>
2026-03-11 18:55:05 +0100

    Add UT

--- maven-core/src/test/java/org/apache/maven/toolchain/RequirementMatcherFactoryTest.java
+++ maven-core/src/test/java/org/apache/maven/toolchain/RequirementMatcherFactoryTest.java
@@ -18,6 +18,10 @@
  */
 package org.apache.maven.toolchain;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -70,5 +74,36 @@ public class RequirementMatcherFactoryTest {
         // Ensure it is not printed as 1.5.0
         matcher = RequirementMatcherFactory.createVersionMatcher("1.5");
         assertEquals("1.5", matcher.toString());
+    }
+
+    @Test
+    public void testCreateVersionMatcherWithJavaVersions() {
+        RequirementMatcher java25 = RequirementMatcherFactory.createVersionMatcher("25.0.2");
+        RequirementMatcher java21 = RequirementMatcherFactory.createVersionMatcher("21.0.10");
+        RequirementMatcher java17 = RequirementMatcherFactory.createVersionMatcher("17.0.18");
+        RequirementMatcher java11 = RequirementMatcherFactory.createVersionMatcher("11.0.30");
+        RequirementMatcher java8 = RequirementMatcherFactory.createVersionMatcher("1.8.0_482");
+        List<RequirementMatcher> matchers = Arrays.asList(java25, java21, java17, java11, java8);
+
+        testMatch("11", matchers, java11);
+        testMatch("11+", matchers, java25, java21, java17, java11);
+        testMatch("11-", matchers, java8);
+        testMatch("[11,21)", matchers, java17, java11);
+        testMatch("1.8", matchers, java8);
+        testMatch("1.8+", matchers, java25, java21, java17, java11, java8);
+        testMatch("8", matchers);
+        testMatch("8+", matchers, java25, java21, java17, java11);
+    }
+
+    private static void testMatch(
+            String requirement, Collection<RequirementMatcher> allMatchers, RequirementMatcher... requiredMatchers) {
+        int matches = 0;
+        for (RequirementMatcher matcher : allMatchers) {
+            if (matcher.matches(requirement)) {
+                matches++;
+                assertTrue(Arrays.asList(requiredMatchers).contains(matcher));
+            }
+        }
+        assertEquals(matches, requiredMatchers.length);
     }
 }
