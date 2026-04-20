7a868defabc3c5e25615db1d0ec58a7c7c8ba9cf
Slawomir Jaranowski <s.jaranowski@gmail.com>
2026-04-20 21:47:24 +0200

    Remove redundant implementation of getLogger method in MavenSimpleLoggerFactory

--- maven-slf4j-provider/src/main/java/org/slf4j/simple/MavenSimpleLoggerFactory.java
+++ maven-slf4j-provider/src/main/java/org/slf4j/simple/MavenSimpleLoggerFactory.java
@@ -24,19 +24,6 @@ import org.slf4j.Logger;
  * MavenSimpleLoggerFactory
  */
 public class MavenSimpleLoggerFactory extends SimpleLoggerFactory {
-    /**
-     * Return an appropriate {@link MavenSimpleLogger} instance by name.
-     */
-    public Logger getLogger(String name) {
-        Logger simpleLogger = loggerMap.get(name);
-        if (simpleLogger != null) {
-            return simpleLogger;
-        } else {
-            Logger newInstance = new MavenSimpleLogger(name);
-            Logger oldInstance = loggerMap.putIfAbsent(name, newInstance);
-            return oldInstance == null ? newInstance : oldInstance;
-        }
-    }
 
     @Override
     protected Logger createLogger(String name) {
