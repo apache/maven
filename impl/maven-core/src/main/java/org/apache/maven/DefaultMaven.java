55b9dbb45cf248b03533f00e0a6985374e612ba2
Tamas Cservenak <tamas@cservenak.net>
2026-05-20 11:59:58 +0200

    Add comment about fix in 3.10.x

--- impl/maven-core/src/main/java/org/apache/maven/DefaultMaven.java
+++ impl/maven-core/src/main/java/org/apache/maven/DefaultMaven.java
@@ -217,7 +217,7 @@ public class DefaultMaven implements Maven {
             session.setSession(defaultSessionFactory.newSession(session));
 
             sessionScope.seed(MavenSession.class, session);
-            sessionScope.seed(RepositorySystemSession.class, closeableSession);
+            sessionScope.seed(RepositorySystemSession.class, closeableSession); // fixed in Maven 3.10.x
             sessionScope.seed(Session.class, session.getSession());
             sessionScope.seed(InternalMavenSession.class, InternalMavenSession.from(session.getSession()));
 
