252b6525576a0a278a3cedaa8fd884b01775b3dd
Guillaume Nodet <gnodet@gmail.com>
2026-08-28 15:37:25 +0200

    fix: add missing Set import for backport

--- impl/maven-impl/src/test/java/org/apache/maven/impl/model/DefaultModelBuilderTest.java
+++ impl/maven-impl/src/test/java/org/apache/maven/impl/model/DefaultModelBuilderTest.java
@@ -28,6 +28,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.maven.api.RemoteRepository;
 import org.apache.maven.api.Session;
